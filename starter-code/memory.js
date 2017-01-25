5e7541966c3c4f8cd9019cbc19127c6808c9deb0
Anthony Bruscantini <ajbruscantini@gmail.com>
2017-01-25 01:26:38 +0100

    game appears to be working. just need to do some refactoring.

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -38,27 +38,25 @@ function MemoryGame (){
   this.cardFlips = 0;
 
   this.printGrid = function(){
-    for (var i = 0; i < 4; ++i){
-      var pString = "[";
-      for (var j = 0; j < 6; ++j){
-        if (this.grid[i][j].isShowing)
-          pString += this.grid[i][j].card.name.toUpperCase() + ", ";
-        else pString += this.grid[i][j].card.name + ", ";
+    for (var i = 0; i < 24; ++i){
+      if (this.grid[i].isShowing){
+        console.log(i + ": " + this.grid[i].card.name.toUpperCase());
+      } else {
+        console.log(i + ": " + this.grid[i].card.name);
       }
-      console.log(pString + "]");
     }
   };
 
   this.isWin = function(){
     this.pairsMatched++;
-    return (this.pairsMatched === this.cards.length/2) ? true : false;
+    return (this.pairsMatched === this.cards.length) ? true : false;
   };
 
   // think about timing. we should put a delay once the second card is
   // flipped over. Also, not allow someone to click on a third card while
   // this delay is in effect.
-  this.play = function(x, y){
-    var currentCard = this.grid[x][y];
+  this.play = function(x){
+    var currentCard = this.grid[x];
     if (!currentCard.isShowing){
       this.cardFlips++;
       currentCard.isShowing = true;
@@ -75,6 +73,7 @@ function MemoryGame (){
           console.log("sorry. no match...");
           currentCard.isShowing = false;
           this.firstCard.isShowing = false;
+          unShowMe(this.firstCard.childNumber);
         }
         this.onSecondPlay = false;
       }
@@ -94,23 +93,20 @@ function MemoryGame (){
     seen.push(0);
   }
 
-  for (i = 0; i < 4; ++i){
-    var row = [];
-    for (j = 0; j < 6; ++j){
-      //grid item will be random card
-      var nextCard = shuffleCard(this.cards);
-      while (seen[nextCard.id] === 2){
-        nextCard = shuffleCard(this.cards);
-      }
-      //we got our card
-      seen[nextCard.id] += 1;
-      var gridItem = {card: nextCard, isShowing: false};
-      row.push(gridItem);
+  for (i = 0; i < 24; ++i){
+    //grid item will be random card
+    var nextCard = shuffleCard(this.cards);
+    while (seen[nextCard.id] === 2){
+      nextCard = shuffleCard(this.cards);
     }
-    this.grid.push(row);
+    //we got our card
+    seen[nextCard.id] += 1;
+    var gridItem = {card: nextCard, isShowing: false, childNumber: i};
+    this.grid.push(gridItem);
   }
 
-
+  console.log("New game has been created! Here's the board:");
+  this.printGrid();
 }
 
 
@@ -118,6 +114,40 @@ function MemoryGame (){
 // HTML/CSS Interactions
 //******************************************************************
 
+var myGame = null;
+
+function unShowMe(id){
+  var cssID = String(id).length < 2 ?
+    'card-0' + String(id) : 'card-' + String(id);
+  console.log('stringID in unShowMe = ' + cssID);
+  $('#' + cssID).attr('src', '');
+}
 
 $(document).ready(function(){
+  //set listeners
+  $("#memory_board").on('click', function(){
+    myGame = (myGame === null) ? new MemoryGame() : myGame;
+  });
+
+  $(".pic").on('click', function(){
+    myGame = (myGame === null) ? new MemoryGame() : myGame;
+    var cssID = $(this).attr('id');          //ex: "card-02"
+    var cssIDString = cssID.slice(-2);          //ex: "02"
+    var cssIDInt = parseInt(cssIDString);       //ex: 2
+    var myGridItem = myGame.grid[cssIDInt];
+
+
+    console.log("My cssID is " + cssID);
+    console.log("My cssIDString is " + cssIDString);
+    console.log("My cssIDInt is " + cssIDInt);
+    console.log(myGridItem);
+    console.log("you chose " + myGridItem.card.name);
+
+    myGame.play(cssIDInt);
+    if (myGridItem.isShowing){
+      $('#' + cssID).attr('src', './img/' + myGridItem.card.name + ".jpg");
+    } else {
+      $('#' + cssID).attr('src', '');
+    }
+  });
 });
