a3c8c94ab26f633ae5d836f5ef9e2641c8d3e093
Javier <jvrcueto@outlook.com>
2017-02-08 20:06:56 -0500

    three hours in, almost done

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -1,6 +1,64 @@
+/*====
+I might have been going for some fancy points in this code, lol.
+Totally not KISS.
+====*/
+
 //******************************************************************
 // Game Logic
 //******************************************************************
+
+class MemoryGame {
+
+  constructor () {
+
+    this.won = false;
+    this.matchedPairs = [];
+    this.prevImg = null;
+    this.prevCoordinate = null;
+    this.pair = this.makePairs();
+  }
+
+  makePairs() {
+
+    let coors = [];
+    let rowNum = $('.row').length + 1;
+    let colNum = ($('.col-sm-2').length / $('.row').length) + 1;
+    for (let row = 1; row < rowNum; row++) {
+      for (let col = 1; col < colNum; col++) {
+        coors.push(row + '-' + col);
+      }
+    }
+
+    let pairs = {};
+    let possibleCoors = coors.slice(0);
+    let firstCoor, secondCoor;
+    for (var i = 0; i < coors.length / 2; i++) {
+
+      firstCoor  = _.sample(possibleCoors);
+      secondCoor = _.sample(possibleCoors);
+
+      while(firstCoor === secondCoor) {
+        firstCoor  = _.sample(possibleCoors);
+        secondCoor = _.sample(possibleCoors);
+      }
+
+      possibleCoors.splice(possibleCoors.indexOf(firstCoor), 1);
+      possibleCoors.splice(possibleCoors.indexOf(secondCoor), 1);
+
+      pairs[firstCoor] = secondCoor;
+      pairs[secondCoor] = firstCoor;
+
+    }
+
+    return pairs;
+  }
+
+
+
+}
+
+
+
 
 //******************************************************************
 // HTML/CSS Interactions
@@ -8,4 +66,34 @@
 
 
 $(document).ready(function(){
+
+  let game = new MemoryGame();
+
+  $('img').click(function checkMatch() {
+
+    let img = $(this);
+    if (img.prop('src') === 'img/riddler.jpg') {
+
+      let coor = img.prop('alt');
+
+      if (game.pairs[coor] === game.prevCoordinate) {
+        img.prop('src', 'img/aquaman'); //need to know what img to use
+      }
+      else {
+        //flip them back
+      }
+
+      game.prevImg = img;
+      game.prevCoordinate = coor;
+
+    }
+  });
+
+  // function getImgEl(rowNum, colNum) {
+  //   return $('.container .row:nth-child(' +
+  //   rowNum + ') .col-sm-3:nth-child(' +
+  //   colNum + ') .pic:first');
+  // }
+
+
 });
