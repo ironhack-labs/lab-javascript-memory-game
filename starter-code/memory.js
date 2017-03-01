5ac133d2a8a9d62b78371f3bb99053fb45f1faf8
Nick Borbe <nickborbe@gmail.com>
2017-02-28 19:08:20 -0500

    add more js in starter code

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -1,11 +1,61 @@
-//******************************************************************
-// Game Logic
-//******************************************************************
+var MemoryGame = function() {
+  this.Cards = [
+  		{ name: "aquaman",         img: "aquaman.jpg" },
+  		{ name: "batman",          img: "batman.jpg" },
+  		{ name: "captain america", img: "captain-america.jpg" },
+  		{ name: "fantastic four",  img: "fantastic-four.jpg" },
+  		{ name: "flash",           img: "flash.jpg" },
+      { name: "green arrow",     img: "green-arrow.jpg" },
+  		{ name: "green lantern",   img: "green-lantern.jpg" },
+  		{ name: "ironman",         img: "ironman.jpg" },
+  		{ name: "spiderman",       img: "spiderman.jpg" },
+  		{ name: "superman",        img: "superman.jpg" },
+  		{ name: "the avengers",    img: "the-avengers.jpg" },
+  		{ name: "thor",            img: "thor.jpg" },
+      { name: "aquaman",         img: "aquaman.jpg" },
+  		{ name: "batman",          img: "batman.jpg" },
+  		{ name: "captain america", img: "captain-america.jpg" },
+      { name: "fantastic four",  img: "fantastic-four.jpg" },
+  		{ name: "flash",           img: "flash.jpg" },
+  		{ name: "green arrow",     img: "green-arrow.jpg" },
+  		{ name: "green lantern",   img: "green-lantern.jpg" },
+  		{ name: "ironman",         img: "ironman.jpg" },
+  		{ name: "spiderman",       img: "spiderman.jpg" },
+  		{ name: "superman",        img: "superman.jpg" },
+  		{ name: "the avengers",    img: "the-avengers.jpg" },
+  		{ name: "thor",            img: "thor.jpg" },
+  	];
+  this.picked_cards  = [];
+  this.pairs_clicked = 0;
+  this.pairs_guessed = 0;
+  this._shuffleCard();
+};
+// this function just takes the array of cards above and shuffles them into a random order
+MemoryGame.prototype._shuffleCard = function() {
+  var counter = this.Cards.length;
 
-//******************************************************************
-// HTML/CSS Interactions
-//******************************************************************
+  while (counter > 0) {
+    index = Math.floor(Math.random() * counter);
+    counter--;
+    temp = this.Cards[counter];
+    this.Cards[counter] = this.Cards[index];
+    this.Cards[index] = temp;
+  }
+  return;
+};
 
 
+
+
+
+
+
+var memoryGame;
 $(document).ready(function(){
+  memoryGame = new MemoryGame();
+
+
+
+
+
 });
