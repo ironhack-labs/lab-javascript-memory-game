2849a80721484b3cdca9c03a4637cc15e049a8c9
Alejandro <ajimenez1724@gmail.com>
2017-03-03 14:12:23 -0500

    Saving

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -1,3 +1,6 @@
+
+
+
 var MemoryGame = function() {
   this.Cards = [
   		{ name: "aquaman",         img: "aquaman.jpg" },
@@ -25,34 +28,85 @@ var MemoryGame = function() {
   		{ name: "the avengers",    img: "the-avengers.jpg" },
   		{ name: "thor",            img: "thor.jpg" },
   	];
-  this.picked_cards  = [];
-  this.pairs_clicked = 0;
-  this.pairs_guessed = 0;
-  this._shuffleCard();
+  this.pickedCards  = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+  this.shuffleCard();
 };
-// this function just takes the array of cards above and shuffles them into a random order
-MemoryGame.prototype._shuffleCard = function() {
-  var counter = this.Cards.length;
+// ---------------_> PROGRAM UNIVERSE  <--------------------
 
-  while (counter > 0) {
-    index = Math.floor(Math.random() * counter);
-    counter--;
-    temp = this.Cards[counter];
-    this.Cards[counter] = this.Cards[index];
-    this.Cards[index] = temp;
+// ------- Method to shuffle the cards
+
+
+MemoryGame.prototype.shuffleCard = function() {
+  var counter = this.Cards.length, j, temp;
+
+  while (--counter > 0) {
+    j = Math.floor(Math.random() * (counter+1));
+    temp = this.Cards[j];
+    this.Cards[j] = this.Cards[counter];
+    this.Cards[counter] = temp;
   }
-  return;
 };
 
 
+// ------- Method to shuffle the cards   ENDS
+
+
+  $( document ).ready(function() {
+
+    // Your code here.
+
+var theGame = new MemoryGame();
+
+console.log(theGame.Cards);
+
+
+theGame.shuffleCard();
+$('.pic').append('<img src="#">');
+for (i = 0; i < theGame.Cards.length; i++) {
+  $('.pic img').eq(i).attr('src', 'img/' + theGame.Cards[i].img);
+}
+
+
+// ------- FUNCTION THAT MATCHS CARDS
+
+
+MemoryGame.prototype.selectCard = function(card) {
+
+  // this.shuffleCard
+};
+
+// ------- FUNCTION THAT MATCHS CARDS   ENDS
+
+
+
+// ------- FUNCTION  WIN WIN WIN WIN  A R E A
+
+
+MemoryGame.prototype.finished = function() {
+  if (this.pairsGuessed >= 16) {
+  alert("You are done");
+}
+};
+
+// ------- FUNCTION  WIN WIN WIN WIN  A R E A  ENDS
 
 
 
 
 
-var memoryGame;
-$(document).ready(function(){
-  memoryGame = new MemoryGame();
+
+
+// ---------------_> USER UNIVERSE  <--------------------
+
+
+//Event Listener
+
+
+$('.pic').on('click', function(){
+
+});
 
 
 
