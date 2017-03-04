acd1f70ad619575789daa6d31b0e5fbcf82ba551
Gustavo Gutierrez <imguss@gmail.com>
2017-03-04 09:54:38 -0500

    done

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -45,17 +45,67 @@ MemoryGame.prototype._shuffleCard = function() {
 };
 
 
-
-
-
-
-
 var memoryGame;
+
 $(document).ready(function(){
   memoryGame = new MemoryGame();
 
+  var imgArray = [];
+  var pairArray = [];
+  var pairsFound = 0;
+  var pairsTried = 0;
+  var pairAmount = $('.pic').length / 2;
+
+  memoryGame._shuffleCard();
 
 
+  $('.pic').click(function(e){
+
+    var curInd = $('.pic').index(e.currentTarget);
+    var curImg = memoryGame.Cards[curInd].img;
+    console.log(curImg);
 
 
+    $(e.currentTarget).addClass('flipped');
+
+    if ($('.flipped').length % 2 !== 0 ) {
+      $(e.currentTarget).css('background-image', 'url(img/' + curImg + ')');
+      imgArray.push(curImg);
+      pairArray.push(e.currentTarget);
+      console.log(imgArray);
+
+    } else {
+      $(e.currentTarget).css('background-image', 'url(img/' + curImg + ')');
+      imgArray.push(curImg);
+      pairArray.push(e.currentTarget);
+      console.log(imgArray);
+      console.log(pairArray);
+      pairsTried += 1;
+      $('#pairs-tried').html("Pairs tried: " + pairsTried);
+
+      if (imgArray[0] === imgArray[1]) {
+        pairsFound += 1;
+        $('#pairs-found').html("Pairs found: " + pairsFound);
+        for (i = 0; i < pairArray.length; i++) {
+          $(pairArray[i]).addClass('found');
+          console.log(pairArray[x]);
+        }
+        imgArray = [];
+        pairArray = [];
+        if (pairsFound === pairAmount) {
+          alert("YOU WON! It took you " + pairsTried + " tries.");
+        }
+
+      } else {
+        setTimeout(function(){
+          for (x = 0; x < pairArray.length; x++) {
+            $(pairArray[x]).css('background-image', '');
+            $(pairArray[x]).removeClass('flipped');
+          }
+          imgArray = [];
+          pairArray = [];
+        }, 1000);
+      }
+    }
+  });
 });
