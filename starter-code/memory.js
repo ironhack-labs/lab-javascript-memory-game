8e80275fda2eb6420e4441001c174c988becace0
Andreas Karpasitis <andreasmkar@gmail.com>
2017-04-12 09:40:11 +0200

    code with issues

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -1,3 +1,5 @@
+
+
 var MemoryGame = function() {
   this.Cards = [
   		{ name: "aquaman",         img: "aquaman.jpg" },
@@ -40,21 +42,129 @@ MemoryGame.prototype._shuffleCard = function() {
     temp = this.Cards[counter];
     this.Cards[counter] = this.Cards[index];
     this.Cards[index] = temp;
+    // console.log(this.Cards[counter]);
   }
+
   return;
 };
 
+function doUrlLoop() {
+  var tmpArray = [];
+  var tmp;
+  for (var i=0; i<24; i++) {
+    tmp = memoryGame.Cards[i].img;
+    tmp = String(tmp);
+    console.log(tmp);
+    tmp = "url('./img/" + tmp + "')";
+    tmpArray[i] = tmp;
+    // console.log(tmp);
+  }
+  return tmpArray;
+}
+
+function updateCssLoop(textArray) {
+  var tmpClassHolder;
+  for (var x=0; x<24; x++) {
+    tmpClassHolder = "pic";
+    tmpClassHolder = tmpClassHolder + (x + 1);
+    $('.' + tmpClassHolder).css('background-image',textArray[x]);
+    $('.' + tmpClassHolder).attr('id','\'' + memoryGame.Cards[x].name + '\'');
+  }
+}
+
+function updateRandomPictures() {
+  var textArray = [];
+  textArray = doUrlLoop();
+  updateCssLoop(textArray);
+}
+
+function showPicturesShortly() {
+  $("#overlay").hide();
+  setTimeout(function(){
+    $("#overlay").show();
+}, 2000);
+
+}
+
+function eventListener() {
+  var name1, name2;
+  var choices = [];
+  var clickCount = 0;
+  $('#overlay').click(function (e) {
+      if (clickCount < 2) {
+    if (e.target.id.match('pic')) {
+        var entry = $(e.target);
+        console.log(entry);
+        entry.css('opacity','0');
+        choices[clickCount] = entry;
+        console.log("clickCount: " + clickCount);
+        clickCount++;
+
+      }
+    }
+
+    console.log("test");
+    var choice1 = choices[0];
+    var choice2 = choices[1];
+    console.log(choice1);
+    var id1overlay;
+    id1overlay = choice1.attr('id');
+    name1 = $('.' + id1overlay).attr('id');
+    var id2overlay;
+    id2overlay = choice2.attr('id');
+    name2 = $('.' + id2overlay).attr('id');
+    // console.log("id",id1overlay);
+    // console.log("id",id2overlay);
+
+    // name1 = $('.' + id1overlay).attr('id');
+    // name2 = $('.' + id2overlay).attr('id');
+    // console.log("name", name1);
+    // console.log("name", name2);
+    if (name1 != name2 && name2 !== null && name1 !== null) {
+      setTimeout(function(){
+        choice1.css('opacity','1');
+        choice2.css('opacity','1');
+        clickCount = 0;
+        name1=null;
+        name2=null;
+      }, 2500);
 
 
+      eventListener();
+    } else {
 
+      var imageLink = $('.'+id1overlay).css('background-image');
+      choice1.css('background-image',imageLink);
+      choice1.css('background-size','contain');
+      choice2.css('background-image',imageLink);
+      choice2.css('background-size','contain');
+      name1=null;
+      name2=null;
+      clickCount = 0;
+      eventListener();
+      // console.log(imageLink);
+      // choice1.css('background-image','\'' + m);
+      // choice2.css('disabled','true');
+    }
 
+  //  if (choice1.attr('id') != choice2.attr('')) {
 
+  //  }
+
+  });
+}
+
+function playRules(choices) {
+}
 
 var memoryGame;
 $(document).ready(function(){
   memoryGame = new MemoryGame();
-
-
+  $("#overlay").css('background','transparent');
+  // console.log(memoryGame.Cards[0]);
+  updateRandomPictures();
+  showPicturesShortly();
+  eventListener();
 
 
 
