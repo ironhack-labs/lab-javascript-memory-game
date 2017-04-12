174dfbaa95c7e9660ef64b9d9a405bc472502fe9
TuckerDS <jjperez.tucker.dev@gmail.com>
2017-04-12 15:18:29 +0200

    iteration 2

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -2,7 +2,8 @@ var MemoryGame = function() {
   this.numTry = 0;
   this.numSuc = 0;
   this.board = $(".board");
-  this.grid = [];
+  this.userSelect1 = "";
+  this.userSelect2 = "";
 
   this.Cards = [
   		{ name: "aquaman",         img: "aquaman.jpg" },
@@ -33,6 +34,7 @@ var MemoryGame = function() {
   this.picked_cards  = [];
   this.pairs_clicked = 0;
   this.pairs_guessed = 0;
+
   this._shuffleCard();
 
 
@@ -71,28 +73,41 @@ MemoryGame.prototype.createBoard = function(){
   for (var i=0; i<this.Cards.length; i++) {
     currentDivID = ("#"+i);
     currentDiv = $(currentDivID);
-    $(this.board).append("<div class='pic " + this.Cards[i].name + "' id='" + i + "'></div>");
-    $("#"+i).css("background-image", "url('img/"+ this.Cards[i].img + "')");
+    $(this.board).append("<div class='pic data-card-name=''" + this.Cards[i].name + "' id='" + i + "'></div>");
+    //$("#"+i).css("background-image", "url('img/"+ this.Cards[i].img + "')");
+    $("#"+i).css("background-image", '');
+    that = this;
     $("#"+i).on("click", this.divClick);
   }
 };
 
 MemoryGame.prototype.divClick = function (e){
     //console.log(e.currentTarget.id);
-    var ele = $("#"+e.currentTarget.id);
-
-    if (ele.css("background-image") === 'url("")') {
-
+    var id = e.currentTarget.id;
+    console.log(id);
+    var ele = $("#"+id);
+    if (ele.css("background-image")=="none") {
       //e.css("background-image","url('img/"+ this.Cards[i].img + "')");
-      ele.css("background-image","url('img/"+ this.Cards[$("#"+e.currentTarget.id)].img + "')");
+      ele.css("background-image", "url('img/"+ that.Cards[id].img + "')");
     } else {
-      ele.removeAttr("background-image");
-      console.log(ele.css("background-image"));
-      console.log(ele.prop("background-image"));
+      ele.css("background-image", '');
     }
 };
 
 var memoryGame;
+
+clicks = [];
+MemoryGame.prototype.checkClick = function(id) {
+  if (clicks.length < 2 ) {
+    if (clicks.length == 1) {
+
+    } else {
+      clicks.push(id);
+    }
+  } else {
+
+  }
+};
 
 
 
