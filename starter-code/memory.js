b96d416fbd128083f276897fa7ae88b222226e72
TuckerDS <jjperez.tucker.dev@gmail.com>
2017-04-12 08:36:46 +0200

    1 iteration

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -1,4 +1,9 @@
 var MemoryGame = function() {
+  this.numTry = 0;
+  this.numSuc = 0;
+  this.board = $(".board");
+  this.grid = [];
+
   this.Cards = [
   		{ name: "aquaman",         img: "aquaman.jpg" },
   		{ name: "batman",          img: "batman.jpg" },
@@ -29,6 +34,10 @@ var MemoryGame = function() {
   this.pairs_clicked = 0;
   this.pairs_guessed = 0;
   this._shuffleCard();
+
+
+  //rellenar el grid
+  this.createBoard();
 };
 // this function just takes the array of cards above and shuffles them into a random order
 MemoryGame.prototype._shuffleCard = function() {
@@ -44,17 +53,51 @@ MemoryGame.prototype._shuffleCard = function() {
   return;
 };
 
+MemoryGame.prototype.compareCards = function(cards) {
+  if (cards[0].name === cards[i].name) {
+    //Dejar fijas
+    //Añadir contador de exitos
+    this.numSuc+=1;
+  } else {
+    //Voltear
+    //Añadir contador intentos
+    this.numTry+=1;
+  }
+};
 
+MemoryGame.prototype.createBoard = function(){
+  var currentDivID;
+  var currentDiv;
+  for (var i=0; i<this.Cards.length; i++) {
+    currentDivID = ("#"+i);
+    currentDiv = $(currentDivID);
+    $(this.board).append("<div class='pic " + this.Cards[i].name + "' id='" + i + "'></div>");
+    $("#"+i).css("background-image", "url('img/"+ this.Cards[i].img + "')");
+    $("#"+i).on("click", this.divClick);
+  }
+};
 
+MemoryGame.prototype.divClick = function (e){
+    //console.log(e.currentTarget.id);
+    var ele = $("#"+e.currentTarget.id);
 
+    if (ele.css("background-image") === 'url("")') {
 
-
+      //e.css("background-image","url('img/"+ this.Cards[i].img + "')");
+      ele.css("background-image","url('img/"+ this.Cards[$("#"+e.currentTarget.id)].img + "')");
+    } else {
+      ele.removeAttr("background-image");
+      console.log(ele.css("background-image"));
+      console.log(ele.prop("background-image"));
+    }
+};
 
 var memoryGame;
+
+
+
 $(document).ready(function(){
   memoryGame = new MemoryGame();
-
-
 
 
 
