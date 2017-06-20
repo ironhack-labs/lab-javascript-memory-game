de35d4b5ee10ac938ed067c6aaba304c2d12d163
Maria Mayer <mayermaru@gmail.com>
2017-06-20 23:49:11 +0200

    done

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -1,3 +1,13 @@
+
+var flipBack = function(item){
+  $('#'+item).find('.front').hide();
+  console.log(item);
+};
+
+var updateHtml = function(node,value){
+  $(node).html(value);
+};
+
 // //******************************************************************
 // // Game Logic
 // //******************************************************************
@@ -33,6 +43,37 @@ var MemoryGame = function() {
     this.correctPairs = 0;
 };
 
+MemoryGame.prototype._shuffleCards = function(array) {
+  for(var j, x, i = array.length; i; j = Math.floor(Math.random() * i), x = array[--i], array[i] = array[j], array[j] = x);
+  return array;
+};
+
+MemoryGame.prototype.selectCard = function(card){
+  this.selectedCards.push(card);
+
+  if(this.selectedCards.length==2){
+    this.pairsClicked+=1;
+    updateHtml('#pairs_clicked',this.pairsClicked);
+
+    if(this.selectedCards[0]==card){
+      this.correctPairs += 1;
+      updateHtml('#pairs_guessed',this.correctPairs);
+      this.selectedCards = [];
+    }else{
+      var prev=this.selectedCards[0];
+      var current=card;
+      setTimeout(function(){
+        flipBack(prev);
+      }, 1000);
+      setTimeout(function(){
+        flipBack(current);
+      }, 1500);
+      console.log('wroong!');
+      this.selectedCards = [];
+    }
+  }
+};
+
 // //******************************************************************
 // // HTML/CSS Interactions
 // //******************************************************************
@@ -43,6 +84,8 @@ $(document).ready(function(){
   memoryGame = new MemoryGame();
   var html = '';
 
+  memoryGame._shuffleCards(memoryGame.cards);
+
   memoryGame.cards.forEach(function(pic, index) {
     var sanitizedName = pic.name.split(' ').join('_');
 
@@ -50,14 +93,21 @@ $(document).ready(function(){
     html += '<div class="back"';
     html += '    name="img/' + pic.name + '"';
     html += '    id="'       + pic.img +  '">';
-    html += '</div>';
     html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + '") no-repeat"';
+    html += 'style="background: url(img/' + pic.img + ') no-repeat"';
     html += '    id="'       + pic.img +  '">';
+    html += '</div>';
     html += '</div>';
     html += '</div>';
   });
 
   // Add all the divs to the HTML
   document.getElementById('memory_board').innerHTML = html;
+
+  var counter=0;
+  $('.back').on('click', function(){
+    var card=$(this).parent().attr('id');
+    memoryGame.selectCard(card);
+    $(this).find('div').show();
+  });
 });
