e9b9bc5bc87ed17a0db7706d8037449d40bc0a78
alejandro <alejandro.m.gomez.92@gmail.com>
2017-06-20 19:55:32 +0200

    a

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -32,6 +32,7 @@ var MemoryGame = function() {
     this.pairsClicked = 0;
     this.correctPairs = 0;
     this.shuffleCards();
+    this.isCorrect = false;
 };
 MemoryGame.prototype.shuffleCards = function() {
   this.cards = _.shuffle(this.cards);
@@ -40,21 +41,22 @@ MemoryGame.prototype.shuffleCards = function() {
 
 MemoryGame.prototype.selectCard = function(card){
   this.selectedCards.push(card);
-  console.log(this.selectedCards[0]);
-    console.log(this.selectedCards[1]);
-  console.log(this.selectedCards);
+  //console.log(this.selectedCards);
   if (this.selectedCards.length == 2) {
     this.pairsClicked++;
 
       if (this.selectedCards[0] === this.selectedCards[1]) {
         this.correctPairs++;
         this.selectedCards = [];
+        this.isCorrect = true;
 
         console.log("Parejas correctas: " + this.correctPairs);
             console.log("Parejas elegidas: "+ this.pairsClicked);
+            return true;
       }else{
         console.log('no coinciden, reiniciamos turno');
         this.selectedCards = [];
+        return false;
       }
   }else {
     console.log("elige otra carta");
@@ -77,7 +79,7 @@ $(document).ready(function(){
     var sanitizedName = pic.name.split(' ').join('_');
 
     html += '<div class= "card" id="' + sanitizedName + '">';
-    html += '<div class="back visible"';
+    html += '<div class="back"';
     html += '    name="img/' + pic.name + '"';
     html += '    id="'       + pic.img +  '">';
     html += '</div>';
@@ -89,14 +91,27 @@ $(document).ready(function(){
   });
 
   // Add all the divs to the HTML
+
   document.getElementById('memory_board').innerHTML = html;
 
-
+   function flip(){
+     if (memoryGame.isCorrect){
+       console.log('acertado');
+     }else {
+        $(".card").find(':first-child').addClass('back');
+        $(".card").find(':first-child').next().removeClass('back');
+      }
+  }
   $(".card").click(function(){
      var card = $(this).attr('id');
-     $(thi).toggleClass('back');
-    memoryGame.selectCard(card);
-    console.log(card);
+     console.log(card);
+
+  if(memoryGame.selectCard(card);) 
+   $(this).find(':first-child').removeClass('back');
+    $(this).find(':first-child').next().addClass('back');
+
+
+
   });
 
 });
