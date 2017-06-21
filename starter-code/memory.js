50a6bbadc7af2fbd9fc71cc4f12fb8da0e3032b4
Alex Fontanellas <alexfontanellas@hotmail.com>
2017-06-21 18:50:45 +0200

    missing logic

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -31,51 +31,72 @@ var MemoryGame = function() {
     this.selectedCards = [];
     this.pairsClicked = 0;
     this.correctPairs = 0;
+
 };
 
 // //******************************************************************
 // // HTML/CSS Interactions
 // //******************************************************************
 var memoryGame;
+memoryGame = new MemoryGame();
+var arrayCurrentCards = [1];
 
 $(document).ready(function(){
-  memoryGame = new MemoryGame();
+
+  MemoryGame.prototype.shuffleCards = function() {
+    var m = memoryGame.cards.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = memoryGame.cards[m];
+      memoryGame.cards[m] = memoryGame.cards[i];
+      memoryGame.cards[i] = t;
+    }
+  };
+
+  memoryGame.shuffleCards();
   var html = '';
 
   memoryGame.cards.forEach(function(pic, index) {
     var sanitizedName = pic.name.split(' ').join('_');
 
-    html += '<div class= "card" id="card_' + sanitizedName + '">';
+    html += '<div class= "card actual-image" id="card_' + sanitizedName + '">';
     html += '<div class="back"';
     html += '    name="img/' + pic.name + '"';
     html += '    id="'       + pic.img +  '">';
     html += '</div>';
     html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + '") no-repeat"';
+    html += 'style="background: url(\'img/' + pic.img + '\') no-repeat"';
     html += '    id="'       + pic.img +  '">';
     html += '</div>';
     html += '</div>';
+
   });
 
   // Add all the divs to the HTML
   document.getElementById('memory_board').innerHTML = html;
-});
 
-MemoryGame.prototype._shuffleCards = function() {
-  for(var i = 0;i<this.cards.length;i++){
-    var randomInteger = Math.floor(Math.random()*this.cards.length);
-    if(this.cards[i]!==this.cards[randomInteger]){
-      this.cards[i]=this.cards[randomInteger];
-    }
-  }
-};
-MemoryGame.prototype.selectCard = function(card){
-  
-};
+  MemoryGame.prototype.selectCard = function(card){
+    card.children(":first-child").toggleClass("back");
+    card.children(".front").toggleClass("actual-image");
+
+  };
 
 
-memoryGame._shuffleCards();
 
-memoryGame.forEach(function(element){
-  console.log(element);
+  $(".card").on("click",function(){
+    arrayCurrentCards.push($(this));
+    memoryGame.selectCard($(this));
+    arrayCurrentCards.splice(0,1);
+
+  });
+
+
+
+
 });
