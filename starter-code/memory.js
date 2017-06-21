c1f9070fa0ade043d3a11e11691a7d9b8e7c2237
Brad Le <brad.le444@gmail.com>
2017-06-21 20:46:20 +0200

    working shuffle and card flip

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -32,15 +32,51 @@ var MemoryGame = function() {
     this.pairsClicked = 0;
     this.correctPairs = 0;
 };
+//memoryGame.shuffleCards();
+MemoryGame.prototype.shuffleCards = function() {
+ var currentIndex = this.cards.length, temporaryValue, randomIndex;
+ while(0 !==currentIndex){
+   randomIndex = Math.floor(Math.random()*currentIndex);
+   currentIndex -=1;
+   temporaryValue = this.cards[currentIndex];
+   this.cards[currentIndex] = this.cards[randomIndex];
+   this.cards[randomIndex] = temporaryValue;
+ }
+  return this.cards;
+};
+// SELECT A CARD
+MemoryGame.prototype.selectCard = function(card) {
+  this.selectedCards.push(card);
+  if(this.selectedCards.length === 2) {
+    this.pairsClicked++;
+  } else {
+  this.selectedCards.push(card);
+} if (this.selectedCards[0] === this.selectedCards[1]){
+  this.selectedCards = [];
+  this.correctedPairs++;
+  //FLIP CARD
+} else {
+  return "WRONG GUESS";
+}
+};
+// WIN FUNCTION
+MemoryGame.prototype.finished = function () {
+  if(this.correctedPairs === this.cards/2) {
+    return ("YOU WIN!");
+}
+};
+//Psuedo-Code
+// Shuffle cards
+//
 
 // //******************************************************************
 // // HTML/CSS Interactions
 // //******************************************************************
 
 var memoryGame;
-
 $(document).ready(function(){
   memoryGame = new MemoryGame();
+  memoryGame.shuffleCards();
   var html = '';
 
   memoryGame.cards.forEach(function(pic, index) {
@@ -52,7 +88,7 @@ $(document).ready(function(){
     html += '    id="'       + pic.img +  '">';
     html += '</div>';
     html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + '") no-repeat"';
+    html += 'style="background: url(\'img/' + pic.img + '\') no-repeat"';
     html += '    id="'       + pic.img +  '">';
     html += '</div>';
     html += '</div>';
@@ -60,4 +96,27 @@ $(document).ready(function(){
 
   // Add all the divs to the HTML
   document.getElementById('memory_board').innerHTML = html;
+  $('div.front').hide(); // This hides all front displays at the beginning of game
+  $('.back').on('click',function(){
+    var index = $('.back').index(this);
+    console.log(index);
+    $(this).hide();
+    $('div.front').eq(index).show();
+    // $('.front').css("display","block");
+    // $(this).toggleClass('front');
+
+  });
+
 });
+
+//Toggle back class
+//enable front class
+
+// 'style="background: url(\'img/' + pic.img + '\') no-repeat"'
+
+//
+// <div class="back" name="img/aquaman" id="aquaman.jpg"></div>
+// <div class="front" style="background: url('img/aquaman.jpg') no-repeat;" id="aquaman.jpg"></div>
+//$('div.back').hide(); this will hide all backs and show front
+//$('div.car:nth-child(index starting from 1)') returns card
+//$('div.card').get(0) returns specific card to flip
