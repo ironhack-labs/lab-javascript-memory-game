bd2137068d5aa8d1a6ddd3f0863c4a52b0f2c35f
Luis Amez <luis.amez.vitoria@gmail.com>
2017-06-21 18:11:32 +0200

    Done

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -33,6 +33,68 @@ var MemoryGame = function() {
     this.correctPairs = 0;
 };
 
+// Shuffles the cards (applies Fisher-Yates)
+MemoryGame.prototype.shuffleCards = function() {
+  cards = this.cards;
+  var currentIndex = cards.length;
+  var temporaryValue;
+  var randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = cards[currentIndex];
+    cards[currentIndex] = cards[randomIndex];
+    cards[randomIndex] = temporaryValue;
+  }
+  return cards;
+};
+
+// Selects a card
+MemoryGame.prototype.selectCard = function(card) {
+  if (this.selectedCards.length === 0) {
+    this.selectedCards.push(card);
+    showCard(card);
+  } else if (this.selectedCards.length === 1) {
+    this.selectedCards.push(card);
+    showCard(card);
+    if(this.isMatch()) {
+      this.correctPairs++;
+      this.selectedCards = [];
+    } else {
+      setTimeout(function() {
+        hideCard(this.selectedCards[0]);
+        hideCard(card);
+        this.selectedCards = [];
+      }.bind(this), 1000);
+    }
+    this.pairsClicked++;
+  }
+  if (this.correctPairs === 12) {
+    this.finished();
+  }
+};
+
+// Compares two cards, returns true if they are equals
+MemoryGame.prototype.isMatch = function () {
+  var match = false;
+  var sameHero = (this.selectedCards[0].attr('id') === this.selectedCards[1].attr('id'));
+  if(sameHero) {
+    match = true;
+  }
+  return match;
+};
+
+//
+MemoryGame.prototype.finished = function () {
+  $("#congratulations").show();
+};
+
 // //******************************************************************
 // // HTML/CSS Interactions
 // //******************************************************************
@@ -41,6 +103,7 @@ var memoryGame;
 
 $(document).ready(function(){
   memoryGame = new MemoryGame();
+  memoryGame.shuffleCards();
   var html = '';
 
   memoryGame.cards.forEach(function(pic, index) {
@@ -52,7 +115,8 @@ $(document).ready(function(){
     html += '    id="'       + pic.img +  '">';
     html += '</div>';
     html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + '") no-repeat"';
+    html += 'style="background: url(\'img/' + pic.img + '\') no-repeat"';
+    // html += 'style="background: url(img/' + pic.img + '") no-repeat"';
     html += '    id="'       + pic.img +  '">';
     html += '</div>';
     html += '</div>';
@@ -60,4 +124,28 @@ $(document).ready(function(){
 
   // Add all the divs to the HTML
   document.getElementById('memory_board').innerHTML = html;
+
+  // Listener for user click
+  $('.back').on('click', function(){
+    memoryGame.selectCard($(this).parent());
+    updateScore(memoryGame);
+  });
 });
+
+// Shows a card
+function showCard(card) {
+  card.find('.back').hide();
+  card.find('.front').addClass('visible');
+}
+
+// Hides a card
+function hideCard(card) {
+  card.find('.back').show();
+  card.find('.front').removeClass('visible');
+}
+
+// Updates displayed score
+function updateScore(game) {
+  $('#pairs_clicked').text(game.pairsClicked);
+  $('#pairs_guessed').text(game.correctPairs);
+}
