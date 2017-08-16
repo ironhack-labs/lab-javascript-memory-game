3cf852d8d158a4773f47e5137a9d84d712a9fc74
ta-web-mad <ta-web-mad@ironhack.com>
2017-08-16 13:03:51 +0200

    Update memory.js

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -46,14 +46,13 @@ $(document).ready(function(){
   memoryGame.cards.forEach(function(pic, index) {
     var sanitizedName = pic.name.split(' ').join('_');
 
-    html += '<div class= "card" id="card_' + sanitizedName + '">';
+   html += '<div class= "card" name="card_' + sanitizedName + '">';
     html += '<div class="back"';
-    html += '    name="img/' + pic.name + '"';
-    html += '    id="'       + pic.img +  '">';
+    html += '    name="' + pic.name + '">';
     html += '</div>';
     html += '<div class="front" ';
     html += 'style="background: url(img/' + pic.img + '") no-repeat"';
-    html += '    id="'       + pic.img +  '">';
+    html += '    name="'       + pic.name +  '">';
     html += '</div>';
     html += '</div>';
   });
