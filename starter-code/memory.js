26e59a87bba022d1c0b6c84749ddf26081c818c3
Jad ch <jad.chalita@gmail.com>
2017-09-13 09:34:13 +0200

    done!!

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -40,7 +40,7 @@ MemoryGame.prototype._shuffleCards = function () {
     var j = Math.floor(Math.random() * (length - i)) + i
     this.cards = shuffle(i, j, this.cards)
   }
-  console.log(this.cards)
+  // console.log(this.cards)
 }
 
 function shuffle (IndexCardA, IndexCardB, array) {
@@ -55,24 +55,47 @@ function shuffle (IndexCardA, IndexCardB, array) {
 
 MemoryGame.prototype.selectCard = function (card) {
   if (this.selectedCards.length === 0) {
+    // Case: Adding a first card
     this.selectedCards.push(card)
   }
   else {
+    // Case: Adding a second card
+    this.selectedCards.push(card)
     this.pairsClicked += 1
     var cardA = this.selectedCards[0]
-    if (cardA.name === cardB.name) {
-      this.selectedCards = []
+    var cardB = this.selectedCards[1]
+
+    if (cardA === cardB) {
+      // Case: Good guess
+      this.selectedCards = [] // resetting selectedCards
       this.correctPairs += 1
-      //Flip the cards
-    } else {
-      //Flip both cards to the backside
+
+      // Updating game score-board on screen
+      $('#pairs_guessed').text(this.correctPairs)
+      $('#pairs_clicked').text(this.pairsClicked)
+
+      // Hiding the front of the two cards, and displaying the backs
+      $('.toUndisplay[name="' + cardA + '"]').removeClass('toUndisplay')
+      $('.toUndisplay[name="' + cardB + '"]').removeClass('toUndisplay')
+      $('.toDisplay[name="' + cardA + '"]').removeClass('toDisplay')
+      $('.toDisplay[name="' + cardB + '"]').removeClass('toDisplay')
+    }
+    else {
+      // Case: Wrong guess
+      $('#pairs_clicked').text(this.pairsClicked)
+
+      setTimeout(function () {
+        $('.toUndisplay').css('display', 'none')
+        $('.toDisplay').show()
+      }, 1000) // We wait a bit before hiding the two (different) cards
+      this.selectedCards = [] // Resetting selectedcards *after* closing
     }
   }
 }
 
 MemoryGame.prototype.finished = function () {
   if (this.correctPairs === this.cards.length / 2) {
-    //Win!
+    console.log('You won!') //=====> Not tested yet <====
   }
 }
 
@@ -83,7 +106,8 @@ MemoryGame.prototype.finished = function () {
 var memoryGame;
 
 $(document).ready(function(){
-  memoryGame = new MemoryGame();
+  memoryGame = new MemoryGame();  // init
+  memoryGame._shuffleCards()      // shuffle at every new game
   var html = '';
 
   memoryGame.cards.forEach(function(pic, index) {
@@ -103,7 +127,29 @@ $(document).ready(function(){
   // Add all the divs to the HTML
   document.getElementById('memory_board').innerHTML = html;
 
+  // ==================
+  //   Click Handling
+  // ==================
+  // Handling clicks on a backwards-facing card
   $('.back').on('click', function () {
-    console.log($(this))
+    $(this).css('display', 'none') // Hiding the back card
+    $(this).parent().children('.front').show() // Showing the front card
+    $(this).parent().children('.front').addClass('blocked') // Blocks the open card, so that it can't be closed immediately
+    var card_name = ($(this).parent().children('.front').attr("name"))
+    // console.log(card_name)
+
+    //We add the following classes so that we can easily select and flip the cards back if the user guesses wrong:
+    $(this).parent().children('.front').addClass('toUndisplay')
+    $(this).addClass('toDisplay')
+
+    memoryGame.selectCard(card_name)
+    rules_enforcer(memoryGame) //Game police: prevents the user from opening too many cards
   });
+
+  // Stopping the user from clicking more than 2 cards at once
+  function rules_enforcer (memoryGame) {
+    if (memoryGame.selectedCards.length >= 2) {
+      $('.back').addClass('blocked')
+    }
+  }
 });
