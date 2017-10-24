e18024820c8e6995c4c4ba69905048eab38ccf88
Alber Pedraza <alberpedrazaes@gmail.com>
2017-10-24 18:15:01 +0200

    Shuffling

---
+++ starter-code/main.js
@@ -0,0 +1,38 @@
+
+// //******************************************************************
+// // HTML/CSS Interactions
+// //******************************************************************
+
+var memoryGame;
+
+$(document).ready(function(){
+  //
+  memoryGame = new MemoryGame();
+
+  console.log(memoryGame);
+
+  var html = '';
+
+//llamada a funciones prototipadas del constructuror
+  memoryGame.shuffleCards();
+
+  memoryGame.cards.forEach(function(pic, index) {
+    var sanitizedName = pic.name.split(' ').join('_');
+
+   html += '<div class= "card" name="card_' + sanitizedName + '">';
+    html += '<div class="back"';
+    html += '    name="' + pic.name + '">';
+    html += '</div>';
+    html += '<div class="front" ';
+    html += 'style="background: url(img/' + pic.img + '") no-repeat"';
+    html += '    name="'       + pic.name +  '">';
+    html += '</div>';
+    html += '</div>';
+  });
+
+  // Add all the divs to the HTML
+  document.getElementById('memory_board').innerHTML = html;
+
+//creamos objeto de MemoryGame
+
+});
--- starter-code/memory.js
+++ starter-code/memory.js
@@ -5,7 +5,7 @@ var MemoryGame = function() {
   this.cards = [
   		{ name: "aquaman",         img: "aquaman.jpg" },
   		{ name: "batman",          img: "batman.jpg" },
-  		{ name: "captain america", img: "captain-america.jpg" },
+  		{ name: "captain amer ica", img: "captain-america.jpg" },
   		{ name: "fantastic four",  img: "fantastic-four.jpg" },
   		{ name: "flash",           img: "flash.jpg" },
       { name: "green arrow",     img: "green-arrow.jpg" },
@@ -32,31 +32,8 @@ var MemoryGame = function() {
     this.pairsClicked = 0;
     this.correctPairs = 0;
 };
-
-// //******************************************************************
-// // HTML/CSS Interactions
-// //******************************************************************
-
-var memoryGame;
-
-$(document).ready(function(){
-  memoryGame = new MemoryGame();
-  var html = '';
-
-  memoryGame.cards.forEach(function(pic, index) {
-    var sanitizedName = pic.name.split(' ').join('_');
-
-   html += '<div class= "card" name="card_' + sanitizedName + '">';
-    html += '<div class="back"';
-    html += '    name="' + pic.name + '">';
-    html += '</div>';
-    html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + '") no-repeat"';
-    html += '    name="'       + pic.name +  '">';
-    html += '</div>';
-    html += '</div>';
-  });
-
-  // Add all the divs to the HTML
-  document.getElementById('memory_board').innerHTML = html;
-});
+//funciones prototipadas del constructuror
+MemoryGame.prototype.shuffleCards = function() {
+  //shuffle
+  this.cards = _.shuffle(this.cards);
+};
