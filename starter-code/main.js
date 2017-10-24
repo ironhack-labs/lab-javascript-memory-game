e891fa91634da8a9fa319f145ab074676c975a90
Maria Ruiz <mariaruizestevez@gmail.com>
2017-10-24 22:15:13 +0200

    added shuffle method

---
+++ starter-code/main.js
@@ -0,0 +1,30 @@
+
+// //******************************************************************
+// // HTML/CSS Interactions
+// //******************************************************************
+
+var memoryGame;
+
+$(document).ready(function(){
+  memoryGame = new MemoryGame();
+  memoryGame.shuffleCards();
+  var html = '';
+
+  memoryGame.cards.forEach(function(pic, index) {
+    var sanitizedName = pic.name.split(' ').join('_');
+
+   html += '<div class= "card" name="card_' + sanitizedName + '">';
+    html += '<div class="back"';
+    html += '    name="' + pic.name + '">';
+    html += '</div>';
+    html += '<div class="front" ';
+    html += 'style="background: url(img/' + pic.img + '") no-repeat"';
+    html += '    name="'       + pic.name +  '">';
+    html += '</div>';
+    html += '</div>';
+  });
+
+  // Add all the divs to the HTML
+  document.getElementById('memory_board').innerHTML = html;
+
+});
