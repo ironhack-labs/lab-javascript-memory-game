cf7699a8f0cf1bacb176bc75b9c7d5d11f35f107
FranRom <maildefranz@gmail.com>
2017-10-24 18:36:49 +0200

    Shuffling cards

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -33,30 +33,9 @@ var MemoryGame = function() {
     this.correctPairs = 0;
 };
 
-// //******************************************************************
-// // HTML/CSS Interactions
-// //******************************************************************
+// Method to shuffle the cards
 
-var memoryGame;
-
-$(document).ready(function(){
-  memoryGame = new MemoryGame();
-  var html = '';
-
-  memoryGame.cards.forEach(function(pic, index) {
-    var sanitizedName = pic.name.split(' ').join('_');
-
-   html += '<div class= "card" name="card_' + sanitizedName + '">';
-    html += '<div class="back"';
-    html += '    name="' + pic.name + '">';
-    html += '</div>';
-    html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + '") no-repeat"';
-    html += '    name="'       + pic.name +  '">';
-    html += '</div>';
-    html += '</div>';
-  });
-
-  // Add all the divs to the HTML
-  document.getElementById('memory_board').innerHTML = html;
-});
+MemoryGame.prototype.shuffleCards = function() {
+  var newCards = _.shuffle(this.cards);
+  console.log(newCards);
+};
