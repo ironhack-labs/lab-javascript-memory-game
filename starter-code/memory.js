9a9576d6205420f391b3bb2e4c8a3c3e37fcf0c2
alm00001 <alm00001@gmail.com>
2017-10-24 21:55:49 +0200

    1-iteration-in-progress

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -5,7 +5,7 @@ function init(){
 // // Game Logic
 // //******************************************************************
 var MemoryGame = function() {
-  this.cards = [
+  this.cards = _.shuffle[
   		{ name: "aquaman",         img: "aquaman.jpg" },
   		{ name: "batman",          img: "batman.jpg" },
   		{ name: "captain america", img: "captain-america.jpg" },
@@ -35,34 +35,6 @@ var MemoryGame = function() {
     this.pairsClicked = 0;
     this.correctPairs = 0;
 };
-
-// //******************************************************************
-// // HTML/CSS Interactions
-// //******************************************************************
-
-var memoryGame;
-
-$(document).ready(function(){
-  memoryGame = new MemoryGame();
-  var html = '';
-
-  memoryGame.cards.forEach(function(pic, index) {
-    var sanitizedName = pic.name.split(' ').join('_');
-
-   html += '<div class= "card" name="card_' + sanitizedName + '">';
-    html += '<div class="back"';
-    html += '    name="' + pic.name + '">';
-    html += '</div>';
-    html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + '") no-repeat"';
-    html += '    name="'       + pic.name +  '">';
-    html += '</div>';
-    html += '</div>';
-  });
-
-  // Add all the divs to the HTML
-  document.getElementById('memory_board').innerHTML = html;
-});
 
 
 ///////////////////////////////////
