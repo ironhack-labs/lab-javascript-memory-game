e2c7666965fab46715cd47bd3d9a4df9c5f8b7e3
Rocio Garcia <gooblyn@gmail.com>
2017-10-24 18:49:49 +0200

    shuffle done

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -1,3 +1,4 @@
+// Class of the game
 // //******************************************************************
 // // Game Logic
 // //******************************************************************
@@ -33,30 +34,7 @@ var MemoryGame = function() {
     this.correctPairs = 0;
 };
 
-// //******************************************************************
-// // HTML/CSS Interactions
-// //******************************************************************
-
-var memoryGame;
-
-$(document).ready(function(){
-  memoryGame = new MemoryGame();
-  var html = '';
-
-  memoryGame.cards.forEach(function(pic, index) {
-    var sanitizedName = pic.name.split(' ').join('_');
-
-   html += '<div class= "card" name="card_' + sanitizedName + '">';
-    html += '<div class="back"';
-    html += '    name="' + pic.name + '">';
-    html += '</div>';
-    html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + '") no-repeat"';
-    html += '    name="'       + pic.name +  '">';
-    html += '</div>';
-    html += '</div>';
-  });
-
-  // Add all the divs to the HTML
-  document.getElementById('memory_board').innerHTML = html;
-});
+MemoryGame.prototype.suffleCarts = function(){
+  var baraja = _.shuffle(this.cards);
+  this.cards = baraja;
+};
