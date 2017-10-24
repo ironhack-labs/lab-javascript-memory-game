644a589ffde668e1950fe17f25f31a670f674154
Dario Vigil <dariovigil@gmail.com>
2017-10-24 21:50:23 +0200

    Logic finished

--- starter-code/main.js
+++ starter-code/main.js
@@ -25,30 +25,37 @@ $(document).ready(function(){
 
   MemoryGame.prototype.selectCard = function(card) {
     this.selectedCards.push(card);
+    // console.table(this.cards);
+    // console.log(`selectedCards are: ${this.selectedCards}`);
     if(this.selectedCards.length === 2) {
-      //compares the attr name of both cards
       if (this.selectedCards[0] ===  this.selectedCards[1]) {
-        //empty array if match
-        this.selectedCards = [];
-        //increment correct pairs
+        console.log('MATCH!');
         this.correctPairs ++;
-        //flip the card
+        this.removeMatchedCards(card);
+
       } else {
-        //if cars are different
-        //tell user wrong guess
-        // show back side of both
+        console.log('FAIL!');
       }
+      $("#pairs_clicked").html(this.pairsClicked);
+      $("#pairs_guessed").html(this.correctPairs);
+      this.pairsClicked ++;
+      this.selectedCards = [];
     }
-//check if
 };
 
+  MemoryGame.prototype.removeMatchedCards = function removeMatchedCards(card) {
+  var cardOnlyName = card.substr(5, card.length);
+  this.cards = this.cards.filter(function(el) {
+    return el.name !== cardOnlyName;
+    if(this.cards.length === 0) {
+      console.log('YOU WON!');
+    }
+});
+};
   // Add all the divs to the HTML
   document.getElementById('memory_board').innerHTML = html;
 
   $('.card').click(function() {
-    memoryGame.selectCard($(this));
-    // console.log(memoryGame.selectedCards);
-    console.log($(this).attr('name'));
-
+    memoryGame.selectCard($(this).attr('name'));
   });
 });
