af33745f47053702a0ee4e92f2498333a84e9d34
Virginia Álvarez <virginiaalvarez@MacBook-Pro-de-Virginia.local>
2017-10-24 22:01:51 +0200

    1st. part done

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -33,30 +33,12 @@ var MemoryGame = function() {
     this.correctPairs = 0;
 };
 
-// //******************************************************************
-// // HTML/CSS Interactions
-// //******************************************************************
+MemoryGame.prototype.shuffleCards = function(){
+  this.cards = _.shuffle(this.cards);
+  return this.cards;
+};
 
-var memoryGame;
+MemoryGame.prototype.selectCard = function(card){
 
-$(document).ready(function(){
-  memoryGame = new MemoryGame();
-  var html = '';
 
-  memoryGame.cards.forEach(function(pic, index) {
-    var sanitizedName = pic.name.split(' ').join('_');
-
-   html += '<div class= "card" name="card_' + sanitizedName + '">';
-    html += '<div class="back"';
-    html += '    name="' + pic.name + '">';
-    html += '</div>';
-    html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + '") no-repeat"';
-    html += '    name="'       + pic.name +  '">';
-    html += '</div>';
-    html += '</div>';
-  });
-
-  // Add all the divs to the HTML
-  document.getElementById('memory_board').innerHTML = html;
-});
+};
