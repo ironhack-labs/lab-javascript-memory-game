939103a0523d821e8b41b1871ae563c1fd7426bd
Sara <saraeme20@gmail.com>
2017-10-24 17:11:39 +0200

    first commit

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -32,31 +32,3 @@ var MemoryGame = function() {
     this.pairsClicked = 0;
     this.correctPairs = 0;
 };
-
-// //******************************************************************
-// // HTML/CSS Interactions
-// //******************************************************************
-
-var memoryGame;
-
-$(document).ready(function(){
-  memoryGame = new MemoryGame();
-  var html = '';
-
-  memoryGame.cards.forEach(function(pic, index) {
-    var sanitizedName = pic.name.split(' ').join('_');
-
-   html += '<div class= "card" name="card_' + sanitizedName + '">';
-    html += '<div class="back"';
-    html += '    name="' + pic.name + '">';
-    html += '</div>';
-    html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + '") no-repeat"';
-    html += '    name="'       + pic.name +  '">';
-    html += '</div>';
-    html += '</div>';
-  });
-
-  // Add all the divs to the HTML
-  document.getElementById('memory_board').innerHTML = html;
-});
