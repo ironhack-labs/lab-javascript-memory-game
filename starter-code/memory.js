561fbcbedaa987e81cb1a23c46448dcb45f42881
Marcos Monzon <marco238cuba@gmail.com>
2017-11-17 22:18:52 +0100

    feat: each card flips on click

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -33,6 +33,40 @@ var MemoryGame = function() {
     this.correctPairs = 0;
 };
 
+//***********************  shuffle  *****************************//
+MemoryGame.prototype._shuffleCards = function() {
+  var array = this.cards;
+  var size = array.length;
+  var holder, randomIndex;
+
+  while (size) {
+    randomIndex = Math.floor(Math.random() * size--);
+    holder = array[size];
+    array[size] = array[randomIndex];
+    array[randomIndex] = holder;
+  }
+
+  this.cards = array;
+};
+//_______________________________________________________________//
+
+//***********************  score table  *************************//
+MemoryGame.prototype.selectCard = function(card) {
+  var match = false;
+  if(this.selectedCards.length < 1) {
+    this.selectedCards.push(card);
+  } else if (this.selectedCards[0] === card) {
+      this.correctPairs += 1;
+      this.pairsClicked += 1;
+      match = true;
+    } else {
+        this.pairsClicked += 1;
+        this.selectedCards = [];
+      }
+    return match;
+};
+//_______________________________________________________________//
+
 // //******************************************************************
 // // HTML/CSS Interactions
 // //******************************************************************
@@ -42,6 +76,7 @@ var memoryGame;
 $(document).ready(function(){
   memoryGame = new MemoryGame();
   var html = '';
+  memoryGame._shuffleCards();
 
   memoryGame.cards.forEach(function(pic, index) {
     var sanitizedName = pic.name.split(' ').join('_');
@@ -59,4 +94,21 @@ $(document).ready(function(){
 
   // Add all the divs to the HTML
   document.getElementById('memory_board').innerHTML = html;
+
+  $(".front").hide();
+
+  $(".back").click(function(){
+    $(this).next().show();
+    $(this).hide();
+    if(memoryGame.selectCard($(this).attr("name"))){
+      $(this).next().hide();
+      $(this).show();
+    }
+  });
+
+  $(".front").click(function(){
+    $(this).prev().show();
+    $(this).hide();
+  });
+
 });
