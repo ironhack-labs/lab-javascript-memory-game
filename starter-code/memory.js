80fce97caa105e79d0df84c789466012f0c460d9
Marcos Monzon <marco238cuba@gmail.com>
2017-11-19 04:32:50 +0100

    feat: wanna play again?

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -2,107 +2,36 @@
 // // Game Logic
 // //******************************************************************
 var MemoryGame = function() {
-  this.cards = [{
-      name: "aquaman",
-      img: "aquaman.jpg"
-    },
-    {
-      name: "batman",
-      img: "batman.jpg"
-    },
-    {
-      name: "captain america",
-      img: "captain-america.jpg"
-    },
-    {
-      name: "fantastic four",
-      img: "fantastic-four.jpg"
-    },
-    {
-      name: "flash",
-      img: "flash.jpg"
-    },
-    {
-      name: "green arrow",
-      img: "green-arrow.jpg"
-    },
-    {
-      name: "green lantern",
-      img: "green-lantern.jpg"
-    },
-    {
-      name: "ironman",
-      img: "ironman.jpg"
-    },
-    {
-      name: "spiderman",
-      img: "spiderman.jpg"
-    },
-    {
-      name: "superman",
-      img: "superman.jpg"
-    },
-    {
-      name: "the avengers",
-      img: "the-avengers.jpg"
-    },
-    {
-      name: "thor",
-      img: "thor.jpg"
-    },
-    {
-      name: "aquaman",
-      img: "aquaman.jpg"
-    },
-    {
-      name: "batman",
-      img: "batman.jpg"
-    },
-    {
-      name: "captain america",
-      img: "captain-america.jpg"
-    },
-    {
-      name: "fantastic four",
-      img: "fantastic-four.jpg"
-    },
-    {
-      name: "flash",
-      img: "flash.jpg"
-    },
-    {
-      name: "green arrow",
-      img: "green-arrow.jpg"
-    },
-    {
-      name: "green lantern",
-      img: "green-lantern.jpg"
-    },
-    {
-      name: "ironman",
-      img: "ironman.jpg"
-    },
-    {
-      name: "spiderman",
-      img: "spiderman.jpg"
-    },
-    {
-      name: "superman",
-      img: "superman.jpg"
-    },
-    {
-      name: "the avengers",
-      img: "the-avengers.jpg"
-    },
-    {
-      name: "thor",
-      img: "thor.jpg"
-    },
-  ];
-  this.selectedCards = [];
-  this.pairsClicked = 0;
-  this.correctPairs = 0;
-  this.control = 0;
+  this.cards = [
+  		{ name: "aquaman",         img: "aquaman.jpg" },
+  		{ name: "batman",          img: "batman.jpg" },
+  		{ name: "captain america", img: "captain-america.jpg" },
+  		{ name: "fantastic four",  img: "fantastic-four.jpg" },
+  		{ name: "flash",           img: "flash.jpg" },
+      { name: "green arrow",     img: "green-arrow.jpg" },
+  		{ name: "green lantern",   img: "green-lantern.jpg" },
+  		{ name: "ironman",         img: "ironman.jpg" },
+  		{ name: "spiderman",       img: "spiderman.jpg" },
+  		{ name: "superman",        img: "superman.jpg" },
+  		{ name: "the avengers",    img: "the-avengers.jpg" },
+  		{ name: "thor",            img: "thor.jpg" },
+      { name: "aquaman",         img: "aquaman.jpg" },
+  		{ name: "batman",          img: "batman.jpg" },
+  		{ name: "captain america", img: "captain-america.jpg" },
+      { name: "fantastic four",  img: "fantastic-four.jpg" },
+  		{ name: "flash",           img: "flash.jpg" },
+  		{ name: "green arrow",     img: "green-arrow.jpg" },
+  		{ name: "green lantern",   img: "green-lantern.jpg" },
+  		{ name: "ironman",         img: "ironman.jpg" },
+  		{ name: "spiderman",       img: "spiderman.jpg" },
+  		{ name: "superman",        img: "superman.jpg" },
+  		{ name: "the avengers",    img: "the-avengers.jpg" },
+  		{ name: "thor",            img: "thor.jpg" },
+  	];
+    this.selectedCards = [];
+    this.pairsClicked = 0;
+    this.correctPairs = 0;
+    this.control = 0;
 };
 
 //***********************  shuffle  *****************************//
@@ -162,6 +91,8 @@ var memoryGame;
 
 $(document).ready(function() {
   $("#gg").hide();
+  $("#grayBackground").hide();
+  $("#playAgain").hide();
   memoryGame = new MemoryGame();
   var html = '';
   memoryGame._shuffleCards();
@@ -191,6 +122,13 @@ $(document).ready(function() {
     if(memoryGame.control % 2 === 0) {
       setTimeout(hideCard, 500);
     }
-    if(memoryGame.correctPairs === 12) $("#gg").show();
+    if(memoryGame.correctPairs === 12) {
+      $("#gg").fadeIn(1000);
+      $("#playAgain").fadeIn(1000);
+      $("#grayBackground").fadeIn(1000);
+      $("#playAgain").click(function() {
+    location.reload();
+});
+    }
   });
 });
