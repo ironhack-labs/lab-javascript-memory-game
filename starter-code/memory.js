00b79c2a775598e3e67944a1876f043a6e8def4b
Cristina Bertrand <cristina@11870.com>
2017-11-21 14:59:20 +0100

    FEAT: shuffle y function clic ok, function selected card planteada pero necesito ayuda

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -31,16 +31,34 @@ var MemoryGame = function() {
     this.selectedCards = [];
     this.pairsClicked = 0;
     this.correctPairs = 0;
+
+};
+
+//shuffleCards
+
+MemoryGame.prototype.shuffleCards = function(newArray) {
+  var ctr = newArray.length;
+// While there are elements in the array
+  while (ctr > 0) {
+    // Pick a random index
+    index = Math.floor(Math.random() * ctr);
+    // Decrease ctr by 1
+    ctr--;
+    // And swap the last element with it
+    temp = newArray[ctr];
+    newArray[ctr] = newArray[index];
+    newArray[index] = temp;
+  }
+  return newArray;
 };
 
 // //******************************************************************
 // // HTML/CSS Interactions
 // //******************************************************************
 
-var memoryGame;
-
 $(document).ready(function(){
   memoryGame = new MemoryGame();
+  memoryGame.shuffleCards(memoryGame.cards);
   var html = '';
 
   memoryGame.cards.forEach(function(pic, index) {
@@ -51,7 +69,7 @@ $(document).ready(function(){
     html += '    name="' + pic.name + '">';
     html += '</div>';
     html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + '") no-repeat"';
+    html += 'style="background: url(img/' + pic.img + ')" no-repeat"';
     html += '    name="'       + pic.name +  '">';
     html += '</div>';
     html += '</div>';
@@ -59,4 +77,53 @@ $(document).ready(function(){
 
   // Add all the divs to the HTML
   document.getElementById('memory_board').innerHTML = html;
+  //Add some code to call the method whenever a user selects a face-down card.
+  $('.card').on('click', function(){
+    var back= $(this).find('.back');
+    var frontStyle= $(this).find(".front").attr('style');
+    $(back).attr('style', frontStyle);
+  });
 });
+
+//selecting las cards
+MemoryGame.prototype.selectCard = function(card) {
+//If the user has selected a card in the last turn
+    if (memoryGame.selectedCards[0] === undefined) {
+    memoryGame.selectedCards[0] =$(this);
+    }
+    else {
+    memoryGame.selectedCards[1] = $(this);
+    //Increase the pairsClicked attribute of the memoryGame and update the DOM
+    memoryGame.selectedCards.pop();
+    memoryGame.selectedCards.pop();
+    this.pairsClicked++;
+    $("pairs_clicked").text(memoryGame.pairsClicked++);
+    $("pairs_guessed").text(memoryGame.pairsClicked++);
+          //Compare the newly selected card to the previously selected card. Are they of the same type?
+            //If yes
+                  if (this.selectedCards[0].children(".back").attr(name) === this.selectedCards[1].children(".back").attr(name)){
+            //Empty out the selectedCards array for the next round
+                    this.selectedCards =[];
+            //Increase the correctPairs attribute by one
+                    this.correctPairs++;
+            //"Flip" the card
+                    $("div card").toggleClass();
+            //If no
+                  } else {
+            //Possibly add some styling to tell the user "Wrong Guess"
+                  console.log ("Wrong guess");
+            //Flip both cards back to the "back side"
+                  $("div card").toggleClass("div.back");
+            }
+//If the user has not selected a card in the last turn
+this.selectCards.push(card);
+//Add the card to the selectedCards array and move on
+}
+};
+
+
+
+//finishing the Game
+MemoryGame.prototype.finished = function() {
+
+};
