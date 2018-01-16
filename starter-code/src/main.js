f9e8e5967fb9bd7026eefc60268cc4df30e0d331
Victor Arza <papu.arza@gmail.com>
2018-01-16 18:01:59 +0100

    Update main.js

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -1,40 +1,41 @@
 var cards = [
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
+  { name: 'aquaman', img: 'aquaman.jpg' },
+  { name: 'batman', img: 'batman.jpg' },
   { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' },
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
+  { name: 'fantastic four', img: 'fantastic-four.jpg' },
+  { name: 'flash', img: 'flash.jpg' },
+  { name: 'green arrow', img: 'green-arrow.jpg' },
+  { name: 'green lantern', img: 'green-lantern.jpg' },
+  { name: 'ironman', img: 'ironman.jpg' },
+  { name: 'spiderman', img: 'spiderman.jpg' },
+  { name: 'superman', img: 'superman.jpg' },
+  { name: 'the avengers', img: 'the-avengers.jpg' },
+  { name: 'thor', img: 'thor.jpg' },
+  { name: 'aquaman', img: 'aquaman.jpg' },
+  { name: 'batman', img: 'batman.jpg' },
   { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' }
+  { name: 'fantastic four', img: 'fantastic-four.jpg' },
+  { name: 'flash', img: 'flash.jpg' },
+  { name: 'green arrow', img: 'green-arrow.jpg' },
+  { name: 'green lantern', img: 'green-lantern.jpg' },
+  { name: 'ironman', img: 'ironman.jpg' },
+  { name: 'spiderman', img: 'spiderman.jpg' },
+  { name: 'superman', img: 'superman.jpg' },
+  { name: 'the avengers', img: 'the-avengers.jpg' },
+  { name: 'thor', img: 'thor.jpg' }
 ];
 
-$(document).ready(function(){
+$(document).ready(function () {
   var memoryGame = new MemoryGame(cards);
+  memoryGame.shuffleCard(cards);
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
     html += '<div class="back"';
-    html += '    name="'       + pic.img +  '">';
+    html += '    name="' + pic.img + '">';
     html += '</div>';
     html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + '") no-repeat">';
+    html += 'style="background: url(img/' + pic.img + ') no-repeat">';
     html += '</div>';
     html += '</div>';
   });
@@ -42,9 +43,49 @@ $(document).ready(function(){
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
-$('.back').on('click', function () {
-   
-});
-});
+  $('.back').on('click', function () {
 
+    $(this).siblings().toggleClass("back");
+    $(this).toggleClass("back");
 
+    memoryGame.pickedCards.push($(this));
+
+    if (memoryGame.pickedCards.length == 2) {
+      var result = memoryGame.checkIfPair(memoryGame.pickedCards[0].attr("name"), memoryGame.pickedCards[1].attr("name"))
+      //Update counter fields in html, must bei after checkIfPair
+      updateStats();
+      // Game is finished, needs timer do display card
+      if (memoryGame.finished()) {
+        setTimeout(function () {
+          alert("You won!")
+        }, 100);
+      }
+      if (result) {
+        memoryGame.pickedCards = [];
+      } else {
+        $(".back").css("pointer-events", "none");
+        $(".front").css("pointer-events", "none");
+        setTimeout(function () {
+          turnCards(memoryGame.pickedCards);
+        }, 1000);
+      }
+    }
+  });
+
+  function turnCards(cards) {
+    cards[0].toggleClass("back");
+    cards[0].siblings().toggleClass("back");
+    cards[1].toggleClass("back");
+    cards[1].siblings().toggleClass("back");
+    // Unlock divs for clicking
+    $(".back").css("pointer-events", "auto");
+    $(".front").css("pointer-events", "auto");
+    // Remove picked cards
+    memoryGame.pickedCards = [];
+  }
+
+  function updateStats() {
+    $("#pairs_clicked").text(memoryGame.pairsClicked);
+    $("#pairs_guessed").text(memoryGame.pairsGuessed);
+  }
+});
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,37 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  console.log("Before", cardsArr)
+  for (var i = cardsArr.length - 1; i >= 0; i--) {
+    var j = randomNumber(i);
+    console.log("ran", i + ":" + j)
+    var tempObj = Object.assign(cardsArr[j]);
+    cardsArr[j] = Object.assign(cardsArr[i]);
+    cardsArr[i] = tempObj;
+  }
+  console.log("After", cardsArr)
+  return cardsArr;
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++;
+  if (firstCard === secondCard) {
+    this.pairsGuessed++;
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.finished = function () {
+  return this.pairsGuessed === 12;
+};
 
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+function randomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
