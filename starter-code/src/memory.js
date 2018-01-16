97b1aa13f96f0b7bf5dfefc83ef72aec4dd548c1
Alvaro <amartinezfernandez.61@gmail.com>
2018-01-16 15:22:19 +0100

    Primer esquema

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,17 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function(cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function(cardsArr) {
 
-// };
+  return [];
+}
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.checkIfPair = function(firstCard, secondCard) {};
+    this.firstCard = firstCard;
+    this.secondCard = secondCard;
 
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function() {};
