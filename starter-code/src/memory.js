4083ebd0e140afae63df6d395e4fcede9641b20d
Sergio Vélez <sergiovelez.svg@gmail.com>
2018-01-16 23:22:46 +0100

    Terminada parte lógica

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,46 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function(cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function(cardsArr) {
+  var m = cardsArr.length,
+    t,
+    i;
 
-// };
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    // And swap it with the current element.
+    t = cardsArr[m];
+    cardsArr[m] = cardsArr[i];
+    cardsArr[i] = t;
+  }
 
-// }
+  return cardsArr;
+};
 
-// MemoryGame.prototype.finished = function () {
+MemoryGame.prototype.checkIfPair = function(firstCard, secondCard) {
+  this.pairsClicked += 1;
 
-// };
+  if (firstCard == secondCard) {
+    this.pairsGuessed += 1;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+MemoryGame.prototype.finished = function() {
+  if (this.pairsClicked === 0) {
+    return false;
+  } else if (this.pairsGuessed < 12) {
+    return false;
+  } else {
+    return true;
+  }
+};
