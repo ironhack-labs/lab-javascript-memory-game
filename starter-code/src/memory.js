37619abfb088d8dd04ce40e3cc5766a3fa2d1272
Kathia Rieser <kath.medulla@gmail.com>
2018-01-16 20:55:56 +0100

    buff

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,48 @@
 var MemoryGame = function (cards) {
    this.cards = cards;
+   this.pickedCards = [];
+   this.pairsClicked = 0;
+   this.pairsGuessed = 0;
+
  };
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+      
+       for(var i = cardsArr.length; i > 0 ; i-- ){
+          var j = Math.floor(Math.random() * (i + 1));
+          var temp = cardsArr[i];
+          cardsArr[i] = cardsArr[j];
+          cardsArr[j] = temp;
+       }
 
-// };
+       return cardsArr;
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+};
 
-// }
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
-// MemoryGame.prototype.finished = function () {
+    var iguales = false;
 
-// };
+    this.pairsClicked += 1;
+
+    if(firstCard === secondCard){
+
+        this.pairsGuessed += 1;
+        iguales = true;
+    }
+
+    return iguales;
+
+}
+
+ MemoryGame.prototype.finished = function () {
+
+      var final = false;
+
+      //if(this.pairsGuessed > 0 && this.cards.length > 0){
+
+          //final = (this.pairsGuessed ===   (this.cards.length/2));
+          final = (this.pairsGuessed ===   12);
+      //}
+      return final;
+ };
