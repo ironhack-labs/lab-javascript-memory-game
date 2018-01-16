c9a6bc16a3c352e6995100dc6e96f93a45731724
Adrián Chacón <adrianchaconmadueno@gmail.com>
2018-01-16 19:21:34 +0100

    Iteraciónj 1

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,38 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function(cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function(cardsArr) {
+  var shuffling = [],
+    n = cardsArr.length,
+    i;
 
-// };
+  while (n) {
+    i = Math.floor(Math.random() * cardsArr.length);
+    if (i in cardsArr) {
+      shuffling.push(cardsArr[i]);
+      delete cardsArr[i];
+      n--;
+    }
+  }
+  return shuffling;
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.checkIfPair = function(firstCard, secondCard) {
+  this.pairsClicked++;
+  if (firstCard === secondCard) {
+    this.pairsGuessed++;
+    return true;
+  }
+  return false;
+};
 
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+    if (this.pairsGuessed === 12){
+        return true;
+    }
+    return false;
+};
