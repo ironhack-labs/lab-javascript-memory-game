f8f36f165e0bb4e0169e23d176b47ab25d06a14e
Giorgio Grassini <giorgiograssini@gmail.com>
2018-01-16 23:48:46 +0100

    Done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,26 +25,58 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
-$(document).ready(function(){
+$(document).ready(function () {
   var memoryGame = new MemoryGame(cards);
+  memoryGame.cards = memoryGame.shuffleCard(memoryGame.cards);
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
     html += '<div class="back"';
-    html += '    name="'       + pic.img +  '">';
+    html += '    name="' + pic.img + '">';
     html += '</div>';
     html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + '") no-repeat"';
+    html += 'style="background: url(img/' + pic.img + ') no-repeat">';
     html += '</div>';
     html += '</div>';
   });
 
-  // Add all the div's to the HTML
-  document.getElementById('memory_board').innerHTML = html;
-  // Bind the click event of each element to a function
-$('.back').on('click', function () {
-   
+
+   document.getElementById('memory_board').innerHTML = html;
+
+   var clicks = 0;
+  
+$('.back').on('click', function (e) {
+
+  clicks ++;
+
+  if(clicks%2==0){
+    console.log(memoryGame.pickedCards);
+    $('.card').addClass('blocked');
+    memoryGame.pickedCards.push($(this).attr('name').split('.')[0]);
+    $(this).addClass('visible');
+    if (memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1])) {
+      $('.visible').parent().addClass('matched');
+      memoryGame.pickedCards.length = 0;
+      $('.card').removeClass('blocked');
+    } else {
+      setTimeout(function () {hideNotMatchedCards()}, 500);
+      memoryGame.pickedCards.length = 0;  
+      $('.card').removeClass('blocked');
+    }
+  }else{
+    memoryGame.pickedCards.push($(this).attr('name').split('.')[0]);
+    $(this).addClass('visible');
+  }
+
+  $('#pairs_clicked').text(memoryGame.pairsClicked);
+  $('#pairs_guessed').text(memoryGame.pairsGuessed);
+
 });
+
+function hideNotMatchedCards() {
+  $('.back').removeClass('visible');
+}
+
 });
 
 
