1195256240462f9c08c8d27f4ca1be35f757885a
Henri Delozanne <hnr.delozanne@gmail.com>
2018-01-16 19:19:20 +0100

    done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,32 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function(cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function(cardsArr) {
+  for (let i = cardsArr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [cardsArr[i], cardsArr[j]] = [cardsArr[j], cardsArr[i]];
+  }
+  return cardsArr;
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function(firstCard, secondCard) {
+  this.pairsClicked += 1;
+  if (firstCard === secondCard) {
+    this.pairsGuessed += 1;
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function() {
+  if (this.pairsGuessed === 12) {
+    return true;
+  } else {
+    return false;
+  }
+};
