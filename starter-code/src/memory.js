805aab513c9d64349dda05573067da6905cbd51b
AlvaroMata8 <alvaromataaracil@hotmail.com>
2018-01-17 10:20:38 +0100

    done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,50 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+ var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+ }
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    var mixedArr = [];
 
-// };
+    var nC = cardsArr.length;
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    var i;
 
-// }
+        while (nC) {
+          i = Math.floor(Math.random() * cardsArr.length);
+            if (i in cardsArr) {
+              mixedArr.push(cardsArr[i]);
+              delete cardsArr[i];
+              nC--;
+            }
+        }
 
-// MemoryGame.prototype.finished = function () {
+    return mixedArr;
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+     this.pairsClicked  += 1 ;
+    
+        if(firstCard === secondCard){
+            this.pairsGuessed +=1;
+
+            return true;
+        }else{
+            return false;
+        }
+}
+
+ MemoryGame.prototype.finished = function () {
+     
+        if(this.pairsGuessed == 24/2){
+            return true;
+        }else{
+            return false;
+        }
+};
+
+
+//Siguiendo JASMINE se hace mas ""facil""" pero aun asi me cuesta empezar y localizar 
+// los nombres de los parametros 
