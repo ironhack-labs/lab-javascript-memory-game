b692f3ed63655fa9871916566ed15d1197cadc0a
Pedro Neto <pedro.diogo.neto@outlook.pt>
2018-01-17 19:22:06 +0100

    memoryjs done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,61 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    var currentIndex = cardsArr.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = cardsArr[currentIndex];
+        cardsArr[currentIndex] = cardsArr[randomIndex];
+        cardsArr[randomIndex] = temporaryValue;
+    }
+    return cardsArr;
+};
 
-// };
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+// Fisher-Yates
+// function shuffle(array) {
+//     var currentIndex = array.length, temporaryValue, randomIndex;
+  
+//     // While there remain elements to shuffle...
+//     while (0 !== currentIndex) {
+  
+//       // Pick a remaining element...
+//       randomIndex = Math.floor(Math.random() * currentIndex);
+//       currentIndex -= 1;
+  
+//       // And swap it with the current element.
+//       temporaryValue = array[currentIndex];
+//       array[currentIndex] = array[randomIndex];
+//       array[randomIndex] = temporaryValue;
+//     }
+  
+//     return array;
+//   }
 
-// }
 
-// MemoryGame.prototype.finished = function () {
 
-// };
+
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked += 1;
+    if (firstCard === secondCard) {
+        this.pairsGuessed += 1;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+MemoryGame.prototype.finished = function () {
+    this.nrPairs = this.pickedCards.length / 2;
+    if (this.nrPais === this.pairsGuessed) {
+        return true;
+    } else {
+        return false;
+    }
+};
