d07653ce981ae2516757efa0a3befd7b18b752b3
Kathia Rieser <kath.medulla@gmail.com>
2018-01-17 01:44:25 +0100

    solo queda reiniciar al completar el juego

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -49,7 +49,19 @@ $(document).ready(function(){
     back.addClass("front");
     front.removeClass("front");
     front.addClass("back");
-    front.addClass("visible");
+    //front.addClass("visible");
+
+  }
+  function ocultaTodo(){
+    
+    var front = $(".front");
+    var back = $(".front").next();
+
+    front.removeClass("back");
+    front.addClass("front");
+    //front.removeClass("visible");
+    back.removeClass("front");
+    back.addClass("back");
 
   }
 
@@ -85,14 +97,41 @@ $(document).ready(function(){
     //var primera;
     //console.log($(actual).parent().attr('id'));
     //console.log($(anterior).parent().attr('id'));
-    if($(actual).parent().attr('id')!==$(anterior).parent().attr('id')){
+
+    //!==
+
+    if(!memoryGame.checkIfPair($(actual).parent().attr('id'),$(anterior).parent().attr('id'))){
         
       ocultaCarta(actual);
       ocultaCarta(anterior);
       primera = null;
 
      }else{
+      
+      $("#pairs_guessed").html(memoryGame.pairsGuessed);
       primera = null;
+      //si acabamos volvemos a empezar
+      console.log(memoryGame.finished());
+      if(memoryGame.finished()){
+         
+          //ocultaTodo()
+          //memoryGame.cards =  memoryGame.shuffleCard(cards);
+          //Lo copiamos pero lo podemos mejorar
+          //html = '';
+          //memoryGame.cards.forEach(function (pic, index) {
+          //html += '<div class= "card" id="card_' + pic.name + '">';
+          //html += '<div class="back"';
+          //html += '    name="'       + pic.img +  '">';
+          //html += '</div>';
+          //html += '<div class="front" ';
+          //html += 'style="background: url(img/' + pic.img + ') no-repeat">';
+          //html += '</div>';
+          //html += '</div>';
+          
+            //});  
+
+      }
+
      }
 
      //return primera;
@@ -104,8 +143,11 @@ document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
 
 //var segunda = null;
+
 $('.back').on('click', function () {
 
+     memoryGame.pairsClicked +=1;
+     $("#pairs_clicked").html(memoryGame.pairsClicked);
      if(primera==null){
        muestraCarta(this);
        primera = this;
