08d397b3ca1868035a569eedc9d1409d06a763a4
Myriam <bourbiamyriam@gmail.com>
2018-01-17 09:49:14 +0100

    Memory_Myriam

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -38,13 +38,56 @@ $(document).ready(function(){
     html += '</div>';
     html += '</div>';
   });
-
+  
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
+  memoryGame.shuffleCard(cards);
+  
   // Bind the click event of each element to a function
-$('.back').on('click', function () {
-   
-});
+  var card1 = "";
+  var card2 = "";
+  var count = 0;
+  $('.back').on('click', function () { 
+    if ((count < 2) &&  ($(this).hasClass("face-up")) === false) {
+      
+      // increment guess count, show image, mark it as face up
+      count++;
+      $(this).toggleClass('back').toggleClass('front')
+      $(this).next().toggleClass('front').toggleClass('back')
+      $(this).addClass("face-up");
+      
+      //guess #1
+      if (count === 1 ) { 
+        card1 = $('.face-up').attr("name"); 
+      }   
+      
+      //guess #2
+      else { 
+        card2 = $('.face-up').attr("name"); 
+        
+        // since it's the 2nd guess check for match
+        memoryGame.checkIfPair(card1, card2);
+        memoryGame.finished();
+        if (card1==card2 ){ 
+          $(this).addClass("match");
+        } 
+        
+        // else it's a miss
+        else { 
+          setTimeout(function() {
+            $(this).not(".match").toggleClass('back').toggleClass('front')
+            $(this).not(".match").next().toggleClass('front').toggleClass('back')
+            $(this).not(".match").removeClass("face-up");
+          }, 1000);
+        }
+        
+        // reset
+        count = 0; 
+        setTimeout(function() { console.clear(); }, 6000);      
+      }
+    }
+  });
+  
 });
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,37 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards=[];
+  this.pairsClicked=0;
+  this.pairsGuessed=0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
 
-// };
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  for (var i=0; i<cardsArr.length;i++){
+    var  j = Math.random() ;
+    var bis=cardsArr[i];
+    cardsArr[i]=cardsArr[j];
+    cardsArr[j]=cardsArr[i];
+  }
+  return cardsArr
+  
+};
 
-// }
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++;
+  if (firstCard===secondCard){
+    this.pairsGuessed++;
+  }
+  return (firstCard===secondCard)
+}
 
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+  if (this.pairsGuessed  === 12){
+    return true 
+  }
+  else {
+    return false
+  }
+  
+};
