19f91af6825281a8ef0cdb193eaf166555fa1874
AlbertoSP <ALBERTOSANCHEZHTML@GMAIL.COM>
2018-01-17 09:34:10 +0100

    bn

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,48 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+}
+MemoryGame.prototype.shuffleCard = function shuffle(arrCards) {
+    var shuffling = [],
+        n = arrCards.length,
+        i;
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    // While there remain elements to shuffle…
+    while (n) {
 
-// };
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * arrCards.length);
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+        // If not already shuffled, move it to the new array.
+        if (i in arrCards) {
+            shuffling.push(arrCards[i]);
+            delete arrCards[i];
+            n--;
+        }
+    }
 
-// }
+    return shuffling;
+}
 
-// MemoryGame.prototype.finished = function () {
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked += 1;
 
-// };
+    if (firstCard === secondCard) {
+        this.pairsGuessed += 1;
+        return true;
+    } else {
+        return false;
+    };
+
+};
+
+MemoryGame.prototype.finished = function () {
+    if (this.pairsGuessed == 12) {
+        return true;
+    } else {
+        return false;
+    };
+
+};
