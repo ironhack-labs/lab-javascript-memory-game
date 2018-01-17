63401f8504915918166b9936613ecac4f9ce2a2b
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-01-17 18:40:06 +0100

    all tests are passing, except the two broken ones

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,42 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 1;
+  this.pairsGuessed = 1;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+ var currentIndex = cardsArr.length, temporaryValue, randomIndex;
 
-// };
+ while (0 !== currentIndex) {
+     randomIndex = Math.floor(Math.random() * currentIndex);
+     currentIndex -= 1
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    temporaryValue = cardsArr[currentIndex];
+    cardsArr[currentIndex] = cardsArr [randomIndex];
+    cardsArr[randomIndex] = temporaryValue
+ }
+ return cardsArr;
+};
 
-// }
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    if (firstCard === secondCard) {
+        this.pairsClicked += 1;
+        this.pairsGuessed += 1;
+        return true;
+    } else {
+        this.pairsClicked += 1;
+        return false;
+    }
 
-// MemoryGame.prototype.finished = function () {
+}
 
-// };
+MemoryGame.prototype.finished = function () {
+    this.numberOfPairs = this.pickedCards.length / 2;
+    if (this.pairsGuessed == this.numberOfPairs) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
