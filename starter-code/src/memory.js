b8a9f85dd7f7115588196ac7abd9d0f31542395a
100fue <malvacien@gmail.com>
2018-01-17 09:22:38 +0100

    done all the JS

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,51 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+	  var shuff = [], n = this.cards.length, i;
 
-// };
+	  while (n) {
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+	    i = Math.floor(Math.random() * n--);
 
-// }
+	    shuff.push(this.cards.splice(i, 1)[0]);
+	  }
 
-// MemoryGame.prototype.finished = function () {
+	  return shuff;
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+	
+	this.pairsClicked += 1;
+
+	if (firstCard === secondCard){
+
+		this.pairsGuessed += 1;
+
+		return true;
+
+	}else{
+
+		return false;
+	}
+
+}
+
+MemoryGame.prototype.finished = function () {
+
+	if (this.pairsGuessed === 12){
+
+		return true;
+
+	}
+
+	else{
+
+		return false;
+	}
+
+};
