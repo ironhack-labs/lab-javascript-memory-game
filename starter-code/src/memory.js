6f613d9d8c9b58319cdff150b7b3df5dde9a3c81
Jana Stinn <jana-stinn@gmx.de>
2018-01-18 11:15:57 +0100

    first commit

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,41 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
 
-// };
+MemoryGame.prototype.shuffleCard = function(cardArr) {
+    var i = cardArr.length, j, temp;
+    while (--i > 0) {
+        j = Math.floor(Math.random() * (i+1));
+        temp = cardArr[j];
+        cardArr[j] = cardArr[i];
+        cardArr[i] = temp;
+    }
+    return cardArr;
+}
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
-// }
 
-// MemoryGame.prototype.finished = function () {
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked++;
+    if (firstCard === secondCard ){
+        this.pairsGuessed++;
+        return true;
+    } else {
+        return false;
+    }
+}
 
-// };
+
+MemoryGame.prototype.finished = function () {
+    var pairs = this.cards.length / 2;
+    if (this.pairsGuessed === pairs){
+        return true;
+    } 
+    else {
+        return false;
+    }
+};
