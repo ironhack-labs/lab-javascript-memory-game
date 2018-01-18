6134eeed7b29ac643c85b24f45a736fa3e3116d9
Matt Megarry <mattmegarry@gmail.com>
2018-01-18 09:42:58 +0100

    Done tests, reckon I could finish it if needed

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,43 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+   this.cards = cards;
+   this.pickedCards = []; //Needs it's value
+   this.pairsClicked = 0; //Wrong
+   this.pairsGuessed = 0; //Wrong
+ };
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    var cardsArrShuffled = [];
+    //var cardsArr = cardsArr;
 
-// };
+    var j = 1;
+    for (i = cardsArr.length; i > 0; i--) {
+        j = Math.floor(Math.random() * Math.floor(i));
+        cardsArrShuffled.push(cardsArr[j]);
+    }
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    return cardsArrShuffled;
+};
 
-// }
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    var self = this;
+    self.pairsClicked++;
+    if (firstCard === secondCard) {
+        this.pairsGuessed++;
+        return true;
+    } else if (firstCard !== secondCard) {
+        return false;
+    }
+}
 
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+    var self = this;
+    if (self.pairsGuessed === 0) {    
+        return false;
+    } else if ((self.cards.length/2) < self.pairsGuessed) {
+        return false;
+    } else if ((self.cards.length/2) === self.pairsGuessed) {
+        return true;
+    } else {
+        return false;
+    }
+};
