8fe25d7b82ee76352935ee93521a08c685673f51
Rodrigo Daoud <rodaoud93@gmail.com>
2018-01-18 09:06:51 +0100

    done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,48 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+   this.cards = cards;
+   this.pickedCards = [];
+   this.pairsClicked = 0;
+   this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    var currentIndex = cardsArr.length, temporaryValue, randomIndex;
 
-// };
+    while (currentIndex !== 0){
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+        temporaryValue = cardsArr[currentIndex];
+        cardsArr[currentIndex] = cardsArr[randomIndex];
+        cardsArr[randomIndex] = temporaryValue;
+    }
+    return cardsArr;
+};
 
-// }
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked += 1;
 
-// MemoryGame.prototype.finished = function () {
+    if (firstCard === secondCard){
+        this.pairsGuessed += 1;
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
-// };
+MemoryGame.prototype.finished = function () {
+
+    var totalPairs = cards.length / 2;
+
+    if (this.pairsGuessed === 0){
+        return false;
+    }
+    else if(this.pairsGuessed < totalPairs){
+        return false;
+    }
+    else{
+        return true;    
+    }
+
+};
