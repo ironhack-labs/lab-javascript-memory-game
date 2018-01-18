7ef977ac1f7b2b1c9ff88afab1b7cf9b2123d904
Martin <mar.gardemann@gmail.com>
2018-01-18 09:52:21 +0100

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,6 +27,9 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  var shuffledCards =memoryGame.shuffleCard(memoryGame.cards);
+  memoryGame.cards=shuffledCards.slice();
+
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
@@ -42,8 +45,39 @@ $(document).ready(function(){
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
+  var eventArray =[];
+
 $('.back').on('click', function () {
-   
+   var theTarget= event.target;
+   theTarget.classList.toggle('back');
+   theTarget.nextSibling.classList.toggle('back');
+   eventArray.push(theTarget);
+   theTarget ='';
+
+   if (eventArray.length ===2) { // it seems that the script runs into runtime problems if I don't use a debugger?
+     var pairCheck = memoryGame.checkIfPair(eventArray[0].getAttribute('name'),eventArray[1].getAttribute('name'));
+     if(pairCheck) {
+       eventArray[0].classList.add('blocked');
+       eventArray[0].nextSibling.classList.add('blocked');
+       eventArray[1].classList.add('blocked');
+       eventArray[1].nextSibling.classList.add('blocked');
+     } else {
+      eventArray[0].classList.toggle('back');
+      eventArray[0].nextSibling.classList.toggle('back');
+      eventArray[1].classList.toggle('back');
+      eventArray[1].nextSibling.classList.toggle('back');
+     }
+     eventArray=[];
+   }
+
+   document.querySelector('#pairs_guessed').innerText=memoryGame.pairsGuessed;
+   document.querySelector('#pairs_clicked').innerText=memoryGame.pairsClicked;
+
+  if(memoryGame.finished()){
+    alert("Succeeded! Congratulations");
+    break;
+  }
+
 });
 });
 
