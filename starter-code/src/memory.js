3ee501d77fe1a53d27fb05bf0351d8b808d3b2a5
Bet Mendoza <sbm2391@hotmail.com>
2018-01-30 17:56:51 -0600

    done logic

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,33 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+var numberRandom = Math.floor(Math.random()* cardsArr.length + 1)
+return cardsArr[numberRandom];
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    
+    this.pairsClicked = this.pairsClicked + 1;
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    if(firstCard === secondCard) {
+        this.pairsGuessed = this.pairsGuessed + 1;
+        return true;
+        
+    } else {
+        return false
+    }
+}
 
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+    if (this.cards.length === 0) return false;
+    if (this.pairsGuessed === 12) {
+        return true;
+    } else {
+        return false;
+    }
+};
