b879d353e59e659e99a161ced5bebb5a36f334d7
Diego Arroyo <arroyodiego1@gmail.com>
2018-01-31 11:33:25 -0600

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,6 +27,9 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  
+  memoryGame.shuffleCard(cards);
+
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
@@ -39,12 +42,53 @@ $(document).ready(function(){
     html += '</div>';
   });
 
-  // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
-  // Bind the click event of each element to a function
-$('.back').on('click', function () {
-   
-});
+
+
+  $('.back').on('click', function () {
+
+    //Destapo las cartas
+    $(this).css("display", "none");
+    $(this).siblings().addClass("back");
+
+    //Relleno un array de cartas hasta que llegue un limite de 2
+    memoryGame.pickedCards.push($(this).attr("name"));
+
+    //Solo accederé al if siempre y cuando tenga 2 cartas
+    if (memoryGame.pickedCards.length === 2){
+
+    //Llamo a la funcion CheckIfPair, pasandole como parametros las dos cartas
+      if(memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1])){
+
+    //Actualizo marcadores
+        $("#pairs_clicked").text(memoryGame.pairsClicked)   
+        $("#pairs_guessed").text(memoryGame.pairsGuessed)         
+
+    //Si son iguales, coloco por pantalla que ha ganado, sino, salgo por pantalla
+        if(memoryGame.finished()){
+          alert("You Win")
+        }else {
+          return
+        }
+      
+    //Siguiendo el if donde he pasado las dos cartas, declaro el else.
+    //En  caso que no sean iguales, vuelvo a tapar las cartas.
+      }else{
+        setTimeout(function(){
+          $(".back[name='"+memoryGame.pickedCards[0]+"'").siblings().removeClass("back");
+          $(".back[name='"+memoryGame.pickedCards[0]+"'").css("display", "block");
+
+          $(".back[name='"+memoryGame.pickedCards[1]+"'").siblings().removeClass("back");
+          $(".back[name='"+memoryGame.pickedCards[1]+"'").css("display", "block");
+          memoryGame.pickedCards = [];          
+        }, 500);
+
+    //Actualizo marcadores
+        $("#pairs_clicked").text(memoryGame.pairsClicked)
+        return
+      }
+    }
+  });
 });
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,41 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  var firstArray = cardsArr;
+  var mixArray = firstArray.length
+  var nexIndex, j ;
+  
+  while (mixArray > 0){
+    nexIndex = Math.floor(Math.random() * mixArray);
+     mixArray --;
+     
+     j = cardsArr[mixArray];
+     cardsArr[mixArray] = cardsArr[nexIndex];
+     cardsArr[nexIndex] = j;
+  } 
+   return cardsArr;
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked ++;
+  if(firstCard===secondCard){
+    this.pairsGuessed ++;
+    return true;
+  }else{
+    return false;
+  } 
+} 
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+  var totalCards = cards.length / 2;
+  if(this.pairsGuessed === totalCards){
+    return true;
+  }else{
+    return false;
+  }
+};
