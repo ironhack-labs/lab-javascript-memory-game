df3fdd78a1bf9c087649b02bf9a2d6bfd9267b16
nlevo <nikola.levo@gmail.com>
2018-01-30 22:10:08 -0500

    50% done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,41 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    this.cards = this.shuffleCard(cards);
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    var currentIndex = cardsArr.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = cardsArr[currentIndex];
+      cardsArr[currentIndex] = cardsArr[randomIndex];
+      cardsArr[randomIndex] = temporaryValue;
+    }
+    return cardsArr;
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked++;
+    if(firstCard === secondCard){
+        this.pairsGuessed++;
+    }
+    return (firstCard === secondCard);
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.finished = function () {
+    if(this.pairsClicked === 0)
+        return false;
+    // return this.pairsGuessed === (this.cards.length / 2);
+    return this.pairsGuessed === 12;
+};
 
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
