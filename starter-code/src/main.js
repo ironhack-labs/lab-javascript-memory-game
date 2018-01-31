efcf7401fa5458a231f0881887c27bd6581dea2a
Dimitri.Bonnier <dimitri.bonnierf@gmail.com>
2018-01-30 23:24:47 -0600

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -26,9 +26,13 @@ var cards = [
 ];
 
 $(document).ready(function(){
+
+
   var memoryGame = new MemoryGame(cards);
   var html = '';
-  memoryGame.cards.forEach(function (pic, index) {
+
+memoryGame.cards = memoryGame.shuffleCard(memoryGame.cards); // en memoryGame.cards, hemos guardado todas las cartas, llamamos la Function(metodo del objeto, propiedad del objeto) de shuffleCard, cual necesita un argumento que son las cartas a mezclar).
+  memoryGame.cards.forEach(function (pic, index) { // aqui es donde se van a agreagar las cartas al HTML
     html += '<div class= "card" id="card_' + pic.name + '">';
     html += '<div class="back"';
     html += '    name="'       + pic.img +  '">';
@@ -39,11 +43,44 @@ $(document).ready(function(){
     html += '</div>';
   });
 
+
+  var selectedCards = [];
+  var counter = 0;
+
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
 $('.back').on('click', function () {
-   
+  $(this).css('display','none');
+  $(this).siblings().addClass("back");// sibling = hermano de la tarjeta cual quitamos el display y hermano al cual lo agregamos un clase back :  back = classes que estan en el CSS
+  counter++;
+  selectedCards.push($(this).attr("name"))
+  console.log(selectedCards);
+  if (counter===2){
+    var first =selectedCards[0];
+    var second =selectedCards[1]
+    if (memoryGame.checkIfPair(first, second)===false){
+      setTimeout(function(){
+      $(".back[name='"+first+"']").css('display','block');
+      $(".back[name='"+first+"']").siblings().removeClass("back");
+      $(".back[name='"+second+"']").css('display','block');
+      $(".back[name='"+second+"']").siblings().removeClass("back")
+      }, 500)
+    } 
+    $('#pairs_clicked').text(memoryGame.pairsClicked); 
+    $('#pairs_guessed').text(memoryGame.pairsGuessed);
+    // aqui, modificamos el HTML para reflejar el valor de las variables que creamos (memoryGame= nueve instancia de la CLASSE MemoryGame)
+    setTimeout(function(){
+    if(memoryGame.finished() === true){
+      alert("Ganaste !!!")
+    }
+  },500)
+    counter=0
+  selectedCards=[]
+    
+  }
+
+
 });
 });
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,43 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+var newArray = [];
+var length = cardsArr.length; // creamos una variable para almacenar el length original de cardsArr porque a cardsArr, le vamos ir eliminando elementos y su length va a cambiar, entonces eso cambiara las iteraciones del for a menos veces..
+for (var i=0; i<length ; i++){
+    var numberRandom = Math.floor(Math.random()* cardsArr.length)
+    console.log(numberRandom);
+newArray.push(cardsArr[numberRandom]) // el primer elemento va sacar un elemento random, del array orginal,
+cardsArr.splice(numberRandom,1);
+}
 
-// };
+cardsArr = newArray;
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+return cardsArr;
+};
 
-// }
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    
+    this.pairsClicked = this.pairsClicked + 1;  // this como si era MemoryGame, es Este ! 
 
-// MemoryGame.prototype.finished = function () {
+    if(firstCard === secondCard) {
+        this.pairsGuessed = this.pairsGuessed + 1;
+        return true;
+        
+    } else {
+        return false
+    }
+}
 
-// };
+MemoryGame.prototype.finished = function () {
+    if (this.cards.length === 0) return false; // para que si el juego todavia no ha puesto cartas, que devuelva false : no ha terminado el juego
+    if (this.pairsGuessed === this.cards.length/2) { // cuando el numero de pares adivinados = numero total de cartes / 2 , ahi termina el juego
+        return true;
+    } else {
+        return false;
+    }
+};
