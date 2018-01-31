f9ac3c90bbcda96ec6b98bcb326a46e695d7425b
ElsoMichael <elsomichael@ymail.com>
2018-01-31 09:02:25 -0500

    initial commit

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,34 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    this.cardsArr = cardsArr; 
+    this.newArr = [];
+    this.newArr.push(Math.floor(Math.random() * this.cardsArr.length));
+    return this.newArr;
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.fristCard = firstCard;
+    this.secondCard = secondCard;
+    this.pairsClicked += firstCard;
+    
+    if (firstCard === secondCard) {
+        this.pairsGuessed += 1;
+        return true;
+    } else {
+        return false;
+    }
+}
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+    if (this.pairsGuessed === 12) {
+        return true;
+    } else {
+        return false;
+    }
+};
