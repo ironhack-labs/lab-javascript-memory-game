ea732257802f47d4d2da646f399e59b168645eba
esthvega <esthvega@hotmail.es>
2018-03-27 23:37:57 +0200

    hasta jasmine he llegado

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,41 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+ var MemoryGame = function (cards) {
+   this.cards = cards;
+   this.pickedCards = [];
+   this.pairsClicked = 0;
+   this.pairsGuessed = 0;
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+ };
 
-// };
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    var shuffle = function (deck) {
+        let cardsArr = [];
+        let array = deck.slice();
+        while (array.length !== 0) {
+            let rIndex = Math.floor(array.length * Math.random());
+          cardsArr.push(array[rIndex]);
+          array.splice(rIndex, 1)
+        }
+        return cardsArr;
+      };
+      return shuffle(cardsArr);
+ };
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
-// }
+ MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+     
+if (firstCard == secondCard) {
+this.pairsGuessed=+1;
+    return true;
+} else {
+    this.pairsClicked=+1;
+    return false;
+}
+ }
 
-// MemoryGame.prototype.finished = function () {
-
-// };
+ MemoryGame.prototype.finished = function () {
+if (this.pairsGuessed.length - this.cards.length == 0){
+    return true;
+} else {
+    return false;
+}
+ }
