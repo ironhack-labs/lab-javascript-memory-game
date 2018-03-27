fb1943f3cb046e9993047ead6f0bc467fd51b869
Matuki <matiaspierretti2@hotmail.com>
2018-03-27 23:20:22 +0200

    BCN Matias memorygame / not finished

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,54 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  var self = this;
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+   self.cards = cards;
+   self.pickedCards = [];
+   self.pairsClicked = 0;
+   self.pairsGuessed = 0;
+};
 
-// };
+ MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  var self = this;
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  newArray = [];
 
-// }
+  for (var i = 0; i < cardsArr.length; i++){
+    var randomIndex = Math.floor(Math.random() * cardsArr.length); 
+    newArray.push(cardsArr[randomIndex]);
+  }
 
-// MemoryGame.prototype.finished = function () {
+  return newArray;
 
-// };
+ };
+
+ MemoryGame.prototype.checkIfPair = function () {
+  var self = this;
+
+  
+
+  if (self.pickedCards.length == 1){
+
+    self.pairsClicked++
+
+    if(self.pickedCards[0] == self.pickedCards[1]){
+
+      self.pairsGuessed++;
+       
+    }
+  }
+
+   self.pickedCards = [];
+  
+ }
+
+ MemoryGame.prototype.finished = function () {
+  var self = this;
+  var isTheGameFinished = false;
+
+  if (self.cards == []){
+    return true;
+  }
+
+  return isTheGameFinished;
+
+ };
