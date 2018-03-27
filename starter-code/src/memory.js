47730c4da8942b93446a1755a4dd0fca1e0a8d33
davidtexmex814 <davidtexmex814.com>
2018-03-27 20:36:32 +0200

    jasmin completado

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,31 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+ var MemoryGame = function (cards) {
+   this.cards = cards;
+   this.pickedCards  = [];
+   this.pairsClicked = 0;
+   this.pairsGuessed = 0;
+ };
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+ MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    for (var i = 0; i<cardsArr.length;i++) {
+        var j = Math.random(cardsArr[j] = cardsArr[i]); 
+    }
+    return cardsArr;
+ };
 
-// };
+ MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked += 1;
+  if(firstCard == secondCard){
+      this.pairsGuessed += 1;
+      return true;
+  } else{
+     return false;
+  }
+ }
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+ MemoryGame.prototype.finished = function () {
+    if(this.pairsGuessed == 12){
+        return true;
+    }else {
+        return false;
+    }
+ };
