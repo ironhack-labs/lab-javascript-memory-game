1dd5bbf7757edd2648b202a9692c9729891207a5
Nacho Lopez <ignlopezsanchez@gmail.com>
2018-03-27 21:10:57 +0200

    commit inicial

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,39 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+ var MemoryGame = function (cards) {
+  this.cards = this.shuffleCard(cards);
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  for (var i = 0; i < cardsArr.length; i++) {
+    var j = Math.floor(Math.random()*cardsArr.length);
+    var k = cardsArr[i];
+    cardsArr[i] = cardsArr[j];
+    cardsArr[j] = k;    
+  }
+  return cardsArr;
 
-// };
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
-// }
 
-// MemoryGame.prototype.finished = function () {
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++;
+  if (firstCard == secondCard) {
+    this.pairsGuessed++;
+    return true;
+  }
+  return false;
 
-// };
+}
+
+MemoryGame.prototype.finished = function () {
+  if (this.pairsGuessed == 0){
+    return false;
+  }
+  if (this.pairsGuessed == this.cards.length/2) {
+    return true;
+  }
+  return false;
+};
