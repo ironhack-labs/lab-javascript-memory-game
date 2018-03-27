0f9a4c16c27b122fa7b48f5f863d8c68240971bc
Ioana Iordache <IoanaOrka@Ioanas-MacBook.local>
2018-03-27 23:55:10 +0200

    mbu

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -42,9 +42,33 @@ $(document).ready(function(){
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
-$('.back').on('click', function () {
-   
-});
+  $('.back').on('click', function () {
+    $(this).addClass("1");
+    $(this).siblings().addClass("2");
+    $(".1").addClass("front").removeClass("back");
+    $(".2").addClass("back").removeClass("front");
+    memoryGame.pickedCards.push($(this).parent().attr('id'));
+    console.log(memoryGame.pickedCards);
+    if (memoryGame.pickedCards.length>=2) resetPickedCards();
+  });
+
+  function resetPickedCards(){
+    if (memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1])) {
+      console.log("yessss");
+      memoryGame.pickedCards=[];
+    } else {
+      change2CardsBack(memoryGame.pickedCards);
+      memoryGame.pickedCards=[];
+    }
+  }
+
+  function change2CardsBack(array){
+    array.forEach(function (element){
+      $("#"+element+">.1").addClass("back").removeClass("front 1");
+      $("#"+element+">.2").addClass("front").removeClass("back 2");
+    });
+    
+  }
 });
 
 
