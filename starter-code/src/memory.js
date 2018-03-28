a451452d6926c90c665e49c5b4c79a3959207899
Mapi Carrillo <mp.carrillo@yahoo.es>
2018-03-28 09:31:08 +0200

    Parte lógica, cartas dadas la vuelta también

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,34 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+ var MemoryGame = function (cards) {
+   this.cards = cards;
+   this.pickedCards = [ ];
+   this.pairsClicked = 0;
+   this.pairsGuessed = 0;
+ };
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+ MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    for (var i = 0; i<cardsArr.length; i++) {
+        var t = Math.floor(Math.random()*cardsArr.length);
+        var w = cardsArr[i];
+        cardsArr[i] = cardsArr[t];
+        cardsArr[t] = w;
+    }
+    return cardsArr;
+ };
 
-// };
+ MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked++
+    if(firstCard === secondCard){
+        this.pairsGuessed++
+        return true
+    } else {
+        return false
+    }
+}
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+ MemoryGame.prototype.finished = function () {
+     if (this.pairsGuessed === 12 ) {
+        return true
+     } else {
+         return false
+     }
+ };
