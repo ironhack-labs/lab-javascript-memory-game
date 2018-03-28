0e100ffcc26565cd5e12c82d94a2d6a6fc8188a0
muflonex <muflonex@gmail.com>
2018-03-28 09:14:10 +0200

    Hasta allí hemos llegado. Ahora el problema es la visualización de flip (así luego es más facil ver como pasan las cosas). He visto en la hoja de html que viene cargado el plugin de jQuery de flip, pero no me funciona. Y de otra manera - se que tengo que dar altura a elemento .front, pero en mismo tiempo el otro (.back) no quiere coger nueva altura 0 para darle espacio

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,37 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards,
+  this.pickedCards = [],
+  this.pairsClicked = 0,
+  this.pairsGuessed = 0
+ };
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  shuffle = function(deck){
+    var cardsArr = [];
+    var array = deck.slice();
+    while (array.length !== 0) {
+      var rIndex = Math.floor(array.length * Math.random());
+      cardsArr.push(array[rIndex]);
+      array.splice(rIndex, 1)
+    }
+    return cardsArr;
+  };
+  return shuffle(cardsArr);
+  };
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++
+  if( firstCard === secondCard ){
+    this.pairsGuessed++
+    return true
+  }else{
+    return false
+  }
+}
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+  if(this.pairsGuessed === 12){
+    return true
+  }
+  return false
+};
