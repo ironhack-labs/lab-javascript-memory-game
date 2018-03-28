40e13d1317dcc249dfd1fd139e1f452ef1b13eeb
Tanis Pan <e.pan.gar@gmail.com>
2018-03-28 08:52:44 +0200

    Iteracion 1

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,27 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+   this.cards = cards;
+   this.pickedCards = [];
+   this.pairsClicked = [];
+   this.pairsGuessed= [];
+}
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  
+  //Math.floor(Math.random()*cardsArr.length-1)+1
 
-// };
+  for(i=0:i<=cardsArr.length-1:i++){
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  }
+  
+  
+    return d;
+  
+};
 
-// }
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
-// MemoryGame.prototype.finished = function () {
+}
 
-// };
+MemoryGame.prototype.finished = function () {
+
+};
