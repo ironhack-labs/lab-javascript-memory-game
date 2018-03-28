335a3b413f35d8d73d7680fb7bbe20585ad2f5b5
Yago Vega <yagofvv@gmail.com>
2018-03-28 09:14:08 +0200

    test passed y trabajando en las cartas

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,44 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  return shuffle(cardsArr);
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++;
+  if(firstCard == secondCard){
+    this.pairsGuessed++;
+    return true;
+  }else{
+    return false;
+  }
+}
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.finished = function () {
+  if(this.pairsGuessed == 0){
+    return false;
+  }
+  // if(this.pickedCards.length == 0){
+  //   return false;
+  // }
+  console.log(this.pairsGuessed, this.cards.length/2);
+  if(this.pairsGuessed == this.cards.length/2){
+    return true;
+  }
+  else{
+    return false;
+  }
+};
 
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
