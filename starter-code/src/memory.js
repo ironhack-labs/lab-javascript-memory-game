d11ddcfbdcb3ee1643e089082a306e57293e9719
Javier Valiente <javara00@gmail.com>
2018-03-28 09:27:30 +0200

    sin hacer

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,18 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
 
-// };
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
-// }
+}
 
-// MemoryGame.prototype.finished = function () {
+MemoryGame.prototype.finished = function () {
 
-// };
+};
