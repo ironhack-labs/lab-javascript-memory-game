40d6200001d64df5bb6bbc658b2fe2bdcafa2fef
Richard Batista <stalfos@icloud.com>
2018-04-11 18:00:21 -0400

    Initial commit

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,40 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+	this.cards = cards;
+	this.currentPair = [];
+	this.pairsClicked = 0;
+	this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+	// var shuffledArray = [];
+	// cardsArr.forEach(function(theCard, theIndex){
+	// 	var randomCardIndex = Math.floor(Math.random() * cardsArr.length);
+	// 	shuffledArray.push(cardsArr[randomCardIndex]);
+	// 	cardsArr.splice(randomCardIndex, 1);
+	// });
+	// return shuffledArray;
+	// this.cards = shuffledArray;
 
-// };
+	this.cards = _.shuffle(cardsArr);
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+	this.pairsClicked++;
+	$('#pairs_clicked').text(this.pairsClicked);
+	$('.back').addClass('blocked');
 
-// }
+	if(firstCard === secondCard) {
+		this.pairsGuessed++;
+		$('#pairs_guessed').text(this.pairsGuessed);
+	} else {
+		$('.just-clicked').css("background", "#456783");
+	}
 
-// MemoryGame.prototype.finished = function () {
+	this.currentPair = [];
+	$(".just-clicked").removeClass("just-clicked");
+	$('.back').removeClass('blocked');
+}
 
-// };
+MemoryGame.prototype.finished = function () {
+
+};
