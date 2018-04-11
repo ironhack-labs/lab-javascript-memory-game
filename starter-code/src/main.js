59d5219b5b20c1ceba507fea43657a43e059cf78
Maximo Campo <themaxomoto@gmail.com>
2018-04-10 19:25:15 -0500

    terminado

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,6 +27,7 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  memoryGame.shuffleCard(memoryGame.cards);
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
@@ -38,13 +39,44 @@ $(document).ready(function(){
     html += '</div>';
     html += '</div>';
   });
-
+ 
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
+
+
   // Bind the click event of each element to a function
+
+  var selectedCards = []; 
+  var turn = true;
 $('.back').on('click', function () {
-   
-});
+  if(turn){
+  $(this).toggleClass('back');
+  $(this).next().toggleClass('back');
+  selectedCards.push($(this).attr('name'));
+  if(selectedCards.length === 2){
+    turn = false;
+    if (memoryGame.checkIfPair(selectedCards[0],selectedCards[1])){
+      $('#pairs_guessed').text(memoryGame.pairsGuessed)
+      selectedCards = [];
+
+      $('div [class*="front back"]').addClass('front-blocked')
+    }else{
+      selectedCards = []
+      setTimeout(function(){
+        $('div [class*="front back"]:not(.front-blocked)').prev().toggleClass('back');
+        $('div [class*="front back"]:not(.front-blocked)').removeClass('back');
+        turn = true;
+      }, 500)
+    }
+    $('#pairs_clicked').text(memoryGame.pairsClicked)
+  }
+  if(memoryGame.finished()){
+    $('h1').text('YOU WON!')
+  }
+}
 });
 
 
+
+
+});
