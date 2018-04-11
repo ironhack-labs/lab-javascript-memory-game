06ea39caaa8f7a0b94454d0530fb59b8aa4576d6
Diego Leal <diegoleallinux@gmail.com>
2018-04-11 17:59:43 -0400

    working on it still

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,6 +27,7 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  memoryGame.shuffleCard(cards);
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
@@ -42,8 +43,16 @@ $(document).ready(function(){
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
+
 $('.back').on('click', function () {
-   
+  $(this).addClass("just-clicked")
+  var nameOfImage = $(this).attr("name");
+  memoryGame.pickedCards.push(nameOfImage);
+  $(this).css('background','url(img/' + nameOfImage + ') ')
+  if (memoryGame.pickedCards.length === 2){
+    memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1]);
+  }
+  // $('.back div').toggleClass('back');
 });
 });
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,38 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+function MemoryGame(cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  var copyArr = cardsArr, randomPlaceholder, j;
+  for (var i = copyArr.length - 1; i > 0; i--){
+    j = Math.floor(Math.random() * (i + 1));
+    randomPlaceholder = copyArr[i];
+    copyArr[i] = copyArr[j];
+    copyArr[j] = randomPlaceholder;
+  }
+  // return copyArr;
+  this.cards = copyArr;
+}
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked++;
+    $('#pairs_clicked').text(this.pairsClicked);
+    if (firstCard === secondCard){
+      this.pairsGuessed++;
+        $('.back').addClass('blocked');
+        $('#pairs_guessed').text(this.pairsGuessed);
+      } else {
+        $('.back').addClass('blocked');
+        $('just-clicked').css("background","#456783");
+      }
+    this.pickedCards = [];
+    $('.just-clicked').removeClass('just-clicked');
+    $('.back').removeClass('blocked');
+}
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+  window.alert("You Win!!!");
+}
