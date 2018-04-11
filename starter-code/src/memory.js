9d09d5674d41cd4ce7894decdd89d6e6c84d9f62
Eleyni Rodriguez <eleyni.m.rodriguez@gmail.com>
2018-04-11 09:30:43 -0400

    Initial commit.Started game logic of shuffling cards.

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,14 +1,28 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards, pickedCards, pairsClicked, pairsGuessed) {
+  this.cards = cards;
+  this.pickedCards = pickedCards;
+  this.pairsClicked = pairsClicked;
+  this.pairsGuessed = pairsGuessed;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
 
-// };
+// Shuffle the cards
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  var elm = cardsArr.length, t, i;
+  while (elm) {
+    i = Math.floor(Math.random() * elm--);
+    t = array[elm];
+    array[elm] = array[i];
+    array[i] = t;
+  }
+  return cardsArr;
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
-// }
+// Check if cards are a pair
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+
+}
 
 // MemoryGame.prototype.finished = function () {
 
