79fd4475c439b309675a4b497311c73a441dd33b
Ryan Martin <rmart106@fiu.edu>
2018-04-15 16:41:54 -0400

    Completed.

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -1,3 +1,4 @@
+// Array of card objects which correspond to their images. 
 var cards = [
   { name: 'aquaman',         img: 'aquaman.jpg' },
   { name: 'batman',          img: 'batman.jpg' },
@@ -25,25 +26,50 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
+// When the document is loaded, do this!
 $(document).ready(function(){
-  var memoryGame = new MemoryGame(cards);
-  var html = '';
-  memoryGame.cards.forEach(function (pic, index) {
-    html += '<div class= "card" id="card_' + pic.name + '">';
+  var memoryGame = new MemoryGame(cards); // new Implies a constructor function taking an array as an argument.
+  
+  memoryGame.shuffleCard(memoryGame.cards);
+  
+  var html = ''; // ForEach of the cards in the loop, add the following HTML.
+  memoryGame.cards.forEach(function (pic, index) { // this.cards = cards - Here we're looping through the array.
+    html += '<div class= "card" id="card_' + pic.name + '">'; // Container for the card.
+
     html += '<div class="back"';
-    html += '    name="'       + pic.img +  '">';
+    html += '    name="'       + pic.img +  '">'; // Back of the card.
     html += '</div>';
+
     html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + ') no-repeat">';
+    html += 'style="background: url(img/' + pic.img + ') no-repeat">'; // Front of card + Adds image.
     html += '</div>';
-    html += '</div>';
+
+    html += '</div>'; // Closes the container.
   });
 
-  // Add all the div's to the HTML
-  document.getElementById('memory_board').innerHTML = html;
-  // Bind the click event of each element to a function
+  // Add all the div's to the HTML 
+  document.getElementById('memory_board').innerHTML = html; 
+
+// Bind the click event listener of each element to a function
 $('.back').on('click', function () {
-   
+
+  $(this).addClass('just-clicked');
+  $(this).addClass('blocked');
+  var nameOfImage = $(this).attr("name");
+
+  memoryGame.currentPair.push(nameOfImage);
+
+  // Adds the image to the card.
+  $(this).css('background',"url(img/" + nameOfImage + ")");
+
+  // if this is the first card, do nothing
+  // else if this is the second card, compare the two cards.
+
+  if(memoryGame.currentPair.length === 2){
+    $('.back').addClass('blocked');
+    memoryGame.checkIfPair(memoryGame.currentPair[0], memoryGame.currentPair[1]);
+  } 
+
 });
 });
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,72 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (thingsIPassIn) {
+  this.cards = thingsIPassIn;
+  // When cards are clicked, check the length to see how many cards have been clicked.
+  this.currentPair = []; 
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+// Takes in an array and then shuffles it. 
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
 
-// };
+// Currently buggy. Not showing all the boxes.
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  // var shuffledArray = [];
 
-// }
+  // // Grab a random card and push it into the shuffleArray.  
+  // cardsArr.forEach(function(theCard, theIndex)
+  //   var randomCardIndex = Math.floor(Math.random() * cardsArr.length); // Returns a number.
 
-// MemoryGame.prototype.finished = function () {
+  //   // Pushes the card into our new array.
+  //   shuffledArray.push(cardsArr[randomCardIndex]);
+    
+  //   // Which card do I want to delete? cardsArr.splice(index,count) 
+  //   // Removes the selected number from the array so it can't come up again.
+  //   cardsArr.splice(randomCardIndex,1);
+  // ;
+  // this.cards = shuffledArray; // Gives us the newly created array.
 
-// };
+  // Using Lodash's shuffle method. 
+  this.cards = _.shuffle(cardsArr);
+};
+
+MemoryGame.prototype.selectCard = function(){
+
+};
+
+// If you click a second card, it will run this function.
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+
+  this.pairsClicked++; // Adds one to the pairs clicked counter.
+  $('#pairs_clicked').text(this.pairsClicked); // Updates the pairs clicked in HTML.
+
+    if(firstCard === secondCard){ // If the first card is equal to the second card...
+
+      $('.back').addClass('blocked'); // Can't click anything while DOM is updated.
+      $('.just-clicked').addClass('permaBlocked'); // Permablocks the pair
+
+      this.pairsGuessed ++; // Adds one to the pairs guessed counter.
+      $('#pairs_guessed').text(this.pairsGuessed); // Updates the pairs guessed in HTML.
+
+      $('just-clicked').removeClass('just-clicked');
+      $('.back').removeClass('blocked'); // Can't click anything while DOM is updated.
+      
+    }
+    else{
+      $('.back').addClass('blocked'); 
+
+      setTimeout(function(){
+        $('.just-clicked').css("background","#5685c2"); // This needs to wait a second.
+        $('just-clicked').removeClass('just-clicked');
+        $('.back').removeClass('blocked'); // Can't click anything while DOM is updated.
+      },1000); // Color changes back to blue after a certain duration..      
+    }
+  this.currentPair = []; // Empties the current pairs.     
+  this.finished();
+};
+
+MemoryGame.prototype.finished = function () {
+  if(this.pairsGuessed > 11){
+    alert("You win!");
+  }
+};
