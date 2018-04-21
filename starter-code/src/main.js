e185ea955ff10b00adcdde359cf251b5691b5b09
Juliet Urbina <julieturbina@Juliets-MacBook-Pro.local>
2018-04-21 17:18:00 -0400

    almost done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -26,7 +26,11 @@ var cards = [
 ];
 
 $(document).ready(function(){
+  //create current game
   var memoryGame = new MemoryGame(cards);
+//current card get the shuffle card array
+ memoryGame.shuffleCard(memoryGame.cards);
+
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
@@ -43,7 +47,28 @@ $(document).ready(function(){
   document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
 $('.back').on('click', function () {
-   
+  $(this).addClass("just-clicked");
+  $(this).addClass("blocked");
+
+
+// we are targeting the unique attribute for each image and thats its name
+  var nameOfTheImage = $(this).attr("name");
+  console.log("name is" , nameOfTheImage);
+  // using css we are targeting background property and second parameter we are
+  // passing url concatinated with the name of the image giving us
+  $(this).css("background" , "url(img/" + nameOfTheImage +")");
+memoryGame.pickedCards.push(nameOfTheImage);
+  console.log("memoryGame.pickedCards: ", memoryGame.pickedCards);
+//if length of array is 2  - check if they match
+  if (memoryGame.pickedCards.length===2){
+    // console.log("Checking the pairs");
+    //checking the pairs
+    //id our array has 2 elements it enables us to target them thought
+    var firstInArray=memoryGame.pickedCards[0];  
+    var secondInArray=memoryGame.pickedCards[1]; 
+    //console.log("first i((n the array,")
+    memoryGame.checkIfPair(firstInArray, secondInArray);
+  }
 });
 });
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,58 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+//all cards[24]  
+  this.cards = cards;
+  // the length of this array is always 2 and it contains 2 cards that we compare
+  this.pickedCards = [];
+  //The goal is to get to 12 guessed pairs
+  this.pairsGuessed = 0;
+  //This number updates ecery 2 cards picked
+  this.pairsClicked = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+ MemoryGame.prototype.shuffleCard = function (cardsArr) {
+   this.cards = _.shuffle(cardsArr);
+   // calling lodash method for shuffle and passing parmeter cards array
+   console.log("shuffled Array: ", this.cards);
+ };
 
-// };
+  MemoryGame.prototype.checkIfPair = function(firstCard, secondCard){
+  //cont every 2 clicks
+    this.pairsClicked ++;
+  //update multi
+  $("#pairs_clicked").text(this.pairsClicked);
+  //do the  2 cardlick with current numberss match?
+    
+if(firstCard === secondCard){
+      $(".back").addClass("blocked");
+      $(".just-clicked").addClass("blocked-for-real");
+  // console.log("Match");
+   //count the pairs
+      this.pairsGuessed ++;
+      $("#pairs_guessed").text(this.pairsGuessed);
+      $(".just-clicked").removeClass("just-clicked");
+      $(".back").removeClass("blocked");
+
+    }else{
+      $(".back").addClass("blocked");
+      setTimeout(function(){  
+      $(".just-clicked").css("background", "#456783");
+      $(".just-clicked").removeClass("just-clicked");
+      $(".back").removeClass("blocked");
+
+    }, 1000);
+   
+
+  }
+  this.pickedCards = [];
+  this.finished();
+  } 
 
 // MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
 // }
 
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+if(this.pairsGuessed >11 ){
+  alert("Winner") 
+}
+}
