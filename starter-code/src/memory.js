fb1d2a5c99ab83627c5a23171a1da742b3df7c06
Teresa Plaz <teresaplaz@gmail.com>
2018-04-21 14:54:46 -0400

    starting

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,10 +1,17 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  // all the cards  (24)
+  this.cards = cards;
+  // the length of this array is 2 and it contains 2 cards that we are comparing
+  this.pickedCards = [];
+  // the goals is to get 12 guessed pairs
+  this.pairsGuessed = 0;
+  // this number updates every 2 cards clicked
+  this.pairsPicked = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
-
-// };
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  this.cards = _.shuffle(cardsArr);
+};
 
 // MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
