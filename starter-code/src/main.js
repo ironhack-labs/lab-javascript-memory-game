fb5249f93620bf3f9b4cf994b9dbddae68f568ba
Ariel Perez <arielpgmm@gmail.com>
2018-04-21 16:18:36 -0400

    texting

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -26,7 +26,11 @@ var cards = [
 ];
 
 $(document).ready(function(){
+  // creates current game 
   var memoryGame = new MemoryGame(cards);
+  // current game get the shuffled cards array
+  memoryGame.shuffleCard(memoryGame.cards);
+
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
@@ -43,7 +47,24 @@ $(document).ready(function(){
   document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
 $('.back').on('click', function () {
-   
+  //  console.log("what is this: ",$(this));
+  var nameOfTheImage = $(this).attr("name");
+  // console.log("name is: ", nameOfTheImage);
+  // using css we are targeting background propoerty and as a second parameter we are passing url concatenated with the "nameOfTheImage" and that gives us full path .
+  $(this).css("background","url(img/" + nameOfTheImage + ")");
+  memoryGame.pickedCards.push(nameOfTheImage);
+  console.log("memoryGame.pickedCards:", memoryGame.pickedCards);
+
+  
+  if(memoryGame.pickedCards.length === 2){
+    console.log("checking the pairs");
+    var firstInTheArray = memoryGame.pickedCards[0];
+    // console.log("");
+    var secondInThArray = memoryGame.pickedCards[1];
+    // console.log("1: ",firstInTheArray, "2: ", secondInThArray);
+    memoryGame.checkIfPair(firstInTheArray,secondInThArray);
+  }
+
 });
 });
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,14 +1,25 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  //all the cards (24)
+  this.cards = cards;
+  //the length of this array is 2, and it contains 2 cards that we are comparing
+  this.pickedCards = [];
+  // the goal is to get to 12 guessed pairs
+  this.pairGuessed = 0;
+  // this number updates every 2 cards clicked
+  this.pairClicked = 0;
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+};
 
-// };
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  this.cards = _.shuffle(cardsArr);
+  console.log("shuffled array: ", this.cards);
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  if(firstCard === secondCard){
+    console.log("match!");
+  }
+}
 
 // MemoryGame.prototype.finished = function () {
 
