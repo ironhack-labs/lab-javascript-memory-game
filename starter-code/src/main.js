a3f7dce1af8e4fc1ec78556a9a994f919a73403b
ahmouda12 <ahmouda12@ufl.edu>
2018-04-22 20:32:34 -0400

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -26,7 +26,10 @@ var cards = [
 ];
 
 $(document).ready(function(){
+  // create current game
   var memoryGame = new MemoryGame(cards);
+  // current game gets the shuffle cards array
+  memoryGame.shuffleCard(cards);
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
@@ -43,7 +46,31 @@ $(document).ready(function(){
   document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
 $('.back').on('click', function () {
-   
+    $(this).addClass("just-clicked");
+    $(this).addClass("blocked");
+   // console.log("What is this: ", $(this));
+   // we are targeting the unique attribute for each image and that's name
+   var nameOfTheImage = $(this).attr("name");
+   console.log("name is: ", nameOfTheImage);
+
+   // background: url("imges/blah.jpg")
+   // using css we are targeting property and as second parameter we are
+   // passing url concatenated with the "nameOfTheImage" and that gives us
+   // full path to the real imge
+   $(this).css("background", "url(img/" + nameOfTheImage + ")");
+
+   memoryGame.pickedCards.push(nameOfTheImage);
+   console.log("memoryGame.pickedCards: ", memoryGame.pickedCards);
+   // if the length of the array is 2, check if it is a pair calling .checkIfPair() function
+   if(memoryGame.pickedCards.length === 2){
+     // console.log("checking the pairs")
+     // our array has only two elements which gives us chance to target them through 0 and 1 position
+     var firstInArray = memoryGame.pickedCards[0];
+     var secondInArray = memoryGame.pickedCards[1];
+     //console.log("1: ", firstInArray, "2:", secondInArray)
+     // passing the varibles to check if they match
+     memoryGame.checkIfPair(firstInArray, secondInArray);
+   }
 });
 });
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,62 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  // all the cards (24)
+   this.cards = cards;
+   // the length of this arry is 2, and it contains cards we are comparing
+   this.pickedCards = [];
+   // the goal is to get to 12 guessed pairs
+   this.pairsGuessed = 0;
+   // this number updates every 2 cards clicked
+   this.pairsClicked = 0;
+ };
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  // calling lodash method for shuffle and passing parameter cardsArr
+  this.cards = _.shuffle(cardsArr);
+  console.log("shuffled array: ", this.cards);
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  // counts every 2 clicks
+  this.pairsClicked ++;
+  // update span with id "pairs_check" with current numbers
+  $("#pairs_clicked").text(this.pairsClicked);
+  // check if two cards match
+  if(firstCard === secondCard){
+    $(".back").addClass("blocked");
+    $(".just-clicked").addClass("blocked-for-real");
+    //console.log("match!");
+    // counts the pairs
+    this.pairsGuessed ++;
+    //  update span with id "pairs_guessed" with current numbers 
+    $("#pairs_guessed").text(this.pairsGuessed);
+    $(".just-clicked").removeClass("just-clicked");
+    $(".back").removeClass("blocked");
+  } else{
+    $(".back").addClass("blocked");
+    setTimeout(function(){
+      $(".just-clicked").css("background", "#456783");
+      $(".back").removeClass("blocked");
+      $(".just-clicked").removeClass("just-clicked");
+    }, 1000);
+  }
+  // every 2 cards resets the length of the array to 0
+  // (empties the array because we need only two cards in it)
+  this.pickedCards =[];
+  this.finished();
+}
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+  if(this.pairsGuessed > 1){
+    // alert("Winner!");
+    // just playing with some jQuery methods to practice 
+    $(".card").toggle();
+    $("#memory_board").append("<p class=\"yey\">You won</p>");
+    $(".yey").fadeOut(3000);
+    $("#memory_board").append("<p class=\"yey2\">Play again?</p>");
+    $(".yey2").toggle();
+    $(".yey2").fadeIn(3000);
+    $("#memory_board").append("<a class=\"yey3\" href=\"memory.html\">Click here</a>");
+    $(".yey3").toggle();
+    $(".yey3").fadeIn(2000);
+  }
+};
