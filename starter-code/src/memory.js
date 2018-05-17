0153cf4d9b4477033950594b2facffe858f520a3
Georgina Pacey <paceygeorgina@gmail.com>
2018-05-17 21:42:39 +0200

    first part of first iteration

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,10 +1,27 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+ var MemoryGame = function (cards){
+   this.cards = cards;
+   this.pickedCards = [];
+   this.pairsClicked = [];
+   this.pairsGuesse = [];
+ };
+ console.log(this.cards);
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+ MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    
+    var shuffleArray = [];
+    cardsArr.forEach(function(element, index) {
+        var i = Math.floor(Math.random()*cardsArr.length);
+        index = i;
+        shuffleArray.push(element[index]);
+        cardsArr.splice(index, 0);
 
-// };
+    });
+
+    console.log(shuffleArray);
+
+ };
+
+//MemoryGame.shuffleCard(cards);
 
 // MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
