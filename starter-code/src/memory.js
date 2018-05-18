e1121888c82bef1bf01c6c7254f28a4d348621f0
Miguel Angel Rodriguez <finkel218@gmail.com>
2018-05-18 16:05:34 -0500

    done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,37 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+ var MemoryGame = function (cards) {
+   this.cards = cards;
+   this.pickedCards = [];
+   this.pairsClicked = 0;
+   this.pairsGuessed = 0; 
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+ };
 
-// };
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  for (let i = cardsArr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [cardsArr[i], cardsArr[j]] = [cardsArr[j], cardsArr[i]];
+}
+return cardsArr;
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
-// }
+ MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked += 1
+  if(firstCard === secondCard){
+    this.pairsGuessed += 1;
+    return true;
+  }else{
+    return false;
+  }
+ };
 
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+ if(this.cards.length == 0){
+   return false;
+ }
+ if(this.pairsGuessed == 12){
+   return true;
+ } else{
+   return false;
+ }
+ };
