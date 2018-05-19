b6ff2ebcc0c24188fb7fd875d4722a810e4b586e
huguin65 <huguin_65@hotmail.com>
2018-05-19 06:58:52 -0500

    MexWEBPT Done Hugo Iván Sánchez Rodríguez

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -28,9 +28,10 @@ var cards = [
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
+  //memoryGame.cards = memoryGame.shuffleCard(memoryGame.cards);
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
-    html += '<div class="back"';
+    html += '<div class="back" ';
     html += '    name="'       + pic.img +  '">';
     html += '</div>';
     html += '<div class="front" ';
@@ -41,10 +42,32 @@ $(document).ready(function(){
 
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
+
   // Bind the click event of each element to a function
-$('.back').on('click', function () {
-   
-});
+  $('.back').on('click', function () {
+    $(this).next("div").addClass("back");
+    $(this).toggle();
+    memoryGame.pickedCards[$(".front.back").length-1] = $(this).parent().attr("id");
+    ($(".front.back").length == 2) &&
+      ((!(memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1]))) ?
+        $(".front.back").fadeOut(500,()=>{
+          $(".front.back").toggleClass("back");
+          $(".back:hidden, .front:hidden").toggle();
+        }):
+        $(".front.back").hide(500,()=>{
+          $(".front.back").parent().remove();
+          $("#pairs_guessed").prop("innerText",memoryGame.pairsGuessed);
+          (memoryGame.finished()) && onGameFinished(memoryGame.pairsClicked);
+        }));
+    $("#pairs_clicked").prop("innerText",memoryGame.pairsClicked);
+  });
 });
 
-
+function onGameFinished(tries){
+  $("#pairs_clicked").prop("innerText",tries);
+  var html = '';
+  html += '<div><h1>Game Finished! Congratulations!!!</h1><div>';
+  html += '<div><h1>You did it with ' + tries + ' tries</h1><div>';
+  html += '<a href="javascript:window.location.reload(true)"><h2>Play Again!!!</h2></a>';
+  document.getElementById('memory_board').innerHTML = html;
+}
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,24 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+   this.cards = cards;
+   this.pickedCards = [];
+   this.pairsClicked = 0;
+   this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    for (let i = cardsArr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [cardsArr[i], cardsArr[j]] = [cardsArr[j], cardsArr[i]];
+    }
+    return cardsArr;
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked++;
+    return (firstCard == secondCard) ? (this.pairsGuessed++ , true) : false;
+}
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+    return (this.cards.length==0) ? false :
+    ((this.pairsGuessed == 12) ? true : false);
+};
