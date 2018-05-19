7c46faecf439adfe4b52924823881991b7f6e87a
Alekz7 <ing.aragon.ipn@gmail.com>
2018-05-18 22:59:56 -0500

    MEXPT-ALEJANDRO_ARAGON_ALVAREZ

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -29,22 +29,28 @@ $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
+
     html += '<div class= "card" id="card_' + pic.name + '">';
+    
     html += '<div class="back"';
     html += '    name="'       + pic.img +  '">';
     html += '</div>';
-    html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + ') no-repeat">';
-    html += '</div>';
+
+    // html += '<div class="front" ';
+    // html += 'style="background: url(img/' + pic.img + ') no-repeat">';
+    // html += '</div>';
+
     html += '</div>';
   });
 
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
-$('.back').on('click', function () {
-   
-});
+  $('.back').on('click', function () {
+    console.log($(this).removeClass("back"));
+    $(this).removeClass("back");
+    $(this).hide();
+  });
 });
 
 
