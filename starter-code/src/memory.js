48dc67ade7cfba221d0170c07d130567a3fbe86d
Cestarck <hernandezv.cesar@gmail.com>
2018-05-19 06:55:08 -0500

    Full

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,26 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+   this.cards = cards;
+   this.pickedCards=[];
+   this.pairsClicked=0;
+   this.pairsGuessed=0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    let shuffledCard=cardsArr;
+    for(let i=shuffledCard.length-1;i>1;i--){
+        let j=Math.floor(Math.random()*shuffledCard.length);
+        let actual=shuffledCard[i];
+        shuffledCard[i]=shuffledCard[j]
+        shuffledCard[j]=actual;
+    }
+    return shuffledCard;
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked++;
+    return ((firstCard==secondCard) ? (this.pairsGuessed++, true): false);
+}
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+    return (this.pairsClicked==0 ? false: (this.pairsGuessed==(this.cards.length/2) ? true: false));
+};
