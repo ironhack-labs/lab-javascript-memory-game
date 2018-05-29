8eb2a236223edd2d167cb01ff62f5c935a3ba8c8
Laure Dassy <laure.dassy@hotmail.com>
2018-05-29 22:10:07 +0200

    Issue with turning cards back around

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -41,10 +41,76 @@ $(document).ready(function(){
 
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
+ 
   // Bind the click event of each element to a function
+  
+  
+
 $('.back').on('click', function () {
-   
-});
+   //front to become back
+
+  if(memoryGame.pairsClicked<2){
+    $(this).toggleClass('back');
+    $(this).toggleClass('front');
+  
+    $(this).next().toggleClass('front');
+    $(this).next().toggleClass('back');
+
+    //if((memoryGame.pickedCards.length)<2){
+      memoryGame.pickedCards.push($(this).parent().prop('id'));
+      //console.log("size",memoryGame.pickedCards.length);
+   // }
+  }
+  console.log(memoryGame.pickedCards);
+
+//check if pair
+    var result=memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1]);
+  if(memoryGame.pairsClicked>=2){
+    
+    if(result===false){
+      console.log("u're there");
+      console.log("arr", memoryGame.pickedCards[0]);
+      setTimeout(function(){
+       // $('#'+memoryGame.pickedCards[0]+':first-child').removeClass('front');
+       // $('#'+memoryGame.pickedCards[0]+':first-child').addClass('back');
+        //$('#'+memoryGame.pickedCards[0]+':first-child').next().removeClass('back');
+        //$('#'+memoryGame.pickedCards[0]+':first-child').next().addClass('back');
+        $('#'+memoryGame.pickedCards[0]+':nth-child(1)').toggleClass('front');
+        $('#'+memoryGame.pickedCards[0]+':nth-child(1)').toggleClass('back');
+        $('#'+memoryGame.pickedCards[0]+':nth-child(2)').toggleClass('back');
+        $('#'+memoryGame.pickedCards[0]+':nth-child(2)').toggleClass('front');
+
+
+        $('#'+memoryGame.pickedCards[1]+':nth-child(1)').removeClass('front');
+        $('#'+memoryGame.pickedCards[1]+':nth-child(1)').addClass('back');
+        $('#'+memoryGame.pickedCards[1]+':nth-child(2)').next().removeClass('back');
+        $('#'+memoryGame.pickedCards[1]+':nth-child(2)').next().addClass('back');
+      }, 2000);
+      console.log('now here');
+     
+    }
+    
+      
+    
+
+  }
+  
+
 });
 
+  
+
+});
+
+/*if(result==false){
+    var count=0;
+    $('.front').on('click', function () {
+      
+        $(this).toggleClass('front');
+        $(this).toggleClass('back');
+        $(this).next().toggleClass('back');
+        $(this).next().toggleClass('front');
+      
+     
+     });*/
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,39 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+}
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    var temp=null;
+    var swap;
+      for(var i=cardsArr.length-1; i>0; i--){
+          swap=Math.floor(Math.random()*i);
+          temp=cardsArr[i];
+          cardsArr[i]=cardsArr[swap];
+          cardsArr[swap]=temp;
+      }
+      return cardsArr;
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked++;
+    if(firstCard=== secondCard){
+        this.pairsGuessed++;
+        return true;
+    }
+    else{
+        return false;
+    }
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+}
 
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+ MemoryGame.prototype.finished = function () {
+    if(this.pairsGuessed===12){
+        return true;
+    }
+    else{
+        return false;
+    }
+};
