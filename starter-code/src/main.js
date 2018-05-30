0ff84eea2cfd49dbfdd68751215b2581186dc08d
cameron skene <cameron.t.skene@gmail.com>
2018-05-30 11:40:32 +0200

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,11 +25,13 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
+var memoryGame = new MemoryGame(cards);
 $(document).ready(function(){
-  var memoryGame = new MemoryGame(cards);
+  
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
-    html += '<div class= "card" id="card_' + pic.name + '">';
+    
+    html += '<div class= "card" data-name="card_' + pic.name + '">';
     html += '<div class="back"';
     html += '    name="'       + pic.img +  '">';
     html += '</div>';
@@ -39,12 +41,59 @@ $(document).ready(function(){
     html += '</div>';
   });
 
+// click on .card
+// hide back
+// add to pickedCards
+// feed to checkIfPair
+  // if true, keep pair up
+  // else false, show back
+ 
+  
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
-$('.back').on('click', function () {
-   
-});
-});
+  $(".back").on('click',(function() {
+    
+    
+    if (memoryGame.pickedCards.length < 2) {
+      $(this).addClass("front clicked").removeClass("back");
+      $(this).next().addClass("back").removeClass("front");
 
+      memoryGame.pickedCards.push($(this).parent().attr("data-name"));
+      console.log(memoryGame.pickedCards);  
+    }
+    
+    if (memoryGame.pickedCards.length === 2) {
+      var isMatch = memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1]);
+      console.log(isMatch);
+      
+      
+      if (!isMatch) {
+        
+        setTimeout(function(){ 
+          memoryGame.pickedCards = [];
+          $(".clicked").addClass("back").removeClass("front");
+          $(".clicked").next().addClass("front").removeClass("back");
+          isMatch = undefined;
+          $(".clicked").removeClass("clicked")
+          $("#pairs_clicked").text(memoryGame.pairsClicked);
+         }, 1200);
+         
+      }
+      if (isMatch) {
+        // rest picked cards array
+        memoryGame.pickedCards = [];
+        isMatch = undefined;
+        $(".clicked").removeClass("clicked")
+        $("#pairs_clicked").text(memoryGame.pairsClicked);
+        $("#pairs_guessed").text(memoryGame.pairsGuessed);
+      }
+    }
 
+    if (memoryGame.finished()) {
+      console.log("you're winner")
+      window.alert("you're winner");
+    }
+    
+})); 
+});
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,44 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  // this.cards = this.shuffleCard(cards);
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
 
-// };
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  var shuffled = [];
+  var max = cardsArr.length;
+  while (max > 0) {
+    var chosen = Math.floor(Math.random() * cardsArr.length);
+    if (chosen in cardsArr) {
+      shuffled.push(cardsArr[chosen]);
+      delete cardsArr[chosen];
+      max--;
+    }
+  }
+  return shuffled;
+};
 
-// }
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  
+  var self = this;
+  self.pairsClicked++;
+  if (firstCard === secondCard) {
+    self.pairsGuessed++;
+    // do I have to remove correct cards from the cards array (aka game board)?
+    self.cards.splice(self.cards.indexOf(firstCard), 1)
+    self.cards.splice(self.cards.indexOf(secondCard), 1)
+    return true;
+  }
+  else {
+    return false;  
+  }   
+}
 
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function() {
+  var self = this;
+  return (self.pairsGuessed === 12)
+};
