c80ee58a817c11c84ca6c960627101452816a371
Roberta Conrad <rjcnrd@gmail.com>
2018-05-30 11:49:57 +0200

    not done but I understand the solution maxence showed, all code was discussed, no corrections needed:)

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
-    html += '<div class= "card" id="card_' + pic.name + '">';
+    html += '<div class= "card" data-name="card_' + pic.name + '">';
     html += '<div class="back"';
     html += '    name="'       + pic.img +  '">';
     html += '</div>';
@@ -37,14 +37,41 @@ $(document).ready(function(){
     html += 'style="background: url(img/' + pic.img + ') no-repeat">';
     html += '</div>';
     html += '</div>';
+
+
+
   });
 
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
 $('.back').on('click', function () {
-   
+
+  if (memoryGame.pickedCards.length<2)
+  {
+    $(this).toggleClass("front back");
+    $(this).next().toggleClass("front back");
+    $(this).parent().addClass('picked');
+
+  memoryGame.pickedCards.push($(this).parent().attr("data-name"));
+  console.log(memoryGame.pickedCards);
+  }
+
+  if (memoryGame.pickedCards.length === 2)
+  {
+    var isMatch = memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1]);
+    if (isMatch) this.pairsGuessed ++;
+    this.pairsClicked++;
+    this.pickedCards =0;
+
+
+    setTimeout(function(){
+    $('.picked').toggleClass.children("front back");
+    },2000);
+  }
+
 });
+
 });
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,31 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  // this.cards = this.shuffleCard(cards);
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0; 
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr)
+{
+  var cardsAreshuffled = [];
+  var ourLength = cardsArr.length; 
+  for (var index = ourLength-1; index >= 0; index--) 
+  {
+    var randomNumber = Math.floor(Math.random()*(cardsArr.length));
+    cardsAreshuffled.push(cardsArr[randomNumber]);
+    cardsArr.splice(randomNumber,1);
+  }
 
-// };
+  return cardsAreshuffled;
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+if (firstCard === secondCard) this.pairsGuessed++;
+this.pairsClicked++;
+return (firstCard === secondCard);
+}
 
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+return (this.pairsGuessed===12);
+};
