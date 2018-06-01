87d887a7c22aa8e12a4ca3a074af7f7ee1eeb92e
Javier Chacón <j.chacon.alvarez@gmail.com>
2018-06-02 00:13:20 +0200

    logic done, flips cards

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,35 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsGuessed = 0;
+  this.pairsClicked = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  var newArr = cardsArr;
 
-// };
+  for (i = 0; i < newArr.length; i++) {
+    var randNumber = Math.floor(Math.random() * i);
+    var temp = newArr[i];
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    newArr[i] = newArr[randNumber];
+    newArr[randNumber] = temp;
+  }
+  return newArr;
+};
 
-// }
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++
+  if (firstCard === secondCard) {
+    this.pairsGuessed++
+    return true;
+  }
+  return false;
+}
 
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+  if (this.pairsGuessed === (this.cards.length / 2)) {
+    return true;
+  }
+  return false;
+};
