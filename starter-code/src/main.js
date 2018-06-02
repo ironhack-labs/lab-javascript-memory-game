f4b394d8c7ec1e807aef9541f849beec03e187b7
Pere Busquet <perebusquet87gmail.com>
2018-06-02 11:04:03 +0200

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
-    html += '<div class= "card" id="card_' + pic.name + '">';
+    html += '<div class= "card" data-name ="card_' + pic.name + '">';
     html += '<div class="back"';
     html += '    name="'       + pic.img +  '">';
     html += '</div>';
@@ -40,11 +40,51 @@ $(document).ready(function(){
   });
 
   // Add all the div's to the HTML
-  document.getElementById('memory_board').innerHTML = html;
+  $('#memory_board').html(html);
   // Bind the click event of each element to a function
-$('.back').on('click', function () {
-   
-});
+  $('.back').on('click', function () {
+    
+    if (memoryGame.pickedCards.length === 2) {
+      return;
+    }
+    
+    if(memoryGame.pickedCards.length < 2){
+      $(this).toggleClass('front back');
+      $(this).next().toggleClass('front back');
+      $(this).parent().addClass('picked');
+      
+      
+      memoryGame.pickedCards.push($(this).parent().attr('data-name'));
+      console.log(memoryGame.pickedCards);
+    }
+    if(memoryGame.pickedCards.length === 2){
+      var firstCard = memoryGame.pickedCards[0];
+      var secondCard = memoryGame.pickedCards[1];
+      var isMatch =  memoryGame.checkIfPair(firstCard, secondCard);
+      console.log(isMatch);
+      
+      updateScore();
+      
+      
+      
+      //if not a pair, flip them back;
+      if(!isMatch){
+        setTimeout(function(){
+          
+          $('.picked').children().toggleClass('front back');
+          memoryGame.pickedCards= [];
+          $('.picked').removeClass('picked');
+        },2000);
+      }else{
+        memoryGame.pickedCards= [];
+        $('.picked').removeClass('picked');
+      }
+    } 
+  });
+  function updateScore() {
+    $('#pairs_clicked').text(memoryGame.pairsClicked);
+    $('#pairs_guessed').text(memoryGame.pairsGuessed);
+  }  
 });
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,33 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+ var MemoryGame = function (cards) {
+  this.cards = this.shuffleCard(cards);
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+ };
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+ MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  var cardsShuffled = [];
+  var cardLength = cardsArr.length; 
+  for (var index = cardLength-1; index >= 0; index--) 
+  {
+    var randomNumber = Math.floor(Math.random()*(cardsArr.length));
+    cardsShuffled.push(cardsArr[randomNumber]);
+    cardsArr.splice(randomNumber,1);
+  }
 
-// };
+  return cardsShuffled;
+};
+ MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked++;
+    if(firstCard === secondCard){
+      this.pairsGuessed++;
+      return true;
+    }else{
+      return false;
+    }
+ };
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+ MemoryGame.prototype.finished = function () {
+    return this.pairsGuessed > 0
+      && this.pairsGuessed === this.cards.length/2;
+ };
