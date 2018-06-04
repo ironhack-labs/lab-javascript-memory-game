e6f010ba91fab2af81b61fe176e03429ed179001
hcorta <hugocorta@hotmail.com>
2018-06-04 22:40:11 +0200

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,7 +27,16 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+
+
+
+  //Shuffeling cards at the beggining:
+memoryGame.cards = memoryGame.shuffleCard(cards);
+
+
+
   var html = '';
+
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
     html += '<div class="back"';
@@ -41,10 +50,62 @@ $(document).ready(function(){
 
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
+  
+
   // Bind the click event of each element to a function
-$('.back').on('click', function () {
-   
+  var firstCard;
+  var secondCard;
+  var pairsClicked = 0;
+  var pairsGuessed = 0;
+
+  
+
+$('.back').on('click', function(){
+  //Setting the Divs in variables to manage them easily:
+  var coverDiv = $(this);
+  var cardDiv = $(this).next();
+
+  //Making visible the card;
+  coverDiv.toggleClass('back front');
+  cardDiv.css('display', 'block');
+
+  //Keeping the cards in variables to check later on: 
+  if (firstCard === undefined){
+     firstCard = cardDiv;
+  }
+  else {
+    //Get the pairsClicked++
+    pairsClicked++;
+    $('#pairs_clicked').html(pairsClicked)
+
+    //Checking the cards;
+     secondCard = cardDiv;
+  
+      var a = firstCard.css('background-image');
+      var b = secondCard.css('background-image');
+
+
+      //Wrong Guess: Reverts the cards and resetting the variable
+      if (a !== b){
+
+        firstCard.css('display', 'none')
+        firstCard.prev().toggleClass('front back');
+
+        secondCard.css('display', 'none')
+        secondCard.prev().toggleClass('front back');
+
+        firstCard = undefined;
+      } 
+      
+      //Right Guess:
+      if(a === b){
+        pairsGuessed++;
+        $('#pairs_guessed').html(pairsGuessed);
+
+        firstCard = undefined;
+      }
+  };
 });
-});
+})
 
 
