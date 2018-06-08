9bce0c25c729a95794b6c33e69e3bb60783e4406
eloji9 <eloji9@hotmail.com>
2018-06-08 16:14:10 +0200

    done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,56 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+// Creating the MemoryGame Constructor :
+
+function MemoryGame(cards){
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+};
+
+
+// Creating the shuffleCard Method :
+
+    // Option 1 : This technique uses lodash :
+
+MemoryGame.prototype.shuffleCard = function(cardsArr) {
+    this.cards= _.shuffle(cardsArr);
+    return this.cards;
+};
+
+    // Option 2 : This technique is "the usual" one :
 
 // MemoryGame.prototype.shuffleCard = function (cardsArr) {
-
+//     var newArrCards = [];
+//     var c = cardsArr.length;
+//     var i;
+//     while (c) {
+//       i = Math.floor(Math.random() * cardsArr.length);
+//       if (i in cardsArr) {
+//         newArrCards.push(cardsArr[i]);
+//         delete cardsArr[i];
+//         c--;
+//       }
+//     }
+//     return newArrCards;
 // };
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+// Creating the checkIfPair Method:
 
-// }
+    // Part 1 : If the cards picked are the same the pair is added to the pairsCLicked and the pairsGuessed
+    // Part 2 : If the cards picked are not the same the pair is ONLY added to the pairsCLicked
 
-// MemoryGame.prototype.finished = function () {
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard){
+    this.pairsClicked +=1;
+    this.pickedCards = [];
+    if (firstCard == secondCard){
+        this.pairsGuessed += 1;
+        return true;
+    }
+    return false;
+}
 
-// };
+// Creating the finished Method
+
+MemoryGame.prototype.finished = function () {
+    return(this.pairsGuessed < 12 ? false : true)
+};
