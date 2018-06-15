6df3995131297b342ee10c983c02fdf229ab5325
Max-N3O <maxime.ferhani@gmail.com>
2018-06-15 17:15:31 +0200

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -26,8 +26,21 @@ var cards = [
 ];
 
 $(document).ready(function(){
+  
+  cards = shuffleCard(cards);
+
   var memoryGame = new MemoryGame(cards);
+  // console.log(cards); //Question : why the heck is this running twice?
+  // console.log(typeof memoryGame);
+  // console.log(memoryGame);
+  // console.log(typeof memoryGame.shuffleCard);
+  // console.log(memoryGame.shuffleCard);
+  // console.log(typeof memoryGame.shuffleCard(cards));
+  // console.log(memoryGame.shuffleCard(cards));
+  // console.log("Go");
+  // console.log(memoryGame.cards);
   var html = '';
+  
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
     html += '<div class="back"';
@@ -37,14 +50,72 @@ $(document).ready(function(){
     html += 'style="background: url(img/' + pic.img + ') no-repeat">';
     html += '</div>';
     html += '</div>';
+
+
   });
+
 
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
+
+
+  //Hide all the front classes
+  $(document).find(".front").hide();
+
+
+  
+
   // Bind the click event of each element to a function
-$('.back').on('click', function () {
+
+$('.back').on('click', function (element) {
+  //Display the card on the first click and on the second click
+  $(this).hide();
+  console.log($(this).attr("name"));
+  pickedCards.push($(this).attr("name"));
+  $(this).parent().find(".front").show();
+  $(this).parent().find(".front").addClass("clicked"); // Add a class to be able to find them back
+  $(this).parent().find(".back").addClass("clicked"); // Add a class to be able to find them back
+  console.log("pickedCards.length " + pickedCards.length);
+
+
+  // memory.checkIfPair
+
+  if(pickedCards.length === 2) {
+    console.log(pickedCards[0]);
+    console.log(pickedCards[1]);
+      
+    if (memoryGame.checkIfPair(pickedCards[0], pickedCards[1]) === false) {
+      setTimeout(function() {
+      $(document).find(".front.clicked").hide();
+      $(document).find(".back.clicked").show();
+      $(document).find(".clicked").removeClass("clicked");
+    },500)
+    } else {
+      $(document).find(".clicked").removeClass("clicked");
+    }
+
+    // pairsClicked += 1;
+    pickedCards=[];
+    $("#pairs_clicked").html(memoryGame.pairsClicked);
+    $("#pairs_guessed").html(memoryGame.pairsGuessed);
+  }
+
+console.log(memoryGame.finished());
+memoryGame.finished();
+
+if (memoryGame.finished() === true ) {
+  $("#pairs_guessed").css("color","green");
+}
    
 });
+
+
+
+
+
+
 });
 
+//Add an end!
+//Don't forget the shuffle!!!
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,55 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+cards = [];
+pickedCards = [];
+pairsClicked = 0;
+pairsGuessed = 0;
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
 
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = pickedCards;
+  this.pairsClicked = pairsClicked;
+  this.pairsGuessed = pairsGuessed;
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  var arr = [];
+for (var i = 0 ; i < cardsArr.length;) {
+  var random = Math.floor(Math.random()*(cardsArr.length));
+  // list[i] = cardsArr[random];
+  arr.push(cardsArr[random]);
+  cardsArr.splice(random,1);
+}
+return arr;
+};
 
-// }
+//Same function, but not method of object MemoryGame, because no need to in order to be used
+function shuffleCard (cardsArr) {
 
-// MemoryGame.prototype.finished = function () {
+  var arr = [];
+for (var i = 0 ; i < cardsArr.length;) {
+  var random = Math.floor(Math.random()*(cardsArr.length));
+  // list[i] = cardsArr[random];
+  arr.push(cardsArr[random]);
+  cardsArr.splice(random,1);
+}
+return arr;
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked += 1 ;
+
+  if(firstCard === secondCard) {
+    this.pairsGuessed +=1;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+MemoryGame.prototype.finished = function () {
+  if (this.pairsGuessed === 24/2) {
+    return true;
+  } else {
+    return false;
+  }
+};
