79076e1adea1d50e4024c8ad0b81537fc1684bb5
Andrés Martin <monkeyandres@protonmail.com>
2018-06-19 17:25:50 +0200

    Finished

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,36 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+var MemoryGame = function (cards) {
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+};
 
-// };
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    var currentIndex = cardsArr.length, temporaryValue, randomIndex;
+    
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+    
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+    
+        // And swap it with the current element.
+        temporaryValue = cardsArr[currentIndex];
+        cardsArr[currentIndex] = cardsArr[randomIndex];
+        cardsArr[randomIndex] = temporaryValue;
+    }
+    
+    return cardsArr;
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked++;
+    if(firstCard == secondCard){this.pairsGuessed++;}
+    return firstCard == secondCard;
+}
 
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+    return this.pairsGuessed == 12;
+};
