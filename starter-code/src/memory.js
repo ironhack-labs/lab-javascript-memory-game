36f03f44aaeba17b623d79d7a157878e8c397357
Dunnia <dunnia.alvarez@gmail.com>
2018-06-19 23:22:42 +0200

    done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,30 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+ var MemoryGame = function (cards) {
+   this.cards = cards;
+   this.pickedCards=[];
+   this.pairsClicked=[];
+   this.pairsGuessed=[];
+ };
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+ MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  
+    return _.shuffle(cardsArr);
+ };
 
-// };
+ MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked ++;
+    if (firstCard == secondCard){
+        this.pairsGuessed ++;
+        return true;
+    }else {
+        return false};
+ }
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+ MemoryGame.prototype.finished = function () {
+    if(this.pairsGuessed === 12)
+    {
+             
+         return true;
+        
+     }
+     return false;
+ };
