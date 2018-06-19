6828d67a96678a9b552be7bf3d09751134826e2c
Sarai Vasquez <svmarinez@hotmail.com>
2018-06-19 17:32:39 +0200

    Logic done and it feels as good as a basket of puppies, next step is the game

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,34 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+ var MemoryGame = function (cards) {
+   this.cards = cards;
+   this.pickedCards = [];
+   this.pairsClicked = [];
+   this.pairsGuessed = [];
+   };
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    return _.shuffle(cardsArr);
 
-// };
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+ MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    var that = this;
+    if (firstCard == secondCard) {
+    that.pairsGuessed++;
+    that.pairsClicked++; 
+    return true;
+     } else {
+        that.pairsClicked++;
+         return false;
+     }
+};
 
-// }
+MemoryGame.prototype.finished = function () {
+    if (this.pairsClicked == 0) {
+        return false;
+    } else if (this.pairsGuessed == 12) {
+        return true;
+    } else {
+        return false;
+    }
+};
 
-// MemoryGame.prototype.finished = function () {
-
-// };
