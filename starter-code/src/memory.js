fe3f2e1b73bbd227925a20a89bde6d8f41529763
Minyu Li <minyuli1997@gmail.com>
2018-06-19 17:42:23 +0200

    done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,43 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked=0;
+  this.pairsGuessed=0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    var currentIndex = cardsArr.length, temporaryValue, randomIndex;
 
-// };
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = cardsArr[currentIndex];
+      cardsArr[currentIndex] = cardsArr[randomIndex];
+      cardsArr[randomIndex] = temporaryValue;
+    }
+  
+    return cardsArr;
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked+=1;
+    if(firstCard==secondCard){
+        this.pairsGuessed+=1;
+        return true;
+    }else{
+        return false;
+    }
+}
 
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+    if (this.pairsGuessed==12){
+        return true;
+    } else {
+        return false;
+    }
+};
