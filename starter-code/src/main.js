c269d14fb5f008e1bdae7c18b8a899c435eb48ea
Enrique Pernia <Epernia96@gmail.com>
2018-06-20 00:13:34 +0200

    Testeando algunas cosas

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -1,37 +1,107 @@
-
-var cards = [
-	{ name: "aquaman", img: "aquaman.jpg" },
-	{ name: "batman", img: "batman.jpg" },
-	{ name: "captain america", img: "captain-america.jpg" },
-	{ name: "fantastic four", img: "fantastic-four.jpg" },
-	{ name: "flash", img: "flash.jpg" },
-	{ name: "green arrow", img: "green-arrow.jpg" },
-	{ name: "green lantern", img: "green-lantern.jpg" },
-	{ name: "ironman", img: "ironman.jpg" },
-	{ name: "spiderman", img: "spiderman.jpg" },
-	{ name: "superman", img: "superman.jpg" },
-	{ name: "the avengers", img: "the-avengers.jpg" },
-	{ name: "thor", img: "thor.jpg" },
-	{ name: "aquaman", img: "aquaman.jpg" },
-	{ name: "batman", img: "batman.jpg" },
-	{ name: "captain america", img: "captain-america.jpg" },
-	{ name: "fantastic four", img: "fantastic-four.jpg" },
-	{ name: "flash", img: "flash.jpg" },
-	{ name: "green arrow", img: "green-arrow.jpg" },
-	{ name: "green lantern", img: "green-lantern.jpg" },
-	{ name: "ironman", img: "ironman.jpg" },
-	{ name: "spiderman", img: "spiderman.jpg" },
-	{ name: "superman", img: "superman.jpg" },
-	{ name: "the avengers", img: "the-avengers.jpg" },
-	{ name: "thor", img: "thor.jpg" }
+var cards = [{
+		name: "aquaman",
+		img: "aquaman.jpg"
+	},
+	{
+		name: "batman",
+		img: "batman.jpg"
+	},
+	{
+		name: "captain america",
+		img: "captain-america.jpg"
+	},
+	{
+		name: "fantastic four",
+		img: "fantastic-four.jpg"
+	},
+	{
+		name: "flash",
+		img: "flash.jpg"
+	},
+	{
+		name: "green arrow",
+		img: "green-arrow.jpg"
+	},
+	{
+		name: "green lantern",
+		img: "green-lantern.jpg"
+	},
+	{
+		name: "ironman",
+		img: "ironman.jpg"
+	},
+	{
+		name: "spiderman",
+		img: "spiderman.jpg"
+	},
+	{
+		name: "superman",
+		img: "superman.jpg"
+	},
+	{
+		name: "the avengers",
+		img: "the-avengers.jpg"
+	},
+	{
+		name: "thor",
+		img: "thor.jpg"
+	},
+	{
+		name: "aquaman",
+		img: "aquaman.jpg"
+	},
+	{
+		name: "batman",
+		img: "batman.jpg"
+	},
+	{
+		name: "captain america",
+		img: "captain-america.jpg"
+	},
+	{
+		name: "fantastic four",
+		img: "fantastic-four.jpg"
+	},
+	{
+		name: "flash",
+		img: "flash.jpg"
+	},
+	{
+		name: "green arrow",
+		img: "green-arrow.jpg"
+	},
+	{
+		name: "green lantern",
+		img: "green-lantern.jpg"
+	},
+	{
+		name: "ironman",
+		img: "ironman.jpg"
+	},
+	{
+		name: "spiderman",
+		img: "spiderman.jpg"
+	},
+	{
+		name: "superman",
+		img: "superman.jpg"
+	},
+	{
+		name: "the avengers",
+		img: "the-avengers.jpg"
+	},
+	{
+		name: "thor",
+		img: "thor.jpg"
+	}
 ];
 
-$(document).ready(function() {
-  var memoryGame = new MemoryGame(cards);
-  memoryGame.pickedCards=cards;
-  memoryGame.cards = memoryGame.shuffleCard(memoryGame.pickedCards);
+$(document).ready(function () {
+	var memoryGame = new MemoryGame(cards);
+	memoryGame.pickedCards = cards;
+	memoryGame.cards = memoryGame.shuffleCard(memoryGame.pickedCards);
 	var html = "";
-	memoryGame.cards.forEach(function(pic, index) {
+	memoryGame.cards.forEach(function (pic, index) {
 		html += '<div class= "card" id="card_' + pic.name + '">';
 		html += '<div class="back"';
 		html += '    name="' + pic.img + '">';
@@ -40,7 +110,6 @@ $(document).ready(function() {
 		html += 'style="background: url(img/' + pic.img + ') no-repeat">';
 		html += "</div>";
 		html += "</div>";
-		
 	});
 
 	// Add all the div's to the HTML
@@ -48,7 +117,19 @@ $(document).ready(function() {
 	// Bind the click event of each element to a function
 	var contador = 0;
 	var container = [];
-	$(".back").on("click", function() {
+	var a = "Mas suerte la proxima vez crack";
+	var b = "Buen intento"
+	var c ="Uyyyy casi eh!"
+	var g="No es tu dia... "
+	var d="Ha sido suerte..."
+	var e="Bien hecho!"
+	var f="Por fin!!"
+	var h="Progresas adecuadamente"
+	var frasesMalas=[a,b,c,g];
+	var frasesNoTanMalas=[d,e,f,h];
+	var introduceTexto = $(".texto");
+	introduceTexto.toggle();
+	$(".back").on("click", function () {
 		contador++;
 		var bro = $(this).next();
 		$(this).toggleClass("front");
@@ -59,41 +140,45 @@ $(document).ready(function() {
 		if (contador == 2) {
 			$("#memory_board").addClass("negativo");
 			contador = 0;
-			if (
-				!memoryGame.checkIfPair(
+			if (!memoryGame.checkIfPair(
 					$(container[0]).attr("name"),
 					$(container[1]).attr("name")
-				)
-			) {
-				var timeoutId = setTimeout(function() {
+				)) {
+					setTimeout(function() {
+						aleatorio=Math.floor(Math.random()*3);
+						introduceTexto.text(frasesMalas[aleatorio]).fadeIn("1000");
+					 },1);
+					var timeoutId = setTimeout(function () {
 					$(container[1]).toggleClass("back");
 					$(container[1]).toggleClass("front");
-					$(container[1])
-						.next()
-						.toggleClass("front");
-					$(container[1])
-						.next()
-						.toggleClass("back");
+					$(container[1]).next().toggleClass("front");
+					$(container[1]).next().toggleClass("back");
 					$(container[0]).toggleClass("back");
 					$(container[0]).toggleClass("front");
-					$(container[0])
-						.next()
-						.toggleClass("front");
-					$(container[0])
-						.next()
-						.toggleClass("back");
+					$(container[0]).next().toggleClass("front");
+					$(container[0]).next().toggleClass("back");
 					container = [];
 					$("#pairs_clicked").text(memoryGame.pairsClicked);
 					$("#pairs_guessed").text(memoryGame.pairsGuessed);
-					$("#memory_board").removeClass("negativo");
 				}, 1000);
+				setTimeout(function() {
+					introduceTexto.fadeOut("1000");
+					$("#memory_board").removeClass("negativo");
+				 },1000);
 			} else {
-				var timeoutId = setTimeout(function() {
+				setTimeout(function() {
+					aleatorio=Math.floor(Math.random()*3);
+					introduceTexto.text(frasesNoTanMalas[aleatorio]).fadeIn("1000");
+				 },1);
+				var timeoutId = setTimeout(function () {
 					$("#pairs_clicked").text(memoryGame.pairsClicked);
 					$("#pairs_guessed").text(memoryGame.pairsGuessed);
 					container = [];
-					$("#memory_board").removeClass("negativo");
 				}, 1000);
+				setTimeout(function() {
+					introduceTexto.fadeOut("1000");
+					$("#memory_board").removeClass("negativo");
+				 },1000);
 			}
 			if (memoryGame.finished()) {
 				location.reload();
