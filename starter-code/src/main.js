242722c8e73d6be8c4ab3e30ba55ae8cf788e5a0
Jorge L <kolmax@gmail.com>
2018-06-19 19:15:29 +0200

    Finish logic and start print on dom

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,8 +25,41 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
+//Function that runs the game
+// function startGame(memoryGame) {
+//   var id = setInterval(function () {
+//
+//
+//     //Check if pair
+//
+//     //Check if fiished
+//     if (checkIfFinished(memoryGame.pairsGuessed)) {
+//       clearInterval(id)
+//     }
+//   }, 2000)
+// }
+
+
+// function storeCard(card) {
+//   if
+//   memoryGame.pickedCards.push(card)
+// }
+
+//Function that returns if a pair es equal
+// function checkIfPair(card1, card2) {
+//   return memoryGame.checkIfPair(card1, card2)
+// }
+
+//Function that return if the game is finished
+// function checkIfFinished(pairsGuessed) {
+//   return memoryGame.finished(pairsGuessed)
+// }
+
+
+//Function that is called just after the docuents loads
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  memoryGame.cards = memoryGame.shuffleCard(memoryGame.cards)
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
@@ -42,9 +75,32 @@ $(document).ready(function(){
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
-$('.back').on('click', function () {
-   
-});
-});
+  $('.back').on('click', function () {
+    if (memoryGame.pickedCards.length < 2) {
+      //Change the cards order
+      $(this).removeClass('back')
+      $(this).addClass('front')
+      $(this).next().addClass('back')
+      $(this).next().removeClass('front')
+      memoryGame.pickedCards.push($(this))
 
+      if (memoryGame.pickedCards.length > 1) {
+        var that = $(this)
+        setTimeout(function() {
+          if (memoryGame.checkIfPair(memoryGame.pickedCards[0].name, memoryGame.pickedCards[1])) {
+            $(that).addClass('blocked')
+            $(that).next().addClass('blocked')
+          } else {
+            $(that).removeClass('front')
+            $(that).addClass('back')
+            $(that).next().addClass('front')
+            $(that).next().removeClass('back')
+            memoryGame.pickedCards = []
+          }
+        }, 1500)
+      }
+    }
+  });
 
+  //startGame(memoryGame)
+});
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,28 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+//Constructor
+var MemoryGame = function (cards) {
+  this.cards = cards
+  this.pickedCards = []
+  this.pairsClicked = 0
+  this.pairsGuessed = 0
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+//Function thar shuffles the cards
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  return _.shuffle(cardsArr)
+};
 
-// };
+//Function that checks if two cards are the same
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++
+  if (firstCard == secondCard) {
+    this.pairsGuessed++
+    return true
+  } else {
+    return false
+  }
+}
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+//Function thar checks if the game has finished
+MemoryGame.prototype.finished = function () {
+  return (this.pairsGuessed >= 12)
+};
