f995ddaf10772a46da3103687f8abc92f5198bf9
MateoReparaz <mateoreparaz1989@gmail.com>
2018-06-19 17:07:32 +0200

    Primeros pasos

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,40 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function(cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function(cardsArr) {
+  var copy = [],
+    n = cardsArr.length,
+    i;
 
-// };
+  while (n) {
+    i = Math.floor(Math.random() * cardsArr.length);
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+    if (i in cardsArr) {
+      copy.push(cardsArr[i]);
+      delete cardsArr[i];
+      n--;
+    }
+  }
+  this.cards = copy;
+  return copy;
+};
+MemoryGame.prototype.checkIfPair = function(firstCard, secondCard) {
+  this.pairsClicked += 1;
+  if (firstCard == secondCard) {
+    this.pairsGuessed += 1;
+    return true;
+  } else {
+    return false;
+  }
+};
+MemoryGame.prototype.finished = function() {
+if (this.pairsGuessed == 12){
+    return true;
+}else{
+    return false;
+}
+};
