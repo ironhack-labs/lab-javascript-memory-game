a98341d078a82b312b6ba368b78b26c97dc7f33f
Pablo Rezola <pablorezola20@yahoo.es>
2018-06-20 09:10:09 +0200

    memory game

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,57 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
 
-// };
+var MemoryGame = function (cards) {
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+  //this.pairsClicked = [];
 
-// }
+};
 
-// MemoryGame.prototype.finished = function () {
 
-// };
+
+MemoryGame.prototype.pickedCards = function () {
+
+};
+
+MemoryGame.prototype.pairsClicked = function () {
+
+  pairsClicked = [];
+
+};
+
+
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+
+
+  return _.shuffle(cardsArr);
+
+};
+
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+
+  this.pairsClicked += 1;
+
+  if (firstCard == secondCard) {
+    
+    this.pairsGuessed += 1; 
+    return true;
+  } else {
+    return false;
+  }
+
+};
+
+MemoryGame.prototype.finished = function () {
+ 
+   if (this.pairsGuessed==12){
+        return true;
+    } else {
+        return false;
+    }
+
+};
+
+
