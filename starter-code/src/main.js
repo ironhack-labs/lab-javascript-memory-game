be082266d7b5ca9f8a6a16293d1b3b475141934f
Beth Shook <beth.shook@gmail.com>
2018-06-20 01:09:48 -0500

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -28,6 +28,7 @@ var cards = [
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
+  memoryGame.cards = memoryGame.shuffleCard(cards);
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
     html += '<div class="back"';
@@ -37,14 +38,49 @@ $(document).ready(function(){
     html += 'style="background: url(img/' + pic.img + ') no-repeat">';
     html += '</div>';
     html += '</div>';
-  });
+  })
+  
 
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
 $('.back').on('click', function () {
-   
-});
+
+    $(this).toggleClass('back');
+    $(this).toggleClass('front');
+    $(this).next().toggleClass('front');
+    $(this).next().toggleClass('back');
+
+    memoryGame.pickedCards.push($(this));
+    var cardOne = memoryGame.pickedCards[0].attr('name');
+    var cardTwo = memoryGame.pickedCards[1].attr('name');
+
+    if(memoryGame.pickedCards.length == 2) {
+      var pair = memoryGame.checkIfPair(cardOne,cardTwo)
+        if (pair){
+          memoryGame.pickedCards = [];
+          $('#pairs_guessed').text(memoryGame.pairsGuessed);
+          if($('#pairs_guessed').text('12')) {
+            $('h2').text('You won!');
+          }
+          $('#pairs_clicked').text(memoryGame.pairsClicked);
+
+        } else {
+          $('#pairs_clicked').text(memoryGame.pairsClicked);
+          setTimeout(function(){
+            memoryGame.pickedCards[0].removeClass('front').addClass('back')
+            memoryGame.pickedCards[0].next().removeClass('back').addClass('front')
+          
+            memoryGame.pickedCards[1].removeClass('front').addClass('back')
+            memoryGame.pickedCards[1].next().removeClass('back').addClass('front')
+
+            memoryGame.pickedCards = [];
+          }, 500)
+
+        }
+    }
+
+  
 });
 
-
+});
