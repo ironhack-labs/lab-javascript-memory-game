87563d730ac51fb2779a7e88c0a48c2a4af469c2
JuanGPC <juangpc@gmail.com>
2018-06-20 09:57:33 +0200

    version 0.9

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -1,50 +1,82 @@
+// Variables defintion
 var cards = [
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
+  { name: 'aquaman', img: 'aquaman.jpg' },
+  { name: 'batman', img: 'batman.jpg' },
   { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' },
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
+  { name: 'fantastic four', img: 'fantastic-four.jpg' },
+  { name: 'flash', img: 'flash.jpg' },
+  { name: 'green arrow', img: 'green-arrow.jpg' },
+  { name: 'green lantern', img: 'green-lantern.jpg' },
+  { name: 'ironman', img: 'ironman.jpg' },
+  { name: 'spiderman', img: 'spiderman.jpg' },
+  { name: 'superman', img: 'superman.jpg' },
+  { name: 'the avengers', img: 'the-avengers.jpg' },
+  { name: 'thor', img: 'thor.jpg' },
+  { name: 'aquaman', img: 'aquaman.jpg' },
+  { name: 'batman', img: 'batman.jpg' },
   { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' }
+  { name: 'fantastic four', img: 'fantastic-four.jpg' },
+  { name: 'flash', img: 'flash.jpg' },
+  { name: 'green arrow', img: 'green-arrow.jpg' },
+  { name: 'green lantern', img: 'green-lantern.jpg' },
+  { name: 'ironman', img: 'ironman.jpg' },
+  { name: 'spiderman', img: 'spiderman.jpg' },
+  { name: 'superman', img: 'superman.jpg' },
+  { name: 'the avengers', img: 'the-avengers.jpg' },
+  { name: 'thor', img: 'thor.jpg' }
 ];
 
-$(document).ready(function(){
-  var memoryGame = new MemoryGame(cards);
+// when DOM is ready
+$(document).ready(function () {
+
+  var game = new MemoryGame(cards);
+  let bckImage = "https://techsalesgroup.files.wordpress.com/2016/10/ironhack-logo-negro1.jpg"
   var html = '';
-  memoryGame.cards.forEach(function (pic, index) {
-    html += '<div class= "card" id="card_' + pic.name + '">';
-    html += '<div class="back"';
-    html += '    name="'       + pic.img +  '">';
-    html += '</div>';
-    html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + ') no-repeat">';
+
+  game.shuffleCards().forEach(function (pic) {  //el indice no es necesario y no se utiliza
+    //shuffleCards creo que deberia ser plural y no aceptar ninguna entrada puesto que ya se refiera a la propia bajara del juego.
+    html += '<div class="flex-aligner">';
+    html += '<div class="card-wrapper">';// id="card_' + pic.name + '">';
+    html += '<img class="card" src="img/' + pic.img + '" alt="">';
+    html += '<img class="back" id="' + pic.img + '" src="' + bckImage + '" name="' + pic.name + '" alt="">'; //utilizo el atributo name.
     html += '</div>';
     html += '</div>';
   });
 
-  // Add all the div's to the HTML
-  document.getElementById('memory_board').innerHTML = html;
-  // Bind the click event of each element to a function
-$('.back').on('click', function () {
-   
+  const updateScore = function (game) {
+    document.getElementById('pairs_clicked').innerHTML = game.pairsClicked.length.toString();
+    document.getElementById('pairs_guessed').innerHTML = game.pairsGuessed.length.toString();
+  }
+  //cambio algunas propieades de la distribucion de divs en el juego
+  //asi es mas fácil incorporar imágenes de diferentes tamaños en el futuro.
+  // el div de clase card-wrapper servirá para darle tamaño a cada caja.
+  // me parece curioso utilizar name que es mas utilizado para forms no?
+  // he intentado meter un div con clase flex aligner porque queria alinear con between space pero no me ha funcionado.
+
+    document.getElementById('flex-container').innerHTML = html;
+
+  $('.back').click(function () {
+
+    $(this).fadeOut(1000);
+    //$(this).css({'opacity':0});
+    //$(this).css("display","none");
+
+    if (game.finished())
+      alert("Congratulations!\nYou've finished the Game!\n\nPRESS F5 TO START A NEW GAME");
+    else {
+      if (game.bufferCard($(this))) {   //por cambiar el código html he tenido que cambiar la lógica de la clase un poco. añadiendo un metodo que almacena en el buffer solo cuando no hay repeticion
+                                        // de pulsado de carta
+        if (!game.checkIfPair({ name: game.pickedCards[0].name, img:game.pickedCards[0].id },
+                              { name: game.pickedCards[1].name, img:game.pickedCards[1].id })) {
+          game.pairsClicked[game.pairsClicked.length][0].fadeIn(600);
+          game.pairsClicked[game.pairsClicked.length][1].fadeIn(600);
+        }
+        updateScore(game);
+      }
+
+    }
+  });
 });
-});
+
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,44 @@
 // var MemoryGame = function (cards) {
 //   this.cards = cards;
 // };
+function MemoryGame(cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = [];
+  this.pairsGuessed = [];
+}
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCards = function () {
+  return _.shuffle(this.cards);
+};
+//creo que es mas eficiente escoger un numero de cartas aleatoriamente de un 
+//array que escojer un numero de cartas ordanadamente de un array ya aleatoriamente
+//ordenado. Por ello, en el futuro, si me da tiempo creo que hay que cambiar la lógica 
+//para hacerlo asi.
+//tambien he cambiado en que pickedCards no almacena objetos cards sino elementos esto es debido a que necesito almacenar la variable x e y para que no se repitan pulsaciones.
+//Esto yo creo que es un fallo grande de la implementacion. 
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked.push([firstCard, secondCard]);
+  this.pickedCards = [];
+  if (firstCard.name === secondCard.name) {
+    this.pairsGuessed.push([firstCard, secondCard]);
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.finished = function () {
+  return this.pairsGuessed.length === this.cards.length;
+};
 
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.bufferCard = function (e) {
+  if (this.pickedCards.length) {
+    if ((this.pickedCards[0].x != e[0].x) &&
+      (this.pickedCards[0].y != e[0].y))
+      return this.pickedCards.push(e); //2=true
+    return false;
+  }
+  return this.pickedCards.push(e) - 1; //1-1=0=false
+}
