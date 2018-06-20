e9a8f4787c1c6c70f4862ee8ab7128def5b3aa84
javieramirez90 <javier.ramirez@aiesec.net>
2018-06-19 21:33:04 -0500

    DE MEX Javier Ramírez Memory Game

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,52 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+ var MemoryGame = function (cards) {
+   this.cards = cards;
+   this.pickedCards = [""];
+   this.pairsClicked = 0;
+   this.pairsGuessed = 0;
+   this.win = false;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+ MemoryGame.prototype.shuffleCard = function (cardsArr) {
+   
 
-// };
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+   var currentIndex = cardsArr.length, temporaryValue, randomIndex;
+   // While there remain elements to shuffle...
+   while (0 !== currentIndex) {
+     
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    
+    // And swap it with the current element.
+    temporaryValue = cardsArr[currentIndex];
+    cardsArr[currentIndex] = cardsArr[randomIndex];
+    cardsArr[randomIndex] = temporaryValue;
+  }
+  return cardsArr;
 
-// }
+};
 
-// MemoryGame.prototype.finished = function () {
 
-// };
+
+ MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+   this.pairsClicked++;
+   if( firstCard === secondCard){
+     this.pairsGuessed++;
+     return true;
+   } else {
+     return false;
+   }
+  
+ }
+
+ MemoryGame.prototype.finished = function () {
+   
+   while (this.win == false ){
+     if(this.pairsGuessed === 12) {
+     win = true;
+     return win;
+   }
+  }
+
+ };
