98fc65c0a44db34b22b288c4d907c81a7e46f722
dragonINXS <davidmudd@outlook.com>
2018-06-20 19:18:04 -0400

    MIA David

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,36 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (allCards) {
+    this.cards = allCards;
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    this.currentPair = [];
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
 
-// };
+MemoryGame.prototype._shuffleCard = function () {
+    var _shuffledArray = [];
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    while (_shuffledArray.length < 24) {
+        var randNum = Math.floor(Math.random() * this.cards.length);
+        var randElement = this.cards[randNum];
+        _shuffledArray.push(randElement);
+        this.cards.splice(randNum, 1);
+    }
 
-// }
+    this.cards = _shuffledArray;
+    
+};
+    
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
-// MemoryGame.prototype.finished = function () {
+    this.pairsClicked++;
+    if (firstCard.attr("name") === secondCard.attr("name")) {
+        this.pairsGuessed++;
+        return true;
+    }
+    return false;
+};
+    
 
-// };
+MemoryGame.prototype.finished = function () {
+    return this.pairsGuessed >= 12;
+}
