0a1fd6e74a4ad7db349cbeee04af987731b65885
mabvmex <mabvmex@gmail.com>
2018-06-21 05:50:04 -0500

    aun con errores

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -31,20 +31,54 @@ $(document).ready(function(){
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
     html += '<div class="back"';
-    html += '    name="'       + pic.img +  '">';
+    html += '    name="'       + pic.img +  '">';   // Atributo que tiene el nombre de la imagen.
     html += '</div>';
     html += '<div class="front" ';
     html += 'style="background: url(img/' + pic.img + ') no-repeat">';
     html += '</div>';
     html += '</div>';
-  });
+  });;
 
+var pair =[];
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
 $('.back').on('click', function () {
-   
-});
-});
+  //Esto guarda el nombre en el arreglo para comparar.
+  //var name = $(this).attr('name');
+   pairs.push(this);
+   //Esto muestra la imagen.
+   $(this).toogleClass('back');
+   $(this).next().toogleClass('back');
 
+   //Esto checa si hay cartas volteadas
+   if(pairs.length === 2) {
+    $('memory_board').addClass('blocked');
+
+    if (memoryGame.checkIfPair(pairs[0].attr('name'),pairs[1].attr('name'))){
+    pairs=[];
+    $('#memory_board').removeClass('blocked');
+    update();
+    if(memoryGame.finished()){
+      $('h1').text("Bliss Ganó");
+    }
+    return;
+
+    setTimeout(function(){
+      pairs[0].toogleClass('back');
+      pairs[0].next().toogleClass('back');
+      pairs[1].toogleClass('back');
+      pairs[1].next().toogleClass('back');
+      pairs = [];
+      $('memoryBoard')
+    }, 500);
+    }
+  }
+});
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+function updateScore(){
+$('#pairs_clicked').text(memoryGame).text(memoryGame.pairClicked);
+  $('#pairs_guessed').text(memoryGame).text(memoryGame.pairGuessed);
+}
+})
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,31 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {  //clase
+    //Se almacena y contar el las cartas
+    this.cards = this.shuffleCard(cards);
+    this.pairGuessed =0;
+    this.pairClicked =0;
+  };
+ MemoryGame.prototype.shuffleCard = function (cardsArr) {
+cards.forEach(function(){
+     var random = Math.floor(Math.random() * cards.lenght); // Número random, shuffle de cartas
+     var card = cards.splice[random]//(random,1) //Sacar la carta del la función random.
+     var respaldo = cards [0];
+     cards[0]=card;
+     cards[random]=respaldo;
+     });
+     return cards;
+    }
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+ MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairClicked++;
+    if (firstCard===secondCard){
+        this.pairGuessed++;
+        return true;
+    } else {
+        return false;
+    }
+}
 
-// };
-
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+ MemoryGame.prototype.finished = function () {
+        return this.pairGuessed === this.cards.length/2;
+ }
+ 
