20e372389e7bf6d7e645abb9dd808ee8fca043e7
Xiaoyun <zxiaoyun98@gmail.com>
2018-08-08 00:55:24 +0200

    functioning, but need restructure

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,6 +27,7 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  memoryGame.shuffleCard(cards)
   var html = '';
   memoryGame.cards.forEach(function (pic, index) {
     html += '<div class= "card" id="card_' + pic.name + '">';
@@ -42,9 +43,93 @@ $(document).ready(function(){
   // Add all the div's to the HTML
   document.getElementById('memory_board').innerHTML = html;
   // Bind the click event of each element to a function
-$('.back').on('click', function () {
+  var firstCard;
+  var secondCard;
+  var loading = false;
+  var pairsClicked = document.getElementById('pairs_clicked');
+  var pairsGuessed = document.getElementById('pairs_guessed');
+  
+  $('.back').on('click', function () {
+    // var clickedCardName = $(this).attr('name');
+    
+    if (!loading) {
+      toggleCard($(this))
+      addCard($(this))
+    } else {
+      console.log('please wait')
+    }
    
+    // console.log(clickedCardName)
+  });
+ 
+  
+  function toggleCard(card) {
+    console.log(card)
+    card.addClass('front').removeClass('back')
+    card.next().addClass('back').removeClass('front')
+  }
+
+  function disappear(card) {
+    card.addClass('back').removeClass('front')
+    card.next().addClass('front').removeClass('back')
+  }
+  function addCard(card) {
+   
+    if (memoryGame.pickedCards.length === 0) {
+      firstCard = card;
+      // console.log('1', firstCard)
+      var cardName = card.attr('name');
+      memoryGame.pickedCards.push(cardName)
+      // console.log(cardName)
+    } else if (memoryGame.pickedCards.length === 1) {
+      secondCard = card;
+      // console.log(secondCard)
+      var cardName = card.attr('name');
+      memoryGame.pickedCards.push(cardName)
+      // console.log(cardName)
+
+    }
+    if (memoryGame.pickedCards.length === 2) {
+
+      var result = checkResult();
+      if (result) {
+        console.log('do nothing')
+        pairsGuessed.innerHTML = memoryGame.pairsGuessed;
+
+        reset()
+      } else {
+        console.log('disappear')
+        // firstCard.addClass('back')
+        console.log('2', firstCard)
+        loading = true;
+        setTimeout(function() {
+          console.log('interval')
+          disappear(firstCard)
+          disappear(secondCard)
+          reset()
+        }, 1000)
+
+        
+        
+        // toggleCard(firstCard)
+        // toggleCard(secondCard)
+      }
+    }
+  }
+  function checkResult() {
+    var result = memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1]);
+    pairsClicked.innerHTML = memoryGame.pairsClicked;
+    return result;
+  }
+
+  function reset() {
+    memoryGame.pickedCards = [];
+    firstCard = undefined;
+    secondCard = undefined;
+    loading = false;
+  }
 });
-});
+
+
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,43 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (array) {
+  
+    var copy = [], n = array.length, i;
+  
+    // While there remain elements to shuffle…
+    while (n) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * array.length);
+  
+      // If not already shuffled, move it to the new array.
+      if (i in array) {
+        copy.push(array[i]);
+        delete array[i];
+        n--;
+      }
+    }
+    // make a new array
+    this.cards = copy;
+  
+    return copy;
+  
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked ++;
+  // console.log('back', firstCard)
+  if (firstCard === secondCard) {
+    this.pairsGuessed ++;
+  }
+  return firstCard === secondCard;
+}
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+  return this.pairsGuessed === 12;
+};
