5e600e97e89fdf1f8e2a010171efe5a4e654d49e
katros <kasia.roszak@gmail.com>
2018-08-08 12:08:56 +0200

    not finished yet

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -1,50 +1,78 @@
 var cards = [
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' },
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' }
+    { name: 'aquaman', img: 'aquaman.jpg' },
+    { name: 'batman', img: 'batman.jpg' },
+    { name: 'captain america', img: 'captain-america.jpg' },
+    { name: 'fantastic four', img: 'fantastic-four.jpg' },
+    { name: 'flash', img: 'flash.jpg' },
+    { name: 'green arrow', img: 'green-arrow.jpg' },
+    { name: 'green lantern', img: 'green-lantern.jpg' },
+    { name: 'ironman', img: 'ironman.jpg' },
+    { name: 'spiderman', img: 'spiderman.jpg' },
+    { name: 'superman', img: 'superman.jpg' },
+    { name: 'the avengers', img: 'the-avengers.jpg' },
+    { name: 'thor', img: 'thor.jpg' },
+    { name: 'aquaman', img: 'aquaman.jpg' },
+    { name: 'batman', img: 'batman.jpg' },
+    { name: 'captain america', img: 'captain-america.jpg' },
+    { name: 'fantastic four', img: 'fantastic-four.jpg' },
+    { name: 'flash', img: 'flash.jpg' },
+    { name: 'green arrow', img: 'green-arrow.jpg' },
+    { name: 'green lantern', img: 'green-lantern.jpg' },
+    { name: 'ironman', img: 'ironman.jpg' },
+    { name: 'spiderman', img: 'spiderman.jpg' },
+    { name: 'superman', img: 'superman.jpg' },
+    { name: 'the avengers', img: 'the-avengers.jpg' },
+    { name: 'thor', img: 'thor.jpg' }
 ];
 
-$(document).ready(function(){
-  var memoryGame = new MemoryGame(cards);
-  var html = '';
-  memoryGame.cards.forEach(function (pic, index) {
-    html += '<div class= "card" id="card_' + pic.name + '">';
-    html += '<div class="back"';
-    html += '    name="'       + pic.img +  '">';
-    html += '</div>';
-    html += '<div class="front" ';
-    html += 'style="background: url(img/' + pic.img + ') no-repeat">';
-    html += '</div>';
-    html += '</div>';
-  });
+$(document).ready(function() {
+    var memoryGame = new MemoryGame(cards);
+    memoryGame.cards = memoryGame.shuffleCard();
+    var html = '';
+    memoryGame.cards.forEach(function(pic, index) {
+        html += '<div class= "card" id="card_' + pic.name + '">';
+        html += '<div class="back"';
+        html += '    name="' + pic.img + '">';
+        html += '</div>';
+        html += '<div class="front" ';
+        html += 'style="background: url(img/' + pic.img + ') no-repeat">';
+        html += '</div>';
+        html += '</div>';
+    });
 
-  // Add all the div's to the HTML
-  document.getElementById('memory_board').innerHTML = html;
-  // Bind the click event of each element to a function
-$('.back').on('click', function () {
-   
+    // Add all the div's to the HTML
+    document.getElementById('memory_board').innerHTML = html;
+    // Bind the click event of each element to a function
+
+    $('.back').on('click', function() {
+        var card = $(this).attr('name');
+        console.log('CANOPEN', memoryGame.canOpenNewCard());
+        if (memoryGame.canOpenNewCard()) {
+            memoryGame.registerCard(card);
+            var pairsClicked = memoryGame.getPairsClicked();
+            $('#pairs_clicked').html(pairsClicked);
+            var front = $(this).siblings();
+            $(this).removeClass('back');
+            $(this).addClass('front');
+            front.removeClass('front');
+            front.addClass('back');
+        }
+
+        setTimeout(
+            function() {
+                if (memoryGame.checkIfPair()) {
+                    var pairsGuessed = memoryGame.getPairsGuessed();
+                    $('#pairs_guessed').html(pairsGuessed);
+                }
+
+                memoryGame.unregisterCards();
+
+                $(this).removeClass('front');
+                $(this).addClass('back');
+                front.removeClass('back');
+                front.addClass('font');
+            }.bind(this),
+            3000
+        );
+    });
 });
-});
-
-
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,71 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function(cards) {
+    this.cards = cards;
+    this.firstCard = null;
+    this.secondCard = null;
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function() {
+    this.cards = JSON.parse(JSON.stringify(this.cards));
+    var copy = [],
+        n = this.cards.length,
+        i;
+    // While there remain elements to shuffle…
+    while (n) {
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * this.cards.length);
 
-// };
+        // If not already shuffled, move it to the new array.
+        if (i in this.cards) {
+            copy.push(this.cards[i]);
+            delete this.cards[i];
+            n--;
+        }
+    }
+    return copy;
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.checkIfPair = function() {
+    console.log('CHECKPAIR', this.firstCard, this.secondCard);
+    if (this.firstCard != null && this.secondCard != null && this.firstCard === this.secondCard) {
+        this.pairsGuessed += 1;
+        return true;
+    }
 
-// }
+    return false;
+};
 
-// MemoryGame.prototype.finished = function () {
+MemoryGame.prototype.registerCard = function(card) {
+    console.log(this.firstCard, this.secondCard);
+    if (this.firstCard === null) {
+        this.firstCard = card;
+    } else if (this.secondCard === null) {
+        this.secondCard = card;
+    }
 
-// };
+    if (this.firstCard !== null && this.secondCard !== null) {
+        this.pairsClicked += 1;
+    }
+};
+
+MemoryGame.prototype.getPairsClicked = function() {
+    return this.pairsClicked;
+};
+
+MemoryGame.prototype.getPairsGuessed = function() {
+    return this.pairsGuessed;
+};
+
+MemoryGame.prototype.canOpenNewCard = function() {
+    console.log('CANOPEN:', this.secondCard === null, this.secondCard);
+    return this.secondCard === null;
+};
+
+MemoryGame.prototype.unregisterCards = function() {
+    this.firstCard = null;
+    this.secondCard = null;
+    console.log('UNREGISTERING', this.firstCard, this.secondCard);
+};
+
+MemoryGame.prototype.finished = function() {};
