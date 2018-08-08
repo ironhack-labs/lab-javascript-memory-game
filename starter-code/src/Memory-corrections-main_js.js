d3105d616152579f74650d88aa57a3cf0e40bcb7
GitGut0110 <jsimms0110@gmail.com>
2018-08-08 15:42:52 +0200

    max's correction limit the clicking by changing class

---
+++ starter-code/src/Memory-corrections-main_js.js
@@ -0,0 +1,75 @@
+var cards = [
+  { name: 'aquaman',         img: 'aquaman.jpg' },
+  { name: 'batman',          img: 'batman.jpg' },
+  { name: 'captain america', img: 'captain-america.jpg' },
+  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
+  { name: 'flash',           img: 'flash.jpg' },
+  { name: 'green arrow',     img: 'green-arrow.jpg' },
+  { name: 'green lantern',   img: 'green-lantern.jpg' },
+  { name: 'ironman',         img: 'ironman.jpg' },
+  { name: 'spiderman',       img: 'spiderman.jpg' },
+  { name: 'superman',        img: 'superman.jpg' },
+  { name: 'the avengers',    img: 'the-avengers.jpg' },
+  { name: 'thor',            img: 'thor.jpg' },
+  { name: 'aquaman',         img: 'aquaman.jpg' },
+  { name: 'batman',          img: 'batman.jpg' },
+  { name: 'captain america', img: 'captain-america.jpg' },
+  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
+  { name: 'flash',           img: 'flash.jpg' },
+  { name: 'green arrow',     img: 'green-arrow.jpg' },
+  { name: 'green lantern',   img: 'green-lantern.jpg' },
+  { name: 'ironman',         img: 'ironman.jpg' },
+  { name: 'spiderman',       img: 'spiderman.jpg' },
+  { name: 'superman',        img: 'superman.jpg' },
+  { name: 'the avengers',    img: 'the-avengers.jpg' },
+  { name: 'thor',            img: 'thor.jpg' }
+];
+
+var g;
+$(document).ready(function(){
+  g = new MemoryGame(cards);
+  var html = '';
+  g.cards.forEach(function (pic, index) {
+    html += '<div class= "card" id="card_' + pic.name + '">';
+    html += '<div class="back"';
+    html += '    name="'       + pic.img +  '">';
+    html += '</div>';
+    html += '<div class="front" ';
+    html += 'style="background: url(img/' + pic.img + ') no-repeat">';
+    html += '</div>';
+    html += '</div>';
+  });
+
+  // Add all the div's to the HTML
+  $('#memory_board').html(html);
+  
+  // Bind the click event of each element to a function
+  $('.back').on('click', function () {
+    if (g.pickedCards.length >= 2)
+      return;
+
+    g.pickedCards.push($(this).parent().attr('id'));
+    $(this).parent().addClass('picked')
+    $(this).parent().children().toggleClass("front back");
+
+    if (g.pickedCards.length == 2) {
+      if (!g.checkIfPair(g.pickedCards[0], g.pickedCards[1])) {
+        setTimeout(function() {
+          $('.card.picked').children().toggleClass("front back");
+          $('.card.picked').removeClass('picked');
+          g.pickedCards = [];
+        }, 2000);
+      }
+      else {
+        $('.card.picked').removeClass('picked');
+        g.pickedCards = [];
+      }
+      updateScore(g);
+    }
+  });
+});
+
+function updateScore(g) {
+  $('#pairs_clicked').text(g.pairsClicked);
+  $('#pairs_guessed').text(g.pairsGuessed);
+}
