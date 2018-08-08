61cec5b44b046693f3c8053e20200d381390dc88
Kaschief Johnson <kaschiefj@gmail.com>
2018-08-08 11:52:20 +0200

    unfinished git commit -m unfinished
    git status

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,47 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cardsArray) {
+  this.cards = cardsArray;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+}
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (arr) {
 
-// };
+  function shuffle(arr) {
+    array = JSON.parse(JSON.stringify(array))
+    var copy = [], n = array.length, i;
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    // While there remain elements to shuffle…
+    while (n) {
 
-// }
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * array.length);
 
-// MemoryGame.prototype.finished = function () {
+      // If not already shuffled, move it to the new array.
+      if (i in array) {
+        copy.push(array[i]);
+        delete array[i];
+        n--;
+      }
+    }
 
-// };
+    return copy;
+  }
+}
+
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++
+  if (firstCard === secondCard) {
+    this.pairsGuessed++
+  }
+  if (firstCard === secondCard) {
+    return true
+  } else return false
+}
+
+MemoryGame.prototype.finished = function () {
+
+  if (this.pairsGuessed === 12) {
+    return true
+  } else return false
+}
