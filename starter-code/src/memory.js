61047ba065b54ffd4d419b7706618a08898f8289
Gonzalo Quiroz <kiyegoo@gmail.com>
2018-08-18 10:45:44 -0500

    Finalizacion del ejercicio

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,38 @@
-var MemoryGame = function (cards) {
+var MemoryGame = function(cards) {
   this.cards = cards;
+  this.pickedCards = {};
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function() {
+  let newDeck = this.cards;
+
+  for (let i = newDeck.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = newDeck[i];
+    newDeck[i] = newDeck[j];
+    newDeck[j] = x;
+  }
+  //console.log(newDeck);
+
+  return newDeck;
 };
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-}
+MemoryGame.prototype.checkIfPair = function(firstCard, secondCard) {
+  if (firstCard && secondCard) {
+    this.pairsClicked += 1;
+    if (firstCard == secondCard) {
+      this.pairsGuessed += 1;
+      return true;
+    }
+    return false;
+  }
+};
 
-MemoryGame.prototype.isFinished = function () {
+MemoryGame.prototype.isFinished = function() {
+  if (this.pairsGuessed === this.cards.length / 2) {
+    return true;
+  }
+  return false;
 };
