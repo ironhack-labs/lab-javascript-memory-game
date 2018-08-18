a2bb119a3494d31c62ad86f107d29438371e2e84
Vermont <vermont.ironhack@gmail.com>
2018-08-18 02:59:21 -0500

    Finished :smile:

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -28,20 +28,51 @@ var cards = [
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
+  memoryGame.shuffleCards();
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
   });
-
+  var backClick = 0;
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
   // Bind the click event of each element to a function
   $('.back').click(function () {
     // TODO: write some code here
+    backClick++;
+    if (backClick === 1){
+      fistCard = $(this).parent();
+      fistCard.children().toggleClass('front back');
+    } else if (backClick === 2) {
+      secondCard = $(this).parent();
+      secondCard.children().toggleClass('front back');
+      backClick = 0;            
+      if (memoryGame.checkIfPair(fistCard.attr('data-card-name'),
+          secondCard.attr('data-card-name'))){
+        $('#pairs_guessed').html(memoryGame.pairsGuessed);
+      } else {
+        setTimeout(function() {
+          fistCard.children().toggleClass('front back');
+          secondCard.children().toggleClass('front back');
+        }, 300);        
+      }      
+      $('#pairs_clicked').html(memoryGame.pairsClicked);
+      if (memoryGame.isFinished()){        
+        setTimeout(function(){
+          if (memoryGame.pairsClicked == 12){
+            alert(`Wow, What a Crack, You made a perfech match!!!!\n\n 12 pairs in ${memoryGame.pairsClicked} attempts!`);
+          } else if (memoryGame.pairsClicked < 16){
+            alert(`Great, You are rocking!\n\n 12 pairs in ${memoryGame.pairsClicked} attempts!`);
+          } else if (memoryGame.pairsClicked < 21){
+            alert(`Good, Nice shot!\n\n 12 pairs in ${memoryGame.pairsClicked} attempts!`);
+          } else{
+            alert(`Come on!, I know you can do it better!!\n\n 12 pairs in ${memoryGame.pairsClicked} attempts!`);
+          }          
+        },100);
+      }
+    }      
   });
 });
-
-
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,32 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+  this.pickedCards = [];
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  let cardsNumber = this.cards.length;
+  let newCards = [];
+  for(i=0; i< cardsNumber; i++){
+    newCards.push(this.cards.splice(Math.floor(Math.random()*this.cards.length),1)[0]);
+  }
+  this.cards = newCards;
+  return;
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++;
+  if(firstCard === secondCard){
+    this.pairsGuessed++;
+    return true;
+  }
+  return false;
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if ( this.pairsGuessed < this.cards.length/2){
+    return false;    
+  }
+  return true;
 };
