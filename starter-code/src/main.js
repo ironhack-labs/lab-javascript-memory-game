ae442fa0b262a04fdb1a4a2fec3451de08f1fd68
Schaum <schaumgarten@gmail.com>
2018-08-18 15:44:02 -0500

    listo

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -34,13 +34,44 @@ $(document).ready(function(){
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
   });
-
+  
   // Add all the div's to the HTML
   $('#memory_board').html(html);
-
+  
   // Bind the click event of each element to a function
   $('.back').click(function () {
-    // TODO: write some code here
+    $(this).toggleClass("back front");
+    $(this).next().toggleClass("back front");
+    memoryGame.pickedCards.push(this);
+    if (memoryGame.pickedCards.length == 2){
+      var bool = memoryGame.checkIfPair($(memoryGame.pickedCards[0]).attr("name"),$(memoryGame.pickedCards[1]).attr("name"))
+     //
+      
+      $('#pairs_clicked').html(memoryGame.pairsClicked);     
+     
+    if (bool == true){      
+      $('#pairs_guessed').html(memoryGame.pairsGuessed);
+      memoryGame.pickedCards = [];
+    } else {
+      setTimeout(function(){
+        $(memoryGame.pickedCards[0]).toggleClass("front back");
+        $(memoryGame.pickedCards[0]).next().toggleClass("front back");
+        $(memoryGame.pickedCards[1]).toggleClass("front back");
+        $(memoryGame.pickedCards[1]).next().toggleClass("front back");
+        console.log("perdiste")
+        memoryGame.pickedCards = [];
+      },1000);
+     // memoryGame.pickedCards = [];
+    } 
+    
+    }
+    if (memoryGame.isFinished() == true){
+      console.log("Fin de juego");
+      $('div.front').toggleClass("back front");
+      $('div.back').next().toggleClass("back front");
+    }
+    
+    
   });
 });
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,39 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
-MemoryGame.prototype.shuffleCards = function () {
-};
+MemoryGame.prototype.shuffleCards = function(cards) {
+  var newArrayOfCards = [];
+  var fixedLength = cards.length; 
+  for (i=0; i<fixedLength; i++){
+    var chooseRandomCard = Math.floor(Math.random()*(cards.length));
+    
+    newArrayOfCards.push(cards[chooseRandomCard]);
+    
+    cards.splice(chooseRandomCard, 1);
+    }
+    return newArrayOfCards;
+}
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++;
+  if (firstCard == secondCard){
+    this.pairsGuessed++;
+    return true;
+  } else {
+    return false; 
+  }
+
 }
 
 MemoryGame.prototype.isFinished = function () {
+  let pairs = this.cards.length/2;
+  if (this.pairsGuessed < pairs ){
+    return false;
+  } else {
+    return true;
+  }
 };
