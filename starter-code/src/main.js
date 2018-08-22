6eb3d3ed6cad1eec0ced5838e9359d1e063da554
Eran Usha <eusha5@gmail.com>
2018-08-22 17:10:37 -0400

    Initial Commit

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,6 +27,7 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  memoryGame.shuffleCards(memoryGame.cards)
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
@@ -38,9 +39,36 @@ $(document).ready(function(){
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
+
   // Bind the click event of each element to a function
   $('.back').click(function () {
     // TODO: write some code here
+    $(this).toggle();
+    $(this).siblings().toggle();
+
+    memoryGame.pickedCards.push($(this));
+
+    console.log(memoryGame.pickedCards);
+    if(memoryGame.pickedCards.length === 2) {
+      $('.back').addClass('blocked');
+      if(memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1]) !== true) {
+        setTimeout(() => {
+        memoryGame.pickedCards[0].toggle();
+        memoryGame.pickedCards[0].siblings().toggle();
+        memoryGame.pickedCards[1].toggle();
+        memoryGame.pickedCards[1].siblings().toggle();
+        memoryGame.pickedCards = [];
+        $('.back').removeClass('blocked');
+        },1000)
+      } else {
+        memoryGame.pickedCards = [];
+        $('.back').removeClass('blocked');
+      }
+    }
+    memoryGame.updateScores();
+    setTimeout(() => {
+      memoryGame.getResult();
+    },2000)
   });
 });
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,53 @@
-var MemoryGame = function (cards) {
-  this.cards = cards;
-};
+class MemoryGame {
+  constructor(cards) {
+    this.cards = cards
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+  }
 
-MemoryGame.prototype.shuffleCards = function () {
-};
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  shuffleCards(theDeck){
+    let cardsArray = theDeck.length;
+    let tempCard, i;
+    while(cardsArray) {
+      i = Math.floor(Math.random() * cardsArray--);
+      
+      tempCard = theDeck[cardsArray];
+      theDeck[cardsArray] = theDeck[i];
+      theDeck[i] = tempCard;
+    }
+    this.cards = theDeck
+  }
+
+  checkIfPair(firstCard,secondCard){
+    this.pairsClicked += 1;
+  if(firstCard.attr('name') === secondCard.attr('name')) {
+    this.pairsGuessed += 1
+    console.log("true its a pair!");
+    return true;
+  } else {
+    console.log("false its not a pair!");
+    return false;
+  }
+  }
+
+  isFinished() {
+  if(this.pairsGuessed === 8) {
+      return true;
+  } else {
+      return false;
+    }
+  }
+
+  updateScores(){
+    $('#pairs_clicked').html(this.pairsClicked);
+    $('#pairs_guessed').html(this.pairsGuessed);
+  }
+
+  getResult() {
+    if(this.pairsGuessed === 12){
+      prompt ('WooHoo! Now tell me why there are DC Logos in a marvel game :(');
+    }
+  }
 }
-
-MemoryGame.prototype.isFinished = function () {
-};
