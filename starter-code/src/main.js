770424e9b6994dd404267bc697b78e7f8d3bea9b
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-08-22 12:20:45 -0400

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,8 +25,11 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
+
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  memoryGame.shuffleCards();
+
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
@@ -40,8 +43,67 @@ $(document).ready(function(){
 
   // Bind the click event of each element to a function
   $('.back').click(function () {
-    // TODO: write some code here
+
+    if(memoryGame.pickedCards.length === 2) {
+      return;
+    }
+
+      let $siblDiv = $(this).next();
+      let $pairsClicked = $('#pairs_clicked');
+      let $pairsGuessed = $('#pairs_guessed');
+
+      if(!(memoryGame.pickedCards.length >= 2)) {
+          memoryGame.pickedCards.push($(this));
+
+          $(this).toggleClass('back');
+          $(this).toggleClass('front');
+
+          $siblDiv.toggleClass('front');
+          $siblDiv.toggleClass('back');
+      }
+
+      if(memoryGame.pickedCards.length === 2) {
+
+          let checkRes = memoryGame.checkIfPair(...(memoryGame.pickedCards.map(card => {
+            return card.attr('name').replace('.jpg', ' ').trim();
+          })));
+          $pairsClicked.text(memoryGame.pairsClicked);
+          $pairsGuessed.text(memoryGame.pairsGuessed);
+
+          if(memoryGame.isFinished()) {
+              alert("YOU WIN THIS GAME!!!!");
+          }
+
+          if(!checkRes) {
+
+            setTimeout(() => {
+                let $card1 = memoryGame.pickedCards[0];
+                let $card2 = memoryGame.pickedCards[1];
+                let $siblCard1 = $card1.next();
+                let $siblCard2 = $card2.next();
+
+                $card1.toggleClass('back');
+                $card1.toggleClass('front');
+                $siblCard1.toggleClass('back');
+                $siblCard1.toggleClass('front');
+
+                $card2.toggleClass('back');
+                $card2.toggleClass('front');
+                $siblCard2.toggleClass('back');
+                $siblCard2.toggleClass('front');
+
+                memoryGame.pickedCards.length = 0;
+
+            }, 1500);
+
+          } else {
+              memoryGame.pickedCards.length = 0;
+          }
+
+      }
   });
 });
+
+
 
 
