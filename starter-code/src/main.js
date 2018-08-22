09cd95f74d2f5a60b3aad9048f10e5f504fa8bcf
Sat Gavassa <satgavassa@gmail.com>
2018-08-22 15:47:00 -0400

    finished

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,8 +25,20 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
+var backOfAllCards = $('.back')
+var frontOfAllCards = $('.front')
+// function loadClicks() {
+// for (let i = 0; i < backOfAllCards.length ; i++) {
+//   backOfAllCards.eq(i).click(function(e){
+//   $(this).siblings(".front").toggle();
+//   $(this).toggle();
+//   })
+// }
+// }
+
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  // memoryGame.shuffleCards();
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
@@ -41,7 +53,44 @@ $(document).ready(function(){
   // Bind the click event of each element to a function
   $('.back').click(function () {
     // TODO: write some code here
-  });
+    $(this).siblings(".front").toggle();
+    $(this).toggle();
+    memoryGame.pickedCards.push($(this))
+    if (memoryGame.pickedCards.length==2) {
+      $('.back:visible').addClass('blocked')
+      let gameStatus =memoryGame.checkIfPair(memoryGame.pickedCards[0].attr('name'),memoryGame.pickedCards[1].attr('name'));
+        if(gameStatus) {
+          // memoryGame.pickedCards[0].addClass('blocked');
+          // memoryGame.pickedCards[1].addClass('blocked');
+          memoryGame.pickedCards =[];
+          $('.back:visible').removeClass('blocked')
+        } else {
+            setTimeout(function(){
+            memoryGame.pickedCards[0].siblings(".front").toggle();
+            memoryGame.pickedCards[0].toggle();
+            memoryGame.pickedCards[1].siblings(".front").toggle();
+            memoryGame.pickedCards[1].toggle();
+            memoryGame.pickedCards =[];
+            $('.back:visible').removeClass('blocked')
+            }, 2000);
+          }
+    }
+    let displayClicks = $('#pairs_clicked');   
+    displayClicks.html(memoryGame.pairsClicked);
+    let displayGuesses = $('#pairs_guessed');   
+    displayGuesses.html(memoryGame.pairsGuessed);
+  });  
 });
 
-
+// runTheGame() {
+// while (!this.isFinished()) {
+// memoryGame.pickedCards.push($('.front:visible')) = ;
+// if (this.pickedCards.length===2) {
+//   let gameStatus =this.checkIfPair();
+//     if (gameStatus) {
+//       this.pickedCards.addClass('blocked')
+//     } 
+//     }
+//   }
+// }
+// }
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,50 @@
-var MemoryGame = function (cards) {
-  this.cards = cards;
-};
-
-MemoryGame.prototype.shuffleCards = function () {
-};
-
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+class MemoryGame{
+  constructor(arrayOfCards){
+    this.cards = arrayOfCards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    this.shuffleCards();
+  }
+  shuffleCards() {
+    for (let i =0; i <this.cards.length; i++) {
+      let j = Math.floor(Math.random()*(i+1));
+      let a = this.cards[i];
+      let b = this.cards[j];
+      this.cards[i]= b;
+      this.cards[j]= a;
+    }
+    return this.cards;
+  }
+  checkIfPair(firstCard, secondCard){
+    this.pairsClicked++;
+    if (firstCard === secondCard) {
+      this.pairsGuessed++;
+      return true
+    } else {return false}
+  }
+  isFinished(){
+    if (this.pairsGuessed ===this.cards.length/2){
+      alert('You Won!')
+      return true
+    } else {return false}
+  }
 }
 
-MemoryGame.prototype.isFinished = function () {
-};
+
+
+
+
+// var MemoryGame = function (cards) {
+//   this.cards = cards;
+// };
+
+
+// MemoryGame.prototype.shuffleCards = function () {
+// };
+
+// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+// }
+
+// MemoryGame.prototype.isFinished = function () {
+// };
