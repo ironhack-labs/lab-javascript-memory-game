d1f3dc1e5570700da5ba1885bc1a12f0884c68fa
agdwm <almudenaguerraswm@gmail.com>
2018-08-28 19:46:50 +0200

    shuffleCards method done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,13 +1,22 @@
 var MemoryGame = function (cards) {
-	this.cards = [2, 11, 37, 42];
+	this.cards = cards;
 	this.pickedCards = [];
 	this.pairsClicked = 0;
 	this.pairsGuessed = 0;
 };
 
-MemoryGame.prototype.shuffleCards = function (cards) {
+MemoryGame.prototype.shuffleCards = function () {
+	var i = this.cards.length, j, temp;
+	var currentCards = this.cards;
 
+	while(--i > 0){
+		j = Math.floor(Math.random() * (i+1));
+		temp = currentCards[j];
+		currentCards[j] = currentCards[i];
+		currentCards[i] = temp;
+	}
 
+	this.cards = currentCards;
 }
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {}
