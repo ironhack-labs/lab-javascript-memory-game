c9b76a004f5a76342ca56d9a141a07bb94447f2b
Evenstarw1 <lauracorrallopez@gmail.com>
2018-08-28 19:43:41 +0200

    first commit

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,34 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  for (var i = this.cards.length -1; i > 0; i--) {
+    var j = Math.floor(Math.random() * i);
+    var tmp = this.cards[i];
+    this.cards[i] = this.cards[j];
+    this.cards[j]= tmp;
+  }
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+if (firstCard === secondCard){
+    this.pairsClicked++;
+    this.pairsGuessed++;
+    return true;
+  } else {
+    this.pairsClicked++;
+    return false;
 }
+};
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.pairsGuessed == 8) {
+    return true;
+  } else {
+    return false;
+  }
 };
