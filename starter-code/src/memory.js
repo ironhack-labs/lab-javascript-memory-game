5cb0a5dc12d1c0dd2415b7181ba6ee465166b5bb
agdwm <almudenaguerraswm@gmail.com>
2018-08-28 19:29:26 +0200

    Created the Object MemoryGame

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,15 @@
 var MemoryGame = function (cards) {
-  this.cards = cards;
+	this.cards = [2, 11, 37, 42];
+	this.pickedCards = [];
+	this.pairsClicked = 0;
+	this.pairsGuessed = 0;
 };
 
-MemoryGame.prototype.shuffleCards = function () {
-};
+MemoryGame.prototype.shuffleCards = function (cards) {
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+
 }
 
-MemoryGame.prototype.isFinished = function () {
-};
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {}
+
+MemoryGame.prototype.isFinished = function () {};
