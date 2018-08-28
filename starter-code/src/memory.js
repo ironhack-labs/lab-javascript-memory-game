2f772e04f92bb0a51af01fd9f0f54ed6d4742823
Jhon Scarzo <jhon753951@gmail.com>
2018-08-28 22:42:28 +0200

    V3

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,31 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
-};
-
-MemoryGame.prototype.shuffleCards = function () {
-};
-
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pickedcards = [];
+  this.pairsClicked = [];
+  this.pairsGuessed = []
 }
 
-MemoryGame.prototype.isFinished = function () {
+MemoryGame.prototype.shuffleCards = function () {
+
 };
+
+function shuffle(array) {
+  var m = array.length;
+  var t = 0;
+  var i = 0;
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {}
+
+MemoryGame.prototype.isFinished = function () {};
