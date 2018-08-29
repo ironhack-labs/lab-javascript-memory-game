33a743d454f80a09b13519e4a741b86aa6c5f3b6
Enured <cristofer.segundo@gmail.com>
2018-08-28 20:02:17 -0500

    done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,47 @@
+function MemoryGame(cards){
+  this.cards = cards
+  this.guessed = 0
+  this.clicked = 0
+}
+
 var MemoryGame = function (cards) {
   this.cards = cards;
+  
 };
 
 MemoryGame.prototype.shuffleCards = function () {
-};
 
+};
+var guessed=0;
+var clicked=0;
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  clicked++;
+  this.clicked=clicked;
+  $('#pairs_clicked').text(clicked);
+  if(firstCard === secondCard){
+    guessed++;
+    $('#pairs_guessed').text(guessed)
+    this.guessed=guessed;
+    return true;
+  }
+
+
+  else{
+    return false;
+  }
+
 }
 
 MemoryGame.prototype.isFinished = function () {
+  var self = this.guessed 
+  if(self == 12){
+    return true;
+  }
+  return false;
+  /*
+  if($('#pairs_guessed').text==='1'){
+    $('h1').text('YOU WON!')
+  }*/
 };
+
+
