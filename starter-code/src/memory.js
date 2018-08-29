6ae215584c57edbec0b6132dc14bcb0e95501f6c
David Sirit <davidsirit@yahoo.com>
2018-08-29 02:13:10 +0200

    constructor and shuffle method done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,20 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  var randomizedDeck = [];
+  var array = this.cards.slice();
+  while (array.length !== 0) {
+    var rIndex = Math.floor(array.length * Math.random());
+    randomizedDeck.push(array[rIndex]);
+    array.splice(rIndex, 1);
+  }
+  this.cards = randomizedDeck;
+  return ;
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
