a67332ae8d8ce66951a12d1ae416c76471cd2f13
Jorge Rybar <jorgerybar@gmail.com>
2018-08-29 13:29:56 -0500

    done!

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -1,5 +1,6 @@
 var cards = [
   { name: 'aquaman',         img: 'aquaman.jpg' },
+  { name: 'aquaman',         img: 'aquaman.jpg' },
   { name: 'batman',          img: 'batman.jpg' },
   { name: 'captain america', img: 'captain-america.jpg' },
   { name: 'fantastic four',  img: 'fantastic-four.jpg' },
@@ -11,7 +12,6 @@ var cards = [
   { name: 'superman',        img: 'superman.jpg' },
   { name: 'the avengers',    img: 'the-avengers.jpg' },
   { name: 'thor',            img: 'thor.jpg' },
-  { name: 'aquaman',         img: 'aquaman.jpg' },
   { name: 'batman',          img: 'batman.jpg' },
   { name: 'captain america', img: 'captain-america.jpg' },
   { name: 'fantastic four',  img: 'fantastic-four.jpg' },
@@ -27,21 +27,59 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  memoryGame.shuffleCards();
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
-    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+    html += '  <div class="front" style="background: url(./img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
   });
 
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
+
   // Bind the click event of each element to a function
-  $('.back').click(function () {
-    // TODO: write some code here
+
+  var prevCard = null;
+  $('.back').click(function (e) {
+
+    $(this).attr("class", "front")
+    $(this).next().attr("class", "back")
+  
+    var isPair = false;
+
+    if (prevCard !==null){
+      isPair = memoryGame.checkIfPair($(this).attr("name"),prevCard.attr("name"));
+      console.log($(this).attr("name"),prevCard.attr("name"))
+      console.log(isPair);
+      if (!isPair ){
+        setTimeout (() => {
+          $(this).attr("class", "back")
+          $(this).next().attr("class", "front")
+          //add flip to first card
+          prevCard.attr("class", "back")
+          prevCard.next().attr("class", "front")
+          prevCard = null;
+        }, 1000 )  
+      } else {
+          
+          $('#pairs_guessed').text(memoryGame.pairsGuessed)
+          prevCard = null;
+      }
+      $('#pairs_clicked').text(memoryGame.pairsClicked)
+      
+    } else {
+      prevCard = $(this);
+    }
+    if(memoryGame.isFinished()){
+      $('h1').html('YOU WON!')
+    } 
+
   });
+
+
 });
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,32 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = []
+  this.pairsClicked = 0;
+  this.pairsGuessed =0;
+  //shuffleCards();
+
 };
 
 MemoryGame.prototype.shuffleCards = function () {
-};
+    for (let i = this.cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+}
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++; 
+  if(firstCard === secondCard){
+    this.pairsGuessed++;
+    return true;
+  }
+  return false;
 }
 
 MemoryGame.prototype.isFinished = function () {
+  console.log(this.pairsGuessed)
+  console.log(this.cards.length/2)
+  if(this.pairsGuessed === this.cards.length /2 )
+    return true;
+  return false;
 };
