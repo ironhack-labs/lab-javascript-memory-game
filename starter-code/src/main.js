69da94ab317ff5c43db7b91d9e280b531fea4e31
cesararmada <cesar.armada@gmail.com>
2018-08-29 02:10:58 +0200

    Done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,6 +27,7 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  memoryGame.shuffleCards()
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
@@ -38,10 +39,49 @@ $(document).ready(function(){
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
+  function score() {
+    $("#pairs_clicked").html(memoryGame.pairsClicked);
+    $("#pairs_guessed").html(memoryGame.pairsGuessed);
+  }
   // Bind the click event of each element to a function
   $('.back').click(function () {
+    $(this).toggleClass("front back");
+    $(this).next().toggleClass("back front");
+    memoryGame.pickedCards.push($(this).attr("name"));
+    
+    if (memoryGame.pickedCards.length == 2){
+    var checkpair = memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1])
+    var firstElement = memoryGame.pickedCards[0];
+    var secondElement = memoryGame.pickedCards[1];
+      if (checkpair == false){
+        setTimeout(function() {
+        $("[name='" + firstElement + "']").removeClass("front");
+        $("[name='" + firstElement + "']").addClass("back");
+        $("[name='" + firstElement + "']").next().removeClass("back");
+        $("[name='" + firstElement + "']").next().addClass("front");
+        $("[name='" + secondElement + "']").removeClass("front");
+        $("[name='" + secondElement + "']").addClass("back");
+        $("[name='" + secondElement + "']").next().removeClass("back");
+        $("[name='" + secondElement + "']").next().addClass("front");  
+        },800)
+        memoryGame.pickedCards = [];
+        memoryGame.isFinished()
+     
+    } else {
+      memoryGame.pickedCards = [];
+      
+    }
+  }
+    score();
+    if (memoryGame.isFinished() == true){
+      window.alert("YOU WON")
+    }
+
     // TODO: write some code here
   });
+ 
+  
+
 });
 
 
