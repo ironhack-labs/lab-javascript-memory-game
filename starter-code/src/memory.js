7506fae6fbce8d931d07f664ac1777f7e29110f4
almaady <adriana.lopave@gmail.com>
2018-08-28 19:24:59 -0500

    done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,31 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = []
+  this.pairsClicked = 0;
+  this.pairsGuessed =0;
+
 };
+
+
 
 MemoryGame.prototype.shuffleCards = function () {
-};
+    for (let i = this.cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+}
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++; 
+  if(firstCard === secondCard){
+    this.pairsGuessed++;
+    return true;
+  }
+  return false;
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if(this.pairsGuessed === this.cards.length /2 )
+    return true;
+  return false;
 };
