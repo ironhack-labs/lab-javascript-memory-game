1f9fd71e7a6b9870f7d05cd6e6a4800c5882fee4
aemartos <aemartos@gmail.com>
2018-09-27 20:58:59 +0200

    Jasmine approval

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,38 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  var m = this.cards.length, t, i;
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+    // And swap it with the current element.
+    t = this.cards[m];
+    this.cards[m] = this.cards[i];
+    this.cards[i] = t;
+  }
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-}
+  this.pairsClicked++;
+  if (firstCard === secondCard) {
+    this.pairsGuessed++;
+    return true;
+  } else {
+    return false;
+  }
+};
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.pairsGuessed < this.cards.length / 2) {
+    return false;
+  }
+  else if (this.pairsGuessed === this.cards.length / 2) {
+    return true;
+  }
 };
