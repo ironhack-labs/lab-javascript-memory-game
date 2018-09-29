e6b262b20505af8d40183edb69697c622acd4181
alfredohina <41444583+alfredohina@users.noreply.github.com>
2018-09-29 08:49:06 +0200

    Update memory.js

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,39 @@
-var MemoryGame = function (cards) {
+  var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
+
 
 MemoryGame.prototype.shuffleCards = function () {
+    var currentIndex = this.cards.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = this.cards[currentIndex];
+      this.cards[currentIndex] = this.cards[randomIndex];
+      cards[randomIndex] = temporaryValue;
+    }
+  
+    return undefined;
 };
 
+
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked =+ 1;
+  if (firstCard === secondCard) {
+    this.pairsGuessed =+ 1;
+    return true;
+  } else {
+    return false;
+  }
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.pairsGuessed === 8) {
+    return true;
+  } else {
+    return false;
+  }
 };
