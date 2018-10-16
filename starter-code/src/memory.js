36ff05c879c74aea7e4da2fa73e2e84cea529596
beatrice-yun <beatrice.yk.daniel@gmail.com>
2018-10-16 15:07:28 +0200

    not completed

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,11 +1,33 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  var myPseudoLength = this.cards.length;
+
+  for (var i = 0; i < this.cards.length; i++) {
+    var randomIndex = Math.floor(Math.random() * myPseudoLength);
+  
+    var swappy = this.cards[randomIndex];
+    this.cards[randomIndex] = this.cards[myPseudoLength - 1];
+    this.cards[myPseudoLength - 1] = swappy;
+    myPseudoLength--;
+  }
+  return undefined;
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++;
+  if (firstCard === secondCard) {
+    this.pairsGuessed++;
+    return true;
+  }
+  else {
+    return false
+  }
 }
 
 MemoryGame.prototype.isFinished = function () {
