6343d0dd731e93147ce0b3b44b9a10b6c6490321
amine <bouamama.amine@bcg.com>
2018-10-26 09:52:19 +0200

    game logic done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,81 @@
-var MemoryGame = function (cards) {
-  this.cards = cards;
-};
+class MemoryGame{
 
-MemoryGame.prototype.shuffleCards = function () {
-};
+  constructor(cards){
+    this.cards = cards;
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    // list of cards that are currently picked
+    this.pickedCards = [];
+
+    // Number of pairs clicked
+    this.pairsClicked = 0;
+
+    // Number of pairs guesses
+    this.pairsGuessed = 0;
+  }
+
+
+  /**
+   * Method to shuffle the cards
+   */
+  shuffleCards(){
+    this.cards = this._shuffle(this.cards)
+  }
+
+  /**
+   * implements recursively a Fisher-Yates Shuffle
+   * @param arr to shuffle
+   * @private
+   */
+  _shuffle(arr){
+
+    if(arr.length <= 1 || arr === undefined){
+      return arr;
+    }
+    else{
+      // 1. draw one random element
+      var ixRandomElement = Math.floor(Math.random()*arr.length);
+      var randomElement = arr[ixRandomElement];
+
+      // 2. create a new array without the element
+      var arrSmall = arr.slice();
+      arrSmall.splice(ixRandomElement, 1);
+
+      // 3. execute the shuffle function on the former array
+      var shuffledArrSmall = this._shuffle(arrSmall);
+
+      // 4. concatenate the result
+      shuffledArrSmall.push(randomElement)
+
+      return shuffledArrSmall
+    }
+  }
+
+  /**
+   * Returns true if and only if the two cards in the argument of the function are the same
+   * @param firstCard
+   * @param secondCard
+   */
+  checkIfPair(firstCard, secondCard){
+
+    var areEqual = firstCard === secondCard;
+
+    this.pairsClicked += 1;
+    this.pairsGuessed += (areEqual)? 1 : 0;
+
+    return areEqual
+  }
+
+
+  /**
+   * Returns true if and only if all the cards are returned
+   */
+  isFinished(){
+    return (this.pairsGuessed * 2) === this.cards.length;
+
+  }
+
+
+
+
 }
 
-MemoryGame.prototype.isFinished = function () {
-};
