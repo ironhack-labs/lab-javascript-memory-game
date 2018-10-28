8927140cb264c5f8c470100eb2e98ffb07b15463
Laura Stromboni <laura.stromboni@live.fr>
2018-10-28 17:43:15 +0100

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,6 +27,10 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  memoryGame.shuffleCards();
+
+  console.log(memoryGame.cards);
+
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
@@ -39,9 +43,49 @@ $(document).ready(function(){
   $('#memory_board').html(html);
 
   // Bind the click event of each element to a function
-  $('.back').click(function () {
-    // TODO: write some code here
+  var cardsArray = [];
+  // Bind the click event of each element to a function
+  $(".back").click(function () {
+     var button = $(event.target);
+     showCards(button, cardsArray);
+      
+      if(cardsArray.length == 2){
+        $(".back").addClass("blocked");
+        
+        var checkPair = memoryGame.checkIfPair(cardsArray[0], cardsArray[1]);
+        $('#pairs_clicked').html(memoryGame.pairsClicked);
+        if (checkPair == false){
+          setTimeout(function(){
+            hideCards(cardsArray);
+            cardsArray = [];
+            }, 500);
+        } else {
+          $('#pairs_guessed').html(memoryGame.pairsGuessed);
+          cardsArray = [];
+          $(".back").removeClass("blocked");
+        }
+      }
+      
+      memoryGame.isFinished();
+
   });
+
 });
 
+var memoryGame = new MemoryGame(cards);
 
+function showCards(button, cardsArray){
+  button.toggleClass("back");
+  button.next().addClass("back");
+  cardsArray.push(button.attr('name'));
+}
+
+function hideCards(array){
+  var firstCard = document.getElementsByName(array[0]);
+  var secondCard = document.getElementsByName(array[1]);
+  $(firstCard).addClass("back")
+  $(firstCard).next().removeClass("back")
+  $(secondCard).addClass("back")
+  $(secondCard).next().removeClass("back")
+  $(".back").removeClass("blocked")
+}
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,46 @@
-var MemoryGame = function (cards) {
+class MemoryGame {
+
+  constructor(cards){
   this.cards = cards;
-};
-
-MemoryGame.prototype.shuffleCards = function () {
-};
-
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 }
 
-MemoryGame.prototype.isFinished = function () {
+ shuffleCards() {
+    var m = this.cards.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = this.cards[m];
+      this.cards[m] = this.cards[i];
+      this.cards[i] = t;
+    };
+  };
+
+  checkIfPair(cardA, cardB){
+    this.pairsClicked ++;
+    if (cardA === cardB){
+      this.pairsGuessed ++;
+      return true;
+    }
+    return false;
+  }
+
+  isFinished(){
+    if (this.pairsGuessed === 12){
+      setTimeout(function(){
+        alert("You win!");
+        location.reload();
+        }, 800);
+        return true;
+    } 
+    return false;
+  }
+
 };
