a8b2888914464e8c40379a328af475575721bb24
Yared Santiago <yaredmyers@gmail.com>
2018-10-30 19:21:31 +0100

    iteracion1

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,29 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  var newCards = [];
+  for (let i = this.cards.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+  }
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  if (firstCard === secondCard) {
+    this.pairsGuessed++;
+    return true;
+  } else {
+    this.pairsClicked++;
+    return false;
+  }
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.pairsGuessed === (this.cards.length) / 2)  return true;
+  return false;
 };
