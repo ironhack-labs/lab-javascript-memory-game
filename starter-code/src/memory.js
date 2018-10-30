7a68caf2a089cd1180b28ce1f13607c08f789cab
Sonia Ruiz <yune.vk@gmail.com>
2018-10-30 18:37:08 +0100

    Game Logic

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,32 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  var auxCards = [];
+
+  do {
+    auxCards.push(this.cards.splice(Math.floor(Math.random() * this.cards.length),1))
+  } while (this.cards.length);
+
+  this.cards = auxCards;
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  if (firstCard === secondCard) {
+    this.pairsGuessed++;
+    return true;
+  } else {
+    this.pairsClicked++;
+    return false;
+  }
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.pairsGuessed === (this.cards.length)/2) return true;
+
+  return false;
 };
