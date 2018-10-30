836c2fe22c7881d2fedeac9b0140c9d8e014b1a0
Ricardo Cefalo <ricardo.cefalo@gmail.com>
2018-10-30 22:48:35 +0100

    Functions ready

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -24,23 +24,25 @@ var cards = [
   { name: 'the avengers',    img: 'the-avengers.jpg' },
   { name: 'thor',            img: 'thor.jpg' }
 ];
-
+var memory = new MemoryGame(cards);
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  memoryGame.shuffleCards();
   var html = '';
+ 
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
   });
-
+  
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
   // Bind the click event of each element to a function
   $('.back').click(function () {
-    // TODO: write some code here
+    $(this).toggleClass("back");
   });
 });
 
