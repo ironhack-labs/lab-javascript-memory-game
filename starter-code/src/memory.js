0faa0677d37846f950669b76d0a398bf425ebe99
PomPom248 <sofiroggero@gmail.com>
2018-10-30 23:24:18 +0100

    jasmine/js

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -32,7 +32,7 @@ MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 }
 
 MemoryGame.prototype.isFinished = function () {
-  if (this.pairsGuessed===8){
+  if (this.pairsGuessed===this.cards.length/2){
     return true;
   } else {
     return false;
