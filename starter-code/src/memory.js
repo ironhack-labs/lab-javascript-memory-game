d292750759822ec3cd980ab8c46a9c3a454d6a34
PomPom248 <sofiroggero@gmail.com>
2018-10-30 20:35:37 +0100

    jasmine/js

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,40 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards=[];
+  this.pairsClicked=0;
+  this.pairsGuessed=0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
-};
+  var that=this;
+  for (var i=that.cards.length-1;i>0;i--){
+    for (var j=that.cards.length-1;j>0;j--){
+      var randomSet=Math.floor(Math.random()*(i+1));
+      currentVal =that.cards[i];
+      that.cards[i]=that.cards[j];
+      that.cards[j]=currentVal;
+    }
+
+  }
+  return undefined;
+}
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  if (firstCard===secondCard){
+   // this.pairsClicked++;
+    this.pairsGuessed++;
+    return true;
+  } else{
+    this.pairsClicked++;
+    return false;
+  }
+
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.pairsGuessed===8){
+    return true;
+  } else {
+    return false;
+  }
 };
