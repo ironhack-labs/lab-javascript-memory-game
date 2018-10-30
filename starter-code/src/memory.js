273310f622a308272265baacefe9653a6ab6c936
Sandra Luengo <luchosandra@gmail.com>
2018-10-30 18:47:30 +0100

    Iteration 1: The Game Logic

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,37 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+
+  if (!this.cards || this.cards.length === 0) return
+  for (let i = this.cards.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+  }
+
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+
+  if (firstCard === secondCard) {
+    this.pairsGuessed++;
+    return true;
+  } else {
+    this.pairsClicked++;
+    return false;
+  }
+
+
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if(this.pairsGuessed==(this.cards.length)/2){
+    return true;
+  }else{
+    return false;
+  }
 };
