3f4ec5e9bbd41e167f34bf8e189ae171ca54ce1b
Anna Zhuravleva <helloboysandgirls.az@gmail.com>
2018-10-30 23:52:25 +0100

    Logic of the memory game (jasmine passed)

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,34 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+    var currentIndex = this.cards.length, temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = this.cards[currentIndex];
+    this.cards[currentIndex] = this.cards[randomIndex];
+    this.cards[randomIndex] = temporaryValue;
+  }
+
+
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++;
+    if(firstCard === secondCard) {
+        this.pairsGuessed++;
+        return true;
+    }else {
+      return false;
+    }
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.pairsGuessed === this.cards.length/2) {return true;} else return false;
 };
