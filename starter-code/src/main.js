ab3a76aa74222552fc6d499a989bf3b041ea630f
Jesco Wuester <jescowuester@gmail.com>
2018-10-30 16:45:54 +0100

    game works, code is not very clean but I want to fix my slack clone :)

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -28,19 +28,52 @@ var cards = [
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
+  // memoryGame.shuffleCards(memoryGame.cards)
   memoryGame.cards.forEach(function (pic) {
+    // console.log(pic)
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
-    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+    html += '  <div class="front" style="background: url(../img/' + pic.img +') no-repeat"></div>';
     html += '</div>';
   });
+
+  
 
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
   // Bind the click event of each element to a function
   $('.back').click(function () {
-    // TODO: write some code here
+
+    console.log(memoryGame)
+
+    if(memoryGame.pair.length == 2){
+      if(memoryGame.thisPick == false){
+        $(".picked1").toggleClass("front back picked1")
+      $(".picked2").toggleClass("back front picked2")
+      }else{
+        $(".picked1").toggleClass("picked1")
+      $(".picked2").toggleClass("picked2")
+      }
+      memoryGame.pair = []
+      // console.log("toggled")
+    }
+
+    $(this).toggleClass("back front picked1")
+    $(this).next().toggleClass("front back picked2")
+
+
+    let currentCard = memoryGame.cards.find(x => {return x.img == this.attributes["name"].value })
+
+    if(memoryGame.pair.length == 0){
+      memoryGame.pair.push(currentCard.name)
+    }else if(memoryGame.pair.length == 1){
+      memoryGame.pair.push(currentCard.name)
+      memoryGame.checkIfPair()
+    }
+    if(memoryGame.pickedCards.length == 24){
+      alert("you won :)")
+    }
   });
 });
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,11 +1,41 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pair = [];
+  this.clicked = 0
+  this.guessed = 0
+  this.thisPick = false
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function (array) {
+  var n = array.length, temp, i;
+  while (n) {
+    i = Math.floor(Math.random() * n--);
+    temp = array[n];
+    array[n] = array[i];
+    array[i] = temp;
+  }
+  return array;
 };
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.checkIfPair = function () {
+  // console.log(this.pair)
+
+  console.log(this.pair[0]== this.pair[1])
+  if(this.pair[0] === this.pair[1]){
+    console.log("checkifpair if")
+    this.pickedCards.push(this.pair[0],this.pair[1]);
+    this.guessed++
+    this.thisPick = true
+  }else{
+    console.log("checkifpair else")
+    this.thisPick = false
+  }
+  // console.log(this)
+  this.clicked++
+
+  $("#pairs_clicked").text(this.clicked.toString())
+  $("#pairs_guessed").text(this.guessed)
 }
 
 MemoryGame.prototype.isFinished = function () {
