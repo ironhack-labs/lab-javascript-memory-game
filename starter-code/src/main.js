3292569f5ab9560cebed2adcdf89de2ee7d140d9
Sandra Luengo <luchosandra@gmail.com>
2018-10-31 08:58:19 +0100

    Iteration 3: funcionalidad completa

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,9 +27,10 @@ var cards = [
 
 $(function () {
   var contClick = 0;
-
-
+  var isPair
+  
   var memoryGame = new MemoryGame(cards);
+  memoryGame.shuffleCards();
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="' + pic.name + '">';
@@ -37,30 +38,74 @@ $(function () {
     html += '  <div class="front" style="background: url(img/' + pic.img + ') no-repeat"></div>';
     html += '</div>';
   });
+  
 
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
   // Bind the click event of each element to a function
   $('.back').on("click", function () {
+
     $(this).toggleClass("front back");
     $(this).siblings().toggleClass("back front");
     contClick++;
 
     if (contClick == 2) {
       $(".card div:first-child").each(function () {
-        if ($(this).hasClass('front')) {
+        if ($(this).hasClass('front') && !$(this).hasClass("isPair")) {
           memoryGame.pickedCards.push($(this).attr("name"));
+
         }
       });
+      isPair = memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1]);
 
-      var isPair=memoryGame.checkIfPair( memoryGame.pickedCards[0], memoryGame.pickedCards[1]);
       $('#pairs_clicked').text(memoryGame.pairsClicked);
       $('#pairs_guessed').text(memoryGame.pairsGuessed);
-      
+
+      if (isPair) {
+        $("div [name='" + memoryGame.pickedCards[0] + "']").each(function () {
+          $(this).addClass("isPair");
+
+          if (memoryGame.isFinished()) {
+            setTimeout(function () {
+              alert('Has ganado');
+            }, 1000)
+
+          }
+        })
+        memoryGame.pickedCards.length = 0;
+      } else {
+        voltear();
+      }
+      contClick = 0;
+
     }
 
+
+
+
   });
+  function voltear() {
+    setTimeout(function () {
+
+
+
+      if ($("div [name='" + memoryGame.pickedCards[0] + "']:first-child").hasClass("front")) {
+        ;
+
+        $("div [name='" + memoryGame.pickedCards[0] + "']:first-child").removeClass("front").addClass("back");
+        $("div [name='" + memoryGame.pickedCards[0] + "']:first-child").siblings().removeClass("back").addClass("front");
+
+      }
+      if ($("div [name='" + memoryGame.pickedCards[1] + "']:first-child").hasClass("front")) {
+
+
+        $("div [name='" + memoryGame.pickedCards[1] + "']:first-child").removeClass("front").addClass("back");
+        $("div [name='" + memoryGame.pickedCards[1] + "']:first-child").siblings().removeClass("back").addClass("front");
+      }
+      memoryGame.pickedCards.length = 0;
+    }, 2000);
+  }
 
 });
 
