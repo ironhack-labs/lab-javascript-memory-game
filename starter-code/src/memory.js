dd47acc87517a14888cb96e6a2c660c1b6ab089c
danielsuidan <danielsuidan@gmail.com>
2018-10-31 09:22:23 +0100

    Done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,11 +1,26 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards =[];
+  this.pairsClicked=0;
+  this.pairsGuessed=0;
+  
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  var shuffle1=cards.filter(function(card) {
+    return card.img
+  })
+  var shuffle2=cards.filter(function(card) {
+    return card.img
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  if(firstCard===secondCard){
+    this.pairsClicked++;
+    this.pairsGuessed++;
+  }else{
+    this.pairsGuessed++;
+  }
 }
 
 MemoryGame.prototype.isFinished = function () {
