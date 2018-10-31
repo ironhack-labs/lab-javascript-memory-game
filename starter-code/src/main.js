613eedd52932d2d35e94b7164ea74c12cbe46199
Eduardo Gandía Fernández <eduardogandiafernandez@MacBook-Pro-de-Eduardo.local>
2018-10-31 09:21:30 +0100

    first part done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -38,9 +38,10 @@ $(document).ready(function(){
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
+
   // Bind the click event of each element to a function
   $('.back').click(function () {
-    // TODO: write some code here
+    $(this).parent().children().toggleClass('back').toggleClass('front');
   });
 });
 
