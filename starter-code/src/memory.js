28b7e8049d276b52d9b0bd1408ef795f80dbfa62
Javier Samper <jsamperarias@hotmail.com>
2018-10-31 09:37:17 +0100

    [MAD DE] Javier Samper

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,36 @@
-var MemoryGame = function (cards) {
-  this.cards = cards;
+var MemoryGame = function(cards, pickedCards) {
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function() {
+    var cartasRandom = []
+        // for (let i = 0; i < this.cards.length; i++) {
+        //     cartasRandom.push(this.cards.splice(Math.floor(Math.random * this.cards.length)))
+
+    // }
+    this.cards = this.cards.sort(function() {
+        return 0.5 - Math.random();
+    })
+
 };
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.checkIfPair = function(firstCard, secondCard) {
+    if (firstCard === secondCard) {
+        this.pairsGuessed++;
+        return true;
+    } else {
+        this.pairsClicked++;
+        return false;
+    }
 }
 
-MemoryGame.prototype.isFinished = function () {
+MemoryGame.prototype.isFinished = function() {
+    if (this.pairsGuessed == this.cards.length / 2) {
+        return true;
+    } else {
+        return false;
+    }
 };
