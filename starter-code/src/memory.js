0397b033f71a0014f9394e3baacebf76e66958f0
Bianca Castro <biiianca_00@hotmail.com>
2018-10-31 09:14:05 +0100

    1/2 done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,45 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards=[];
+  this.pairsClicked=0;
+  this.pairsGuessed=0;
+
 };
+
 
 MemoryGame.prototype.shuffleCards = function () {
+
+  var m = this.cards.length, t, i;
+     while (m) {
+       i = Math.floor(Math.random() * m--);
+       t = this.cards[m];
+       this.cards[m] = this.cards[i];
+       this.cards[i] = t;
+     };
+
 };
+
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+
+  if(firstCard!==secondCard){
+     this.pairsClicked++ 
+     return false
+     
+  }  else if (firstCard===secondCard) {
+   this.pairsGuessed++
+   return true
+  }
+  
 }
 
-MemoryGame.prototype.isFinished = function () {
-};
+MemoryGame.prototype.isFinished = function() {
+  if (this.pairsGuessed === (this.cards.length) / 2) return true ;
+  
+  
+     return false;
+  
+  
+  };
+
+ 
