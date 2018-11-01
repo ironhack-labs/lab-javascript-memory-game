881dfe8e7ffc1a913ade0b83234ac0bf6f937603
anakellyc <anakcampos@gmail.com>
2018-11-01 11:08:32 +0100

    Memory game using constructor and prototype

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,68 +27,13 @@ var cards = [
 
 $(document).ready(function(){
 
-  function shuffle (array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-    return array;
-  }
-
-  var memoryGame = shuffle(cards)
-  
-  var html = '';
-  memoryGame.forEach(function (pic) {
-    html += '<div class="card" data-card-name="'+ pic.name +'">';
-    html += '  <div class="back" name="'+ pic.img +'"></div>';
-    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
-    html += '</div>';
-  });
-
-  // Add all the div's to the HTML
-  $('#memory_board').html(html);
-
-  // Bind the click event of each element to a function
-
-  var pickedCards = []
-  var pairsGuessed = 0;
-  var pairsClicked= 0;
+  let memoryGame = new MemoryGame(cards)
+  memoryGame.shuffleCards()
+  memoryGame.loadGame()
 
   $(".card").on("click", function () {
-    $(this).children().toggleClass("back front")
-    $(this).children().addClass("turned blocked")
-    $(".card").find(".change").toggleClass("front back")
-    $(".card").find(".change").removeClass("change")
-    pickedCards.push($(this).children().one().attr("name"))
-    if (pickedCards.length == 2) {
-      checkIfPair(pickedCards[0],pickedCards[1]);
-      pickedCards = []
-      if (paired == false) {
-        $(".card").find(".turned").addClass("change")
-        $(".card").find(".turned").removeClass("blocked")
-      }
-      $(".card").find(".turned").removeClass("turned")
-      paired = false
-      } 
-    $("#pairs_clicked").html(pairsClicked)
-    $("#pairs_guessed").html(pairsGuessed)
+    memoryGame.playGame(this)
   }); 
-
-  var paired = false
-  function checkIfPair (firstCard, secondCard) {
-    if (firstCard == secondCard) {
-      paired = true
-      pairsGuessed +=1;
-      pairsClicked +=1;
-    } else {
-      pairsClicked +=1
-    }
-    return paired
-  }
     
 });
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,13 +1,67 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = []
+  this.pairsGuessed = 0;
+  this.pairsClicked= 0;
+  this.paired = false;
 };
-
 
 MemoryGame.prototype.shuffleCards = function () {
+  var currentIndex = this.cards.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = this.cards[currentIndex];
+      this.cards[currentIndex] = this.cards[randomIndex];
+      this.cards[randomIndex] = temporaryValue;
+    }
 };
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.loadGame = function () {
+  let html = '';
+  this.cards.forEach(function (pic) {
+    html += '<div class="card" data-card-name="'+ pic.name +'">';
+    html += '  <div class="back" name="'+ pic.img +'"></div>';
+    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+    html += '</div>';
+  });
+  $('#memory_board').html(html);
 }
+
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    if (firstCard == secondCard) {
+      this.paired = true
+      this.pairsGuessed +=1;
+      this.pairsClicked +=1;
+    } else {
+      this.pairsClicked +=1
+    }
+    return this.paired
+}
+
+MemoryGame.prototype.playGame = function (card) {
+    $(card).children().one().toggleClass("back front")
+    $(card).children().addClass("turned blocked")
+    $(".card").find(".change").toggleClass("front back")
+    $(".card").find(".change").removeClass("change")
+    this.pickedCards.push($(card).children().one().attr("name"))
+    console.log(this.pickedCards)
+    if (this.pickedCards.length == 2) {
+      this.checkIfPair(this.pickedCards[0],this.pickedCards[1]);
+      console.log(this.paired)
+      this.pickedCards = []
+      if (this.paired == false) {
+        $(".card").find(".turned").addClass("change")
+        $(".card").find(".turned").removeClass("blocked")
+      }
+      $(".card").find(".turned").removeClass("turned")
+      this.paired = false
+      } 
+    $("#pairs_clicked").html(this.pairsClicked)
+    $("#pairs_guessed").html(this.pairsGuessed)
+    console.log(this.pairsClicked)
+    console.log(this.pairsGuessed)
+  }; 
 
 MemoryGame.prototype.isFinished = function () {
 };
