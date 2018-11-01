221954f5d551c1159cf943f062e1cf1338263a34
Javier <jarrindaulivi4@gmail.com>
2018-11-01 12:34:16 -0400

    MIA FTWD Javier Arrinda

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,23 +25,67 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
-$(document).ready(function(){
-  var memoryGame = new MemoryGame(cards);
-  var html = '';
-  memoryGame.cards.forEach(function (pic) {
-    html += '<div class="card" data-card-name="'+ pic.name +'">';
-    html += '  <div class="back" name="'+ pic.img +'"></div>';
-    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
-    html += '</div>';
-  });
+// $(document).ready(function(){
+//   console.log("creating new game now !!!!!!")
+//   var memoryGame = new MemoryGame(cards);
+//   var html = '';
+//   memoryGame.cards.forEach(function (pic) {
+//     html += '<div class="card" data-card-name="'+ pic.name +'">';
+//     html += '  <div class="back" name="'+ pic.img +'"></div>';
+//     html += '  <div class="front" style="background: url(img/' + pic.img + ') no-repeat;"></div>';
+//     html += '</div>';
+//   });
 
-  // Add all the div's to the HTML
-  $('#memory_board').html(html);
+//   console.log(html)
+//   // Add all the div's to the HTML
+//   $('#memory_board').html(html);
 
-  // Bind the click event of each element to a function
-  $('.back').click(function () {
-    // TODO: write some code here
-  });
+//   // Bind the click event of each element to a function
+//   $('.back').click(function(){
+   
+//     $(this).hide()
+//     $(this).siblings().addClass('back');
+//   });
+  
+
+// });
+
+let theGame = new MemoryGame(cards);
+  
+
+var html = '';
+
+theGame.cards.forEach(function (pic) {
+  html += '<div class="card" data-card-name="'+ pic.name +'">';
+  html += '  <div class="back" name="'+ pic.img +'"></div>';
+  html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+  html += '</div>';
+});
+
+// Add all the div's to the HTML
+$('#memory_board').html(html);
+
+// Bind the click event of each element to a function
+$('.back').click(function () {
+  
+  $(this).hide();
+  // $(this).removeClass('back') either one of these will work
+
+  $(this).siblings().addClass('back')
+
+  theGame.currentHand.push($(this));
+
+  if(theGame.currentHand.length === 2){
+    $('.back').addClass('blocked');
+    theGame.evaluatePair();
+    updateScore();
+  }
+  
 });
 
 
+
+function updateScore(){
+  $('#pairs_clicked').text(theGame.attempts);
+  $('#pairs_guessed').text(theGame.score);
+}
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,138 @@
-var MemoryGame = function (cards) {
-  this.cards = cards;
-};
+// class MemoryGame {
+//   constructor(cards) {
+//     console.log(this);
+//     this.cards = cards;
+//     this.pickedCards = [];
+//     this.pairsClicked = 0;
+//     this.pairsGuessed = 0;
+    
+    
+//   }
 
-MemoryGame.prototype.shuffleCards = function () {
-};
+//   class MemoryGame{
+//     constructor(theArg){
+//       this.cards = theArg;
+//       this.shuffleCards()
+//     }
+  
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-}
+//   shuffleCards(){
+//     let m = this.cards.length;
+//     let t;
+//     let i;
 
-MemoryGame.prototype.isFinished = function () {
-};
+//     while (m){
+//       i = Math.floor(Math.random() * m--);
+
+//       t = this.cards[m];
+//       this.cards[m] = this.cards[i];
+//       this.cards[i] = t;
+//     }
+//     return array;
+//   }
+
+// }
+  
+  // choosingCards(card){
+    
+  //   this.pairsClicked += 1;
+    
+  //   $(card).toggleClass('show');
+  //   $(card).siblings().toggleClass('show');
+    
+  //   let carName = $(card).attr('name')
+  //   this.pickedCards.push(carName);
+  //   console.log(this.pickedCards)
+    
+    
+  // }
+  
+  // checkIfPair(){
+  //   firstCard = this.pickedCards[0];
+  //   secondCard = this.pickedCards[1];
+    
+  //   if(firstCard === secondCard){
+  //     $("#pairs_guessed").text(guessCount+=1);
+  //     this.pickedCards.splice(0, 2);
+  //     console.log(this.pickedCards)
+  //     console.log("right Combination")
+  //     return true
+  //   }  if(this.pickedCards.length > 1 && firstCard !== secondCard ) {
+  //       this.pickedCards.splice(0, 2);
+   
+  //       $(firstCard).toggleClass("show");
+  //       console.log("Wrong Combination");
+  //       $("#pairs_clicked").text(clickCount++);
+  //     return false
+  //   }
+  
+  // }
+
+    // $('#pairs_clicked')[0].innerHtml = this.pairsClicked;
+    // $('#pairs_guessed')[0].innerHTML = this.pairsGuessed;
+
+// }
+  
+class MemoryGame{
+  constructor(theArg){
+    this.cards = theArg;
+    this.currentHand = [];
+    this.attempts = 0;
+    this.score = 0;
+
+    // this.shuffle();
+  }
+
+  shuffle() {
+    let m = this.cards.length; 
+    let t;
+    let i;
+  
+    // While there remain elements to shuffle…
+    while (m > 0) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = this.cards[m];
+      this.cards[m] = this.cards[i];
+      this.cards[i] = t;
+    } 
+  }
+
+
+      evaluatePair(){
+        this.attempts++;
+        if(this.currentHand[0].attr('name') === this.currentHand[1].attr('name')){
+          this.score++;
+          this.currentHand = [];
+          this.checkToSeeIfIWon();
+          $('.back').removeClass('blocked');
+        }else {
+          setTimeout(()=>{ 
+            this.currentHand[0].show();
+            this.currentHand[1].show();
+            this.currentHand[0].siblings().removeClass('back');
+            this.currentHand[1].siblings().removeClass('back');
+            this.currentHand = [];
+            $('.back').removeClass('blocked');
+          },800)
+        }
+      }
+
+
+      checkToSeeIfIWon(){
+        if(this.score === 12){
+          setTimeout(()=>{
+            alert('yay, you wom!');
+          },1)
+        }
+      }
+
+
+
+
+
+} 
+
