046b5e7966e0bd79d455fd29e05c6aabd8b0d373
Luise Schneider <luise.schneider@berlin.de>
2018-11-02 20:27:40 +0100

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -42,18 +42,85 @@ $(document).ready(function() {
   $("#memory_board").html(html);
 
   // Bind the click event of each element to a function
-  $(".card").click(function(e) {
-    $(this)
+  var openCards = [];
+
+  $("#memory_board").on("click", ".card", function(e) {
+    var currentCard = $(this);
+    // console.log(currentCard);
+    //turn card
+    currentCard
       .children()
       .last()
       .toggleClass("front")
       .toggleClass("back");
-    $(this)
+    currentCard
       .children()
       .first()
       .toggleClass("front")
       .toggleClass("back");
-    memoryGame.pairsClicked += 0.5;
-    $("#pairs_clicked").html(Math.floor(memoryGame.pairsClicked));
+
+    // store cards that are currently open
+    openCards.push(currentCard.attr("data-card-name"));
+    currentName = currentCard.attr("data-card-name");
+
+    //compare stored cards (same)
+
+    if (openCards.length === 2) {
+      memoryGame.checkIfPair(openCards[0], openCards[1]);
+      $("#pairs_clicked").html(memoryGame.pairsClicked);
+      $("#pairs_guessed").html(memoryGame.pairsGuessed);
+
+      if (openCards[0] === openCards[1]) {
+        $("[data-card-name='" + currentName + "']").addClass("blocked");
+        $("[data-card-name='" + currentName + "']")
+          .children()
+          .addClass("blocked");
+        openCards = [];
+      }
+
+      //compare stored cards (not the same) and turn them back around
+
+      if (openCards[0] !== openCards[1]) {
+        setTimeout(function() {
+          $(".card div:first-child")
+            .not(".blocked")
+            .removeClass()
+            .addClass("back");
+          $(".card div:last-child")
+            .not(".blocked")
+            .removeClass()
+            .addClass("front");
+        }, 1500);
+        openCards = [];
+      }
+    }
+
+    //send alert and reset game, if all cards are open
+
+    if (memoryGame.isFinished()) {
+      alert(
+        "Good job! Your score: " + memoryGame.pairsClicked + ". Another round?"
+      );
+      memoryGame.pairsClicked = 0;
+      $("#pairs_clicked").html(memoryGame.pairsClicked);
+      memoryGame.pairsGuessed = 0;
+      $("#pairs_guessed").html(memoryGame.pairsGuessed);
+
+      //shuffle cards and create a new memory board
+
+      memoryGame.shuffleCards();
+      var newHtml = "";
+      memoryGame.cards.forEach(function(pic) {
+        newHtml += '<div class="card" data-card-name="' + pic.name + '">';
+        newHtml += '  <div class="back" name="' + pic.img + '"></div>';
+        newHtml +=
+          '  <div class="front" style="background: url(img/' +
+          pic.img +
+          ') no-repeat"></div>';
+        newHtml += "</div>";
+      });
+      $("#memory_board").html("");
+      $("#memory_board").html(newHtml);
+    }
   });
 });
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -5,12 +5,28 @@ var MemoryGame = function(cards) {
   this.pairsGuessed = 0;
 };
 
-MemoryGame.prototype.shuffleCards = function() {};
+MemoryGame.prototype.shuffleCards = function() {
+  var m = this.cards.length,
+    t,
+    i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = this.cards[m];
+    this.cards[m] = this.cards[i];
+    this.cards[i] = t;
+  }
+};
 
 MemoryGame.prototype.checkIfPair = function(firstCard, secondCard) {
   this.pairsClicked += 1;
   if (firstCard === secondCard) {
     this.pairsGuessed += 1;
+    // console.log("matchfound");
     return true;
   } else {
     return false;
