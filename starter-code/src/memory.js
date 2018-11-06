a0edf7c361ef1706afd374addcaa465c2cd879e6
Katharina Koenig <k.koenig1811@gmail.com>
2018-11-06 16:38:27 +0100

    Berlin-Katharina-first version

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,9 +1,21 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
+
+
 MemoryGame.prototype.shuffleCards = function () {
+/*  var newSet = [];
+  for (i = 0; i < cards.length, i++) {
+
+  }
+  return newSet.push(this.cards)
+*/
 };
+
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 }
