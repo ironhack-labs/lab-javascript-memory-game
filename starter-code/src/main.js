9e5e1c55c189960931509e14c35b0f3145a92668
Rene Manzano <rob_rene199027@hotmail.com>
2018-11-06 19:31:09 -0600

    Ya estuvo

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,6 +27,7 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  memoryGame.shuffleCards()
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
@@ -37,11 +38,39 @@ $(document).ready(function(){
 
   // Add all the div's to the HTML
   $('#memory_board').html(html);
-
+  var arr = []
+  var cl = 0
+  var par = 0
   // Bind the click event of each element to a function
   $('.back').click(function () {
-    // TODO: write some code here
+    
+    $(this).toggleClass('front')
+    $(this).toggleClass('back')
+    $(this).siblings().toggleClass('front')
+    $(this).siblings().toggleClass('back')
+    var name = $(this).parent().attr("data-card-name")
+    arr.push(name)
+    if(arr.length ==2){
+      if(memoryGame.checkIfPair(arr[0],arr[1])){
+          par ++
+      }
+      
+      cl += 1
+      $('#pairs_clicked').text(cl)
+      $('#pairs_guessed').text(par)
+      memoryGame.isFinished()
+      arr = []
+    }
+
   });
+  $('.front').click(function () {
+    $(this).toggleClass('front')
+    $(this).toggleClass('back')
+    $(this).siblings().toggleClass('front')
+    $(this).siblings().toggleClass('back')
+  });
+
 });
+
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,39 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = []
+  this.pairsClicked = 0
+  this.pairsGuessed = 0
+  this.shuffleCards = function(){
+    for(let i=this.cards.length-1; i>0;i--){
+      var j = Math.floor((Math.random()*((i)-0+1)+0))
+      var temp = this.cards[j]
+      var temp2 = this.cards[i]
+      this.cards[i] = temp
+      this.cards[j] = temp2
+    }
+  }
+  this.checkIfPair = function(firstCard, secondCard){
+    this. pairsClicked++
+    if(firstCard === secondCard){
+      this.pairsGuessed++
+      return true
+    }
+    else{
+      return false
+    }
+  }
+  this.isFinished = function(){
+    if(this.pairsGuessed === this.cards.length/2){
+      alert("El juego ha terminado")
+    } 
+  }
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+/*MemoryGame.prototype.shuffleCards = function () {
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 }
 
 MemoryGame.prototype.isFinished = function () {
-};
+};*/
