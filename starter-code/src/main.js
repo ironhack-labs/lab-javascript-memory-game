f96afb3f8be3b89a3ed1359df9fda51388a88911
Fernando Hernández Báez <Fernando1@MacBook-Pro-de-Fernando-6.local>
2018-11-06 22:00:33 -0600

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,12 +27,14 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+ 
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
+   
   });
 
   // Add all the div's to the HTML
@@ -40,7 +42,32 @@ $(document).ready(function(){
 
   // Bind the click event of each element to a function
   $('.back').click(function () {
-    // TODO: write some code here
+   
+    if(memoryGame.pickedCards.length < 2){
+      memoryGame.pickedCards.push (this.getAttribute('name'))
+      $(this).removeClass('back')
+      $(this).addClass('front')
+    
+      $(this).next().addClass('back')
+      $('#pairs_clicked').text(memoryGame.pairsClicked)
+
+    }else{
+      memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1])
+      console.log( memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1])  )
+      memoryGame.pickedCards = []
+      $('#pairs_guessed').text(memoryGame.pairsGuessed)
+
+    }
+  });
+
+  $('.front').click(function () {
+
+    $(this).removeClass('front')
+    $(this).addClass('back')
+    $(this).next().addClass('front')
+    
+    
+    
   });
 });
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,43 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards =[];
+  this.pairsClicked=0
+  this.pairsGuessed=0
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  var arr = []
+  var r = 0
+ 
+  for (var i=0;i<23;i++){
+    r = Math.floor(Math.random()*this.cards.length)
+    arr.push( this.cards.splice(r,1))
+  }
+console.log(arr)
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++
+  if (firstCard === secondCard){
+   
+    this.pairsGuessed++
+    return true
+  
+  }else return false
 }
 
+
+
 MemoryGame.prototype.isFinished = function () {
+
+
+
+  if(this.pairsGuessed === 0)
+  return false
+
+  if(this.pairsGuessed <12)
+  return false
+  if(this.pairsGuessed ===12)
+  return true
+
 };
