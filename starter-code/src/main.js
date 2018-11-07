d5e66c3d9f5b438bf6db8dd6f8f07dcd36a13094
jaqusa <dev@javierquinte.com>
2018-11-06 21:59:10 -0600

    todo lo que se pudo

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -28,20 +28,53 @@ var cards = [
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
+ //memoryGame.shuffleCards()
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
-    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+    html += '  <div class="front" style="background: url(./img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
-  });
 
+  });
+ 
   // Add all the div's to the HTML
   $('#memory_board').html(html);
-
+ 
   // Bind the click event of each element to a function
   $('.back').click(function () {
-    // TODO: write some code here
+    //memoryGame.pickedCards.push()
+    // this.getAttribute('name')
+    
+    if(memoryGame.pickedCards.length < 2){
+      memoryGame.pickedCards.push (this.getAttribute('name'))
+      $(this).removeClass('back')
+      $(this).addClass('front')
+      //$(this).next().removeClass('front')
+      $(this).next().addClass('back')
+      $('#pairs_clicked').text(memoryGame.pairsClicked)
+
+    }else{
+      memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1])
+      console.log( memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1])  )
+      memoryGame.pickedCards = []
+      $('#pairs_guessed').text(memoryGame.pairsGuessed)
+
+    }
   });
+
+  $('.front').click(function () {
+
+    $(this).removeClass('front')
+    $(this).addClass('back')
+    $(this).next().addClass('front')
+    
+    
+    
+  });
+
+  
+
+
 });
 
 
