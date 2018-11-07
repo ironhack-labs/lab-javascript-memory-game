e7f188b1ca67c1d0083642a51a5f7f8da7a3b9b7
Omar Avelar <omar.avelar.f@gmail.com>
2018-11-07 02:31:03 -0600

    Done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,18 @@
-var MemoryGame = function (cards) {
+var MemoryGame = function(cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function() {
+  for (let i = 0; i < this.cards.length; i++) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let a = this.cards[i];
+    let b = this.cards[j];
+    this.cards[i] = b;
+    this.cards[j] = a;
+  }
 };
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-}
 
-MemoryGame.prototype.isFinished = function () {
-};
