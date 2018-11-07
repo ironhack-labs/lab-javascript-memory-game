5c4408ac54e69985464f0088067b77e6a6d4942d
sanmiranda <sanmiranda@hotmail.com>
2018-11-06 22:39:48 -0600

    va

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -28,6 +28,8 @@ var cards = [
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
+memoryGame.shuffleCards()
+
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
@@ -35,13 +37,16 @@ $(document).ready(function(){
     html += '</div>';
   });
 
+
   // Add all the div's to the HTML
-  $('#memory_board').html(html);
+
 
   // Bind the click event of each element to a function
-  $('.back').click(function () {
-    // TODO: write some code here
-  });
-});
+
+  $(this).toggleClass('front')
+  $(this).toggleClass('back')
+
+
+
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,37 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pairsClicked = this.pairsClicked ++
+this.pairsGuessed = console.log(checkIfPair())
 };
 
 MemoryGame.prototype.shuffleCards = function () {
-};
+ for ( var i = this.cards.length-1; i > 0; i-- ) {
+  var j = Math.floor( i * Math.random() );
+  var card1 = this.cards[ j ];
+  var card2 = this.cards[i];
+  this.cards[ j ] = card1
+  this.cards[ i ] = card2
+}
+}
+console.log(shuffleCards())
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.checkIfPair = function (card1, card2) {
+if (card1 === card2) {
+  return true
+}else{ return false
+}
+if (this.checkIfPair === true){
+  this.pairsGuessed ++
+}else{
+  this.pairsGuessed === this.pairsGuessed
 }
 
+
+
 MemoryGame.prototype.isFinished = function () {
-};
+if (this.pairsGuessed === 12){
+  alert('Ganaste!!!')
+}else{
+  false
+}
+}; 
