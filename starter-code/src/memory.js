257b3a0b8e60e094888637e3557d5d465c81573e
Nerea <nereaparicio@gmail.com>
2018-11-14 20:47:50 +0100

    start work

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -10,3 +10,10 @@ MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
 MemoryGame.prototype.isFinished = function () {
 };
+
+
+//Funcion random
+
+// [].sort(function(a,b){
+//   return Math.random()-0,5;
+// });
