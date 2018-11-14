67f31bcdaee8a6b4a36242642e97e8481416f196
Clara Garcia <clara.garcia.pascual@accenture.com>
2018-11-14 21:22:23 +0100

    feat: The Game Logic

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,31 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  this.cards.sort(function () {
+    return Math.random() - 0.5;
+  })
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked ++;
+
+  if (firstCard === secondCard) {
+    this.pairsGuessed ++;
+    return true;
+  } else {
+  return false;
+  }
 }
 
 MemoryGame.prototype.isFinished = function () {
+  var pairs = this.cards.length / 2;
+  if (this.pairsGuessed === pairs) {
+    return true;
+  }
+  return false;
 };
