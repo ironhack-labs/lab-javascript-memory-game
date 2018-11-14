19cd6b0b90073c905695692c83f363fbf4e60492
Javier Requejo Melchor <javierrequejomelchor@gmail.com>
2018-11-14 21:34:43 +0100

    MemoryGame methods completed!

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,30 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  this.cards.sort(function () {
+    return Math.random() - 0.5;
+  })
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  var pairedCards = false;
+  this.pairsClicked++;
+  if(firstCard === secondCard) {
+    pairedCards = true;
+    this.pairsGuessed++;
+  }
+  return pairedCards;
 }
 
 MemoryGame.prototype.isFinished = function () {
+  var isFinish = false;
+  if(this.cards.length / 2 === this.pairsGuessed) {
+    isFinish = true;
+  }
+  return isFinish;
 };
