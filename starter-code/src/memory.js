98d05c40c6d3c109ca185287a2f4bf11b2a11ff4
AntonioSalazar <antonio.salazar.emrsn@gmail.com>
2018-11-15 23:18:17 -0600

    Work in progress, Jasmine specs completed

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,32 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function (cardsArray) {
+    var cardsArray = this.cards;
+    for (let i = cardsArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cardsArray[i], cardsArray[j]] = [cardsArray[j], cardsArray[i]];  
+      return ;
+    }
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-}
+  this.pairsClicked += 1;
+  if(firstCard === secondCard){
+    this.pairsGuessed += 1;
+    return true
+  } else{
+    return false
+  }
+};
 
 MemoryGame.prototype.isFinished = function () {
+  if(this.pairsGuessed === 8){
+    return true
+  }
+  return false
 };
