09527ff1d2fdb179d11b557f53eeb9abb83f634a
chaveroricardo <chavero.ricardo@gmail.com>
2018-11-15 22:08:16 -0600

    empezando

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,20 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  var pickedCards = [0];
+  var pairsClicked = 0;
+  var pairsGuessed = 0;
+  this.pickedCards = pickedCards;
+  this.pairsClicked = pairsClicked;
+  this.pairsGuessed = pairsGuessed;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  this.cards = cards;
+    for (let i = this.cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    
+}
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
