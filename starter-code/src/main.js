289df7ce635f7c9099754e601060827ea486bfb0
Diego Rodríguez <diegorodriguezgzz@gmail.com>
2018-11-16 00:07:41 -0600

    Finished exercise

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,9 +25,12 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
+var selected = [];
+
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
-  var html = '';
+  memoryGame.shuffleCards();
+  var html = ''; //Inner html of #memory_board
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
@@ -36,12 +39,38 @@ $(document).ready(function(){
   });
 
   // Add all the div's to the HTML
-  $('#memory_board').html(html);
+  $('#memory_board').html(html); //Set inner HTML of element #memory_board
 
   // Bind the click event of each element to a function
   $('.back').click(function () {
-    // TODO: write some code here
+    console.log($(this));
+    $(this).toggle(0);
+    $(this).next().toggle(0);
+    selected.push($(this));
+    if (selected.length === 2) {
+      if (memoryGame.checkIfPair(selected[0].attr("name"), selected[1].attr("name"))) {
+        //Remove both cards or replace with transparent div.
+        for(let i = 0; i < selected.length; i++) {
+          selected[i].parent().css("background", "rgba(255,255,255,0)");
+          selected[i].parent().children().css("background", "rgba(255,255,255,0)");
+        }
+        $("#pairs_guessed").text(memoryGame.pairsGuessed);
+      }
+      else {
+        for(let i = 0; i < selected.length; i++) {
+          // posible corrección: el timer en toggle es 0 si es sexta columna
+          selected[i].parent().children(".front").toggle(500);
+          selected[i].parent().children(".back").toggle(500);
+        }
+      }
+      selected = [];
+      $("#pairs_clicked").text(memoryGame.pairsClicked);
+    }
+    if (memoryGame.isFinished()) {
+      alert("You won in only " + memoryGame.pairsClicked + " guesses!");
+    }
   });
+
 });
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,30 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
+//Pendiente
 MemoryGame.prototype.shuffleCards = function () {
+  var m = this.cards.length, t, i;
+  while (m) { // Wow!!!!!
+    i = Math.floor(Math.random() * m--);
+    t = this.cards[m];
+    this.cards[m] = this.cards[i];
+    this.cards[i] = t;
+  }
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++;
+  if (firstCard === secondCard) {
+    this.pairsGuessed++;
+    return true;
+  }
+  return false;
 }
 
 MemoryGame.prototype.isFinished = function () {
+  return (this.pairsGuessed === this.cards.length/2) ? true : false;
 };
