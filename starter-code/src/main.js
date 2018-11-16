1761dfb993a7397e45afa990af512efef4021bed
Nerea Aparicio <nerea.aparicio@acuntia.dom>
2018-11-16 16:24:35 +0100

    help

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -43,37 +43,54 @@ $(document).ready(function(){
   $('.back').click(function () {
     // TODO: write some code here
 
-    do {
-
-      if (memoryGame.legth === 2) {
+     if (memoryGame.pickedCards.length === 2) {
+       
         memoryGame.pickedCards = [];
+         $(this).removeClass('front');
+         //$(this).addClass('back');
+         $(this).siblings().addClass('back');
+         $(this).siblings().addClass('front');
+        
+      
       } else {
-           var firstCard = $(".pairsClicked").click( function (){
               //sumamos 1 carta al array
-               memoryGame.pickedCards.push (this); //(esta bien hecho asi?)
+              memoryGame.pickedCards.push(this);
+
+              //Sumamos y mostramos en pantalla el numero de cartas seleccionas
+              memoryGame.pairsClicked++;
+              $(".pairs_clicked").html ("<span id=" + "pairs_clicked>" + memoryGame.pickedCards + "</span></p>"); //(revisar si esta bien hecho)
+            
               //voltear la carta seleccionada
-              $()
+              $(this).addClass('front');
+              $(this).removeClass('back');
+              $(this).siblings().addClass('back');
+              $(this).siblings().addClass('front');
 
-              });
-
-             var secondCard = $(".pairsClicked").click( function (){
+              
+          
                //sumamos 1 carta al array
-               memoryGame.pickedCards.push (this); //(esta bien hecho asi?)
-                //voltear la carta seleccionada
+              // memoryGame.pickedCards = memoryGame.pickedCards.push($(this).parent());
+               
+               //Sumamos y mostramos en pantalla el numero de cartas seleccionas
+               memoryGame.pairsClicked++;
+               $(".pairs_clicked").html ("<p>Pairs Clicked: <span id=" + "pairs_clicked>" + memoryGame.pickedCards + "</span></p>"); //(revisar si esta bien hecho)
 
-              });
+               //voltear la carta seleccionada
+               $(this).addClass('front');
+               $(this).removeClass('back');
+               $(this).siblings().addClass('back');
+               $(this).siblings().addClass('front');
+            
 
-          if (checkIfPair (firstCard, secondCard)){
+          if (memoryGame.checkIfPair){
 
-            //bloquear las cartas levantadas
-            pairsGuessed++;
-
+            //bloquear las cartas levantadas, cambiar la clase de la carta a .card .back.blocked
+            //$(".pairs_guessed").text
+            //sumamos una pareja acertada y la pintamos en pantalla
+            memoryGame.pairsGuessed++;
+            
             }
         }
-
-      }while (pairsGuessed);
-
-  });
+      });
 });
-
 
