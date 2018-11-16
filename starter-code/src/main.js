3ecad804095a5fcad29c9a009e101e4d715d0675
Roberto <roberto.m.aragon@gmail.com>
2018-11-16 22:40:10 +0100

    almost Done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -28,20 +28,73 @@ var cards = [
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
+  
+
+  memoryGame.shuffleCards(cards);
+  //Baraja las cartas
+
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
   });
+  //crea los divs (back y front) en base al número de imágenes que haya en el array cards.
 
   // Add all the div's to the HTML
   $('#memory_board').html(html);
+  //Mete todas las cartas en #memory_board.
 
   // Bind the click event of each element to a function
+  function changeClass(elementClass) {
+    //Se saca en una función aparte el "darle la vuelta a las cartas" para que cuando haga click solo tenga que invocarla.
+    
+    $(elementClass).toggleClass("front");
+    $(elementClass).toggleClass("back");
+    $(elementClass).siblings().toggleClass("front");
+    $(elementClass).siblings().toggleClass("back");
+    // Las dos primeras funciones añade la funcion front y quita la función back. Dándole la vuelta
+    //las dos siguientes hacen efectos en los hermanos.
+
+    $(elementClass).toggleClass("control");
+    //Le metemos una clase "control" que es la que verifica que si la segunda no es correcta deben volver a darse la vuelta.
+  }
+
   $('.back').click(function () {
-    // TODO: write some code here
+    memoryGame.pickedCards.push($(this).attr("name"));
+    //Añade a pickedCards las cartas clicadas
+    
+    /*
+    $(elementClass).addClass("front").removeClass("back");
+    $(elementClass).siblings().addClass("back").removeClass("front");
+    */
+
+    changeClass(this);
+
+    if (memoryGame.pickedCards.length === 2) {
+      var clicked = memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1]);
+      //Creamos una variable para chequear si las dos cartas son iguales. Es un booleano.
+      $("#pairs_clicked").text(memoryGame.pairsClicked);
+      $("#pairs_guessed").text(memoryGame.pairsGuessed);
+      //Edita el texto del Score
+
+    if ( clicked ) {
+      $('.control').toggleClass("control");
+      
+      if ( memoryGame.isFinished()) {
+        setTimeout(function() {
+          alert("You Win!");
+        },1000);
+      }      
+    } else {
+      setTimeout(function() {
+        changeClass('.control');
+      },500);
+      //Si no es acertada la pareja, se quita el control.
+        }
+    memoryGame.pickedCards = [];
+    //Vacía el array de cartas seleccionadas
+    }
   });
 });
-
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,35 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+  
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  this.cards.sort(function() {
+    return Math.random() - 0.5;
+  })
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+this.pairsClicked++;
+
+if (firstCard === secondCard) {
+  this.pairsGuessed++;
+  return true;
+} else {
+  return false;
+}
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.pairsGuessed < 8 ) {
+    return false;
+  } else  if(this.pairsGuessed === this.cards.length / 2) {
+    return true;
+  }
 };
+
+//PISTA PARA BLOQUEAR CARTAS .blocked
