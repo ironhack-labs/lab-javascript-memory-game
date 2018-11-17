7db52998f93225826c758811244ed9b181cb11a1
hobglobin27 <hobglobin27@gmail.com>
2018-11-16 22:03:43 -0600

    Version completa Memory game

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,21 +27,92 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  memoryGame.shuffleCards();
   var html = '';
-  memoryGame.cards.forEach(function (pic) {
-    html += '<div class="card" data-card-name="'+ pic.name +'">';
-    html += '  <div class="back" name="'+ pic.img +'"></div>';
-    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
-    html += '</div>';
-  });
+  var card1="";
+  var card2="";
+  var card1Name="";
+  var card2Name="";
 
+  creaTablero();
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
   // Bind the click event of each element to a function
-  $('.back').click(function () {
-    // TODO: write some code here
+  $(document).on("click",".back",function(){
+
+    $(this).parent().find("div:nth-child(1)").toggleClass("back front blocked");
+    $(this).parent().find("div:nth-child(2)").toggleClass("front back blocked");
+
+    if(card1===""){
+      card1=$(this).parent();
+      card1Name=$(this).parent().attr("data-card-name");
+      }
+    else{
+      card2=$(this).parent();
+      card2Name=$(this).parent().attr("data-card-name");
+      }
+
+    if(validaSeleccionadas())
+      if(card1.parent()!==card2.parent()){
+        console.log("Son diferentes")
+      }
+      else {
+        console.log("Son iguaes");
+      }
+
+    if(validaSeleccionadas()){
+      if(memoryGame.checkIfPair(card1Name,card2Name)){
+        card1="";
+        card2="";
+        $("#pairs_clicked").text(memoryGame.pairsClicked.toString());
+        $("#pairs_guessed").text(memoryGame.pairsGuessed.toString());
+        if(memoryGame.isFinished()){
+          alert("El juego a terminado!!!!. Presione Sobre Score para reiniciar.");
+        }
+      }
+      else{
+        setTimeout (function(){
+          card1.find("div:nth-child(1)").toggleClass("front blocked back");
+          card1.find("div:nth-child(2)").toggleClass("back blocked front");
+          card2.find("div:nth-child(1)").toggleClass("front blocked back");
+          card2.find("div:nth-child(2)").toggleClass("back blocked front");
+          card1="";
+          card2="";
+        }, 380);
+        $("#pairs_clicked").text(memoryGame.pairsClicked.toString());
+        $("#pairs_guessed").text(memoryGame.pairsGuessed.toString());
+      }
+    }
+
   });
+
+  $("#score").click(function(){
+    memoryGame.shuffleCards();
+    memoryGame.pairsClicked=0;
+    memoryGame.pairsGuessed=0;
+    $("#memory_board>div.card").empty();
+    html='';
+    creaTablero(memoryGame);
+
+    // Add all the div's to the HTML
+    $('#memory_board').html(html);
+    $("#pairs_clicked").text(memoryGame.pairsClicked.toString());
+    $("#pairs_guessed").text(memoryGame.pairsGuessed.toString());
+
+  });
+  function creaTablero(){
+    memoryGame.cards.forEach(function (pic) {
+      html += '<div class="card" data-card-name="'+ pic.name +'">';
+      html += '  <div class="back" name="'+ pic.img +'"></div>';
+      html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+      html += '</div>';
+    });
+  }
+  function validaSeleccionadas(){
+    if(card1!="" && card2!="")
+      return true;
+    else
+      return false;
+  }
 });
-
-
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,35 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards=[];
+  this.pairsClicked=0;
+  this.pairsGuessed=0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  let cardTemp;
+  let j;
+  for(let i=this.cards.length-1; i>0; i--){
+    j=Math.floor(Math.random()*(i+1));
+    cardTemp=this.cards[i];
+    this.cards[i]=this.cards[j];
+    this.cards[j]=cardTemp;
+  }
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked=this.pairsClicked+1;
+  if(firstCard===secondCard){
+    this.pairsGuessed++;
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if(this.pairsGuessed===this.cards.length/2)
+    return true;
+  else
+    return false;
 };
