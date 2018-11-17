cadf7b3ec07792377e3e3d2a0b7860f7390f109c
Santiago Hinojosa <santiago@Santiagos-MacBook-Pro.local>
2018-11-17 10:48:38 -0600

    almost done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,19 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 }
+
+
 
 MemoryGame.prototype.isFinished = function () {
 };
