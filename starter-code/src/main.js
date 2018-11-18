39c2354c52eefaa593e09ec0fb758484d6ff5d30
Moises Garcia <moisesgarcia83@gmail.com>
2018-11-18 22:46:53 +0100

    work finished

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -28,20 +28,64 @@ var cards = [
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
+
+  memoryGame.shuffleCards(); //barajeamos las cartas
+
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
+
   });
 
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
+
   // Bind the click event of each element to a function
+  
+  
+  setTimeout(function() {
+    $(".front").switchClass("front","back",100);
+    $(".back").switchClass("back","front",100);
+  },1500);
+  
+  $(".front").switchClass("front","back",100);
+  $(".back").switchClass("back","front",100);
+  // hacemos un setTimeout para monstrar durante un tiempo las cartas y volvemos a poner boca abajo.
+
   $('.back').click(function () {
-    // TODO: write some code here
+
+   memoryGame.pickedCards.push(this); //añadimos a pickedCatd la carta pulsada
+   classChange(this); //llamamos a la función que cambia las clases para poder mostrar la carta que hemos pulsado
+   $("#pairs_clicked").text(memoryGame.pairsClicked); //añadimos el nº de veces que hemos clickeado carta
+   $("#pairs_guessed").text(memoryGame.pairsGuessed); //añadimos el nº de parejas acertadas
+   
+   if (memoryGame.pickedCards.length === 2) {
+      if (memoryGame.checkIfPair(memoryGame.pickedCards[0].getAttribute("name"), memoryGame.pickedCards[1].getAttribute("name"))){
+        $(".faceup").toggleClass("faceup");
+        if (memoryGame.isFinished()) {
+          $("#pairs_clicked").text(memoryGame.pairsClicked); //añadimos el nº de veces que hemos clickeado carta
+          $("#pairs_guessed").text(memoryGame.pairsGuessed); //añadimos el nº de parejas acertadas
+          
+          setTimeout(function() {
+            alert ("The mission is finished!!!");
+          },100);//mandamos un alert cuando termina el juego, con un setTimeour para monstrar la última carta.
+        }
+      } 
+      else {
+        setTimeout(function() {
+          classChange(".faceup");
+        },100);      
+      } 
+    // si hemos pulsado dos cartas comprobamos que esas cartas son pareja, si lo son, las dejamos con la
+    // clase faceup y las mostramos, si no le cambiamos la clase y con setTimeout marcamos el tiempo que se muestra la segunda carta
+    memoryGame.pickedCards = [];
+    }
   });
 });
+
+
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,35 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
+
 MemoryGame.prototype.shuffleCards = function () {
+  this.cards.sort(function(a,b){
+    return Math.random() - 0.5;
+  });
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+this.pairsClicked++;
+if(firstCard === secondCard){
+  this.pairsGuessed++;
+  return true;
+} else {return false;}
 }
 
 MemoryGame.prototype.isFinished = function () {
-};
+if (this.pairsGuessed === this.cards.length/2){
+  return true;
+} else {return false;};
+}; //si la cantidad de parejas adivinadas son iguales que la mitad de la longitud de las cartas, el juego termina
+
+function classChange(elm) {
+  $(elm).toggleClass("front");
+  $(elm).toggleClass("back");
+  $(elm).siblings().toggleClass("front");
+  $(elm).siblings().toggleClass("back");
+  $(elm).toggleClass("faceup");
+}; // con esta función simplificamos las llamadas para cambiar la clase durante el juego.
