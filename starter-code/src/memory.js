a95a9c0695a19c6d5763278b43efbeb7fdc1c66b
Mario Quiroga <marioqui135@gmail.com>
2018-11-20 12:22:17 +0100

    WIP

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,21 @@
-var MemoryGame = function (cards) {
-  this.cards = cards;
-};
-
-MemoryGame.prototype.shuffleCards = function () {
-};
-
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++
+  
+
+  if (firstCard === secondCard) {
+    this.pairsGuessed++
+    return true
+  } else {
+    return false
+  }
 }
 
 MemoryGame.prototype.isFinished = function () {
-};
+   if (this.pairsGuessed.length < (cards.length/2)) {
+     return false
+   } else if (this.pairsGuessed === (cards.length/2)) {
+     return true
+   } else {
+     return false
+   }
+  }
