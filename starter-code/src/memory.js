114cb0df20644b9a9d5ac108464cc9085d0b9361
Chris Rodriguez <crodr521@gmail.com>
2018-11-20 19:46:27 -0500

    First commit - added constructor values and checkIfPair function; Yet to be tested

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,11 +1,22 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.cardCounter = 0;
+  this.firstCard = "";
+  this.secondCard = "";
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  if($(this.firstCard).attr("name") == $(this.secondCard).attr("name")) {
+    this.pairsGuessed++;
+  }
+  else {
+    return false;
+  }
 }
 
 MemoryGame.prototype.isFinished = function () {
