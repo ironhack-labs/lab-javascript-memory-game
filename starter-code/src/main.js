6cdd88dc552dc5f11a2b5f5a20a131e260a88a40
sogchs <i2implantologia@Mac-Pro-de-i2.local>
2018-11-21 10:30:48 +0100

    juego terminado

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,8 +27,8 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
-  memoryGame.shuffleCards(); //meto dentro la llamada y creacion del nuevo juego la funcion mezcla de cartas
-  
+  memoryGame.shuffleCards();
+
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
@@ -40,20 +40,68 @@ $(document).ready(function(){
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
-  // Bind the click event of each element to a function
+  //Bind the click event of each element to a function
   $('.back').click(function () {
-    
-    $(this).hide();
-    $(this).siblings().addClass('back'); //siblings llama a cada uno de los hermmanos de la clase back
-    
-    //tengo que meter la condicional de las parejas de cartas
-    
-    
-  
+    memoryGame.pickedCards.push(this.getAttribute('name'));
+    console.log(memoryGame.pickedCards);
    
-  });
-  //tengo que meter la condición juego terminado
-  
+    //siblings llama a cada uno de los hermmanos de la clase back
+    $(this).removeClass('back').addClass('front');
+    $(this).siblings().addClass('back').removeClass('front');
+    
+      
+    //condicion si tengo dos cartas levantadas
+    if (memoryGame.pickedCards.length === 2){
+      console.log("Tengo dos cartas levantadas");
+      // condicion si las dos cartas son iguales
+      if (memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1])) {
+        console.log("Las dos cartas levantadas son iguales");
+        //suma los click
+        $("#pairs_clicked").text(memoryGame.pairsClicked);
+        //suma la pareja encontrada
+        $("#pairs_guessed").text(memoryGame.pairsGuessed);
+        // si son iguales vuelve a dejar pickerCards a cero
+        memoryGame.pickedCards = [];
+        //si has llegado al final vuelve a empezar
+        if (memoryGame.isFinished()) {
+          console.log("Has terminado")
+          setTimeout (function(){
+          if (window.confirm("Enhorabuena, has completado el juego, si quieres jugas otra vez pulsa Aceptar")) { 
+                      window.open("window.location.reload(true);", "Buena suerte!");
+                    }
+          }, 300);
+          
+        }
+      }
+      //codicion si las dos cartas no so iguales
+      else {
+        console.log("Las dos cartas no son iguales")
+        //suma los click
+        $("#pairs_clicked").text(memoryGame.pairsClicked);
+        //voltea de nuevo las dos cartas
+        setTimeout(function() {
+          for (var i = 0; i < memoryGame.pickedCards.length; i++) {
+            $('[name="' + memoryGame.pickedCards[i] + '"]').addClass("back").removeClass("front");
+            $('[name="' + memoryGame.pickedCards[i] + '"]').siblings().addClass("front").removeClass("back");
+            //$('[name="' + memoryGame.pickedCards[i] + '"]').addClass("front").removeClass("back");
+          }
+          memoryGame.pickedCards = [];
+        }, 200);
+      
+
+      }
+    }
+  })
+
 });
+
+
+/**
+ * css
+
+[name="carlos"] {
+  color: red;
+}
+ */
 
 
