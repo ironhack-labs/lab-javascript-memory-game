cac2f543c4751f6b71ee74ad7ab7341eb51c986c
Jamie Egan <j.egan@mac.com>
2019-01-15 18:40:59 +0100

    BER - Jamie Done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,33 @@
-var MemoryGame = function (cards) {
-  this.cards = cards;
-};
-
-MemoryGame.prototype.shuffleCards = function () {
-};
-
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-}
-
-MemoryGame.prototype.isFinished = function () {
+class MemoryGame {
+  constructor(cards) {
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsGuessed = 0;
+    this.pairsClicked = 0;
+  }
+  shuffleCards() {
+    var shuffled = [];
+    var arrLength = this.cards.length
+    for(var i = 0; i< arrLength; i++) {
+      shuffled.push(this.cards
+        .splice(Math.floor(Math.random() * this.cards.length),1)[0]);
+    }
+    this.cards = shuffled;
+  }
+  checkIfPair(firstCard, secondCard) {
+    this.pairsClicked += 1;
+    if(firstCard === secondCard) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    if(this.pairsClicked < (this.cards.length/2)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 };
