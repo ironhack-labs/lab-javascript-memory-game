e575e5a0f772776969967630f55bc25517d22b6d
Sofía <sofianombela@gmail.com>
2019-01-15 20:50:32 +0100

    Check if pair and is Finished done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,26 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
-};
+}
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked ++;
+  
+  if (firstCard === secondCard){
+    this.pairsGuessed ++;
+    return true;
+  } else {
+    return false;
+  }
 }
+
+
+
 
 MemoryGame.prototype.isFinished = function () {
 };
