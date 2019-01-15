2597c0f996f67c541407999ade676304d6469467
jsanz24 <jesus240492@gmail.com>
2019-01-15 22:43:53 +0100

    version final

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -6,7 +6,7 @@ var cards = [
   { name: 'flash',           img: 'flash.jpg' },
   { name: 'green arrow',     img: 'green-arrow.jpg' },
   { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
+  { name: 'ironman',         img: 'ironman.jpg' },// Bind the click event of each element to a function
   { name: 'spiderman',       img: 'spiderman.jpg' },
   { name: 'superman',        img: 'superman.jpg' },
   { name: 'the avengers',    img: 'the-avengers.jpg' },
@@ -28,19 +28,48 @@ var cards = [
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
+  memoryGame.shuffleCards();
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
   });
-
-  // Add all the div's to the HTML
+  
   $('#memory_board').html(html);
 
-  // Bind the click event of each element to a function
+  var card1;
+  var card2;
   $('.back').click(function () {
-    // TODO: write some code here
+    if(card1 == undefined){
+      card1 = $(this);
+      $(this).toggleClass("back front");
+      $(this).next().toggleClass("back front");
+    } 
+    else {
+      card2 = $(this);
+      $(this).toggleClass("back front");
+      $(this).next().toggleClass("back front");
+      var result = memoryGame.checkIfPair($(card1).attr("name"),$(card2).attr("name"));
+      $("#pairs_clicked").text(memoryGame.pairsClicked);
+      $("#pairs_guessed").text(memoryGame.pairsGuessed);
+      setTimeout(function(){
+        if(result){
+          card1 = undefined;
+          card2 = undefined;
+          var fin = memoryGame.isFinished();
+          if(fin) alert("Congatulations, you win with " + memoryGame.pairsClicked + " moves");
+        }
+        else{
+          $(card2).toggleClass("back front");
+          $(card2).next().toggleClass("back front");
+          $(card1).toggleClass("back front");
+          $(card1).next().toggleClass("back front");
+          card1 = undefined;
+          card2 = undefined;
+        } 
+      },200);
+    }
   });
 });
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,30 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  var newArr = [];
+  for(var i = 0; i < this.cards.length; i++){
+    var random = Math.trunc(Math.random()*cards.length);
+    if((newArr.indexOf(cards[random]) != -1) && (newArr.indexOf(cards[random],newArr.indexOf(cards[random])) != -1)) i--;
+    else newArr.push(cards[random]);
+  }
+  this.cards = newArr;
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++
+  if(firstCard == secondCard) {
+    this.pairsGuessed++;
+    return true;
+  }
+  else return false;
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if(this.pairsGuessed === this.cards.length/2) return true;
+  else return false;
 };
