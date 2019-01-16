3a02257e05f941db2cbecf1bd3559c3c56594a9d
Rojiblanco182 <robertosalinas1988@gmail.com>
2019-01-16 09:48:34 +0100

    incomplete

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,34 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
-};
+
+  var newCards = [];
+  var randomHero = 0;
+
+  while (cards.length > 0) {
+    randomHero = Math.floor(Math.random * cards.length);
+    newCards.push(cards[randomHero]);
+    cards.splice(randomHero, 1);
+  }
+  cards = newCards;
+}
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++;
+  if (firstCard == secondCard) {
+    this.pairsGuessed++;
+    return true;
+  }
+  else return false;
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.pairsClicked == 0) return false;
+  else if (this.pairsGuessed == this.cards.length / 2) return true;
+  else return false;
 };
