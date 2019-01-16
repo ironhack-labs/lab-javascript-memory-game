e617cd0e33a88dcd43741c87bc3a6e65eb174cc4
mzclas <Mzclas@gmail.com>
2019-01-16 15:02:36 +0100

    primera parte completa

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,32 @@
-var MemoryGame = function (cards) {
+var MemoryGame = function(cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function() {
+  var carsdRandom = [];
+  for (var i = this.cards.length; i > 0; i--) {
+    var card = Math.floor(Math.random() * i);
+    cardOut = this.cards.splice(card, 1);
+    carsdRandom.push(cardOut);
+  }
+  this.cards = carsdRandom;
 };
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-}
+MemoryGame.prototype.checkIfPair = function(firstCard, secondCard) {
+  this.pairsClicked += 1;
+  if (firstCard === secondCard) {
+    this.pairsGuessed += 1;
+    return true;
+  }
+  return false;
+};
 
-MemoryGame.prototype.isFinished = function () {
+MemoryGame.prototype.isFinished = function() {
+  if (this.pairsGuessed === this.cards.length / 2) {
+    return true;
+  }
+  return false;
 };
