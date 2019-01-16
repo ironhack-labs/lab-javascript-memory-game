383b9f1e0e27b8a260fedff95c41788ce49a0c46
Pedro Arantes <pedro.hga90@gmail.com>
2019-01-16 11:19:46 +0100

    DONE

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,8 +25,10 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
+var memoryGame;
+
 $(document).ready(function(){
-  var memoryGame = new MemoryGame(cards);
+  memoryGame = new MemoryGame(cards);
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
@@ -39,8 +41,40 @@ $(document).ready(function(){
   $('#memory_board').html(html);
 
   // Bind the click event of each element to a function
+
   $('.back').click(function () {
-    // TODO: write some code here
+    if (memoryGame.pickedCards.length < 2) {
+      // Flip the card by changing 'front'->'back' and 'back'->'front'
+      $(this).parent().children().toggleClass('front back')
+      // Save the DOM card element in memoryGame.pickedCards
+      memoryGame.pickedCards.push($(this).parent())
+
+      if (memoryGame.pickedCards.length === 2) {
+        // Take the attribute "data-card-name" from the DOM elements we saved
+        var firstCard = memoryGame.pickedCards[0].data('card-name')
+        var secondCard = memoryGame.pickedCards[1].data('card-name')
+        if (memoryGame.checkIfPair(firstCard,secondCard)){
+          // Reinitialize memoryGame.pickedCards so that the user can pick cards again
+          memoryGame.pickedCards = []
+          if (memoryGame.isFinished()){
+            // Remove the cards from the '#memory_board' to display: you win!
+            $('#memory_board').html("You win!");
+          }
+        }
+        else {
+          // Flip the 2 picked cards after 1 second
+          setTimeout(function(){
+            // Flip the card by changing 'front'->'back' and 'back'->'front'
+            memoryGame.pickedCards[0].children().toggleClass('front back')
+            memoryGame.pickedCards[1].children().toggleClass('front back')
+            memoryGame.pickedCards = []
+          }, 1000)
+        }
+      }
+    }
+    // Update Pairs Clicked and Pairs Guessed
+    $('#pairs_clicked').text(memoryGame.pairsClicked)
+    $('#pairs_guessed').text(memoryGame.pairsGuessed)
   });
 });
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,42 @@
 var MemoryGame = function (cards) {
-  this.cards = cards;
+  this.cards = cards,
+  this.pickedCards = [],
+  this.pairsClicked = 0,
+  this.pairsGuessed = 0,
+  this.shuffleCards()
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  for(var tmp, cur, top = this.cards.length; top--;) {
+    cur = (Math.random()* (top + 1)) << 0 ;
+    tmp = this.cards[cur];
+    this.cards[cur] = this.cards[top];
+    this.cards[top] = tmp
+  }
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+ 
+  this.pairsClicked++
+  if(firstCard === secondCard){
+    this.pairsGuessed++
+    return true
+  } else return false
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if(this.pairsGuessed === this.cards.length/2){
+    return true
+  } else return false
 };
+
+
+
+
+// function shuffle(array) {
+//   for (var tmp, cur, top=array.length; top--;){
+//     cur = (Math.random() * (top + 1)) << 0;
+//     tmp = array[cur]; array[cur] = array[top]; array[top] = tmp;
+//   }
+//   return array;
+// }
