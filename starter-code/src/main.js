ee1e8734179c96f9dc743d3b311b9776c732862a
Henrique Mendes <henrique.gmendes@hotmail.com>
2019-01-16 10:52:05 -0200

    Commit to Memory Game Exercise

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -28,9 +28,10 @@ var cards = [
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
+  // memoryGame.shuffleCards(cards);  HÁ ALGUM ERRO NA FUNÇÃO SHUFFLECARDS QUE NAO RETORNA A QUANTIDADE TOTAL DE CARTAS NO CROWSER AO CARREGAR O HTML
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
-    html += '  <div class="back" name="'+ pic.img +'"></div>';
+    html += '  <div class="back " name="'+ pic.img +'"></div>';
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
   });
@@ -39,9 +40,28 @@ $(document).ready(function(){
   $('#memory_board').html(html);
 
   // Bind the click event of each element to a function
-  $('.back').click(function () {
-    // TODO: write some code here
+  $('.card').click(function () {
+    $(this).children().toggleClass('back front'); //change class from back to front between each card
+    memoryGame.pickedCards.push($(this)); //add .card element data to 'pickedCards' array
+    console.log(memoryGame.pickedCards); //print in console to test
+    if (memoryGame.pickedCards.length === 2) {
+      if (memoryGame.checkIfPair(memoryGame.pickedCards[0].attr('data-card-name'), memoryGame.pickedCards[1].attr('data-card-name'))) { //conditional calling 'checkIfPair' function to test if both selected cards are equal
+        $(memoryGame.pickedCards[0]).addClass('front blocked'); //if equal, add 'front block' classes to prevent this card to be unflipped
+        $(memoryGame.pickedCards[1]).addClass('front blocked'); //if equal, add 'front block' classes to prevent this card to be unflipped
+        memoryGame.pickedCards = []; //clear 'pickedCards' array to receive next pair of cards
+        $('#pairs_clicked').text(memoryGame.pairsClicked); //add +1 to 'Pairs Clicked' score
+        $('#pairs_guessed').text(memoryGame.pairsGuessed); //add +1 to 'Pairs Guessed' score
+      } else { //if cards are NOT equal, it will process the below code
+          setTimeout (function() { //setTimeOut called to give time to player see what cards he flipped before they unflip again.
+          $('#pairs_clicked').text(memoryGame.pairsClicked); //add +1 to 'Pairs Clicked' score
+          $(memoryGame.pickedCards[0]).children().toggleClass('back front'); //element related to pickedCards[0] will unflip by toggling classes 'back' and 'front'
+          $(memoryGame.pickedCards[1]).children().toggleClass('back front'); ////element related to pickedCards[1] will unflip by toggling classes 'back' and 'front'
+          memoryGame.pickedCards = []; //clear 'pickedCards' array to receive next pair of cards
+        }, 500); 
+      }
+    }
+    if(memoryGame.isFinished()) {
+      alert('PARABENS!! ATUALIZE A PAGINA PARA REINICIAR O GAME!'); //display an alert in the browser with instructions to reset the game
+    } 
   });
 });
-
-
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,37 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function (cards) {
+  let randomNumber = 0;
+  let n = this.cards.length;
+  while (n >= 0) {
+    randomNumber = Math.floor(Math.random() * this.cards.length);
+    this.pickedCards.push(this.cards[randomNumber]);
+    delete this.cards[randomNumber];
+    n -= 1;
+  }
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-}
+  if (firstCard === secondCard) {
+    this.pairsClicked += 1;
+    this.pairsGuessed += 1;
+    return true;
+  }
+  this.pairsClicked += 1;
+  return false;
+};
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.pairsClicked === 0) {
+    return false;
+  }
+  if (this.pairsGuessed >= 12) {
+    return true;
+  }
+  return false
 };
