8d069649e6caacfafaf357d62b6d5735120cc289
CGuillotG <c.guillot@outlook.com>
2019-01-22 17:45:53 -0600

    Jasmine exitoso, memory.js completado

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,27 @@
-var MemoryGame = function (cards) {
+var MemoryGame = function(cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function() {
+  let counter = this.cards.length;
+  while (counter > 0) {
+    let i = Math.floor(Math.random() * counter);
+    counter--;
+    let temp = this.cards[counter];
+    this.cards[counter] = this.cards[i];
+    this.cards[i] = temp;
+  }
 };
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-}
+MemoryGame.prototype.checkIfPair = function(firstCard, secondCard) {
+  this.pairsClicked++;
+  if (firstCard === secondCard) this.pairsGuessed++;
+  return firstCard === secondCard;
+};
 
-MemoryGame.prototype.isFinished = function () {
+MemoryGame.prototype.isFinished = function() {
+  return (this.pairsGuessed === this.cards.length/2)
 };
