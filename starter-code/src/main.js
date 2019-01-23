1de8ce49726952692eeb749ffc9d123ba41b045b
iamalexrodriguez <alex.rdz.t@gmail.com>
2019-01-23 09:38:03 -0600

    ejercicio avanzado

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,9 +25,14 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
+
+
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
+
+  // Para hacer el shuffle del array
+  //var shuffledArray = memoryGame.shuffleCards(cards)
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
@@ -38,8 +43,65 @@ $(document).ready(function(){
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
+
+
+
+
+
   // Bind the click event of each element to a function
   $('.back').click(function () {
+
+    //Verificar si son dos cartas
+    if (memoryGame.pickedCards.length < 2){
+      var nameCard = $(this).attr('name')
+      memoryGame.pickedCards.push(nameCard)
+      console.log(memoryGame.pickedCards)
+      //Voltear cartas
+      $(this).toggleClass('back')
+      $(this).next().toggleClass('back')
+    }
+
+
+    //Ya que son dos cartas
+    if(memoryGame.pickedCards.length == 2){
+      var evaluation = memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1])
+      if (evaluation) {
+        //Reset array
+        memoryGame.pickedCards.splice(0,2)
+        console.log(memoryGame.pairsGuessed)
+      } else {
+        //Reset array, voltear cartas
+
+        memoryGame.pickedCards.splice(0,2)
+       // setTimeout(function(){ 
+        $(this).next().toggleClass('back')
+        $(this).toggleClass('back')
+        
+
+        console.log("false desde if")
+     // },700)
+      }
+  
+    }
+
+
+    //Actualizar score
+    $("#pairs_clicked").text(memoryGame.pairsClicked)
+    $("#pairs_guessed").text(memoryGame.pairsGuessed)
+
+    //Revisar si gano
+
+    if(memoryGame.isFinished()){
+      alert("Ganaste!")
+      //$('.card').addClass("back front")
+      $('.card').next().toggleClass('back')
+      $('.card').toggleClass('back')
+
+
+    }
+    
+
+    
     // TODO: write some code here
   });
 });
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,54 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = []
+  this.pairsClicked = 0
+  this.pairsGuessed = 0
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function (array) {
+
+  array = this.cards
+
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+
+
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+ 
+  if(firstCard === secondCard){
+    this.pairsClicked++
+    this.pairsGuessed++
+    return true
+  } else{
+    this.pairsClicked++
+    return false
+  }
+
 }
 
 MemoryGame.prototype.isFinished = function () {
+
+  if (this.cards.length/ 2 === this.pairsGuessed){
+    return true
+  } else {
+    return false
+  }
+
+  
+
 };
