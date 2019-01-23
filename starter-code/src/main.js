9f3c7778bcdacf5b9dfbb07a0df99ecf231ab7a5
Pol Goma <polgoide@gmail.com>
2019-01-22 19:52:10 -0600

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -28,6 +28,7 @@ var cards = [
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
+  let newCards =  memoryGame.shuffleCards(cards)
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
@@ -37,10 +38,56 @@ $(document).ready(function(){
 
   // Add all the div's to the HTML
   $('#memory_board').html(html);
-
+console.log($('.card > div:nth-child(2)'))
+  let cartaArriba = ""
+  let cartaNode
+  let cartaNode2
+ 
   // Bind the click event of each element to a function
   $('.back').click(function () {
-    // TODO: write some code here
+  if(cartaArriba === "") {
+    $(this).toggleClass('front')
+    $(this).toggleClass('back')
+    $(this).next().toggleClass('back')
+    $(this).next().toggleClass('front')
+    cartaArriba = $(this).parent().attr("data-card-name")
+    cartaNode = $(this)
+  } else {
+    $(this).toggleClass('front')
+    $(this).toggleClass('back')
+    $(this).next().toggleClass('back')
+    $(this).next().toggleClass('front')
+    if(memoryGame.checkIfPair($(this).parent().attr("data-card-name"),cartaArriba) === true) {
+      cartaArriba = ""
+      if(memoryGame.isFinished() ===  true) {
+        alert("You made it! Well done!")
+        $('.card > div:first-child').removeClass('front')
+        $('.card > div:first-child').addClass('back')
+        $('.card > div:nth-child(2)').removeClass('back')
+        $('.card > div:nth-child(2)').addClass('front')
+        memoryGame.pairsClicked = 0
+        memoryGame.pairsGuessed = 0
+        memoryGame.shuffleCards(memoryGame.cards)
+      }
+    } else {
+      cartaNode2 = $(this)
+      setTimeout(delay, 200);
+      function delay(){
+        cartaNode2.removeClass('front')
+        cartaNode2.addClass('back')
+        cartaNode2.next().removeClass('back')
+        cartaNode2.next().addClass('front')
+        cartaNode.removeClass('front')
+        cartaNode.addClass('back')
+        cartaNode.next().removeClass('back')
+        cartaNode.next().addClass('front')
+      }
+      cartaArriba = ""
+    }
+   
+  }
+$('#pairs_clicked').text(memoryGame.pairsClicked)
+$('#pairs_guessed').text(memoryGame.pairsGuessed)
   });
 });
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,42 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = []
+  this.pairsClicked = 0
+  this.pairsGuessed = 0
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function (arr) {
+  arr = this.cards
+ var currentIndex = arr.length, temporaryValue, randomIndex;
+
+ // While there remain elements to shuffle...
+ while (0 !== currentIndex) {
+
+   // Pick a remaining element...
+   randomIndex = Math.floor(Math.random() * currentIndex);
+   currentIndex -= 1;
+
+   // And swap it with the current element.
+   temporaryValue = arr[currentIndex];
+   arr[currentIndex] = arr[randomIndex];
+   arr[randomIndex] = temporaryValue;
+ }
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++
+  if (firstCard === secondCard) {
+    this.pairsGuessed++
+    return true
+  } else {
+    return false
+  }
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.cards.length /  2 === this.pairsGuessed) {
+    return true
+  } else {
+    return false
+  }
 };
