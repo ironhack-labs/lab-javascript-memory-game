19fa635dd698aa64005c3bf935df0715d6c422bc
Isabella Onate <isabellaonate@gmail.com>
2019-01-23 09:37:43 -0600

    No pude hacer que se voltearan las cartas por los hermanos

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -28,6 +28,8 @@ var cards = [
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
+  let globalClicks = 0
+  // memoryGame.shuffleCards(cards)
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
@@ -40,8 +42,32 @@ $(document).ready(function(){
 
   // Bind the click event of each element to a function
   $('.back').click(function () {
-    // TODO: write some code here
-  });
-});
+    $(this).toggleClass('back') //NO TIENE LIMITE Y SI HACES CLICK EN UNA YA ABIERTA SE VA A CERRAR Y VOLVER A EVALUAR PERO EQUIS
+    $(this).next().toggleClass('back')
+
+    memoryGame.pickedCards.push(this)
+    if (memoryGame.checkIfPair(memoryGame.pickedCards)) {
+      $("#pairs_guessed").text(memoryGame.pairsGuessed)
+    } else {
+      if (memoryGame.pickedCards.length === 2) {
+        $(memoryGame.pickedCards[0]).toggleClass('back')
+        $(memoryGame.pickedCards[1]).toggleClass('back')
+      }
+    }
+    globalClicks++
+    if (globalClicks%2 === 0) {
+      memoryGame.pairsClicked++
+      $("#pairs_clicked").text(memoryGame.pairsClicked)
+      memoryGame.pickedCards = [] 
+    }
+
+    // for (let i = 0; i < 2; i++) {
+      // $(pickedCards).append($(this)) //NO SIRVE PUTAMADREEEEE
+      // MemoryGame.checkIfPair(pickedCards)
+    // }
+    
+  })
+
+})
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,81 @@
+
+
 var MemoryGame = function (cards) {
-  this.cards = cards;
+  this.cards = cards
+  this.pickedCards= []
+  this.pairsClicked = 0
+  this.pairsGuessed = 0
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+// let shufflemuffle = (arr) => {
+//   let randomNumber = Math.floor(Math.random()*arr.length)
+//   let chosenOne
+//   let shuffledArray = []
+//   for (i = 0; i <= arr.length; i++) {
+//     chosenOne = arr[randomNumber]
+//     shuffledArray.push(chosenOne)
+//   }
+//   return shuffledArray
+// }
+
+MemoryGame.prototype.shuffleCards = function (arr) {
+  // let randomNumber = Math.floor(Math.random()*arr.length)
+  // for (i = 0; i <= arr.length; i++) {
+  //   arr[randomNumber]
+  // }
+
+    var currentIndex = arr.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = arr[currentIndex];
+      arr[currentIndex] = arr[randomIndex];
+      arr[randomIndex] = temporaryValue;
+    }
+
+
 };
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+
+
+// console.log(shufflemuffle(this.cards))
+
+MemoryGame.prototype.checkIfPair = function (arr) {
+
+if (arr.length === 2) {
+
+  const firstCard = arr[0]
+  const secondCard = arr[1]
+
+  if ($(firstCard).attr('name') === $(secondCard).attr('name')) {
+    this.pairsGuessed++
+    return true
+  }
+  return false
+}
+return false
+
+
+
+// if (firstCard[name] === secondCard[name]) {
+//   this.pairsGuessed++
+//   this.pairsClicked++
+//   removeClass('back') //COMO DIRECCIONO EL REMOVE CLASS A LO QUE HICE CLICK?????
+// } else {
+//   toggleClass('back') 
+//   this.pairsClicked++
+// }
+
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.pairsClicked === this.cards.length/2) {
+    alert("Tha winner issa yu!")
+  }
 };
