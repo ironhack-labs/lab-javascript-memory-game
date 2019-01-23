ef58956f59b65e683b2cdcf253e431fddf5b731e
Maldonado <henry4frank1@gmail.com>
2019-01-23 08:14:49 -0600

    Terminado con dificultad y ayuda

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,8 +25,10 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
+var memoryGame = 
+
 $(document).ready(function(){
-  var memoryGame = new MemoryGame(cards);
+  memoryGame = new MemoryGame (cards)
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
@@ -38,10 +40,45 @@ $(document).ready(function(){
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
+
   // Bind the click event of each element to a function
-  $('.back').click(function () {
-    // TODO: write some code here
+  $(".back").click(function() {
+    if (memoryGame.pickedCards.length < 2) {
+      $(this)
+        .parent()
+        .children()
+        .toggleClass("back front");
+      //Save DOM element into array
+      memoryGame.pickedCards.push($(this).parent());
+    }
+    if (memoryGame.pickedCards.length === 2) {
+      //Takes the attribute once picked a pair of cards
+      var firstCard = memoryGame.pickedCards[0].data("card-name");
+      var secondCard = memoryGame.pickedCards[1].data("card-name");
+      if (memoryGame.checkIfPair(firstCard, secondCard)) {
+        memoryGame.pickedCards = [];
+      }
+      if (memoryGame.isFinished()) {
+        alert('YOU WON')
+      } else {
+        setTimeout(function() {
+          //Flips the cards once tested if they are equal
+          memoryGame.pickedCards[0].children().toggleClass("back front");
+          memoryGame.pickedCards[1].children().toggleClass("back front");
+          memoryGame.pickedCards = [];
+        }, 1000);
+      }
+    }
+ 
+     $("#pairs_clicked").text(memoryGame.pairsClicked);
+    $("#pairs_guessed").text(memoryGame.pairsGuessed);
   });
-});
+ })
+ 
+
+
+
+
+
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,43 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards=[]
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+  this.shuffleCards()
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  this.cards.sort(function(a,b){
+       return Math.random()-0.5;
+     });
 };
 
+
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked ++;
+  if (firstCard === secondCard) {
+    this.pairsGuessed ++;
+    return true;
+  } else {
+    return false;
+  }
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.pairsGuessed === this.cards.length/2) {
+    return true;
+  } else {
+    return false;
+    }
 };
+
+
+
+
+
+
+
+
+ 
+ 
+
