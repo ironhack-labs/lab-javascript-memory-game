bc940e60eb232ee46dc0a080acb66768554e7b15
Mike <troianello@protonmail.com>
2019-02-01 16:12:10 -0500

    FTWD Miami Javascript Memory Game

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,9 +25,13 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
+
+
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
+  MemoryGame.prototype.shuffleCards();
+  
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
@@ -35,13 +39,32 @@ $(document).ready(function(){
     html += '</div>';
   });
 
+  
+
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
   // Bind the click event of each element to a function
   $('.back').click(function () {
     // TODO: write some code here
-  });
+    
+   if (memoryGame.pickedCards.length < 2 ) {
+    $(this).parent().children().toggleClass('back front');
+    memoryGame.pickedCards.push($(this).parent());
+   }
+   if (memoryGame.pickedCards.length == 2) {
+     var firstCard = memoryGame.pickedCards[0].data('cardName');
+     var secondCard = memoryGame.pickedCards[1].data('cardName');
+     if (memoryGame.checkIfPair(firstCard, secondCard)) {
+      memoryGame.pickedCards = [];
+     }
+     else {
+      memoryGame.pickedCards = [];
+     }
+     
+   }
+
+   })
+  
+
 });
-
-
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,49 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  this.cards = cards;
+    var currentIndex = this.cards.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = this.cards[currentIndex];
+      this.cards[currentIndex] = this.cards[randomIndex];
+      this.cards[randomIndex] = temporaryValue;
+    }
+  
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  console.log(firstCard, secondCard)
+  if (firstCard == secondCard) {
+    this.pairsGuessed ++;
+    this.pairsClicked ++;
+    return true;
+  }
+  else {
+    this.pairsClicked ++;
+    return false;
+  }
+  
 }
 
 MemoryGame.prototype.isFinished = function () {
+  console.log(this)
+  if(this.pairsGuessed >= (8)) {
+    return true;
+  }
+  else {
+    return false;
+  }
 };
