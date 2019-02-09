e9db9d7ac6bf16f209e5e84c42628b50e3fa1dcc
Jelte Homminga <jeltehomminga@gmail.com>
2019-02-09 13:44:28 +0100

    add methods to Class MemoryGame

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -1,81 +1,52 @@
 var guessCardOpen = "";
-var guessCardOpenTwo ="";
+var guessCardOpenTwo = "";
 var pairsGuessed = 0;
 var pairsClicked = 0;
 
 
 var cards = [
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
+  { name: 'aquaman', img: 'aquaman.jpg' },
+  { name: 'batman', img: 'batman.jpg' },
   { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' },
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
+  { name: 'fantastic four', img: 'fantastic-four.jpg' },
+  { name: 'flash', img: 'flash.jpg' },
+  { name: 'green arrow', img: 'green-arrow.jpg' },
+  { name: 'green lantern', img: 'green-lantern.jpg' },
+  { name: 'ironman', img: 'ironman.jpg' },
+  { name: 'spiderman', img: 'spiderman.jpg' },
+  { name: 'superman', img: 'superman.jpg' },
+  { name: 'the avengers', img: 'the-avengers.jpg' },
+  { name: 'thor', img: 'thor.jpg' },
+  { name: 'aquaman', img: 'aquaman.jpg' },
+  { name: 'batman', img: 'batman.jpg' },
   { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' }
+  { name: 'fantastic four', img: 'fantastic-four.jpg' },
+  { name: 'flash', img: 'flash.jpg' },
+  { name: 'green arrow', img: 'green-arrow.jpg' },
+  { name: 'green lantern', img: 'green-lantern.jpg' },
+  { name: 'ironman', img: 'ironman.jpg' },
+  { name: 'spiderman', img: 'spiderman.jpg' },
+  { name: 'superman', img: 'superman.jpg' },
+  { name: 'the avengers', img: 'the-avengers.jpg' },
+  { name: 'thor', img: 'thor.jpg' }
 ];
 
 // $(document).ready(function(){
-  var memoryGame = new MemoryGame(cards);
-  var html = '';
-  memoryGame.cards.forEach(function (pic) {
-    html += '<div class="card" data-card-name="'+ pic.name +'">';
-    html += '  <div class="back" name="'+ pic.img +'"></div>';
-    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
-    html += '</div>';
-  });
+var memoryGame = new MemoryGame(cards);
+memoryGame.shuffleCards();
 
-  // Add all the div's to the HTML
-  $('#memory_board').html(html);
+var html = '';
+memoryGame.cards.forEach(function (pic) {
+  html += '<div class="card" data-card-name="' + pic.name + '">';
+  html += '  <div class="back" name="' + pic.img + '"></div>';
+  html += '  <div class="front" style="background: url(img/' + pic.img + ') no-repeat"></div>';
+  html += '</div>';
+});
 
+// Add all the div's to the HTML
+$('#memory_board').html(html);
 
-  const toggleGuessCards = (card1, card2) => {
-    card1.toggleClass('back front');
-    card1.siblings().toggleClass('front back');
-    if (card2) {
-      card2.toggleClass('back front'); 
-      card2.siblings().toggleClass('front back');      
-      guessCardOpen = "" ;
-      guessCardOpenTwo = "";      
-  }}  
+$('.back').click(function () {
+  memoryGame.guessCard($(this))
+});
 
-  const clearGuessCards = () =>{
-    
-  }
-
-  // Bind the click event of each element to a function
-  $('.back').click(function () {        
-    if (!guessCardOpenTwo) {      
-      toggleGuessCards($(this));
-      if ( guessCardOpen ) {        
-        guessCardOpenTwo = $(this);
-        if ( guessCardOpenTwo.parent().data('card-name') === guessCardOpen.parent().data('card-name'))   {          
-          pairsGuessed ++;
-          pairsClicked ++;
-          guessCardOpen = "" ;
-          guessCardOpenTwo = "";          
-        } else {
-          pairsClicked ++;
-          setTimeout( () => toggleGuessCards( guessCardOpenTwo, guessCardOpen), 1000);       
-        }
-      } else {        
-        guessCardOpen = $(this) ;        
-      }
-    }
-  })
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,4 +1,4 @@
-class MemoryGame  {
+class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     this.guessCardOpenOne = "";
@@ -6,22 +6,40 @@ class MemoryGame  {
     this.pairsGuessed = 0;
     this.pairsClicked = 0;
   }
-  shuffleCards(){
-    for (i =0; i < cards.length; i++) {
+  shuffleCards() {
+    for (let i = 0; i < cards.length; i++) {
       let randomNumber = Math.floor(Math.random() * this.cards.length);
       let tempShuffleOne = this.cards[randomNumber];
       this.cards[randomNumber] = this.cards[i];
       this.cards[i] = tempShuffleOne;
     }
   }
+  guessCard(card) {
+    if (!this.guessCardOpenTwo) {
+      this.toggleGuessCards(card);
+      this.guessCardOpenOne ? this.guessCardTwo(card) : this.guessCardOpenOne = card;
+    }
+  }
+  guessCardTwo(cardTwo) {
+    this.guessCardOpenTwo = cardTwo;
+    this.pairsClicked++
+    this.checkIfPair() ? (this.pairsGuessed++ , this.resetGuessCards()) :
+      setTimeout(() => (this.toggleGuessCards(this.guessCardOpenOne, cardTwo), this.resetGuessCards()), 1000);
+  }
   checkIfPair() {
-    this.guessCardOpenOne === this.guessCardOpenTwo;
+    return this.guessCardOpenOne.parent().data('card-name') === this.guessCardOpenTwo.parent().data('card-name');
   }
-  toggleGuessCards() {
-    
+  toggleGuessCards(card1, card2) {
+    card1.toggleClass('back front');
+    card1.siblings().toggleClass('front back');
+    card2 ? (card2.toggleClass('back front'), card2.siblings().toggleClass('front back')) : "";
   }
-  isFinished() {
+  resetGuessCards() {
+    this.guessCardOpenOne = "";
+    this.guessCardOpenTwo = "";
   }
+  // isFinished() {
+  // }
 }
 
 
