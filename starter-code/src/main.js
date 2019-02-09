f6e57989d6a609cc35722ac8047cb330f6925ddf
Martin <martin.mihalik.nr@gmail.com>
2019-02-09 12:34:44 +0100

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,23 +25,57 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
+var memoryGame = new MemoryGame(cards);
+
+//Shuffling cards
+memoryGame.shuffleCards(cards);
+
 $(document).ready(function(){
-  var memoryGame = new MemoryGame(cards);
   var html = '';
   memoryGame.cards.forEach(function (pic) {
-    html += '<div class="card" data-card-name="'+ pic.name +'">';
+    html += '<div class="card" data-card="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
   });
-
+  
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
   // Bind the click event of each element to a function
   $('.back').click(function () {
-    // TODO: write some code here
+
+    // Changing the classes on click
+    $(this).toggleClass('back front'); 
+    $(this).siblings().toggleClass('back front');
+
+    // Pushing the two cards into an array, so it is possible to compare
+    memoryGame.pickedCards.push($(this));
+
+    // Comparing the cards. If they match, increment the pairs that have been clicked and increment the pairs that has been guessed as well.
+    // If they do not match, setTimeout function will be initialized. After one second, the cards will toggle back the classes to their
+    // backside. In both cases, the array of the two picked cards is cleared.
+    if (memoryGame.pickedCards.length == 2) {
+      if(memoryGame.pickedCards[0].parent().data('card') == memoryGame.pickedCards[1].parent().data('card')) {
+        memoryGame.pairsGuessed++;
+        memoryGame.pairsClicked++;
+        $('#pairs_clicked').html(memoryGame.pairsClicked);
+        $('#pairs_guessed').html(memoryGame.pairsGuessed);
+        memoryGame.pickedCards = [];
+      } else {
+        setTimeout(function(){
+          memoryGame.pickedCards[0].toggleClass('back front');
+          memoryGame.pickedCards[0].siblings().toggleClass('back front');
+          memoryGame.pickedCards[1].toggleClass('back front');
+          memoryGame.pickedCards[1].siblings().toggleClass('back front');
+          memoryGame.pairsClicked++;
+          $('#pairs_clicked').html(memoryGame.pairsClicked);
+          memoryGame.pickedCards = [];
+        }, 1000);
+      }
+    }
+
+    // Victorious method
+    memoryGame.isFinished();
   });
 });
-
-
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,24 @@
+// Constructor
 var MemoryGame = function (cards) {
-  this.cards = cards;
+  this.cards = cards,
+  this.pickedCards = [],
+  this.pairsClicked = 0,
+  this.pairsGuessed = 0
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+// Method for shuffle the cards
+MemoryGame.prototype.shuffleCards = function (cards) {
+  var j, x, i;
+  for(var i = cards.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1))
+    x = cards[i];
+    cards[i] = cards[j];
+    cards[j] = x;
+  }
+  return cards;
 };
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-}
-
+// Method that returns alert, informing that you guessed all the cards
 MemoryGame.prototype.isFinished = function () {
+  return this.pairsGuessed == 12 ? alert('Well played') : '';
 };
