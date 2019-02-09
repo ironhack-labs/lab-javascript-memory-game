e31243c3849df354e3b2459e891466f2ffafab76
Ricardo <unrhs@hotmail.com>
2019-02-09 11:02:07 -0600

    Lab Memory

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,11 +1,32 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = cards;
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0; 
+  this.MemoryGame = 1;
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.pickedCards = function () {
+};
+
+MemoryGame.prototype.pairsClicked = function () {
+
+};
+
+MemoryGame.prototype.pairsGuessed = function () {
+};
+
+
+
+MemoryGame.prototype.shuffleCards = function (pickedCards) {
+this.$cards = $(this.shuffleCards(this.pickedCards));
+
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+if($("firstCard === secondCard"){
+  
+})
 }
 
 MemoryGame.prototype.isFinished = function () {
