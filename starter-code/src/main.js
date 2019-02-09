23237a0cc12491b6bd0ebd0d77b637b8d5f56b9a
Ivonne Yanez Mendoza <ivonne.wftm@gmail.com>
2019-02-09 23:03:58 +0100

    first version

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -26,22 +26,63 @@ var cards = [
 ];
 
 $(document).ready(function(){
-  var memoryGame = new MemoryGame(cards);
+  var memoryGame = new MemoryGame(cards); 
+  //pick cards and shuffle
+  memoryGame.shuffleCards();
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
-  });
 
+    
+  });
+ 
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
+  
+  
+
   // Bind the click event of each element to a function
   $('.back').click(function () {
-    // TODO: write some code here
+    
+    //flip cards front and back
+    $(this).parent().children().toggleClass("front back");
+
+    //put choose cards in a empty array (picked cards from constructor)
+    memoryGame.pickedCards.push($(this).parent());
+    
+    // check if lenght of array (pickedcards) it's strictly large of 2 and start the game 
+    if(memoryGame.pickedCards.length ===2){
+      setTimeout(function(){
+        //check if two cards are the same or not
+        if(!memoryGame.checkIfPair(memoryGame.pickedCards[0].html(), memoryGame.pickedCards[1].html())){
+          //save cards clicked and change score board
+          $("#pairs_clicked").text(memoryGame.pairsClicked);
+          
+          var card = memoryGame.pickedCards[0].children();
+          card[0].className = "back";
+          card[1].className = "front";
+
+          var card1 = memoryGame.pickedCards[1].children();
+          card1[0].className = "back";
+          card1[1].className = "front";
+
+        }else{ //if the cards are the same, refresh score board with clicked and guessed cards
+          $("#pairs_clicked").text(memoryGame.pairsClicked);
+          $("#pairs_guessed").text(memoryGame.pairsClicked);
+        }
+        memoryGame.pickedCards=[];
+
+      },1000);
+    }
+    if(memoryGame.isFinished()){
+      $("#memory_board").html("You win");
+    }
   });
+
 });
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,38 @@
+//constructor
 var MemoryGame = function (cards) {
-  this.cards = cards;
+  this.cards = cards; //call an object cards
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed= 0;
+
 };
 
+//take an array of cards, shuffle with Durstenfeld Shuffle algorithm
 MemoryGame.prototype.shuffleCards = function () {
-};
-
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    var array= this.cards;
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }   
 }
 
+//check if cards are pairs
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked +=1 //first to if, a card is clicked
+  if(firstCard === secondCard){
+    this.pairsGuessed +=1
+    return true;
+  }else{
+    return false;
+  }
+}
+
+//game finished
 MemoryGame.prototype.isFinished = function () {
+  if(this.pairsGuessed === this.cards.length/2){
+    return true;
+  }else{
+    return false;
+  }
 };
+  
