62d1baef53c7a5bc812f301aaf03fe6f088ab52e
Jack Fischer <jrfisch95@gmail.com>
2019-02-10 21:37:00 -0500

    Finished javascript logic for card memory game. Now need to connect it to the DOM.

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,3 +1,38 @@
+class MemoryGame{
+  constructor(cards){
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+  }
+
+  shuffleCards(){
+    let shuffledCards = [];
+    while(this.cards.length > 0){
+      shuffledCards.push(this.cards.splice(Math.floor(Math.random() * this.cards.length), 1));
+    }
+    this.cards = shuffledCards;
+  }
+
+  checkIfPair(firstCard, secondCard){
+    this.pairsClicked++;
+    if(firstCard == secondCard){
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
+  }
+  
+  isFinished(){
+    return this.pairsGuessed == this.cards.length / 2;  
+  }
+}
+
+
+
+
+
+/*
 var MemoryGame = function (cards) {
   this.cards = cards;
 };
@@ -10,3 +45,4 @@ MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
 MemoryGame.prototype.isFinished = function () {
 };
+ */
