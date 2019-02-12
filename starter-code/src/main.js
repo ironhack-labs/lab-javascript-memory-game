5fe034ab48fb55e6691cc80093c76e67327a697c
kikeanaya <luiseanayam@gmail.com>
2019-02-12 23:26:18 +0100

    de done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -39,9 +39,37 @@ $(document).ready(function(){
   $('#memory_board').html(html);
 
   // Bind the click event of each element to a function
-  $('.back').click(function () {
+
+  $('.back').on("click",function (e) {
     // TODO: write some code here
+    $ (e.currentTarget).toggleClass("back front").next().toggleClass("front back")
+
+    if (memoryGame.pickedCards.length === 0){
+      memoryGame.pickedCards[0]=$(e.currentTarget)
+      console.log(memoryGame.pickedCards)
+    } else if (memoryGame.pickedCards.length === 1){
+      memoryGame.pickedCards[1]=$(e.currentTarget)
+      console.log(memoryGame.pickedCards)
+    }
+    
+    if(memoryGame.pickedCards.length === 2){
+      setTimeout(function(){
+      if(memoryGame.checkIfPair ($(memoryGame.pickedCards[0]).attr("name"), memoryGame.pickedCards[1].attr("name"))){
+        $("#pairs_guessed").html(memoryGame.pairsGuessed)
+        
+        
+      } else{
+          $(memoryGame.pickedCards[0]).toggleClass("front back").next().toggleClass("back front")
+          $(memoryGame.pickedCards[1]).toggleClass("front back").next().toggleClass("back front")
+          $("#pairs_clicked").html(memoryGame.pairsClicked)
+        }
+      if(memoryGame.isFinished()){
+        alert("You have great memory! You beat the game!")
+      }  
+      memoryGame.pickedCards = []
+    },300)
+    }
   });
-});
+});   
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,38 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
-};
+  this.pickedCards = []
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+}
 
 MemoryGame.prototype.shuffleCards = function () {
-};
+  var stackOfCards = this.cards
+  var stackLength = stackOfCards.length;
+  var temporal;
+  var randomPick;
+  
+  while(stackLength){
+    randomPick = Math.floor(Math.random() * stackLength--)
+
+    temporal = stackOfCards[stackLength]
+    stackOfCards[stackLength] = stackOfCards[randomPick]
+    stackOfCards[randomPick] = temporal
+  }
+}
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked++
+  if(firstCard === secondCard){
+    this.pairsGuessed++
+  }
+  return firstCard === secondCard
 }
 
 MemoryGame.prototype.isFinished = function () {
+  
+  if (this.pairsGuessed===this.cards.length/2){
+    return true
+  }
+  return false;
 };
+
