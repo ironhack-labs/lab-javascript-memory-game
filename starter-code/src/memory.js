96c5f9631b5c8730bac07e02892ca1e56a0c4c3e
Abel Colmenares Napolitano <abelpronet@gmail.com>
2019-02-13 09:15:45 +0100

    done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,46 @@
-var MemoryGame = function (cards) {
-  this.cards = cards;
+var MemoryGame = function(cards) {
+    this.cards = cards
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function() {
+
+    var m = this.cards.length,
+        t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = this.cards[m];
+        this.cards[m] = this.cards[i];
+        this.cards[i] = t;
+    }
+
 };
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.checkIfPair = function(firstCard, secondCard) {
+    // this.pairsClicked++
+    // if (firstCard !== secondCard) {
+    //     
+    //         return false
+    // } else {
+    //     this.pairsGuessed++
+    //         return true
+    // }
+    this.pairsClicked++
+        if (firstCard === secondCard) {
+            this.pairsGuessed++
+        }
+
+    return (firstCard === secondCard)
 }
 
-MemoryGame.prototype.isFinished = function () {
+MemoryGame.prototype.isFinished = function() {
+    return (this.pairsGuessed === this.cards.length / 2)
 };
