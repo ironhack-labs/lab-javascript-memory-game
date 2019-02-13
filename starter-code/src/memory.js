34d038a762c894855587783d6791c1d485531192
Javier <javiermaker@gmail.com>
2019-02-13 09:08:20 +0100

    primeros planteamientos

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,49 @@
-var MemoryGame = function (cards) {
-  this.cards = cards;
+var MemoryGame = function(cards) {
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function() {
+    var array = this.cards
+    var shuffle = function(array) {
+
+        var currentIndex = array.length;
+        var temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+
+    };
+
 };
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+MemoryGame.prototype.checkIfPair = function(firstCard, secondCard) {
+    var firstCard = this.cards.name
+    var secondCard = this.cards.name
+    pairsClicked += 1
+    if (firstCard.name == secondCard.name) {
+        pairGuessed += 1
+        return true
+    }
+    return false
+
 }
 
-MemoryGame.prototype.isFinished = function () {
+
+
+MemoryGame.prototype.isFinished = function() {
+    return pairsGuessed == 12
 };
