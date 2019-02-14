eaf76ab8e32a8d80791bf04cb71917a201e7980a
Fran Enriquez <fjenriglez@gmail.com>
2019-02-14 18:43:40 +0100

    commit inicial

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,5 +1,8 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
