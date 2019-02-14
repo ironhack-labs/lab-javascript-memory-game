9a76f1efeb643c87b5a9e9d1c10d820c9acd080c
Antonio Méndez <antonio.mendez.martin89@gmail.com>
2019-02-14 23:16:42 +0100

    jasmine done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -6,12 +6,28 @@ var MemoryGame = function (cards) {
 };
 
 MemoryGame.prototype.shuffleCards = function () {
-  var i;
-  for (i=0; i<24; i++);
-};
+  for (i=0; i<this.cards.length; i++){
+  var s = Math.floor(Math.random() * (this.cards.length-i))+0;
+  this.cards[i] = this.cards[s]}
+}
+
+
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked = this.pairsClicked + 1;
+  if (firstCard == secondCard) {
+    this.pairsGuessed = this.pairsGuessed + 1;  
+    return true
+  } else {
+    return false
+  } 
 }
 
 MemoryGame.prototype.isFinished = function () {
-};
+  if(this.pairsGuessed == this.cards.length/2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
