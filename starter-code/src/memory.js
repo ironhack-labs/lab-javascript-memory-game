8e74f11f0094545becfb9a43f771fe5183fb2981
Patricia Delgado <patricia.delgado.morcillo@gmail.com>
2019-02-14 21:48:49 +0100

    checkIfPair function fixed

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -12,56 +12,20 @@ MemoryGame.prototype.shuffleCards = function (cards) {
     var cardMixed = Math.floor(Math.random() * (this.cards.length -i));
     this.cards[i] = this.cards[cardMixed];
   }
-  /* this.cards.sort(function(){
-    this.cards( Math.random() *  - 0.5)
-  }); */
-
- 
-    //return (Math.round(Math.random())-0.5);
-
-  //Esto te coge una carta aleatoria
-  //this.cards = this.cards[Math.floor(Math.random() * this.cards.length)];
-  //console.log(this.cards)
-  //return undefined;
-
-  /* var currentIteration = this.cards.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIteration) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIteration);
-    currentIteration -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = this.cards[currentIteration];
-    this.cards[currentIteration] = this.cards[randomIndex];
-    this.cards[randomIndex] = temporaryValue;
-  }
-
-  return this.cards; */
-
-  /* var m = myCards.length, t, i;
-
-  // While there remain elements to shuffle…
-  while (m) {
-
-    // Pick a remaining element…
-    i = Math.floor(Math.random() * m--);
-
-    // And swap it with the current element.
-    t = myCards[m];
-    myCards[m] = myCards[i];
-    myCards[i] = t;
-  }
-
-  return myCards; */
 };
 
   
 
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked = this.pairsClicked +1
+  if(firstCard == secondCard){
+    this.pairsGuessed = this.pairsGuessed + 1;
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 MemoryGame.prototype.isFinished = function () {
