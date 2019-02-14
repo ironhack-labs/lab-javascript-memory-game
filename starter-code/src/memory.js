4ec9f0184845e5f39dff8cd3c8284736f6a9b062
danidimarti <dani.dimarti@gmail.com>
2019-02-14 22:01:35 +0100

    first commit

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,38 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+  this.cardsFlipped = 0;
 };
+
+
+
 
 MemoryGame.prototype.shuffleCards = function () {
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  firstCard = pickedCard[0];
+  secondCard = pickedCard[1];
+  this.pairsClicked++;
+
+  if (firstCard.name === secondCard.name) {
+    this.pairsGuessed++;
+    return true;
+
+  } return false;
+
 }
 
+
+
 MemoryGame.prototype.isFinished = function () {
+  this.pairsGuessed == this.cards.length / 2; 
+
 };
+
+
+
+
