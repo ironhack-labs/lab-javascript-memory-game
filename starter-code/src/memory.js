e91a2ef48d24a4c8cac1176eea39b3664ac8612f
Patricia Delgado <patricia.delgado.morcillo@gmail.com>
2019-02-14 19:57:28 +0100

    created properties contructor

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,11 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function (cards) {
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
