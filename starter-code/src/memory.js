28300e0848eb8b6dae96cdc8c6f53f5c9e42687f
Antonio Méndez <antonio.mendez.martin89@gmail.com>
2019-02-14 19:55:43 +0100

    memory game properties

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,13 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  var i;
+  for (i=0; i<24; i++);
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
