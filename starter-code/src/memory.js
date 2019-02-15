918d7e02ba2672bbd7f1a0ebcd8bfe7e5fd48420
MartaVaqgut <martavaquerogutierrez@gmail.com>
2019-02-15 19:19:22 +0100

    first iteration

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,38 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
-};
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+}
 
 MemoryGame.prototype.shuffleCards = function () {
+  if (!cards.length ==0){
+    for (i =0; i< this.cards.length; i++){
+      var z = Math.floor(Math.random () * (this.cards.length -i)) + 0
+      this.cards[i] = this.cards [z];
+    }
+  }else {
+    return undefined;
+  }
+
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-}
+ if (firstCard === secondCard){
+   this.pairsClicked++;
+   this.pairsGuessed++;
+   return true;
+ }
+ if (firstCard != secondCard){
+   this.pairsClicked++;
+   return false;
+ }
+};
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.pairsGuessed === this.cards.length / 2) {
+    return true;
+  } else {
+    return false;
+  }
 };
