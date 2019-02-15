a073ba7b9fd98d0a87ecfbf16c4b13e325d2015c
Patricia Delgado <patricia.delgado.morcillo@gmail.com>
2019-02-15 18:43:57 +0100

    toggle cards classes

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -29,19 +29,29 @@ $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   //pintamos las cartas ya barajadas con la función de shuffle de jasmine
   memoryGame.shuffleCards();
+  //console.log(cards)
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
-    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat;"></div>';
     html += '</div>';
+    //comprobar que trae las imágenes
+    console.log(pic.img)
   });
+
 
   // Add all the div's to the HTML
   $('#memory_board').html(html);
 
   // Bind the click event of each element to a function
   $('.back').click(function () {
+    //cuando hagas click en el elemento añadimos la calse front a back y eliminamos back.
+    $(this).toggleClass('front').toggleClass('back');
+  
+    //Añadimos la clase back a front
+    $(this).siblings().toggleClass('back').toggleClass('front');
+    console.log('estou en el click')
     // TODO: write some code here
   });
 });
