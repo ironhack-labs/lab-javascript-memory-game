475472b341256ab8e5a89395a0562380561a3d99
David Gallego Gomez <davizs@hotmail.com>
2019-02-15 22:27:39 +0100

    Uniendo inteligencia al juego

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,6 +27,9 @@ var cards = [
 
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
+  
+  //memoryGame.shuffleCards(); // DGG: Partidas aleatorias
+  
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
@@ -37,15 +40,51 @@ $(document).ready(function(){
 
   // Add all the div's to the HTML
   $('#memory_board').html(html);
-
+  
   // Bind the click event of each element to a function
+  var counterClick = 0;
+  var nameArray = [], objArray = [];  
+  var name, obj;
+  var endGame = false;
+
   $('.back').click(function () {
     // TODO: write some code here
 
-    // $("li.item").closest("ul")
-    console.log($(this).html());
+    obj = $(this);
+    name = $(this).parent().attr('data-card-name');
+    nameArray.push(name);
+    objArray.push(obj);
+    $(this).toggleClass('front').toggleClass('back');
+    $(this).siblings().toggleClass('back').toggleClass('front');
+    counterClick++;
+    console.log('Clicks: ' + counterClick);
+    console.log('Card: ' + name);
+    console.log(nameArray);
+    if ( nameArray.length > 1 && nameArray[nameArray.length - 1] === nameArray[nameArray.length - 2] ) {
+      console.log('Iguales!!!');
+      nameArray = [];
+    }
+    else if ( nameArray.length > 1 && nameArray[nameArray.length - 1] !== nameArray[nameArray.length - 2] ) {
+      console.log('Distintas!!!');
+      
+      objArray[objArray.length - 1].toggleClass('back', 2000).toggleClass('front', 2000);
+      objArray[objArray.length - 1].siblings().toggleClass('front', 2000).toggleClass('back', 2000);
+      
+      objArray[objArray.length - 2].toggleClass('back', 2000).toggleClass('front', 2000);
+      objArray[objArray.length - 2].siblings().toggleClass('front', 2000).toggleClass('back', 2000);
 
+      nameArray = [];
+    }
+    endGame = memoryGame.isFinished(); // Si es true se desactiva el click    
   });
+
+  /*$('.front').click(function () {
+    // TODO: write some code here
+    console.log( $(this).parent().attr('data-card-name') );
+    $(this).toggleClass('back').toggleClass('front');
+    $(this).siblings().toggleClass('front').toggleClass('back');
+  });*/
+
 });
 
 
