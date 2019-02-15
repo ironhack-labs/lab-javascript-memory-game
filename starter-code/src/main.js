cf88b972681d305417823f6fa829262de4c588ce
Fernando Lacaba <fernando.lacaba.herrera@gmail.com>
2019-02-15 18:45:54 +0100

    Terminado, excepto la parte de volver a empezar el uego

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,9 +25,33 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
+function compareCards (memoryGame,cardA,cardB) {
+  memoryGame.pairsClicked ++;
+  $("#pairs_clicked").text(memoryGame.pairsClicked);
+  if (memoryGame.pickedCards[0] === memoryGame.pickedCards[1]){
+    memoryGame.pairsGuessed ++;
+    $("#pairs_guessed").text(memoryGame.pairsGuessed);
+    if(memoryGame.isFinished() === true) {
+      setTimeout (function(){
+        $(".card").remove();
+        memoryGame.shuffleCards();
+        $("#pairs_guessed").text(memoryGame.pairsGuessed);
+        $("#pairs_clicked").text(memoryGame.pairsClicked);
+      },1000)
+    }
+  } else {
+    setTimeout (function() {
+      $(cardA).removeAttr("style");
+      $(cardB).removeAttr("style");
+    },1000)
+  }
+  memoryGame.pickedCards.fill("");
+}
+
 $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
+
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
@@ -37,9 +61,21 @@ $(document).ready(function(){
 
   // Add all the div's to the HTML
   $('#memory_board').html(html);
-
+  var cardA;
+  var cardB;
   // Bind the click event of each element to a function
   $('.back').click(function () {
+     var image = $(this).siblings().attr("style");
+     var whichCard = $(this).attr("name");
+     $(this).attr("style",image);
+     if (memoryGame.pickedCards[0] === "") {
+      cardA = this;
+       memoryGame.pickedCards[0] = whichCard;
+     } else {
+      cardB = this;
+      memoryGame.pickedCards[1] = whichCard;
+      compareCards(memoryGame,cardA,cardB);
+     }
     // TODO: write some code here
   });
 });
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,38 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = new Array(2).fill("");
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  var m = this.cards.length;
+  var t;
+  var i;
+  while (m) {
+    i = Math.floor(Math.random()*m--);
+    t = this.cards[m];
+    this.cards[m] = this.cards[i];
+    this.cards[i]=t;
+  }
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked ++;
+  if (firstCard === secondCard) {
+    this.pairsGuessed ++;
+    return true;
+  } else {
+    return false;
+  }
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.pairsGuessed===this.cards.length/2) {
+    this.pairsGuessed = 0;
+    this.pairsClicked = 0;
+    return true;
+  } else {
+    return false;
+  }
 };
