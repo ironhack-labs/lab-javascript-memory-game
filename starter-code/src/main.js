6a48beaaa62965ed03f813ed30d7c81f8af0ee17
eskalona17 <eskalona_17@hotmail.com>
2019-02-16 09:35:04 +0100

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -37,11 +37,31 @@ $(document).ready(function(){
 
   // Add all the div's to the HTML
   $('#memory_board').html(html);
-
-  // Bind the click event of each element to a function
   $('.back').click(function () {
-    // TODO: write some code here
+    memoryGame.pickedCards.push(this);
+   classChange(this);
+   $("#pairs_clicked").text(memoryGame.pairsClicked);
+   $("#pairs_guessed").text(memoryGame.pairsGuessed); 
+
+   if (memoryGame.pickedCards.length === 2) {
+      if (memoryGame.checkIfPair(memoryGame.pickedCards[0].getAttribute("name"), memoryGame.pickedCards[1].getAttribute("name"))){
+        $(".faceup").toggleClass("faceUp");
+        if (memoryGame.isFinished()) {
+          $("#pairs_clicked").text(memoryGame.pairsClicked);
+          $("#pairs_guessed").text(memoryGame.pairsGuessed); 
+            setTimeout(function() {
+              alert ("The game is completed");
+            },500);
+        }
+      } 
+          else {
+            setTimeout(function() {
+              classChange(".faceUp");
+            },500);      
+          } 
+          memoryGame.pickedCards = [];
+        }
+      });
   });
-});
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,38 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  this.cards.sort(function(a,b){
+    return Math.random() - 1;
+  });
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked++;
+    if(firstCard === secondCard){
+      this.pairsGuessed++;
+      return true;
+      } else {
+      return false;
+    }
 }
 
 MemoryGame.prototype.isFinished = function () {
-};
+  if (this.pairsGuessed === this.cards.length/2){
+    return true;
+  } else {
+    return false;
+  }
+}; 
+  
+  function classChange(changed) {
+    $(changed).toggleClass("front");
+    $(changed).toggleClass("back");
+    $(changed).siblings().toggleClass("front");
+    $(changed).siblings().toggleClass("back");
+    $(changed).toggleClass("faceUp"); 
+}
