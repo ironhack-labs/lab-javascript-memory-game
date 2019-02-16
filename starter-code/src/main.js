46549c63f0941e51875a507c70dd21def9e722b9
iMAC <iMAC@MacBook-Pro-de-MACBOOK.local>
2019-02-16 11:39:26 +0100

    ejercio memory

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
   var memoryGame = new MemoryGame(cards);
   var html = '';
   memoryGame.cards.forEach(function (pic) {
-    html += '<div class="card" data-card-name="'+ pic.name +'">';
+    html += '<div class="card" add-class-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
@@ -39,9 +39,95 @@ $(document).ready(function(){
   $('#memory_board').html(html);
 
   // Bind the click event of each element to a function
-  $('.back').click(function () {
+  $('.card').click(function () {
     // TODO: write some code here
+
+var carta=$(this);
+var liso=$(this).children(":first");
+var imagen=$(this).children(":nth-child(2)");
+
+if(liso.attr("class")=="back"){
+
+  if(memoryGame.pairsClicked===0){  
+  memoryGame.pickedCards.push(carta);
+
+  liso.toggleClass("back",false);
+  liso.toggleClass("front",true);
+  imagen.toggleClass("back",true);
+  imagen.toggleClass("front",false);
+  
+    
+    memoryGame.pairsClicked=1;
+
+  }else if(memoryGame.pairsClicked===1){
+    
+    memoryGame.pickedCards.push(carta);
+
+    liso.toggleClass("back",false);
+    liso.toggleClass("front",true);
+    imagen.toggleClass("back",true);
+    imagen.toggleClass("front",false);
+
+    memoryGame.pairsClicked=0;
+
+ 
+
+    memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1]);
+
+    if(memoryGame.pairsGuessed===1){
+
+    memoryGame.pairsGuessed=0;
+    memoryGame.pickedCards=[];
+    }else if(memoryGame.pairsGuessed===0){ 
+
+var capa1;
+var capa2;
+var liso1;
+var imagen1;
+var liso2;
+var imagen2;
+
+//do something special 
+capa1 = memoryGame.pickedCards[0];
+capa2 = memoryGame.pickedCards[1];
+
+liso1=capa1.children(":first");
+imagen1=capa1.children(":nth-child(2)");
+
+liso2=capa2.children(":first");
+imagen2=capa2.children(":nth-child(2)");
+
+
+setTimeout(
+  function(){ 
+ 
+
+  liso1.toggleClass("back",true);
+  liso1.toggleClass("front",false);
+  imagen1.toggleClass("back",false);
+  imagen1.toggleClass("front",true);
+
+  liso2.toggleClass("back",true);
+  liso2.toggleClass("front",false);
+  imagen2.toggleClass("back",false);
+  imagen2.toggleClass("front",true);
+
+
+
+ }, 1000); 
+
+ memoryGame.pairsGuessed=0;
+ memoryGame.pickedCards=[];
+
+}
+
+
+  }
+
+    
+}
+    
+
   });
 });
-
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,11 +1,26 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards=[];
+  this.pairsClicked=0;
+  this.pairsGuessed=0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+
+
+if(firstCard.attr("add-class-name")==secondCard.attr("add-class-name")){
+
+  this.pairsGuessed=1;
+
+}else{
+
+
+this.pairsGuessed=0;
+}
+
 }
 
 MemoryGame.prototype.isFinished = function () {
