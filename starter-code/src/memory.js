b07e83ab624f804fd84369d68f4562217aed54aa
Abel Rodriguez <abelrodriguez199@gmail.com>
2019-02-25 21:18:03 +0100

    Ejercicio Realizado

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,34 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed=0;
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+  this.cards.sort(function(){
+    return Math.random() - 0.5;
+  })
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+  this.pairsClicked = 1;
+
+  if (firstCard === secondCard ){
+    this.pairsGuessed++;
+    return true;
+  }
+  else {
+    return false;
+  }
+
 }
 
 MemoryGame.prototype.isFinished = function () {
+  if (this.pairsGuessed != this.cards.length / 2) {
+    return false;
+  }
+  else {
+    return true;
+  }
 };
