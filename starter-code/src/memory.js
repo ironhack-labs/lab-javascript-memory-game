94bbdf270d264f185df55089285757b2fb703469
AnthonySBR <asbr1811@gmail.com>
2019-03-26 18:01:41 +0100

    Jasmine test passed

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,35 @@
 class MemoryGame {
-  constructor(card){
+  constructor(cards){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    var i,j,temp
+    for (var i = 0; i < this.cards.length; i++){
+    j = Math.floor(Math.random() * (i+1));
+    temp = this.cards[i];
+    this.cards[i] = this.cards[j];
+    this.cards[j] = temp;
+    }
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2){
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    if(this.pairsGuessed === this.cards.length/2){
+      return true;
+    } else{
+      return false;
+    }
+
+  }
 }
+
