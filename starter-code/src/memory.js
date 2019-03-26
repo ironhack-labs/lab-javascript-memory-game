b0dd476a13662f8bbc0e4739b964054b278c4ff0
laura <lauraabad83@gmail.com>
2019-03-26 18:40:57 +0100

    added isFinished

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -33,5 +33,13 @@ class MemoryGame {
 
 
     }
-    isFinished() {}
+
+    isFinished() {
+        if (this.pairsGuessed === this.cards.length / 2) {
+            return true
+        } else {
+            return false
+        }
+
+    }
 }
