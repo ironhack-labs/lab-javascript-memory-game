5f13ac24e9f47a3817f1f5386640563a63cbe82e
Raul Contreras <raulc98@gmail.com>
2019-03-26 22:52:37 +0100

    Finished a raw implementation of the game

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,8 +27,46 @@ var cards = [
 var memoryGame = new MemoryGame(cards);
 
 
+function toggleCard(card){
+    var firstChildren = card.children[0];
+    var shownClass = firstChildren.classList[0];
+    var hiddenClass = firstChildren.nextSibling.classList[0];
+    // Cambia las clases
+    firstChildren.classList = hiddenClass;
+    firstChildren.nextSibling.classList = shownClass;
+    // Añade
+    if(firstChildren.classList[0] === 'front'){
+        memoryGame.pickCard(firstChildren);
+    }
+    if(firstChildren.classList[0] === 'back'){
+        memoryGame.deleteCard(firstChildren);
+    }
+    // Si hay dos cartas seleccionadas hay que evaluarlas
+    if(memoryGame.pickedCards.length === 2){
+        var first = memoryGame.pickedCards[0];
+        var firstName = first.getAttribute('name');
+        var second = memoryGame.pickedCards[1];
+        var secondName = second.getAttribute('name');
+        if(memoryGame.checkIfPair(firstName, secondName)){
+            first.parentElement.onclick = null;
+            second.parentElement.onclick = null;
+            memoryGame.deleteCards();
+        }else{
+            toggleCard(first.parentElement);
+            setTimeout(() => toggleCard(second.parentElement), 150);
+        }
+        // Actualizamos la puntuación
+        document.querySelector('#score p:nth-of-type(1)').innerHTML = `Pairs Clicked: ${memoryGame.pairsClicked}`;
+        document.querySelector('#score p:nth-of-type(2)').innerHTML = `Pairs Guessed: ${memoryGame.pairsGuessed}`;
+    }
+
+    // Comprobar si hemos ganado. En caso afirmativo, reiniciar el juego
+
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
     var html = '';
+    //memoryGame.shuffleCards();
     memoryGame.cards.forEach(function(pic) {
         html += '<div class="card" data-card-name="' + pic.name + '">';
         html += '<div class="back" name="' + pic.img + '"></div>';
@@ -40,10 +78,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.querySelector('#memory_board').innerHTML = html;
 
     // Bind the click event of each element to a function
-    document.querySelectorAll('.back').forEach(function(card) {
+    document.querySelectorAll('.card').forEach(function(card) {
         card.onclick = function() {
+            toggleCard(card);
             // TODO: write some code here
-            console.log('Card clicked')
+            
         }
     });
 });
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,45 +1,56 @@
 class MemoryGame {
-    constructor(cards) {
-        this.cards = cards;
-        this.pickedCards = [];
-        this.pairsClicked = 0;
-        this.pairsGuessed = 0;
+  constructor(cards) {
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+  }
+
+  shuffleCards() {
+    var aleatorycards = [],
+      n = this.cards.length,
+      i;
+
+    while (n) {
+      i = Math.floor(Math.random() * n--);
+
+      aleatorycards.push(this.cards.splice(i, 1)[0]);
     }
 
-    shuffleCards() {
-        var aleatorycards = [],
-            n = this.cards.length,
-            i;
+    this.cards = aleatorycards;
+  }
 
-        while (n) {
+  pickCard(card){
+      this.pickedCards.push(card);
+  }
 
-            i = Math.floor(Math.random() * n--);
+  getCardId(card){
+      return this.pickedCards.indexOf(card);
+  }
 
-            aleatorycards.push(this.cards.splice(i, 1)[0]);
-        }
+  deleteCard(card){
+      this.pickedCards.splice(this.getCardId(card), 1);
+  }
 
-        this.cards = aleatorycards;
+  deleteCards(){
+      this.pickedCards = [];
+  }
 
-
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
     }
-    checkIfPair(card1, card2) {
-        this.pairsClicked++
-            if (card1 === card2) {
-                this.pairsGuessed++
-                    return true
-            } else {
-                return false
-            }
+  }
 
-
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else {
+      return false;
     }
-
-    isFinished() {
-        if (this.pairsGuessed === this.cards.length / 2) {
-            return true
-        } else {
-            return false
-        }
-
-    }
+  }
 }
