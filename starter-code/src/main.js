061f178f7542c2be2a6453139303871bb91e606d
Raúl Contreras <raulc98@gmail.com>
2019-03-27 09:10:48 +0100

    Refactorized code but the restarted code does not word when shuffle the cards

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -26,63 +26,85 @@ var cards = [
 ];
 var memoryGame = new MemoryGame(cards);
 
-
 function toggleCard(card){
     var firstChildren = card.children[0];
     var shownClass = firstChildren.classList[0];
     var hiddenClass = firstChildren.nextSibling.classList[0];
-    // Cambia las clases
+
     firstChildren.classList = hiddenClass;
     firstChildren.nextSibling.classList = shownClass;
-    // Añade
+    
     if(firstChildren.classList[0] === 'front'){
         memoryGame.pickCard(firstChildren);
     }
     if(firstChildren.classList[0] === 'back'){
         memoryGame.deleteCard(firstChildren);
     }
-    // Si hay dos cartas seleccionadas hay que evaluarlas
-    if(memoryGame.pickedCards.length === 2){
-        var first = memoryGame.pickedCards[0];
-        var firstName = first.getAttribute('name');
-        var second = memoryGame.pickedCards[1];
-        var secondName = second.getAttribute('name');
-        if(memoryGame.checkIfPair(firstName, secondName)){
-            first.parentElement.onclick = null;
-            second.parentElement.onclick = null;
-            memoryGame.deleteCards();
-        }else{
-            toggleCard(first.parentElement);
-            setTimeout(() => toggleCard(second.parentElement), 150);
-        }
-        // Actualizamos la puntuación
-        document.querySelector('#score p:nth-of-type(1)').innerHTML = `Pairs Clicked: ${memoryGame.pairsClicked}`;
-        document.querySelector('#score p:nth-of-type(2)').innerHTML = `Pairs Guessed: ${memoryGame.pairsGuessed}`;
-    }
 
-    // Comprobar si hemos ganado. En caso afirmativo, reiniciar el juego
+    if(memoryGame.pickedCards.length === 2){
+        evaluateCards();
+    }
 
 }
 
+function evaluateCards(){
+    var firstCard = memoryGame.pickedCards[0];
+    var secondCard = memoryGame.pickedCards[1];
+    checkPair(firstCard, secondCard);
+    updateScore();
+    checkIfWon();
+}
+
+function checkPair(firstCard, secondCard){
+    var firstCardName = firstCard.getAttribute('name');
+    var secondCardName = secondCard.getAttribute('name');
+    if(memoryGame.checkIfPair(firstCardName, secondCardName)){
+        firstCard.parentElement.onclick = null;
+        secondCard.parentElement.onclick = null;
+        memoryGame.deleteCards();
+    }else{
+        toggleCard(firstCard.parentElement);
+        setTimeout(() => toggleCard(secondCard.parentElement), 150);
+    }
+}
+
+function updateScore(){
+    document.querySelector('#score p:nth-of-type(1)').innerHTML = `Pairs Clicked: ${memoryGame.pairsClicked}`;
+    document.querySelector('#score p:nth-of-type(2)').innerHTML = `Pairs Guessed: ${memoryGame.pairsGuessed}`;
+}
+
+function checkIfWon(){
+    if(memoryGame.isFinished()){
+        alert(`You have finished the game on attempt ${memoryGame.pairsClicked}!`);
+        memoryGame = new MemoryGame(cards);
+        loadGame(memoryGame);
+        updateScore();
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
+    loadGame(memoryGame);
+});
+
+function loadGame(obj){
+
     var html = '';
-    //memoryGame.shuffleCards();
+    memoryGame.shuffleCards();
     memoryGame.cards.forEach(function(pic) {
         html += '<div class="card" data-card-name="' + pic.name + '">';
         html += '<div class="back" name="' + pic.img + '"></div>';
         html += '<div class="front" style="background: url(img/' + pic.img + ') no-repeat"></div>';
         html += '</div>';
     });
-
+    
     // Add all the div's to the HTML
     document.querySelector('#memory_board').innerHTML = html;
 
     // Bind the click event of each element to a function
     document.querySelectorAll('.card').forEach(function(card) {
         card.onclick = function() {
-            toggleCard(card);
             // TODO: write some code here
-            
+            toggleCard(card);
         }
     });
-});
+}
