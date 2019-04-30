db7c17520c691d79b799a8fe8340c5c167b49cfd
Lucia Astray <lu@MacBook-Pro-de-Lucia.local>
2019-04-30 19:41:58 +0200

    The Game Logic

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,40 @@
 class MemoryGame {
-  constructor(card){
+  constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    var m = this.cards.length;
+    let t;
+    let i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = this.cards[m];
+      this.cards[m] = this.cards[i];
+      this.cards[i] = t;
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    if (card1 == card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      this.pairsClicked++;
+      return false;
+    }
+  }
+  isFinished() {
+    if (this.pairsGuessed == this.cards.length / 2) {
+      return true;
+    }
+    return false;
+  }
 }
