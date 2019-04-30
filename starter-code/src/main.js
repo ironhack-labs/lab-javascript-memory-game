bd5df7518859636baa21ed46e6efa2908364ccc3
benjamingarciap <bengarc@gmail.com>
2019-05-01 00:14:50 +0200

    first commit

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -1,3 +1,7 @@
+//const comparingArr = []
+let selected = document.getElementsByClassName("selected")
+let div = document.getElementsByTagName("div")
+
 var cards = [
   { name: 'aquaman',         img: 'aquaman.jpg' },
   { name: 'batman',          img: 'batman.jpg' },
@@ -25,7 +29,8 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 var memoryGame = new MemoryGame(cards);
-
+  //first shuffle of the cards
+  memoryGame.shuffleCards()
 
 document.addEventListener("DOMContentLoaded", function(event) { 
   var html = '';
@@ -34,16 +39,66 @@ document.addEventListener("DOMContentLoaded", function(event) {
     html += '  <div class="back" name="'+ pic.img +'"></div>';
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
+    console.log("clicked on doc")
   });
-
+  //memoryGame.shuffleCards()
   // Add all the div's to the HTML
   document.querySelector('#memory_board').innerHTML = html;
-
+    
   // Bind the click event of each element to a function
-  document.querySelectorAll('.back').forEach(function(card) {
-    card.onclick = function() {
+  document.querySelectorAll('.back').forEach(function(card, i) {
+    card.onclick = function(e) {
+     
       // TODO: write some code here
+      // back and front of the card event selectors
+      let front = e.currentTarget.parentElement.children[1]
+      let back = e.currentTarget.parentElement.children[0]
+      // fliping of the card
+      back.classList.toggle("back")
+      front.classList.toggle("front")
+      back.classList.toggle("front")
+      front.classList.toggle("back")
+      // add identificator
+      back.classList.add("selected");
+      front.classList.add("selected")
+      // saving the clicks to the global scope array for comparing
+      if(memoryGame.pairsClicked < 2){
+        memoryGame.pickedCards.push(front.outerHTML)
+      }
+      //all cards go back to flipped and reset pairs clicked
+      if(memoryGame.pairsClicked === 2){
+       //check if cards are equal 
+        if(memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1]) === true){
+          memoryGame.pairsGuessed += 2
+          memoryGame.pickedCards = []
+          memoryGame.pairsClicked = 0
+        }
+        else{
+          for(i=0; i<selected.length; i++){
+            selected[i].classList.toggle("front")
+            selected[i].classList.toggle("back")
+          }
+          for(i=0; i<div.length; i++){
+            div[i].classList.remove("selected")
+          }
+          memoryGame.pairsClicked = -1
+          memoryGame.pickedCards = []
+        }
+
+      }
+    
+      
+
+      //console.log('selected class' + document.getElementsByClassName("selected"))
+      // logic for comparing the elements in array
+      memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1])
+     
+      
+      console.log(e.target.parentElement.children[1])
       console.log('Card clicked')
+     
+
+      
     }
   });
 });
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,46 @@
 class MemoryGame {
-  constructor(card){
+  constructor(cards){
     this.cards = cards;
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+      let currentIndex = this.cards.length;
+      let temporaryValue;
+      let randomIndex;
+    
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+    
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+    
+        // And swap it with the current element.
+        temporaryValue = this.cards[currentIndex];
+        this.cards[currentIndex] = this.cards[randomIndex];
+        this.cards[randomIndex] = temporaryValue;
+      }
+    
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++
+    if(card1 === card2){
+      this.pairsGuessed++
+      return true
+    }
+    else if(card1 !== card2){
+      return false
+    }
+  }
+  isFinished() {
+    if(this.cards.length / 2 !== this.pairsGuessed ){
+      return false
+    }
+   else {
+     return true
+   }
+  }
 }
