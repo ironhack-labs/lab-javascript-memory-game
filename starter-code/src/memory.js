b52900ce5f5d25bbf04f1b4715477c4fbd49c751
Sachin Tekchandani <sachin6291@gmail.com>
2019-05-01 09:39:48 +0200

    no me funciona jasmin, no se abre por el chrome o firefox

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,42 @@
 class MemoryGame {
-  constructor(card){
+  constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    this.pickedCards;
+    let n = this.cards.length;
+    let i;
+
+    while (n) {
+      i = Math.floor(Math.random() * n);
+
+      if (i in this.cards) {
+        this.pickedCards.push(this.cards[i]);
+        delete this.cards[i];
+        n--;
+      }
+    }
+
+    return this.pickedCards;
+  }
+  checkIfPair(card1, card2) {
+    if (card1 == card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      this.pairsClicked++;
+      return false;
+    }
+  }
+  isFinished() {
+    if (this.pairsGuessed == 8) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
