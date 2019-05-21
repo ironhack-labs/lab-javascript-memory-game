2fd30dd158e730c5f5988f18d36f3bd69747b092
juancarloselorriaga <juancarloselorriaga@gmail.com>
2019-05-20 22:40:02 -0500

    score component pending

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -1,74 +1,140 @@
-
 Vue.component('card', {
   props: ['single-card'],
+  data: function() {
+    return {
+      cards: [
+        { name: 'aquaman',         img: 'aquaman.jpg', back: 'back', front: 'front' },
+        { name: 'batman',          img: 'batman.jpg', back: 'back', front: 'front' },
+        { name: 'captain america', img: 'captain-america.jpg', back: 'back', front: 'front'  },
+        { name: 'fantastic four',  img: 'fantastic-four.jpg', back: 'back', front: 'front'  },
+        { name: 'flash',           img: 'flash.jpg', back: 'back', front: 'front'  },
+        { name: 'green arrow',     img: 'green-arrow.jpg', back: 'back', front: 'front'  },
+        { name: 'green lantern',   img: 'green-lantern.jpg', back: 'back', front: 'front'  },
+        { name: 'ironman',         img: 'ironman.jpg', back: 'back', front: 'front'  },
+        { name: 'spiderman',       img: 'spiderman.jpg', back: 'back', front: 'front'  },
+        { name: 'superman',        img: 'superman.jpg', back: 'back', front: 'front'  },
+        { name: 'the avengers',    img: 'the-avengers.jpg', back: 'back', front: 'front'  },
+        { name: 'thor',            img: 'thor.jpg', back: 'back', front: 'front'  },
+        { name: 'aquaman',         img: 'aquaman.jpg', back: 'back', front: 'front'  },
+        { name: 'batman',          img: 'batman.jpg', back: 'back', front: 'front'  },
+        { name: 'captain america', img: 'captain-america.jpg', back: 'back', front: 'front'  },
+        { name: 'fantastic four',  img: 'fantastic-four.jpg', back: 'back', front: 'front'  },
+        { name: 'flash',           img: 'flash.jpg', back: 'back', front: 'front'  },
+        { name: 'green arrow',     img: 'green-arrow.jpg', back: 'back', front: 'front'  },
+        { name: 'green lantern',   img: 'green-lantern.jpg', back: 'back', front: 'front'  },
+        { name: 'ironman',         img: 'ironman.jpg', back: 'back', front: 'front'  },
+        { name: 'spiderman',       img: 'spiderman.jpg', back: 'back', front: 'front'  },
+        { name: 'superman',        img: 'superman.jpg', back: 'back', front: 'front'  },
+        { name: 'the avengers',    img: 'the-avengers.jpg', back: 'back', front: 'front'  },
+        { name: 'thor',            img: 'thor.jpg', back: 'back', front: 'front'  }
+      ],
+      pairsArr: [],
+      pairsClicked: 0
+    }
+  },
+  methods: {
+    openCard: function(index){
+
+      let myCard = this.cards[index];
+
+      if(myCard.back === 'back'){
+        myCard.back = 'front';
+        myCard.front = 'back';
+      }
+      else{
+        myCard.back = 'back';
+        myCard.front = 'front';
+      }
+      
+  },
+    checkIfPairs: function(index){
+
+      let memoryGame = new MemoryGame(this.cards);
+      let myCard = {
+        name: this.cards[index].name,
+        index: index
+      }
+
+      if(this.pairsArr.length < 2){
+        this.pairsArr.push(myCard);
+      }
+
+      if(this.pairsArr.length === 2){
+        let checkIfPairs = memoryGame.checkIfPair(this.pairsArr[0].name, this.pairsArr [1].name);
+        this.pairsClicked++;
+        
+        if(checkIfPairs === true && this.pairsArr[0].index != this.pairsArr[1].index){
+          this.pairsArr = []
+        }
+        else{
+
+          let cardClose = () =>{ 
+
+              //Error handling para evitar tres cartas seleccionadas
+              if(this.pairsArr[1] != undefined && this.pairsArr[0] != undefined ){
+                this.openCard(this.pairsArr[0].index);
+                this.openCard(this.pairsArr[1].index);
+                this.pairsArr = []
+              }
+              else{
+                this.openCard(index);
+                this.pairsArr = [];
+                this.pairsClicked--;
+              }
+          }
+          let timeOut = setTimeout(cardClose, 300)
+          return timeOut;
+        }
+      }
+
+
+    },
+    shuffleCards: function(){
+      return new MemoryGame(this.cards).shuffleCards();
+    }
+  },
+  beforeMount(){
+    this.shuffleCards()
+  },
   template: 
-  `<div class="card" :data-card-name="singleCard.name">
-    <div class="back" :name="singleCard.img"></div>
-    <div class="front" :style="{ background: 'url(img/' + singleCard.img + ') no-repeat' }"></div>
+  `<div>
+    <div
+      class="card"
+      :data-card-name="singleCard.name"
+      v-for="(singleCard, index) in cards"
+      :key="index"
+      :single-card="index"
+      @click="openCard(index); checkIfPairs(index)"
+      >
+      <div :class="singleCard.back" :name="singleCard.img"></div>
+      <div :class="singleCard.front" :style="{ background: 'url(img/' + singleCard.img + ') no-repeat' }"></div>
+    </div>
    </div>`
+   
 })
+
+//:single-card="card"
 
 var app = new Vue({
   el: '#memory_board',
   data: {
-    cards: [
-      { name: 'aquaman',         img: 'aquaman.jpg' },
-      { name: 'batman',          img: 'batman.jpg' },
-      { name: 'captain america', img: 'captain-america.jpg' },
-      { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-      { name: 'flash',           img: 'flash.jpg' },
-      { name: 'green arrow',     img: 'green-arrow.jpg' },
-      { name: 'green lantern',   img: 'green-lantern.jpg' },
-      { name: 'ironman',         img: 'ironman.jpg' },
-      { name: 'spiderman',       img: 'spiderman.jpg' },
-      { name: 'superman',        img: 'superman.jpg' },
-      { name: 'the avengers',    img: 'the-avengers.jpg' },
-      { name: 'thor',            img: 'thor.jpg' },
-      { name: 'aquaman',         img: 'aquaman.jpg' },
-      { name: 'batman',          img: 'batman.jpg' },
-      { name: 'captain america', img: 'captain-america.jpg' },
-      { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-      { name: 'flash',           img: 'flash.jpg' },
-      { name: 'green arrow',     img: 'green-arrow.jpg' },
-      { name: 'green lantern',   img: 'green-lantern.jpg' },
-      { name: 'ironman',         img: 'ironman.jpg' },
-      { name: 'spiderman',       img: 'spiderman.jpg' },
-      { name: 'superman',        img: 'superman.jpg' },
-      { name: 'the avengers',    img: 'the-avengers.jpg' },
-      { name: 'thor',            img: 'thor.jpg' }
-    ]
-  },
-  methods: {
-    openCard: function(index){
-      let shuffled = this.cards;
-      return shuffled[index].img;
-    }
   }
 })
 
-let memoryGame = new MemoryGame(app.cards);
-memoryGame.shuffleCards();
+var app = new Vue({
+  el: '#score',
+  data: {
+    passedPairsClicked: 0
+  },
+  template:
+  ` <div class="score">
+      <h2>Score</h2>
+      <p>Pairs Clicked: <span id="pairs_clicked">{{ passedPairsClicked }}</span></p>
+      <p>Pairs Guessed: <span id="pairs_guessed">0</span></p>
+    </div>
+    `
+})
 
 
-// document.addEventListener("DOMContentLoaded", function(event) { 
-//   var html = '';
-//   memoryGame.cards.forEach(function (pic) {
-//     html += '<div class="card" data-card-name="'+ pic.name +'">';
-//     html += '  <div class="back" name="'+ pic.img +'"></div>';
-//     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
-//     html += '</div>';
-//   });
-
-//   // Add all the div's to the HTML
-//   document.querySelector('#memory_board').innerHTML = html;
-
-//   // Bind the click event of each element to a function
-//   document.querySelectorAll('.back').forEach(function(card) {
-//     card.onclick = function() {
-//       // TODO: write some code here
-//       console.log('Card clicked')
-//     }
-//   });
-// });
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -35,3 +35,5 @@ class MemoryGame {
   }
 }
 
+
+
