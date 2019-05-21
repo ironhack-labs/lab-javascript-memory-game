2130f540f4c15faf324d358a0f8d9ee6a43fe818
geguiluz <memo.eguiluz@gmail.com>
2019-05-21 02:28:30 -0500

    Cards showing images in reverse mode

--- starter-code/src/main-vue.js
+++ starter-code/src/main-vue.js
@@ -1,12 +1,13 @@
 Vue.component('card-square',{
-  props: ['card'],
-  template: '<div class="card"><div class="back"></div><div class="front"></div></div>'
+  props: ['e'],
+  template: `<div class="card"><div class="front"></div><div class="back" v-bind:style="{background: 'url(' + e.img +') no-repeat'}"></div></div>`
 });
+
+// v-bind:style="{background: 'url(' + e.img +') no-repeat'}"
 
 var game = new Vue({
   el: '#memory_board',
   data: {
-    "data-card-name": "",
     cards: [
       { name: 'aquaman',         img: 'img/aquaman.jpg' },
       { name: 'batman',          img: 'img/batman.jpg' },
