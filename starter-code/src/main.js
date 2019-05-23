9451d79fe051ef15a11121def3afcacfd2a873fc
tuttifrutsy <yoratita@hotmail.com>
2019-05-23 16:20:40 -0500

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -30,9 +30,9 @@ var memoryGame = new MemoryGame(cards);
 document.addEventListener("DOMContentLoaded", function(event) { 
   var html = '';
   memoryGame.cards.forEach(function (pic) {
-    html += '<div class="card" data-card-name="'+ pic.name +'">';
-    html += '  <div class="back" name="'+ pic.img +'"></div>';
-    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+    html += '<div class="card" data-name="'+ pic.name +'">';
+    html += '  <div class="card-front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+    html += '  <div class="card-back"></div>';
     html += '</div>';
   });
 
@@ -40,12 +40,71 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.querySelector('#memory_board').innerHTML = html;
 
   // Bind the click event of each element to a function
-  document.querySelectorAll('.back').forEach(function(card) {
-    card.onclick = function() {
-      // TODO: write some code here
-      console.log('Card clicked')
+
+  let hasFlippedCard = false;
+  let lockBoard = false;
+  let firstCard, secondCard;
+
+ const cards= document.querySelectorAll('.card').forEach(function(card) {
+
+    card.onclick = function flipCard() {
+      if(lockBoard)return;
+      if(this === firstCard) return;
+
+      this.classList.add('flip');
+      
+      if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+
+      } else{
+        
+        secondCard = this;
+        
+        checkForMatch();
+        //console.log('function was executed');
+      }
+      
+      function checkForMatch(){
+        let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+         isMatch ?  disableCards() : unflipCards();
+        }
+      
+
+      function disableCards(){
+        
+        firstCard.removeEventListener('click', flipCard);
+        firstCard.removeEventListener("click", flipCard);
+
+        resetBorad();
+      }
+
+      function unflipCards(){
+        lockBoard= true;
+
+        setTimeout(()=>{
+          firstCard.classList.remove('flip');
+          secondCard.classList.remove('flip');
+          
+         resetBorad();
+        }, 1500);
+        
+      }
+
+      function resetBorad(){
+        [hasFlippedCard, lockBoard] = [false, false];
+        [firstCard, secondCard] = [null, null]
+      }
+
+      (function shuffle(){
+        cards.forEach(card =>{
+          let randomPos = Math.floor(Math.random()* 24);
+          card.style.order = randomPos;
+        });
+      })();
     }
   });
 });
+
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,10 @@
 class MemoryGame {
-  constructor(card){
+  constructor(card) {
     this.cards = cards;
   }
   shuffleCards() {}
   checkIfPair(card1, card2) {}
   isFinished() {}
 }
+
+
