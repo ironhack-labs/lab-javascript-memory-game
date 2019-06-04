20df1bd25be6f311ea84fabfbc0399420551bbe6
Majdi Chargui <mejdichergui@gmail.com>
2019-06-04 17:45:57 +0100

    done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,28 @@
 class MemoryGame {
-  constructor(card){
+  constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
   shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    }
+    return false;
+  }
+  isFinished() {
+    if (this.pairsClicked === 0) {
+      return false;
+    }
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else if (this.pairsGuessed === 0) {
+      return false;
+    }
+  }
 }
+MemoryGame.prototype.shuffleCards = function() {};
