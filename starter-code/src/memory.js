a2f29a786ae77b66bc21615c6ffd71f956e19c11
leaandrea <leaandreapaumier@gmail.com>
2019-06-10 09:29:39 +0200

    Finished Jasmine

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,42 @@
 class MemoryGame {
-  constructor(card){
+  constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    for (var i = this.cards.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = temp;
+
+    }
+    this.cards
+
+  }
+
+
+  checkIfPair(card1, card2) {
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    }
+    if (card1 !== card2) {
+      this.pairsClicked += 1;
+      return false;
+    }
+  }
+
+
+  isFinished() {
+    if (this.pairsGuessed == this.cards.length / 2) {
+      return true
+    }
+    return false;
+
+  }
+
 }
