ac169bc2dc34bcd96a5d9ccb4b0f61f0a053e8cb
Francho Garcia <franchog79@gmail.com>
2019-06-11 21:43:30 +0200

    All tests in green

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,29 @@
 class MemoryGame {
-  constructor(card){
-    this.cards = cards;
-  }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+    constructor(cards) {
+        this.cards = cards;
+        this.pickedCards = [];
+        this.pairsClicked = 0;
+        this.pairsGuessed = 0;
+    }
+    shuffleCards() {
+        let RandomPos = Math.floor(Math.random() * this.cards.length) + 1
+        this.cards.sort(() => { return RandomPos / 2 - RandomPos });
+    }
+    checkIfPair(card1, card2) {
+        this.pairsClicked += 1
+        if (card1 == card2) {
+            this.pairsGuessed += 1
+            return true
+        } else {
+            return false
+        }
+    }
+
+    isFinished() {
+        if (this.pairsGuessed == this.cards.length / 2) {
+            return true
+        } else {
+            return false
+        }
+    }
 }
