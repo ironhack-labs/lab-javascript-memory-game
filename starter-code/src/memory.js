a897dc7fd29166eebcf0fe019830732cece59221
Vanesa Puerta <Vanesa@MacBook-Air-de-Vanesa.local>
2019-06-11 23:40:41 +0200

    done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,37 @@
 class MemoryGame {
   constructor(card){
     this.cards = cards;
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+    
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    let m = cards.length, i;
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      [cards[m], cards[i]]= [cards[i], cards[m]];
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked +=1
+
+    if (card1 === card2){
+      this.pairsGuessed +=1
+       return true
+    } else {
+      return false
+    }
+  }
+  
+  isFinished() {
+
+    let result = false;
+    if(this.pairsGuessed === 8) {
+      result = true;
+    }
+    return result;
+  }
+
 }
