d16bae6cc0bb4ea9740cf6120a0f7d44c9f08e7f
MicaelZV <micaelzv@gmail.com>
2019-06-12 09:31:32 +0200

    me dio tiempo a tocar la parte que lleva el test, imposible para mi llegar al DOM... bastante martillito en los programas, aunque comprendiendo algo mas

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,38 @@
 class MemoryGame {
   constructor(card){
     this.cards = cards;
+    this.pickedCards=null;
+    this.pairsClicked=0;
+    this.pairsGuessed=0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+let shuffledCards=this.cards.sort(x=>a.Math.round(Math.random)*100 - b.Math.round(Math.random)*100);
+  }
+  checkIfPair(card1, card2) {
+this.pairsClicked+=1;
+if (card1===card2){
+  this.pairsGuessed+=1;
+  return true;
+}
+  else{
+    return false;
+  }
+}
+
+  
+  isFinished() {
+if (this.pairsGuessed==0){
+  return false;
+}
+
+ else if (this.pairsGuessed===8){
+  return true;
+}
+
+ else {
+  return false;
+}
+
+  
+}
 }
