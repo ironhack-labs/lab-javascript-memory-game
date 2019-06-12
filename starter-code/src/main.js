4877dad50c5082824db41d75d140d9b5473671e8
Rafael Ruiz <rafaelruizgallardon@hotmail.com>
2019-06-12 09:30:49 +0200

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -27,24 +27,27 @@ var cards = [
 var memoryGame = new MemoryGame(cards);
 
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+
+document.addEventListener("DOMContentLoaded", function(event) {
   var html = '';
-  memoryGame.cards.forEach(function (pic) {
-    html += '<div class="card" data-card-name="'+ pic.name +'">';
-    html += '  <div class="back" name="'+ pic.img +'"></div>';
-    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
-    html += '</div>';
+  memoryGame.cards.forEach(function(pic) {
+      html += '<div class="card" data-card-name="' + pic.name + '">';
+      html += '  <div class="back" name="' + pic.img + '"></div>';
+      html += '  <div class="front" style="background: url(img/' + pic.img + ') no-repeat"></div>';
+      html += '</div>';
   });
 
   // Add all the div's to the HTML
   document.querySelector('#memory_board').innerHTML = html;
 
   // Bind the click event of each element to a function
-  document.querySelectorAll('.back').forEach(function(card) {
-    card.onclick = function() {
-      // TODO: write some code here
-      console.log('Card clicked')
-    }
+  document.querySelectorAll('.card').forEach(function(card) {
+      card.onclick = function() {
+          // TODO: write some code here
+
+          flipCard(card)
+          console.log('Card clicked')
+      }
   });
 });
 
