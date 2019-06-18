f48ae4e3e388ffb4bda07706fe636cc2c0ec35bb
confidenti-al <steve@confidenti.al>
2019-06-18 16:32:05 -0400

    Adding memory.js logic to start

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,36 @@
 class MemoryGame {
   constructor(card){
     this.cards = cards;
-  }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
-}
+    this.pickedCards = pickedCards;
+    this.pairsClicked = pairsClicked;
+    this.pairsGuessed = pairsGuessed;
+  };
+  shuffleCards(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;  
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+      }
+    return array;
+  };
+  checkIfPair(card1, card2) {
+    if(card1 === card2){
+      this.pairsClicked++;
+      this.pairsGuessed++;
+      return true;
+    } else {
+      this.pairsClicked++;
+      return false;
+    }
+  };
+  isFinished() {
+    if (this.pairsGuessed >= 12) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+};
