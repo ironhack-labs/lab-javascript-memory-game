a69f5ee37519731f3be0ca51a2514b7f19d2852a
Ashley Arrue <ashleydarrue@gmail.com>
2019-06-18 23:40:09 -0400

    20% complete

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -35,17 +35,53 @@ document.addEventListener("DOMContentLoaded", function(event) {
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
   });
-
+  
+  
   // Add all the div's to the HTML
   document.querySelector('#memory_board').innerHTML = html;
-
+  
   // Bind the click event of each element to a function
-  document.querySelectorAll('.back').forEach(function(card) {
-    card.onclick = function() {
-      // TODO: write some code here
-      console.log('Card clicked')
-    }
+  // $('.back').click(function() {
+  //   let currentCard = $(this)[0].attributes.name.value;
+  //   $(this).css('backgroundImage', `url(img/${currentCard})`)
+  // })
+  
+  // function checkIfPair(card, cardd){
+    // function flipFlap (front , back){
+    //    ($this).toggleClass(".back", 1000);
+    // }
+    $('.back').click(function() {
+      let currentCard = $(this)[0].attributes.name.value;
+      let anotherCard = $(this)[0].attributes.name.value;
+      
+      if (currentCard === anotherCard){
+
+        $(this).css('backgroundImage', `url(img/${currentCard})`);
+      }
+    //     memoryGame.selectedCards.push(currentCard);
+    //     memoryGame.isFinished();
+    console.log(anotherCard);
   });
+  
+
+      // function flapFlip(front, back){
+      //   (".back").toggleClass(".front",1000);
+      // }
+    
+  
+
+      
+  //   let card1= $(this)[0].attributes.name.value;
+  //   let card2= $(this)[0].attributes.name.value;
+  //   if ($card1.name === $card2.name){
+  //     $(this).css('backgroundImage', `url(img/${currentCard})`)
+  //   }
+  //   else {
+  //     $(this).css('background-color', ("#456783"))
+  //   }
+  // }
+    
+    
+
+
 });
-
-
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,27 @@
 class MemoryGame {
   constructor(card){
     this.cards = cards;
+    this.selectedCards = [];
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
-}
+  shuffleCards(card) {
+    // for (i=0 ; i<this.cards.length; i--){
+    //   return i-2;
+    // }
+  }
+  checkIfPair(card1, card2) {
+    
+  }
+//   isFinished() {
+//     console.log(this.selectedCards);
+//     if (this.selectedCards.length === 2){
+//       if (this.selectedCards[0].name === this.selectedCards[1].name){
+//        $(this).css('backgroundImage', `url(img/${currentCard})`);
+//       }
+//       else {
+//         $(this).unbind()
+//       }
+//       console.log(this.isFinished);
+//     }
+//     // every time click add, compare
+//   }
+// }
