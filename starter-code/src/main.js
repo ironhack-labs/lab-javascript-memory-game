f7c6ab26cd484fa8c9c72b16339c67aed0bf169a
mario salvatierra <msalv006@fiu.edu>
2019-06-19 11:30:15 -0400

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,9 +25,11 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 var memoryGame = new MemoryGame(cards);
-
+memoryGame.shuffleCards();
+console.log(memoryGame.cards);
 
 document.addEventListener("DOMContentLoaded", function(event) { 
+
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
@@ -40,12 +42,56 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.querySelector('#memory_board').innerHTML = html;
 
   // Bind the click event of each element to a function
-  document.querySelectorAll('.back').forEach(function(card) {
-    card.onclick = function() {
-      // TODO: write some code here
-      console.log('Card clicked')
+  var backvar = $('.back'); 
+
+  backvar.click(function(){
+    var childrenJQueryReference = $(this).parent().children();
+      childrenJQueryReference.first().addClass('front').removeClass('back');
+      childrenJQueryReference.last().addClass('back').removeClass('front');
+    
+    memoryGame.pickedCards.push(childrenJQueryReference);
+    let sizePickedCards = memoryGame.pickedCards.length;
+
+    if (sizePickedCards%2 == 0){
+        
+        let lastCard = memoryGame.pickedCards[sizePickedCards-1];
+        let beforeLastCard = memoryGame.pickedCards[sizePickedCards-2];
+
+        let areTheSame = memoryGame.checkIfPair(lastCard, beforeLastCard);
+
+        $("#pairs_clicked").html(memoryGame.pairsClicked);
+        $("#pairs_guessed").html(memoryGame.pairsGuessed);
+
+        if(!areTheSame) {
+
+          $("#memory_board").css({
+            // "cursor": "wait",
+            "pointer-events": "none"
+        });
+
+          setTimeout(function() { 
+            lastCard.first().addClass('back').removeClass('front');
+            lastCard.last().addClass('front').removeClass('back');
+
+            beforeLastCard.first().addClass('back').removeClass('front');
+            beforeLastCard.last().addClass('front').removeClass('back');
+            
+            $("#memory_board").css({
+              // "cursor": "wait",
+              "pointer-events": "auto"
+          });
+           }, 2000);
+          
+        }
+
+
+    } // end of if (sizePickedCards%2 == 0)
+    if (memoryGame.isFinished()) {
+      setTimeout(alert('You Won!!!'), 500);
+      
     }
-  });
+  })
+
 });
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,35 @@
 class MemoryGame {
+  
   constructor(card){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    let sizeOfDeck = this.cards.length;
+      for (let i = sizeOfDeck - 1; i >= 0; i-- ){
+        // console.log("shuffle i= " + i);
+        let j = Math.floor(Math.random()*i);
+        let temporal = this.cards[j];
+        this.cards[j] = this.cards[i];
+        this.cards[i] = temporal;
+      }
+  }
+  checkIfPair(card1, card2) {
+    
+    this.pairsClicked +=1;
+    
+    if (card1.parent().attr('data-card-name') === card2.parent().attr('data-card-name')) {
+      this.pairsGuessed +=1;
+      
+      return true;
+    }
+    
+    return false;
+  }
+  isFinished() {
+    return this.pairsGuessed === (this.cards.length/2);
+  }
 }
