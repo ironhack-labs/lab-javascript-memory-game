6e7c7b533a434c039d8803c3e2787d93f78f6439
Taluama Velludo <taluama@gmail.com>
2019-06-19 19:06:13 -0400

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -26,6 +26,7 @@ var cards = [
 ];
 var memoryGame = new MemoryGame(cards);
 
+memoryGame.shuffleCards(cards);
 
 document.addEventListener("DOMContentLoaded", function(event) { 
   var html = '';
@@ -39,13 +40,42 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Add all the div's to the HTML
   document.querySelector('#memory_board').innerHTML = html;
 
+  let blocked = function(whatToBlock) {
+    $(whatToBlock).toggleClass('blocked');
+  }
+
   // Bind the click event of each element to a function
-  document.querySelectorAll('.back').forEach(function(card) {
+  document.querySelectorAll('.card').forEach((card) => {
     card.onclick = function() {
-      // TODO: write some code here
-      console.log('Card clicked')
+      $(this.children).toggleClass('back front');
+
+      if ($('div:first-child.front')[1] !== undefined) {
+        
+        // let dataCardName = $(this).attr('data-card-name');
+        let cardOne = document.querySelector('div:first-child.front').getAttribute('name');
+        let cardTwo = document.querySelectorAll('div:first-child.front')[1].getAttribute('name');
+
+        memoryGame.checkIfPair(cardOne, cardTwo);
+        $('#pairs_clicked').html(memoryGame.pairsClicked);
+        $('#pairs_guessed').html(memoryGame.pairsGuessed);
+
+        if (cardOne !== cardTwo) {
+          function flipCardsBack() {
+            blocked(".card div");
+            $(`[name*="${cardOne}"].front, [name*="${cardOne}"].front + div`).toggleClass('front back');
+            $(`[name*="${cardTwo}"].front, [name*="${cardTwo}"].front + div`).toggleClass('front back');
+          }
+          setTimeout(flipCardsBack, 2000);
+          blocked(".card div");
+        } else {
+          $(`[name*="${cardOne}"]`).toggleClass('front');
+        }
+      }
+
+      if (memoryGame.isFinished()) {
+        alert('Good job! :D');
+        window.location.reload();
+      }
     }
   });
 });
-
-
