0c55a34667436efa9bf0ee99e7bbb8097a398dab
Andre Cabrera <cabrerandre@gmail.com>
2019-06-20 09:11:10 -0400

    completed game except shuffling

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -36,16 +36,81 @@ document.addEventListener("DOMContentLoaded", function(event) {
     html += '</div>';
   });
 
-  // Add all the div's to the HTML
+  // Adds all the div's to the HTML
   document.querySelector('#memory_board').innerHTML = html;
 
-  // Bind the click event of each element to a function
-  document.querySelectorAll('.back').forEach(function(card) {
-    card.onclick = function() {
-      // TODO: write some code here
-      console.log('Card clicked')
+  // Binds the click event of each element to a function
+  // document.querySelectorAll('.back').forEach(function(card) {
+  //   card.onclick = function() {
+  //   console.log(card.parentElement)
+  //   }
+  // }); 
+  
+  $('.back').click(function(){
+
+    console.log(memoryGame.clickedCards)
+
+      $(this).removeClass('back').addClass('front')  // flips card
+      $(this).siblings('.front').removeClass('front').addClass('back')
+
+      // let pairsGuessed = Number($('#pairs_guessed').innerHTML)
+      // pairsGuessed = 50;
+      // console.log(pairsGuessed)
+      // let pairsClicked = Number($('#pairs_clicked').text())
+      // console.log(pairsClicked)
+      
+    memoryGame.clickedCards.push(this) //pushes to array
+    // console.log("---- ", memoryGame.clickedCards)
+
+    let card1 = memoryGame.clickedCards[0] // references the card within the array
+    let card2 = memoryGame.clickedCards[1]
+    
+    if(memoryGame.clickedCards.length == 2) {  //checks if array has 2 cards   
+      if(!memoryGame.checkIfPair(card1, card2)) { //if it does it checks if the cards have the same value
+        setTimeout(() => {  //if they do not, it flips both cards back with a timer
+              
+          $(card1).removeClass('front').addClass('back')
+          $(card1).siblings('.back').removeClass('back').addClass('front')
+          
+          $(card2).removeClass('front').addClass('back')
+          $(card2).siblings('.back').removeClass('back').addClass('front') 
+          
+        }, 300)
+      } else {
+        
+        memoryGame.guessed += 1;
+        $('#pairs_guessed').text(memoryGame.guessed)
+        
+        // console.log("guessed  ", pairsGuessed)
+      }
+
+      memoryGame.clicked += 1 
+      $('#pairs_clicked').text(memoryGame.clicked)  
+
+      memoryGame.clickedCards = []  // if they do match, it clears the code and leaves the cards alone
     }
+
+    
+      
+    
+
+
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,26 @@
 class MemoryGame {
   constructor(card){
-    this.cards = cards;
+    this.cards = card;
+    this.clickedCards = [];
+    this.guessed = 0;
+    this.clicked = 0;
   }
   shuffleCards() {}
-  checkIfPair(card1, card2) {}
+
+  checkIfPair(card1, card2) {
+
+    // console.log(card1, " === ", card2)
+    // console.log(card2)
+
+  if(card1.attributes[1].value === card2.attributes[1].value){ 
+    // console.log("its true");
+    return true;
+  } else {
+    // console.log("its false");
+    return false;
+  }
+
+  } // checkIfPair
+
   isFinished() {}
 }
