3fde45c1400f61eed3ef0e90cfd380ece23e8e47
Ant-Quanique Dancy <adancy@cc-aquanique-mbp.local>
2019-07-02 16:27:57 -0400

    moved the compare card logic to memory.js file, and updated winner message

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -51,43 +51,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     //flip a card
     $('.card').click( function(){
-      //flips the car when it's clicked
+      //flips the card when it's clicked
       $(this).children(":first").toggleClass("back front");
       $(this).children(":nth-child(2)").toggleClass("front back");
       
       //adds the card to the list of picked cards
       memoryGame.pickedCards.push($(this));
-      console.log(memoryGame.pickedCards)
 
-      setTimeout(function(){
-        //stores each one of the selected cards in a variable
-        let card1 = memoryGame.pickedCards[0];
-        console.log(card1)
-        let card2 = memoryGame.pairsClicked[1];
-        //.children(":first").attr("name")
-        console.log(card2)
-        
-        if(memoryGame.pickedCards.length === 2){
-          console.log(memoryGame.pickedCards.length)
-          //compairs to cards to see if they match
-          let pair = memoryGame.checkIfPair(card1,card2)
-          if(pair){
-            memoryGame.pickedCards.forEach((card, index) => {
-                $(memoryGame.pickedCards[index]).children(":first").toggle()
-              })  
-        } else if(!pair) {
-            memoryGame.pickedCards.forEach(card => {
-              $(card).children(":first").toggleClass("back front");
-              $(card).children(":nth-child(2)").toggleClass("front back"); 
-            })
-          }
-    
-        }   
-    }, 1000)
-    
+      if(memoryGame.pickedCards.length === 2){
+        //checks if two cards have been clicked, and determine if they are the same or not
+        let card1 = [...memoryGame.pickedCards[0]][0].getAttribute('data-card-name');
+        let card2 = [...memoryGame.pickedCards[1]][0].getAttribute('data-card-name');
+        memoryGame.checkIfPair(card1,card2)
+      }
 
     if(memoryGame.isFinished()){
-      alert("Game Over. All the cards have been matched!")
+      h1 = "<h1>Winner! Game Over. All the cards have been matched.</h1>"
+      $("#memory_board").append(h1).addClass('.winner');
+      
     }
    
   })
