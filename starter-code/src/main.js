59c6bf2839ea7f2979b1947caeae42644a31679a
Mark Trevail <trevail.m@gmail.com>
2019-07-05 14:02:20 +0200

    Nearly there

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -1,7 +1,75 @@
-// Run things
+$(document).ready(function(){
+
+// -------------------------------------------------------------------------------------------
+// Set up new game
+// -------------------------------------------------------------------------------------------
 
 var memoryGame = new MemoryGame(cards);
+// memoryGame.shuffleCards();
 
-memoryGame.shuffleCards();
-console.log(memoryGame.cards);
+// -------------------------------------------------------------------------------------------
+// Set up all cards
+// -------------------------------------------------------------------------------------------
+
+// Create the html for all cards
+var html = '';// Run things
+
+memoryGame.cards.forEach((pic) => {
+  html += '<div class="card" data-card-name="'+ pic.name +'">';
+  html += '  <div class="back" name="'+ pic.img +'"></div>';
+  html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+  html += '</div>';
+});
+
+// Add in the card html
+$("#memory_board").html(html);
+
+// Add click events to each card - on the "back" class
+$(".back").on("click", () => {     // Don't need to iterate in jquery - can add events to all elements in array at once
+
+    $(event.target).toggleClass("back front");   // Change back to front on the div clicked
+    $(event.target).siblings().toggleClass("back front");   // Change front to back of the sibling div
+  
+    // Turning cards
+    if(memoryGame.cardsTurned === 0) {
+      // First card turned
+      memoryGame.turnCard();
+      memoryGame.saveCardOne();
+      memoryGame.$card1Node = $(event.target);
+    } else if(memoryGame.cardsTurned === 1) {
+      // Second card turned
+      memoryGame.turnCard();
+      memoryGame.saveCardTwo();
+      memoryGame.$card2Node = $(event.target);
+      memoryGame.addToPairsClicked();
+
+      if(memoryGame.isPair()) {   // Check if we have a pair - (if false, it waits 1s before returning false) 
+        // If a pair is found
+        memoryGame.addToPairsFound();
+        console.log(`Pair found! Num of pairs found is: ${memoryGame.pairsFound}`);
+        } else {
+        // If a pair is not found - flip the TWO CARDS SELECTED back (but wait a bit)
+        console.log("Pair not found");
+        setTimeout( () => {
+          memoryGame.$card1Node.toggleClass("back front");
+          memoryGame.$card1Node.siblings().toggleClass("back front");
+          memoryGame.$card2Node.toggleClass("back front");
+          memoryGame.$card2Node.siblings().toggleClass("back front");
+        }, 1000);
+      };
+      memoryGame.cardsTurned = 0; // Reset
+    };
+
+    // Check if finished each click - bit wasteful but keeps logic simpler
+    if(memoryGame.isFinished()) {
+      console.log("Finished!");
+        setTimeout( () => {
+        alert(`Well done - you won in ${memoryGame.pairsClicked} guesses!`);}, 500);
+    }; 
+
+  });
+
+
+
+});
 
--- starter-code/src/memoryGame.js
+++ starter-code/src/memoryGame.js
@@ -4,9 +4,11 @@ class MemoryGame {
     this.cards = cards;
     this.cardTurned1 = "";
     this.cardTurned2 = "";
+    this.$card1Node = "";
+    this.$card2Node = "";
     this.cardsTurned = 0;
     this.pairsClicked = 0;
-    this.pairsGuessed = 0;
+    this.pairsFound = 0;
   }
 
   shuffleCards() {
@@ -23,19 +25,35 @@ class MemoryGame {
     this.cards = [...shuffledCards]; 
   }
 
-  checkIfPair(card1, card2) {
-    this.pairsClicked += 1;
-    if(card1 === card2) {
-      this.pairsGuessed += 1;
-      return true;
-    } else {
-      return false;
-    }
+  turnCard() {
+      this.cardsTurned +=1;
+  };
+
+  addToPairsClicked() {
+    this.pairsClicked +=1;
+  };
+
+  addToPairsFound() {
+    this.pairsFound +=1;
+  };
+
+  saveCardOne() {
+    this.cardTurned1 = $(event.target).attr("name");
+  };
+
+  saveCardTwo() {
+    this.cardTurned2 = $(event.target).attr("name");
+  };
+
+  isPair() {
+    return(this.cardTurned1 === this.cardTurned2);
   };
 
   isFinished() {
     let numPairs = this.cards.length / 2;
-    return (this.pairsGuessed === numPairs) ? true : false;    
+    return (this.pairsFound === numPairs) ? true : false;    
   };
+
+
 
 };
