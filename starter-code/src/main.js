edf18bf089f6c782583b1947f461642cc7a2ad39
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2019-07-06 01:28:49 +0200

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -26,8 +26,10 @@ var cards = [
 ];
 var memoryGame = new MemoryGame(cards);
 
-
 document.addEventListener("DOMContentLoaded", function(event) { 
+
+  memoryGame.shuffleCards();
+
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
@@ -38,14 +40,52 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   // Add all the div's to the HTML
   document.querySelector('#memory_board').innerHTML = html;
-
   // Bind the click event of each element to a function
   document.querySelectorAll('.back').forEach(function(card) {
+    // var goodGuess = false;
     card.onclick = function() {
-      // TODO: write some code here
-      console.log('Card clicked')
-    }
+
+      // revealing the image of the clicked card
+      $(this).toggleClass("back front");
+      $(this).siblings().toggleClass("back front");
+
+      // retrieving the proper card from the image we clicked on (img + bg)
+      var cardClicked = $(this).parent();
+
+      // storing the card type we just clicked on
+      var nameCardClicked = $(cardClicked).attr("data-card-name");
+      memoryGame.log.push(nameCardClicked);
+
+      // marking the card we want to check
+      cardClicked.addClass("to-check");
+
+      // counting the number of clicks we did...
+      memoryGame.clicks +=1;
+      // so that every 2 clicks we check if it's a pair or not
+      if (memoryGame.clicks % 2 === 0) {
+        let isPair = memoryGame.checkIfPair();
+        // if it is not a pair, we hide the cards after 1 sec
+        if (isPair === false) { 
+          setTimeout(function(){
+            $(".to-check").children().toggleClass("back front");
+            $(".to-check").toggleClass("to-check");
+          },500);
+        // if it's a pair, we remove the class "to-check" because we only want to display them now
+        } else {
+          $(".to-check").toggleClass("to-check");
+        }
+        // we empty our log so that every two clicks we only compare two cards
+        setTimeout(function(){
+          memoryGame.log = [];
+        }, 500);
+      }
+
+      $("#pairs_clicked").html(memoryGame.pairsClicked);
+      $("#pairs_guessed").html(memoryGame.pairsGuessed);
+      memoryGame.isFinished();
+    };
   });
 });
+
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,42 @@
 class MemoryGame {
   constructor(card){
     this.cards = cards;
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    this.clicks = 0;
+    this.log = [];
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    let reshuffledCards = [];
+    let reshuffledIndex = [];
+
+    // store the random index we already used so that we don't use them again
+    while (reshuffledCards.length < this.cards.length) {
+      let randomIndex = Math.floor(Math.random()*this.cards.length);
+      if (!reshuffledIndex.includes(randomIndex)) {
+        reshuffledCards.push(this.cards[randomIndex]);
+        reshuffledIndex.push(randomIndex);
+      } 
+    }
+    this.cards = reshuffledCards;
+  }
+
+
+  checkIfPair() {
+    if(this.log[0] === this.log[1]) {
+      this.pairsClicked+=1;
+      this.pairsGuessed+=1;
+      return true;
+    } else {
+      this.pairsClicked+=1;
+      return false;
+    }
+  }
+
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length/2) {
+      alert("The game is finished!");
+    }
+  }
 }
