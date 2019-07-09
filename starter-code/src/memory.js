ea0356d999915bb1fa2a14971c2fed514dfaf156
laanayam333 <laanayam333@gmail.com>
2019-07-09 23:15:24 +0200

    semi done

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,46 @@
 class MemoryGame {
-  constructor(card){
+  constructor(card) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    this.numberOfPairs = card.length / 2;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    var currentIndex = this.cards.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = this.cards[currentIndex];
+      this.cards[currentIndex] = this.cards[randomIndex];
+      this.cards[randomIndex] = temporaryValue;
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isFinished() {
+    if (this.pairsGuessed == this.numberOfPairs) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
