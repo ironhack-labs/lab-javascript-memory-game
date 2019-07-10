cc6b693709e53fc61d398c7f3a67b16e2c7944a6
Josue Martin <biojosue@hotmail.com>
2019-07-10 09:05:24 +0200

    Memory Game

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,31 @@
 class MemoryGame {
-  constructor(card){
-    this.cards = cards;
+  constructor(card) {
+    this.cards = card;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    var j, x, i;
+    for (i = this.cards.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = x;
+    }
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked = this.pairsClicked + 1;
+    if (card1 === card2) {
+      this.pairsGuessed = this.pairsGuessed + 1;
+      return true;
+    }
+    return false;
+  }
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    }
+    return false;
+  }
 }
