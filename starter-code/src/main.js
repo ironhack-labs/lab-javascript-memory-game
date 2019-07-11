b0ca46454657b4bee654e8d847b2e58b37b85788
Rafael Bueno <rafaelbuenolink@gmail.com>
2019-07-10 21:11:52 -0300

    Imune a erros. We Killed Thanos

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,7 +25,7 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 var memoryGame = new MemoryGame(cards);
-
+memoryGame.shuffleCards(cards);
 
 document.addEventListener("DOMContentLoaded", function(event) { 
   var html = '';
@@ -40,10 +40,46 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.querySelector('#memory_board').innerHTML = html;
 
   // Bind the click event of each element to a function
-  document.querySelectorAll('.back').forEach(function(card) {
+
+  document.querySelectorAll('.card').forEach(function(card) {
     card.onclick = function() {
-      // TODO: write some code here
+      
+      if (memoryGame.pickedCards.length !== 2) {
+        console.log("memoryGame.pickedCards.length: " + memoryGame.pickedCards.length)
+      card.classList.add('selected-card');
+
+      let divBack = card.querySelector('.back');
+      let divFront = card.querySelector('.front');
+      let picName = divBack.getAttribute('name');
+
+      console.log(memoryGame.pickedCards.includes(picName))
+
+      if ((picName)) {
+
+      divBack.classList.toggle('front')
+      divBack.classList.toggle('back')
+
+      divFront.classList.toggle('front')
+      divFront.classList.toggle('back')
+
+      memoryGame.pickedCards.push(picName);
+      
+      if (memoryGame.pickedCards.length === 2) {
+        memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1]);
+
+        document.getElementById('pairs_clicked').innerHTML = memoryGame.pairsClicked;
+        document.getElementById('pairs_guessed').innerHTML = memoryGame.pairsGuessed;
+      }
+      console.log('pair clicked' + memoryGame.pairsClicked);
+      
+      // virar carta
+      // html += '<div class="card" data-card-name="'+ picName +'">';
+      // html += '  <div class="back" name="'+ pic.img +'"></div>';
+      // html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+      // html += '</div>';
+    }
       console.log('Card clicked')
+    }
     }
   });
 });
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,94 @@
 class MemoryGame {
-  constructor(card){
+  constructor(cards){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+   // this.shuffleCards(cards)
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+   
+    //if (arrCards.length === 0) {return undefined};
+
+    let remainLength = this.cards.length;
+    
+
+    while (remainLength > 0) {
+      let randomElement = Math.floor(Math.random() * remainLength);
+      
+      remainLength -= 1;
+
+      let temporaryElement = this.cards[remainLength];
+      this.cards[remainLength] = this.cards[randomElement];
+      this.cards[randomElement] = temporaryElement;
+
+      
+    }
+  
+    
+    //return arrCards;
+
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+
+
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      
+      document.querySelectorAll('.selected-card').forEach(function(card) {
+      card.classList.remove('selected-card');
+      card.classList.add('blocked')
+      
+      });
+
+      
+      setTimeout(() => {
+        this.isFinished();
+      }, 100);
+      this.pickedCards = [];
+      return true
+
+    } else {
+      console.log('NOT GUESSED')
+     setTimeout(() => {
+      document.querySelectorAll('.selected-card').forEach(function(card) {
+        
+        console.log('REMOVE Abaixo')
+          card.classList.remove('selected-card');
+    
+          let divBack = card.querySelector('.back');
+          let divFront = card.querySelector('.front');
+    
+          divBack.classList.toggle('front')
+          divBack.classList.toggle('back')
+    
+          divFront.classList.toggle('front')
+          divFront.classList.toggle('back')
+    
+         
+
+          console.log('Card clicked')
+    
+      });
+
+      this.pickedCards = [];
+      }, 1500);
+      
+      return false;  
+    }
+
+
+  }
+  isFinished() {
+    if (this.pairsGuessed === (this.cards.length/2)) {
+      alert('You Killed Thanos');
+
+      document.location.reload()
+      
+      return true
+    } else {
+      return false;
+    }
+  }
 }
