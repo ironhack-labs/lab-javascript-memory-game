44f7146f702ea831d4a931f800e1757568c1f86e
Augusta Raposo <gutaraposomarques@gmail.com>
2019-08-05 00:47:10 +0200

    update

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,7 +25,7 @@ var cards = [
     { name: 'thor', img: 'thor.jpg' }
 ];
 var memoryGame = new MemoryGame(cards);
-
+var twoUnflip = [];
 
 // document.addEventListener("DOMContentLoaded", function(event) {
 //     var html = '';
@@ -44,39 +44,45 @@ function notify() {
     alert("clicked");
 }
 
-function giveTime(){
+function giveTime() {
 
 }
 
-let blockCards= (cards)=> {
-  let first = cards.first();
-  let second = cards.last();
-  first.children().last().addClass("blocked");
-  second.children().last().addClass("blocked");
+let blockCards = (cards) => {
+    let first = cards.first();
+    let second = cards.last();
+    first.children().last().addClass("blocked");
+    second.children().last().addClass("blocked");
 
 }
 let flipCard = (card) => {
+
+
+    if (twoUnflip.length == 2) {
+        twoUnflip[0].children().last().removeClass('back');
+        twoUnflip[0].children().first().addClass('back');
+        twoUnflip[1].children().last().removeClass('back');
+        twoUnflip[1].children().first().addClass('back');
+        twoUnflip = [];
+    }
     let children = card.children();
     let backCard = children.first()
     let frontCard = children.last();
 
-
     if (backCard.hasClass('back')) {
         backCard.removeClass('back');
         memoryGame.setPair(card.data("card-name"), (ifPairs, card1, card2) => {
-        let firstCard = $(document).find(`[data-card-name='${card1}']`); 
-        let secondCard = $(document).find(`[data-card-name='${card2}']`); 
+            let firstCard = $(document).find(`[data-card-name='${card1}']`);
+            let secondCard = $(document).find(`[data-card-name='${card2}']`);
 
-         
-          if (ifPairs) {
-            blockCards(firstCard);
-          }else{
-            setTimeout(()=>{
-             flipCard(firstCard.first());
-            flipCard(secondCard.first());
-            }, 1000);
+            if (ifPairs) {
+                blockCards(firstCard);
+            } else {
+                console.log("passou aqui");
+                twoUnflip.push(firstCard.first());
+                twoUnflip.push(secondCard.first());
 
-          }
+            }
         });
 
     } else {
@@ -90,7 +96,7 @@ $(document).ready(() => {
     memoryGame.cards.forEach(function(pic) {
         html += '<div class="card" data-card-name="' + pic.name + '">';
         html += '  <div class="back" name="' + pic.img + '"></div>';
-        html += '  <div class="front" style="background: url(img/' + pic.img + ') no-repeat"></div>';
+        html += '  <div class="front blocked" style="background: url(img/' + pic.img + ') no-repeat"></div>';
         html += '</div>';
     });
 
