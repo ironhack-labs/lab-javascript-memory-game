24a463320ff55bdd1ffea4102e4752a95f42e889
Ernesto <02ernesto.rodriguez@gmail.com>
2019-08-10 00:58:59 -0500

    No me quita el error Jazmin

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,21 @@
+// Juego Memorma
+
 class MemoryGame {
   constructor(card){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
+  shuffleCards(cardsArr) {
+    var i, j;
+    for (i = cardsArr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+      
+       
+    }
+    return; 
+  }
   checkIfPair(card1, card2) {}
   isFinished() {}
 }
