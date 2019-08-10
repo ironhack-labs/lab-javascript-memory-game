8f265c7aafa4f65319995436f973239748b7af13
Minatsu Homma <minatsu.homma@gmail.com>
2019-08-10 10:48:06 +0200

    initial commit

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,27 +25,66 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 var memoryGame = new MemoryGame(cards);
+let clickedBack = [];
+let clickedFront = [];
+let compare = [];
+let intervalId = 0;
 
-
-document.addEventListener("DOMContentLoaded", function(event) { 
+$(document).ready(function(){
+  memoryGame.shuffleCards();
   var html = '';
-  memoryGame.cards.forEach(function (pic) {
-    html += '<div class="card" data-card-name="'+ pic.name +'">';
-    html += '  <div class="back" name="'+ pic.img +'"></div>';
-    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+  $.each(memoryGame.cards, function(){
+    html += '<div class="card" data-card-name="'+ this.name +'">';
+    html += '  <div class="back" name="'+ this.img +'"></div>';
+    html += '  <div class="front" style="background: url(img/'+ this.img +') no-repeat"></div>';
     html += '</div>';
-  });
+  })
+  $("#memory_board").html(html);
 
-  // Add all the div's to the HTML
-  document.querySelector('#memory_board').innerHTML = html;
-
-  // Bind the click event of each element to a function
-  document.querySelectorAll('.back').forEach(function(card) {
-    card.onclick = function() {
-      // TODO: write some code here
-      console.log('Card clicked')
+  $(".card").click(function() {
+    const characterName = $(this).data("card-name");
+    const back = $(this).find(".back");
+    const front = $(this).find(".front");
+    back.toggleClass("back front");
+    front.toggleClass("front back");
+    clickedBack.push(back);
+    clickedFront.push(front);
+    compare.push(characterName);
+    if(compare.length % 2 === 0){
+      memoryGame.checkIfPair(compare[0],compare[1]);
+      memoryGame.isFinished();
+      if(compare[0] === compare[1]){
+        console.log("match");
+        clickedBack = [];
+        clickedFront = [];
+      }else if(compare[0] !== compare[1]){
+        console.log("these don't match");
+        setTimeout(function() { 
+          $(clickedBack).toggleClass("front back");
+          $(clickedFront).toggleClass("back front");
+          clickedBack = [];
+          clickedFront = [];
+        }.bind(this), 500);
+      }
+      compare = [];
     }
   });
+  // $(".back").on("click",function(){
+  //     console.log('back clicked');
+  //     clicked.push(this);
+  //     $(this).toggleClass("back front");
+  //     $(this).siblings().toggleClass("front back");
+  //     console.log(clicked[0]);
+  //   })
+  // $(".front").on("click",function(){
+  //     console.log('Front clicked');
+  //       $(this).toggleClass("front back");
+  //       $(this).siblings().toggleClass("back front");
+  //   })
+
+  // function changeBack (element) {
+  //   $(element).toggleClass("back front");
+  //   $(element).siblings().toggleClass("front back");
+  // }
+    
 });
-
-
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,41 @@
 class MemoryGame {
-  constructor(card){
+  constructor(cards){
     this.cards = cards;
+    this.pickedCards = []; // the cards the user have clicked
+    this.pairsClicked = 0; 
+    this.pairsGuessed = 0; 
   }
   shuffleCards() {}
   checkIfPair(card1, card2) {}
   isFinished() {}
 }
+
+MemoryGame.prototype.shuffleCards = function() {
+  this.cards.sort(() => Math.random() - 0.5);
+  //console.log(this.cards);
+};
+
+MemoryGame.prototype.checkIfPair = function(firstCard, secondCard) {
+  this.pairsClicked++;
+  console.log("FirstCardName", firstCard);
+  console.log("SecondCardName", secondCard);
+  console.log("PairsClicked",this.pairsClicked);
+  $("#pairs_clicked").text(this.pairsClicked);
+  if (firstCard === secondCard){
+    this.pairsGuessed++;
+    $("#pairs_guessed").text(this.pairsGuessed);
+    console.log("PairsGuessed",this.pairsGuessed);
+    return true;
+  }else{
+    return false;
+  }
+};
+
+MemoryGame.prototype.isFinished = function() {
+  if (this.pairsGuessed >= this.cards.length/2){
+    alert("finished");
+    return true;
+  }else{
+    return false;
+  }
+};
