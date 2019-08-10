cc58a3ca318b2dd3b150e1f2d6ee3a8bf6a30572
nancy27vazquez <nevadg@outlook.com>
2019-08-09 20:21:28 -0500

    Main Jasmine validations added

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,38 @@
 class MemoryGame {
-  constructor(card){
+  constructor(card) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards(cards) {
+    /*function shuffle(sourceArray) {
+      for (var i = 0; i < sourceArray.length - 1; i++) {
+          var j = i + Math.floor(Math.random() * (sourceArray.length - i));
+  
+          var temp = sourceArray[j];
+          sourceArray[j] = sourceArray[i];
+          sourceArray[i] = temp;
+      }
+      return sourceArray;
+    }*/
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    /*if (pairsGuessed == memoryGame.length - 1) {
+      return true;
+    } else {
+      return false;
+    }*/
+    return false;
+  }
 }
