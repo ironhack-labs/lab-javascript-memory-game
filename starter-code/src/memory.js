8b3c13e2f143581f449830903f979c2151d44fb6
angelmc32 <angel.melendez.c@gmail.com>
2019-08-10 23:09:08 -0500

    Small modifications, still incomplete

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,26 @@
 class MemoryGame {
-  constructor(card){
+  constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    for (let i = 0; i < this.cards.length; i++) {
+      let random = Math.floor(Math.random * this.cards.length);
+      this.cards[i] = this.cards[random];
+    }
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    }
+    return false;
+  }
+  isFinished() {
+    if (this.pairsGuessed * 2 === this.cards.length) return true;
+    return false;
+  }
 }
