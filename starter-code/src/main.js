7b9ee6a02ee0c9ec20eb9e04ad03254385905e2e
Ahernandezl0889 <ahernandezl0889@gmail.com>
2019-08-14 20:26:04 -0400

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -34,18 +34,45 @@ document.addEventListener("DOMContentLoaded", function(event) {
     html += '  <div class="back" name="'+ pic.img +'"></div>';
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
     html += '</div>';
+
+    
   });
 
   // Add all the div's to the HTML
-  document.querySelector('#memory_board').innerHTML = html;
+  document.querySelector("#memory_board").innerHTML = html;
 
-  // Bind the click event of each element to a function
-  document.querySelectorAll('.back').forEach(function(card) {
-    card.onclick = function() {
-      // TODO: write some code here
-      console.log('Card clicked')
-    }
-  });
-});
+$(".back").click(function() {
+let target = $(this)
+if (memoryGame.pickedCards.length <= 2){// if the img has the same
+target.siblings().addClass("back"); // only use dot when selected gives the class of back to front
+target.siblings().addClass("unmatch"); // adds class unmatched, acts a a filter the card that we wat to flip again
+target.removeClass("back"); // takes away back from parent card
+memoryGame.pickedCards.push(target);
+console.log(memoryGame.pickedCards);
+}
+if (memoryGame.pickedCards.length > 1){
+ if (memoryGame.checkIfPair(memoryGame.pickedCards[0].parent().attr("data-card-name"), memoryGame.pickedCards[1].parent().attr("data-card-name")) === true){
+   memoryGame.pickedCards[0].siblings().removeClass("unmatch");
+   memoryGame.pickedCards[1].siblings().removeClass("unmatch");
+ } else {
+   setTimeout(function(){
+     removeElement()
+   },1000)
+ }
+ memoryGame.pickedCards=[]
+}; // end
+})
+})//end of everthing
+function removeElement (){
+ $(".unmatch").each(function(){ // selects unmatched
+ $(this).click()
+ $(this).removeClass('back');
+ $(this).prev().addClass("back")
+ })
+}
+//call the function
+
+
+
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,55 @@
 class MemoryGame {
   constructor(card){
     this.cards = cards;
+    //the length of this arr is 2, and contains 2 cards we will be comparing
+    this.pickedCards = [];
+    // this number opdates every 12
+    this.pairsClicked = 0;
+    // the goal is to get to 12 guessed pairs
+    this.pairsGuessed = 0;
+    this.shuffleCards(cards);
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+
+  shuffleCards(someArr) {
+    let theLength = someArr.length;
+    while (theLength > 0 ){
+      // getting the random number
+      let index = Math.floor(Math.random ()* theLength);
+      theLength --;
+      // create a tem variable that will get the value of the last element in the arr
+      let tempVar = someArr[theLength];
+      //fill up the last element with the random array  elem
+      someArr[theLength]= someArr[index];
+      // fill up the empty space where the random element was in the tempVar
+      someArr[index] = tempVar;
+    }
+    return someArr;
+  }
+
+
+
+  checkIfPair(card1, card2) {
+this.pairsClicked += 1
+if (card1 === card2){
+  this.pairsGuessed +=1
+  $("#pairs_guessed").html(this.pairsGuessed)
+  $("#pairs_clicked").html(this.pairsClicked)
+return true
+
+}else{
+  this.pairsClicked += 1
+  $("#pairs_clicked").html(this.pairsClicked)
+  return false;
 }
+
+  }
+  isFinished() {
+    
+    
+    return false
+  }
+
+  
+}
+
