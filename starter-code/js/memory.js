6696f663ea27396c253e3d9e625ece0ab31f33ee
borgila <elbienesbueno@gmail.com>
2019-08-28 01:27:02 +0200

    first commit

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,38 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards,pickedCards,pairsClicked,pairsGuessed){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    let cards = this.cards;
+    let m = cards.length
+    let i;
+  
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+  
+      [cards[m], cards[i]] = [cards[i], cards[m]]; 
+    }
+    // return this;no termino de entender la linea 17
+    return undefined ;
+  }
+
+  
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++
+    if(card1==card2){
+      this.pairsGuessed++
+      return true
+    }else return false
+  }
+  isFinished()  {
+    if (this.pairsGuessed === (this.cards.length / 2)) {
+      return true;
+    } else {
+      return false;
+    }}
 }
