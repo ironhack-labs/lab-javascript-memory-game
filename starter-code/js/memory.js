0e21f575bd26a5eaff9b0468cb68310e5860cb08
Lorena Poza Díaz <lorenapozadiaz@MacLPD.local>
2019-08-27 23:16:35 +0200

    First commit: iterations 1-3 done

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,44 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards(cards) {
+    const newCards = this.cards;
+    let currentIndex = newCards.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      temporaryValue = newCards[currentIndex];
+      newCards[currentIndex] = newCards[randomIndex];
+      newCards[randomIndex] = temporaryValue;
+    }
+    return undefined;
+  
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
+  
+  }
+
+  isFinished() {
+    if (this.pairsGuessed === (this.cards.length / 2)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
