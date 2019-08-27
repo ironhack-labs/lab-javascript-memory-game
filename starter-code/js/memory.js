851e2a9e97866cc47a0742a76c53c436f163c8b7
“Natalia <natgarea@gmail.com>
2019-08-28 00:40:04 +0200

    iteration 1-3

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,38 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    const deck = this.cards;
+    let shuffledCards = [];
+    let n = deck.length;
+    let i = 0;
+    while (n) {
+      i = Math.floor(Math.random() * n--);
+      shuffledCards.push(deck(i,1)[0]);
+    }
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    let result = false;
+    let str1 = JSON.stringify(card1);
+    let str2 = JSON.stringify(card2);
+    if (str1 == str2) {
+      this.pairsGuessed++;
+      result = true;
+    }
+    return result;
+  }
+  isFinished() {
+    let finished = false;
+    let pairsInGame = this.cards.length / 2;
+    if (this.pairsGuessed == pairsInGame) {
+      finished = true;
+    }
+    return finished;
+  }
 }
