29c3b9a8ff224cc39dab239e5641dbfa02db55d2
Maximiliano Sussini <maxsus60@gmail.com>
2019-08-28 09:29:32 +0200

    pending

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -27,6 +27,8 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+let twoCardsFlipped = false;
+
 document.addEventListener("DOMContentLoaded", function(event) { 
   let html = '';
   memoryGame.cards.forEach(pic => {
@@ -39,11 +41,59 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Add all the divs to the HTML
   document.querySelector('#memory_board').innerHTML = html;
 
+  let pairsToCompare = [];
+  let pairsClickedDOMEl = document.getElementById("pairs_clicked");
+  let paisrGuessedDOMEl = document.getElementById("pairs_guessed");
+
   // Bind the click event of each element to a function
   document.querySelectorAll('.back').forEach( card => {
-    card.onclick = function() {
+    card.onclick = function(e) {
       // TODO: write some code here
+
+      const cardBack = e.target;
+      const cardName = cardBack.parentNode.dataset.cardName;
+
+      cardBack.classList.toggle("front")
+      cardBack.nextElementSibling.classList.toggle("front");
+      cardBack.classList.toggle("back")
+      cardBack.nextElementSibling.classList.toggle("back");
+      memoryGame.pickedCards.push(cardName);
+      console.log(memoryGame.pickedCards)
+
+      const firstCard = memoryGame.pickedCards[0];
+      const secondCard = memoryGame.pickedCards[1];
+
+      if (memoryGame.pickedCards.length === 2) {
+        const cardsEqual = memoryGame.checkIfPair(firstCard, secondCard);
+
+        if (!cardsEqual) {
+          setTimeout(() => {
+            console.log('asdadas', cardBack.parentNode.childNodes[0])
+            cardBack.parentNode.childNodes[0].classList.toggle("front")
+            cardBack.parentNode.childNodes[0].nextElementSibling.classList.toggle("front");
+            cardBack.parentNode.childNodes[0].classList.toggle("back")
+            cardBack.parentNode.childNodes[0].nextElementSibling.classList.toggle("back");
+
+            console.log(firstCard)
+            console.log(secondCard)
+
+          }, 1000);
+        }
+        
+        document.getElementById("pairs_guessed").innerText = `${memoryGame.pairsGuessed}`;        
+      }
+
+      document.getElementById("pairs_clicked").innerText = `${memoryGame.pairsClicked}`;
+
+      const finishGame = memoryGame.isFinished()
+      if(finishGame === true){
+        window.alert(`The game is over!`)
+      }
+
+      
+      console.log("pairsGuessed", memoryGame.pairsGuessed)        
       console.log('Card clicked: ', card);
+
     };
   });
 });
--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,43 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards(cards) {
+    let shuffledCars = [], 
+        cardsArrayLength = cards.length, 
+        idx;
+    
+      while (cardsArrayLength) {
+        idx = Math.floor(Math.random() * cardsArrayLength);
+    
+        // If not already shuffled, move it to the new array.
+        if (idx in cards) {
+          shuffledCars.push(cards[idx]);
+          delete cards[idx];
+          cardsArrayLength--;
+        }
+      }
+    
+      return shuffledCars;
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true
+    }
+
+    return false;
+  }
+
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length / 2) return true;
+    return false
+  }
 }
