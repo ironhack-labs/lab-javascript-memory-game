d679d70fe5cc39435169fb178a71c4888201c7e4
Unai Bolivar <ujbolivar@gmail.com>
2019-08-28 09:31:19 +0200

    done

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -2,8 +2,32 @@ class MemoryGame {
   constructor(cards){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
+  
+  shuffleCards(cards) {
+    const shuffledCards = this.cards;
+    let currentIndex = shuffledCards.length, temporaryValue, randomIndex
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = shuffledCards[currentIndex];
+      shuffledCards[currentIndex] = shuffledCards[randomIndex];
+      shuffledCards[randomIndex] = temporaryValue;
+    }
+    return undefined;
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true
+    } else {
+      return false;
+    }
+  }
+  
   isFinished() {}
 }
