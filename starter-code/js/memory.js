fe80b6bbf2fa5814ff5c35379f00851ff0d84ff1
Antonio Llamas <antoniollamasln@gmail.com>
2019-08-28 04:00:26 +0200

    Add Iteration 1.1

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,6 +1,9 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pairsClicked;
+    this.pairsGuessed;
+    this.pickedCards = [];
     // add the rest of the class properties here
   }
   shuffleCards() {}
