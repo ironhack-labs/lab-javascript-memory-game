9ff65b8393b2e52a5a75ee9b2e2a7b5b6ba8db03
Brais Vidal <brais.vidal.rodriguez@gmail.com>
2019-08-28 09:12:04 +0200

    game complete

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -27,6 +27,13 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+let pairsToCompare = [];
+let twoCardsFlipped = false;
+let pairsClickedDOMEl = document.getElementById("pairs_clicked");
+let paisrGuessedDOMEl = document.getElementById("pairs_guessed");
+
+memoryGame.shuffleCards(cards);
+ 
 document.addEventListener("DOMContentLoaded", function(event) { 
   let html = '';
   memoryGame.cards.forEach(pic => {
@@ -36,18 +43,64 @@ document.addEventListener("DOMContentLoaded", function(event) {
     html += `</div>`;
   });
 
+
 // Add all the divs to the HTML
   document.querySelector('#memory_board').innerHTML = html;
 
 // Bind the click event of each element to a function
   document.querySelectorAll('.back').forEach( card => {
-    card.onclick = function(e) {
-// TODO: write some code here
-      console.log('Card clicked: ', card);
+    card.onclick = function() {
+                  
+      const hideCards = cards => {
+        cards.forEach(card => {
+          let firstCardChild = card.firstElementChild;
+                              
+          if (firstCardChild.classList.contains("front")) {
+            firstCardChild.className = "back";
+            firstCardChild.nextElementSibling.classList = "front";
+          }
+        });
+      };                             
 
+    if (!twoCardsFlipped) {
+      pairsToCompare.push(card.parentElement.dataset.cardName);
+                              
+      card.className = "front";
+      card.nextElementSibling.className = "back";
+                              
+      if (pairsToCompare.length === 2) {
+        twoCardsFlipped = !twoCardsFlipped;
+                              
+        if (memoryGame.checkIfPair(pairsToCompare[0], pairsToCompare[1])) {
+          setTimeout(() => {
+            twoCardsFlipped = !twoCardsFlipped;
+            pairsToCompare = [];
+            paisrGuessedDOMEl.textContent = memoryGame.pairsGuessed;
+                              
+            if (memoryGame.isFinished()) {
+              alert("Has ganado!");
+            }
+          }, 1000);
+        } else {
 
+          setTimeout(() => {
+            hideCards([
+            ...document.querySelectorAll(`[data-card-name="${pairsToCompare[0]}"]`)
+            ]);
+            
+            hideCards([
+            ...document.querySelectorAll(`[data-card-name="${pairsToCompare[1]}"]`)
+            ]);
+                              
+            twoCardsFlipped = !twoCardsFlipped;
+            pairsToCompare = [];
+            }, 1000);
+          }
+        }
+                              
+        pairsClickedDOMEl.textContent = `${memoryGame.pairsClicked}`;
+      
+      }
     };
   });
 });
-
-            
