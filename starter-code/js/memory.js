7bdba2582c21556b05ea19733b8f966facae1821
Nestler, Florian <nestlerf@mediamarktsaturn.com>
2019-09-02 11:27:45 +0200

    This is the memory game - Florian

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,31 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards, pickedCards, pairsClicked, pairsGuessed) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [pickedCards];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    this.cards.sort(function () { return 0.5 - Math.random() });
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  isFinished() {
+    if (this.cards.length === this.pairsGuessed * 2) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
+
+
