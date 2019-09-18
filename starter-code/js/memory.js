bccd8ff2f937b410e7d5746179b084f71a72e74a
Jeferson Silva <jeferson.s.silva1@gmail.com>
2019-09-18 19:38:23 -0300

    lab-memory-game

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,27 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.paisGuessed = 0;
+
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if(this.card1 === this.card2){
+      this.paisGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    if (this.paisGuessed === this.pickedCards.length / 2){
+      return `Game Over`;
+    }
+  }
 }
