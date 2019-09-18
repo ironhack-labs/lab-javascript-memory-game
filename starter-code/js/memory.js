1ad6aca9f2a35a5f1cfd3da246364dbf71e549c6
Guilherme Ribeiro <guilherme@martizi.com>
2019-09-17 22:31:05 -0300

    first commit

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,34 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+	for (let i = 0; i<this.cards.length; i += 1) {
+	    let position = Math.floor(Math.random()*this.cards.length)
+	    let temp = this.cards[i]
+	    let current = this.cards[position]
+	    this.cards[i] =  current
+	    this.cards[position] = temp
+	}
+  }
+  checkIfPair(card1, card2) {
+  	if (card1 == card2) {
+  		this.pairsGuessed += 1
+  		return true
+  	} else {
+  		this.pairsClicked += 1
+  		return false
+  	}
+  }
+  isFinished() {
+  	if (this.pairsGuessed >= (this.cards.length/2)) {
+  		return true
+  	} else {
+  		return false
+  	}
+  }
 }
