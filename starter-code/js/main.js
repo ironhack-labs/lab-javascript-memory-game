e22054d939d6a1ec21734698dd05b5cb435157be
EnriqueBarba <enrique.barbadillo26@gmail.com>
2019-09-21 11:34:21 +0200

    All done, refactoring needed

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -26,6 +26,7 @@ const cards = [
 ];
 
 const memoryGame = new MemoryGame(cards);
+memoryGame.shuffleCards();
 
 document.addEventListener("DOMContentLoaded", function(event) { 
   let html = '';
@@ -44,26 +45,73 @@ document.addEventListener("DOMContentLoaded", function(event) {
     card.onclick = function() {
       let parentCard = card.parentNode
       let front = parentCard.querySelector(".front")
-      card.classList.toggle("back")
-      card.classList.toggle("front")   
-      front.classList.toggle("back") 
-      front.classList.toggle("front") 
       
+      flipCard(card, front);  
       console.log('Card clicked: ', card);
       
-      memoryGame.pickedCards.push(card.parentNode)
+      memoryGame.pickedCards.push(card)
+
       if (memoryGame.pickedCards.length === 2 ) {
-        const isPair = memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1])
-        console.log(memoryGame.pickedCards)
-        console.log(isPair)
+        const isPair = memoryGame.checkIfPair(memoryGame.pickedCards[0].getAttribute("name"),memoryGame.pickedCards[1].getAttribute("name"))
+
         if(!isPair){
-          memoryGame.pickedCards[0].classList.toggle("back");
-          memoryGame.pickedCards[1].classList.toggle("back");
+
+          ifNotPair();
+          
         }
+        memoryGame.pickedCards.splice(0,2);
+
+        document.getElementById("pairs_clicked").innerText = memoryGame.pairsClicked;
+        document.getElementById("pairs_guessed").innerText = memoryGame.pairsGuessed;
+
+        if(memoryGame.isFinished()){
+          setTimeout(()=>{
+            const newGame = confirm("Congratulations!! Would you like to play again?")
+            
+            if(newGame){
+              window.location.reload()
+            }
+
+          },500);
+        }
+
       }  
     
     };
   });
+
+  function flipCard(cardBack, cardFront) {
+    cardBack.classList.toggle("back")
+    cardBack.classList.toggle("front")   
+    cardFront.classList.toggle("back") 
+    cardFront.classList.toggle("front") 
+  }
+
+  function delayedFlipCard(cardBack, cardFront) {
+    setTimeout(function(){
+        cardBack.classList.toggle("back")
+        cardBack.classList.toggle("front")   
+        cardFront.classList.toggle("back") 
+        cardFront.classList.toggle("front") 
+    },1500)
+  }
+
+  function ifNotPair(){
+    const card1Father = memoryGame.pickedCards[0].parentNode;
+    const card2Father = memoryGame.pickedCards[1].parentNode;
+    const frontCard1 = card1Father.querySelector(".front");
+    const backCard1 = card1Father.querySelector(".back");
+    const frontCard2 = card2Father.querySelector(".front");
+    const backCard2 = card2Father.querySelector(".back");
+    
+    //setTimeout((backcard1,frontCard1)=>flipCard(backcard1,frontCard1),2000);
+    //setTimeout((backcard2,frontCard2)=>flipCard(backcard2,frontCard2),2000);
+    delayedFlipCard(backCard1, frontCard1);
+    delayedFlipCard(backCard2, frontCard2);
+  }
+
+
+
 });
 
 
