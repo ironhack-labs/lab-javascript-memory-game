d250be71725c10b477bef594660ecf4a4d615961
root <root@LAPTOP-TCKDMSCV.localdomain>
2019-09-21 11:28:21 +0200

    done

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,33 +1,38 @@
 const cards = [
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
+  { name: 'aquaman', img: 'aquaman.jpg' },
+  { name: 'batman', img: 'batman.jpg' },
   { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' },
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
+  { name: 'fantastic four', img: 'fantastic-four.jpg' },
+  { name: 'flash', img: 'flash.jpg' },
+  { name: 'green arrow', img: 'green-arrow.jpg' },
+  { name: 'green lantern', img: 'green-lantern.jpg' },
+  { name: 'ironman', img: 'ironman.jpg' },
+  { name: 'spiderman', img: 'spiderman.jpg' },
+  { name: 'superman', img: 'superman.jpg' },
+  { name: 'the avengers', img: 'the-avengers.jpg' },
+  { name: 'thor', img: 'thor.jpg' },
+  { name: 'aquaman', img: 'aquaman.jpg' },
+  { name: 'batman', img: 'batman.jpg' },
   { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' }
+  { name: 'fantastic four', img: 'fantastic-four.jpg' },
+  { name: 'flash', img: 'flash.jpg' },
+  { name: 'green arrow', img: 'green-arrow.jpg' },
+  { name: 'green lantern', img: 'green-lantern.jpg' },
+  { name: 'ironman', img: 'ironman.jpg' },
+  { name: 'spiderman', img: 'spiderman.jpg' },
+  { name: 'superman', img: 'superman.jpg' },
+  { name: 'the avengers', img: 'the-avengers.jpg' },
+  { name: 'thor', img: 'thor.jpg' }
 ];
 
 const memoryGame = new MemoryGame(cards);
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+/*Barajar las cartas*/
+memoryGame.shuffleCards();
+let score = document.getElementById("pairs_guessed") ;
+let clicks =  document.getElementById("pairs_clicked");
+
+document.addEventListener("DOMContentLoaded", function (event) {
   let html = '';
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
@@ -40,12 +45,82 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.querySelector('#memory_board').innerHTML = html;
 
   // Bind the click event of each element to a function
-  document.querySelectorAll('.back').forEach( card => {
-    card.onclick = function() {
+  document.querySelectorAll('.back').forEach(card => {
+    card.onclick = function () {
       // TODO: write some code here
+
       console.log('Card clicked: ', card);
+      /*Dar la vuelta a las cartas*/
+
+      flipCard(card);
+      pickCard(card);  
+      clicks.innerText = memoryGame.pairsClicked;
+      score.innerText = memoryGame.pairsGuessed;
+      
     };
   });
+
+  function flipCard(card) {
+
+    const cardParent = card.parentNode;
+    const frontCard = cardParent.querySelector(".front")
+    frontCard.removeAttribute("class", "front")
+    frontCard.setAttribute("class", "back")
+    card.removeAttribute("class", "back")
+    card.setAttribute("class", "front")
+  }
+
+  /*Esconde la cartas */
+  function hideCard(card) {
+    
+    const cardParent = card.parentNode;
+    const frontCard = cardParent.querySelector(".back")
+    frontCard.removeAttribute("class", "back")
+    frontCard.setAttribute("class", "front")
+    card.removeAttribute("class", "front")
+    card.setAttribute("class", "back")
+
+  }
+
+  function pickCard(card) {
+
+    memoryGame.pickedCards.push(card)
+
+    if (memoryGame.pickedCards.length === 2) {
+      if (memoryGame.checkIfPair(memoryGame.pickedCards[0].getAttribute("name"), memoryGame.pickedCards[1].getAttribute("name"))) {
+      
+      memoryGame.pickedCards.pop();
+      memoryGame.pickedCards.pop();
+      
+      if(memoryGame.isFinished()){
+        setTimeout(()=>{
+          const newGame = confirm("Quieres jugar de nuevo?")
+          
+          if(newGame){
+            window.location.reload()
+          }
+        },500);
+      }
+
+      } else {
+
+        setTimeout(() => {
+          
+          hideCard(memoryGame.pickedCards[0]);
+          hideCard(memoryGame.pickedCards[1]);
+          memoryGame.pickedCards.pop();
+          memoryGame.pickedCards.pop();       
+        }, 400);
+
+      }
+    }
+  }
+
+  function reset(){
+    
+    window.location.reload()
+  }
+
 });
 
 
--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,35 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+  
+    //cards.sort(() => Math.random() -0.5)  
+
+    for (let i = cards.length-1; i>=0; i--) {
+      let j = Math.floor(Math.random() * i);
+
+      [cards[i], cards[j]] = [cards[j], cards[i]]
+    }
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++
+    if (card1 === card2){
+      this.pairsGuessed++
+      return true
+    } else {
+      return false
+    }
+  }
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length/2) {
+      return true
+    } else {
+      return false
+    }
+  }
 }
