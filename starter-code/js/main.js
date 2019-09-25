d9c666441536240a2235f50677f41efbfe3b46b0
Ángel Luis Hernández Herrero <angelluishernandezherrero@gmail.com>
2019-09-25 17:15:56 +0200

    done

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -25,7 +25,10 @@ const cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
+//Cards are being suffled and positioned randomly
+
 const memoryGame = new MemoryGame(cards);
+memoryGame.shuffleCards()
 
 document.addEventListener("DOMContentLoaded", function(event) { 
   let html = '';
@@ -42,10 +45,91 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Bind the click event of each element to a function
   document.querySelectorAll('.back').forEach( card => {
     card.onclick = function() {
-      // TODO: write some code here
+      checkingPairs(card) 
+      flip(card)
+      
+      clickScore.innerText = memoryGame.pairsClicked
+      guessedScore.innerText = memoryGame.pairsGuessed
+
+
+
+
       console.log('Card clicked: ', card);
     };
   });
 });
 
+// The following function will flip the following card changing its native class
+const clickScore = document.querySelector("#pairs_clicked")
+const guessedScore = document.querySelector("#pairs_guessed")
+function flip(card){
+  
+  const parentCard = card.parentElement
+  
+  let hiddenCard = parentCard.querySelector(".front")
+  hiddenCard.removeAttribute("class")
+  hiddenCard.setAttribute("class", "back")
+  card.removeAttribute("class")
+  card.setAttribute("class", "front")
+  
+  
+  
 
+}
+
+
+// The following function will flip the card once again to hide it
+
+function hide(card){
+
+  const parentCard = card.parentElement
+  let shownCard = parentCard.querySelector(".back")
+  shownCard.removeAttribute("class")
+  shownCard.setAttribute("class", "front")
+  card.removeAttribute("class")
+  card.setAttribute("class", "back")
+}
+
+
+
+// Checking if cards are equal and execute a hide function if they are not. 
+
+
+
+
+function checkingPairs(card){
+
+  memoryGame.pickedCards.push(card)
+
+  if (memoryGame.pickedCards.length === 2){
+    if (memoryGame.checkIfPair(memoryGame.pickedCards[0].getAttribute("name"), memoryGame.pickedCards[1].getAttribute("name"))){
+      
+        memoryGame.pickedCards.splice(0, 2)
+            
+      }
+      
+    }
+    if (memoryGame.isFinished()){
+      
+      setTimeout(() => {
+        window.location.reload()
+      }, 300)
+    }
+
+    
+    else {
+
+      setTimeout(() => {
+
+        hide(memoryGame.pickedCards[0])
+        hide(memoryGame.pickedCards[1])
+        memoryGame.pickedCards.splice(0, 2)
+      },800)
+      
+      
+      
+    }
+    
+    
+    
+  }
