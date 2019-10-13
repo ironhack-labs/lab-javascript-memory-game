fc1ac246fccd3c9eece9f24ffbeeab193f066b1b
Gonçalo Monteiro de Carvalho da Costa Santos <goncalocostasantos@MBP-de-Goncalo.home>
2019-10-13 18:49:27 +0100

    added the 1st iterations - missing a few things

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,33 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards,pickedCards,pairsClicked,pairsGuessed){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = Number([]);
+    this.pairsGuessed = Number([]);
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+shuffleCards() {
+
+}
+
+checkIfPair(card1, card2) {
+
+  this.pairsClicked += 1;
+
+  if (card1 === card2) {
+    this.pairsGuessed +=1;
+    return true;
+  } else {
+    return false;
+  }
+  
+}
+
+
+
+
+
+isFinished() {} 
+
 }
