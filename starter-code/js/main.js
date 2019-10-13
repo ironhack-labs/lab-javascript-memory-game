5776aa1c85638aef0c639ee1b2edae428ffbbaf5
Gonçalo Monteiro de Carvalho da Costa Santos <goncalocostasantos@MBP-de-Goncalo.home>
2019-10-13 22:37:09 +0100

    added changes

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -31,18 +31,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let html = '';
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
-    html += `<div class="back" name="${pic.img}"></div>`;
-    html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
+    html += `<div class="front" name="${pic.img}"></div>`;
+    html += `<div class="back" style="background: url(img/${pic.img}) no-repeat"></div>`;
     html += `</div>`;
   });
 
   // Add all the divs to the HTML
   document.querySelector('#memory_board').innerHTML = html;
+  
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.back').forEach( card => {
     card.onclick = function() {
       // TODO: write some code here
+
+
       console.log('Card clicked: ', card);
     };
   });
