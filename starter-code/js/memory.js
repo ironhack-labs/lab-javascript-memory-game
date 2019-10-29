1453826ddecee13cb43b42f964450ca8f059b1e0
Daniel Gonzalez <daniel.gonvie@gmail.com>
2019-10-29 22:30:05 +0100

    Done w/ methods

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -2,8 +2,38 @@ class MemoryGame {
   constructor(cards){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    let currentIndex = this.cards.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = this.cards[currentIndex];
+      this.cards[currentIndex] = this.cards[randomIndex];
+      this.cards[randomIndex] = temporaryValue;
+    }
+  
+}
+
+  
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2 ){
+      this.pairsGuessed++;
+      return true;
+    } else {this.pairGuessed += 0;
+      return false;
+  }
+
+  }
+  isFinished() {
+    return this.pairsGuessed *2 == this.cards.length;
+  }
 }
