06108b3531db8a883243118b3140a2ccc7a15809
Laura <laura.vivas.gamboa@gmail.com>
2019-10-29 20:46:26 +0100

    Jasmine is ready

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,13 +1,36 @@
 
 
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    this.pickedCards = pickedCards;
-    this.pairsClicked = pairsClicked;
-    this.pairsGuessed = pairsGuessed;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() { 
+    for(let i = this.cards.length - 1; i > 0; i--){
+      const j = Math.floor(Math.random() * i);
+      const temp = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = temp;
+    } 
+  }
+
+  checkIfPair(card1, card2) {
+    if (card1 === card2) {
+      this.pairsGuessed += 1
+      return true;
+    } else {
+      this.pairsClicked += 1
+      return false;
+    }
+  }
+  isFinished() {
+    if ((this.pairsGuessed * 2) === (this.cards.length)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
