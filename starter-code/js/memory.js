3d1eeed2da4b117ec3e439a02f2ce97639ac5a2d
Jessica <54844929+jessica-git@users.noreply.github.com>
2019-10-29 21:19:30 +0100

    Jasmin: JS

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,25 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+    
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++
+    if (card1 === card2){
+      this.pairsGuessed++
+      return true
+    }else{
+      return false
+    }
+  }
+  isFinished() {
+    return this.pairsGuessed >= this.cards.length / 2;
+  }
 }
