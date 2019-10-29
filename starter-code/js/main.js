c6c8d80b9c770e64b92009e9fef16df100e9bb77
pulpocabezon bonnet molina <pulpocabezon@MacBook-Pro-de-pulpocabezon.local>
2019-10-29 22:59:40 +0100

    memorygame

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,34 +1,34 @@
 const cards = [
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' },
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' }
+  { name: "aquaman", img: "aquaman.jpg" },
+  { name: "batman", img: "batman.jpg" },
+  { name: "captain america", img: "captain-america.jpg" },
+  { name: "fantastic four", img: "fantastic-four.jpg" },
+  { name: "flash", img: "flash.jpg" },
+  { name: "green arrow", img: "green-arrow.jpg" },
+  { name: "green lantern", img: "green-lantern.jpg" },
+  { name: "ironman", img: "ironman.jpg" },
+  { name: "spiderman", img: "spiderman.jpg" },
+  { name: "superman", img: "superman.jpg" },
+  { name: "the avengers", img: "the-avengers.jpg" },
+  { name: "thor", img: "thor.jpg" },
+  { name: "aquaman", img: "aquaman.jpg" },
+  { name: "batman", img: "batman.jpg" },
+  { name: "captain america", img: "captain-america.jpg" },
+  { name: "fantastic four", img: "fantastic-four.jpg" },
+  { name: "flash", img: "flash.jpg" },
+  { name: "green arrow", img: "green-arrow.jpg" },
+  { name: "green lantern", img: "green-lantern.jpg" },
+  { name: "ironman", img: "ironman.jpg" },
+  { name: "spiderman", img: "spiderman.jpg" },
+  { name: "superman", img: "superman.jpg" },
+  { name: "the avengers", img: "the-avengers.jpg" },
+  { name: "thor", img: "thor.jpg" }
 ];
 
 const memoryGame = new MemoryGame(cards);
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-  let html = '';
+document.addEventListener("DOMContentLoaded", function(event) {
+  let html = "";
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
     html += `<div class="back" name="${pic.img}"></div>`;
@@ -37,15 +37,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
   // Add all the divs to the HTML
-  document.querySelector('#memory_board').innerHTML = html;
+  document.querySelector("#memory_board").innerHTML = html;
 
   // Bind the click event of each element to a function
-  document.querySelectorAll('.card').forEach( card => {
+  document.querySelectorAll(".card").forEach(card => {
     card.onclick = function() {
-      // TODO: write some code here
-      console.log('Card clicked: ', card);
+      card.classList.add("turned"); //esto es una intentona de un condicional para ver si me mueve las cartas....osea el fallo mas grande de la historia, me encantaria que me lo explicases :) // TODO: write some code here
+      /* if (card.name[0] === card.name[1]) {
+        return turned();
+      }
+      return !turned();*/ console.log(
+        "Card clicked: ",
+        card
+      );
     };
   });
 });
-
-
--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,30 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards, pickedCards, pairsGuessed) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    this.cards.sort(() => Math.random() - 0.5); //lo he sacado de internet pero me gustaría que me lo explicaras, sobretodo la parte del "-0.5"
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if (card1 == card2) {
+      this.pairsGuessed += 1;
+      return true;
+    }
+    return false;
+  }
+  isFinished() {
+    let restOfcards = this.cards.length;
+    if (this.pairsGuessed === restOfcards / 2) {
+      return true;
+    }
+    return false;
+  }
 }
+//function randomSelector(array) {
+// let random = array[Math.floor(Math.random() * array.length)];
+//return random;
+//}£
