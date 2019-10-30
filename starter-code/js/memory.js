c03bce7d3b38c9e91ba9e75fcc79a29f34444485
USUARIO <usuario@Macs-MacBook-Pro.local>
2019-10-30 09:23:53 +0100

    Completada la lógica, no he tocada la parte DOM

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,36 @@
 class MemoryGame {
-  constructor(cards){
-    this.cards = cards;
+  constructor(cards) {
+    this.cards = cards
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+    console.log(this.cards)
+
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    this.cards.sort(() => Math.random() - 0.5);
+
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 == card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+
+    }
+
+  }
+
+  isFinished() {
+
+    if (this.cards.length === this.pairsGuessed * 2) {
+      return true
+    } else {
+      return false
+    }
+
+  }
 }
