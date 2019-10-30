9b1da4ea67a9b80bcab86538301313f6e9de6978
Borja Valdés Jiménez <borja@MacBook-Air-de-Borja.local>
2019-10-30 09:01:10 +0100

    Auténtico desastre para que engañarnos

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,26 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+
+    this.cards.sort(() => Math.random() - 0, 5);
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 == card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    return this.pairsGuessed * 2 === this.cards.length;
+  }
+
 }
