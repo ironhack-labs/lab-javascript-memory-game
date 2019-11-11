32a514dc079a5e8bc4ad02e4c65d5403c3c27cfe
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2019-11-10 18:28:17 -0600

    Iteration 3 | shuffleCards

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -6,7 +6,13 @@ class MemoryGame {
     this.pairsClicked = 0;
     this.pairsGuessed = 0;
   }
-  shuffleCards() {}
+  shuffleCards() {
+    let numberOfCards = this.cards.length;
+    for (let i = 0; i < numberOfCards; i++){
+      let newIndex = Math.floor(Math.random() * numberOfCards);
+      [this.cards[i], this.cards[newIndex]] = [this.cards[newIndex], this.cards[i]];
+    }
+  }
   checkIfPair(card1, card2) {}
   isFinished() {}
 }
