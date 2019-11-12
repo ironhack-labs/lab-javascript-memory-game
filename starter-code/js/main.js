eef8d1d4ab766249015a1f28428f0c75c69cbe2c
Analin Flores <foglzerika@gmail.com>
2019-11-12 05:32:49 -0600

    Update functions

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -2,7 +2,7 @@ const cards = [
   { name: 'aquaman',         img: 'aquaman.jpg' },
   { name: 'batman',          img: 'batman.jpg' },
   { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
+ /* { name: 'fantastic four',  img: 'fantastic-four.jpg' },
   { name: 'flash',           img: 'flash.jpg' },
   { name: 'green arrow',     img: 'green-arrow.jpg' },
   { name: 'green lantern',   img: 'green-lantern.jpg' },
@@ -10,11 +10,11 @@ const cards = [
   { name: 'spiderman',       img: 'spiderman.jpg' },
   { name: 'superman',        img: 'superman.jpg' },
   { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' },
+  { name: 'thor',            img: 'thor.jpg' },*/
   { name: 'aquaman',         img: 'aquaman.jpg' },
   { name: 'batman',          img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
+  { name: 'captain america', img: 'captain-america.jpg' }
+ /* { name: 'fantastic four',  img: 'fantastic-four.jpg' },
   { name: 'flash',           img: 'flash.jpg' },
   { name: 'green arrow',     img: 'green-arrow.jpg' },
   { name: 'green lantern',   img: 'green-lantern.jpg' },
@@ -22,10 +22,11 @@ const cards = [
   { name: 'spiderman',       img: 'spiderman.jpg' },
   { name: 'superman',        img: 'superman.jpg' },
   { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' }
+  { name: 'thor',            img: 'thor.jpg' }*/
 ];
 
 const memoryGame = new MemoryGame(cards);
+memoryGame.shuffleCards();
 
 document.addEventListener("DOMContentLoaded", function(event) { 
   let html = '';
@@ -43,7 +44,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.querySelectorAll('.card').forEach( card => {
     card.onclick = function() {
       // TODO: write some code here
-      console.log('Card clicked: ', card);
+      //Flip a card   
+      card.classList.add("turned");
+
+      let arrayCards= memoryGame.pickedCards.length;
+
+      memoryGame.pickedCards.push(card);
+
+      if(arrayCards === 1){
+       let areSame= memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1]);
+      
+        if(areSame){
+          memoryGame.addPaisGuessed();
+        }else{
+          memoryGame.turnCard(areSame,memoryGame.pickedCards[0],memoryGame.pickedCards[1]);
+        }
+        
+        memoryGame.pickedCards=[];
+  
+      }
+      memoryGame.addPaisClicked();
+      memoryGame.isFinished();
     };
   });
 });
--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -2,8 +2,63 @@ class MemoryGame {
   constructor(cards){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+  let arr = this.cards;
+  let i = this.cards.length, j, temp;
+  while(--i > 0){
+    j= Math.floor(Math.random() * (i+1));
+    temp= arr[j];
+    arr[j]= arr[i];
+    arr[i]= temp;
+  }
+  return arr;
+
+  }
+  checkIfPair(card1, card2) {
+  
+    if(card1.attributes[1].value === card2.attributes[1].value){
+      this.pairsGuessed++;
+      return true;
+     
+    }else{
+      this.pairsClicked++;
+      return false;
+    }
+    
+  }
+  turnCard(areSame,card1,card2){
+    if(!areSame){
+      setTimeout(function(){
+        card1.classList.toggle("turned");
+        card2.classList.toggle("turned");
+       
+      }, 1000);
+    }
+  }
+  isFinished() {
+   
+    if(this.cards.length/2 ==this.pairsGuessed ){
+
+      setTimeout(function(){
+        alert("You win!!");
+       
+      }, 500);
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  addPaisClicked(){
+    let spanPairClicled= document.getElementById("pairs_clicked");
+    spanPairClicled.innerHTML= this.pairsClicked;   
+  }
+  addPaisGuessed(){
+    let spanPairGuessed= document.getElementById("pairs_guessed");
+    spanPairGuessed.innerHTML= this.pairsGuessed;
+  }
 }
