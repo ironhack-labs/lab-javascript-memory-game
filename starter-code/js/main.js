7724982363f539ffa58de87c8d38ee9eb35f597b
Analin Flores <foglzerika@gmail.com>
2019-11-12 05:55:08 -0600

    Quit comments in arrays of cards

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -2,7 +2,7 @@ const cards = [
   { name: 'aquaman',         img: 'aquaman.jpg' },
   { name: 'batman',          img: 'batman.jpg' },
   { name: 'captain america', img: 'captain-america.jpg' },
- /* { name: 'fantastic four',  img: 'fantastic-four.jpg' },
+  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
   { name: 'flash',           img: 'flash.jpg' },
   { name: 'green arrow',     img: 'green-arrow.jpg' },
   { name: 'green lantern',   img: 'green-lantern.jpg' },
@@ -10,11 +10,11 @@ const cards = [
   { name: 'spiderman',       img: 'spiderman.jpg' },
   { name: 'superman',        img: 'superman.jpg' },
   { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' },*/
+  { name: 'thor',            img: 'thor.jpg' },
   { name: 'aquaman',         img: 'aquaman.jpg' },
   { name: 'batman',          img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' }
- /* { name: 'fantastic four',  img: 'fantastic-four.jpg' },
+  { name: 'captain america', img: 'captain-america.jpg' },
+  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
   { name: 'flash',           img: 'flash.jpg' },
   { name: 'green arrow',     img: 'green-arrow.jpg' },
   { name: 'green lantern',   img: 'green-lantern.jpg' },
@@ -22,7 +22,7 @@ const cards = [
   { name: 'spiderman',       img: 'spiderman.jpg' },
   { name: 'superman',        img: 'superman.jpg' },
   { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' }*/
+  { name: 'thor',            img: 'thor.jpg' }
 ];
 
 const memoryGame = new MemoryGame(cards);
