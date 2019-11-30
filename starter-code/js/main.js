ad754dd7ac6a39c7910ab72231d2101210f511b1
J. A. Hernández <joseanher81@gmail.com>
2019-11-30 20:24:07 +0100

    Completed

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -26,6 +26,7 @@ const cards = [
 ];
 
 const memoryGame = new MemoryGame(cards);
+memoryGame.shuffleCards(); //Shuffle cards at the begining of the game
 
 window.addEventListener("load", event => {
   let html = "";
@@ -43,7 +44,54 @@ window.addEventListener("load", event => {
   document.querySelectorAll(".card").forEach(card => {
     card.addEventListener("click", () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      console.log(`Card clicked: ${JSON.stringify(card)}`);
+
+      card.classList.add('turned'); // Add turned class to card div
+
+      memoryGame.pickedCards.push(card); // Add to pickedCards array
+
+      if(memoryGame.pickedCards.length == 2) {  // Check if there are two cards selected
+        
+        if(memoryGame.checkIfPair(memoryGame.pickedCards[0].getAttribute("data-card-name"), memoryGame.pickedCards[1].getAttribute("data-card-name"))) {  // The cards are the same
+          // Add blocked class to both cards and fix them
+          memoryGame.pickedCards[0].classList.add('blocked'); 
+          memoryGame.pickedCards[1].classList.add('blocked');
+          memoryGame.pickedCards = []; // Reset picked array
+
+        } else {
+          // Wait 1 sec and flip cards to their original state removing class 'turned'
+          setTimeout(function() { 
+            removeClass([memoryGame.pickedCards[0], memoryGame.pickedCards[1]], 'turned'); 
+            memoryGame.pickedCards = []; // Reset picked array
+          },1000);
+        }
+
+        // Update scores and check whether the game has finished
+        updateScores();
+        checkEndGame();
+      }
+
     });
   });
 });
+
+function removeClass(cards, clase){
+  cards.forEach(function(card) {
+    card.classList.remove(clase);
+  });
+}
+
+function updateScores(){
+  let pClicked = document.getElementById('pairs_clicked');
+  let pGuessed = document.getElementById('pairs_guessed');
+  pClicked.innerHTML = memoryGame.pairsClicked;
+  pGuessed.innerHTML = memoryGame.pairsGuessed;
+}
+
+function checkEndGame(){
+  if(memoryGame.isFinished()) {
+    let scoreText = document.getElementsByTagName('h2');
+    scoreText[0].style.color = '#FFFF00';
+    scoreText[0].innerHTML = 'You won!!!';
+  }
+}
