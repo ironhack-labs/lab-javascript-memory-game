b512faf1793fefe55227a2592ca151e7f073175b
Román Méndez <mendezbeck@gmail.com>
2019-11-30 14:07:51 +0100

    shuffleCards() done

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,23 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
+  shuffleCards() {
+    if(this.cards){
+      for (let i = (this.cards.length - 1); i > 0; i -= 1) {
+      let randomIndex = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[randomIndex]] = [this.cards[randomIndex], this.cards[i]];
+      console.log(this.cards[i], this.cards[randomIndex])
+      console.log(this.cards[randomIndex], this.cards[i])
+      }
+      return this.cards;
+    } else {
+      return undefined
+    }
+  }
   checkIfPair(card1, card2) {}
   isFinished() {}
 }
