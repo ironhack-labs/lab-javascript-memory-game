433cb641b14e53e62f091e52ba121726dd866091
Miguel <alonsogalalrdomiguel@gmail.com>
2019-11-30 17:32:58 +0100

    actualizar puntuacion

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -52,16 +52,21 @@ window.addEventListener("load", event => {
 
     
       if(memoryGame.checkIfPair(memoryGame.pickedCards[0].getAttribute("data-card-name") ,memoryGame.pickedCards[1].getAttribute("data-card-name"))){
-
       console.log("es true");
       memoryGame.pickedCards[0].classList.add("blocked");
       memoryGame.pickedCards[1].classList.add("blocked");
       memoryGame.pickedCards = [];
+      //let clicked = document.getElementById("pairs_clicked")
+      //clicked.innerHTML =memoryGame.pairsClicked;
+      document.getElementById("pairs_clicked").innerHTML = memoryGame.pairsClicked;
+      document.getElementById("pairs_guessed").innerHTML = memoryGame.pairsGuessed;      
       } else {      
         console.log("es false")
         memoryGame.pickedCards[0].classList.remove("turned");
         memoryGame.pickedCards[1].classList.remove("turned");
         memoryGame.pickedCards = [];
+        document.getElementById("pairs_clicked").innerHTML = memoryGame.pairsClicked;        
+        document.getElementById("pairs_guessed").innerHTML = memoryGame.pairsGuessed;
       }
     },1000)
     }    
