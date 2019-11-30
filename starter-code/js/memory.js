99bfc5ad1093aadfd4aaf1b1ce325af13871a690
Ana Segarra <anasegarraber@gmail.com>
2019-11-30 14:40:30 +0100

    Finish memoryGame class

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,33 @@
 class MemoryGame {
-  constructor(cards){
-    this.cards = cards;
-    // add the rest of the class properties here
-  }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+	constructor(cards) {
+		this.cards = cards;
+		this.pickedCards = [];
+		this.pairsClicked = 0;
+		this.pairsGuessed = 0;
+	}
+
+	shuffleCards() {
+		if (!this.cards) return undefined;
+
+		for (let i = 0; i < this.cards.length; i++) {
+			let j = Math.floor(Math.random() * (i + 1));
+			let card = this.cards[i];
+			this.cards[i] = this.cards[j];
+			this.cards[j] = card;
+		}
+		return this.cards;
+	}
+
+	checkIfPair(card1, card2) {
+		if (card1 === card2) {
+			this.pairsGuessed++;
+			return true;
+		} else {
+			this.pairsClicked++;
+			return false;
+		}
+	}
+	isFinished() {
+		return this.pairsGuessed === this.cards.length / 2;
+	}
 }
