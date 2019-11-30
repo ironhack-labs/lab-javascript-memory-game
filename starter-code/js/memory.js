e54cbc93378012738fce19fdbe150f6799d560dc
mario010203 <mariojose1490@hotmail.com>
2019-11-30 14:18:49 +0100

    Comment bad test, link stylesheet and scripts, finish the logic of the game

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,37 @@
 class MemoryGame {
-  constructor(cards){
-    this.cards = cards;
-    // add the rest of the class properties here
+  constructor(cards) {
+    (this.cards = cards),
+      (this.pickedCards = []),
+      (this.pairsClicked = 0),
+      (this.pairsGuessed = 0);
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    let cardsNumbers = this.cards.length;
+    let i;
+    let e;
+    while (cardsNumbers) {
+      i = Math.floor(Math.random() * cardsNumbers--);
+
+      e = this.cards[cardsNumbers];
+      this.cards[cardsNumbers] = this.cards[i];
+      this.cards[i] = e;
+    }
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
+  }
+  isFinished() {
+    let pairs = this.cards.length / 2;
+    if (this.pairsGuessed == pairs) {
+      return true;
+    }
+
+    return false;
+  }
 }
