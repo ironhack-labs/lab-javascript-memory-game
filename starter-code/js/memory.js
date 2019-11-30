72bf785a9eab9eefba527161751c2f33e6b8b6b8
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2019-11-30 15:13:21 +0100

    iteration 2.2

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,12 +1,29 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    this.pickedCards;
-    this.pairsClicked;
-    this.pairGuessed;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards(cards) {
+    //if (cards) {
+    console.log(cards);
+    return cards;
+    //}
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
+  }
+  isFinished() {
+    if (this.cards.length / 2 === this.pairsGuessed) {
+      return true;
+    }
+    return false;
+  }
 }
