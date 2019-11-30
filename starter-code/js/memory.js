4c52e842e4569e460e642a8970c5b757e565383b
Juan Espegel <juanespegel@gmail.com>
2019-11-30 14:29:48 +0100

    iteracion2 a medias

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,22 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
+  shuffleCards() {
+    let mixedCards = [];
+    function shuffle(array){
+      array.sort(() => Math.random() - 0.5)
+    }
+    if (this.cards.length <= 0){
+      //mixedCards = shuffle(this.cards);
+      //console.log(mixedCards);
+      return shuffle(this.cards);
+    }
+  }
+  
   checkIfPair(card1, card2) {}
   isFinished() {}
 }
