12ba6845f70bc9c15c3a0893e2653a0820dd9fbe
Javi L <jlopezro@gmail.com>
2019-12-01 20:21:51 +0100

    Iteration 2 - class methods nearly done

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,14 +1,13 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
     this.pickedCards = [];
     this.pairsClicked = 0;
     this.pairsGuessed = 0;
   }
   shuffleCards() {
-
     for (let i = 0; i < this.cards.length; i++) {
-    this.randomN = this.cards[Math.floor(Math.random() * this.cards.length)];
+      this.randomN = this.cards[Math.floor(Math.random() * this.cards.length)];
       for (let j = 0; j < this.cards.length; j++) {
         if (this.cards[i] < this.cards[j]) {
           this.cardIdx = this.cards.indexOf(this.randomN);
@@ -21,7 +20,23 @@ class MemoryGame {
     return this.cards;
   }
 
+  checkIfPair(card1, card2) {
+    if (card1 === card2) {
+      this.pairsClicked ++;
+      this.pairsGuessed ++;
+      return true;
+    } else {
+      this.pairsClicked ++;
+      return false;
+    }
+  }
 
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  isFinished() {
+    if (this.pairsGuessed === 8) {
+    // console.log("done");
+    return true;
+    } else {
+    return false;
+    }
+  }
 }
