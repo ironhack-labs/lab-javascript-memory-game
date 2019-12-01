cf249b6fa39f597d6cbaf3097c053009af836740
Gaby Zapata <thegabyzapata@gmail.com>
2019-12-01 21:22:07 +0100

    All JS tests done

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -6,24 +6,40 @@ class MemoryGame {
     this.pairsGuessed = 0;
 
   }
-  
-  shuffleCards() {}
+
+  shuffleCards() {
+    // Fisher-Yates Shuffle algorithm
+    let currentIndex = this.cards.length,
+      temporaryValue,
+      randomIndex;
+    // While there remain elements to shuffle
+    while (currentIndex) {
+      // Pick a remaing element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element
+      temporaryValue = this.cards[currentIndex];
+      this.cards[currentIndex] = this.cards[randomIndex];
+      this.cards[randomIndex] = temporaryValue;
+    }
+  }
 
 
   checkIfPair(card1, card2) {
     if (card1 === card2) {
       this.pairsGuessed++;
       return true;
-    } else{
+    } else {
       this.pairsClicked++;
       return false;
     }
   }
 
 
-  isFinished() { 
-    if (this.pairsGuessed === this.cards.length/2){
-    return true;
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
     } else {
       return false;
     }
