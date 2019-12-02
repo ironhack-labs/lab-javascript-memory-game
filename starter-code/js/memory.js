bb1d3ff1ddcec07f6adfe5165298417a4efd2bea
Victor Chamizo Rodriguez <vchamizo@sia.es>
2019-12-02 08:57:32 +0100

    Memoey class done

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,33 +1,36 @@
 class MemoryGame {
-	constructor(cards) {
-		this.cards = cards;
-		this.pickedCards = [];
-		this.pairsClicked = 0;
-		this.pairsGuessed = 0;
-	}
+  constructor(cards) {
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+  }
 
-	shuffleCards() {
-		if (!this.cards) return undefined;
+  shuffleCards() {
+    if (this.cards) {
+      for (let i = 0; i < this.cards.length; ++i) {
+        let j = Math.floor(Math.random() * i);
+        let card = this.cards[i];
+        this.cards[i] = this.cards[j];
+        this.cards[j] = card;
+      }
+      return this.cards;
+    }
 
-		for (let i = 0; i < this.cards.length; i++) {
-			let j = Math.floor(Math.random() * (i + 1));
-			let card = this.cards[i];
-			this.cards[i] = this.cards[j];
-			this.cards[j] = card;
-		}
-		return this.cards;
-	}
+    return undefined;
+  }
 
-	checkIfPair(card1, card2) {
-		if (card1 === card2) {
-			this.pairsGuessed++;
-			return true;
-		} else {
-			this.pairsClicked++;
-			return false;
-		}
-	}
-	isFinished() {
-		return this.pairsGuessed === this.cards.length / 2;
-	}
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+
+    return false;
+  }
+  isFinished() {
+    return this.pairsGuessed === this.cards.length / 2;
+  }
 }
