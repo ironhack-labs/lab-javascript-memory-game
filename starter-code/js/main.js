58a9ebc104974951bdad828def9f4dea07697004
Ivan Esteban <ivan.estebanisabel@gmail.com>
2019-12-03 20:40:34 +0100

    iteration 3 - end

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -27,8 +27,13 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+let tempCards = [];
+let clicks = 0;
+let test = "";
+
 window.addEventListener("load", event => {
   let html = "";
+  memoryGame.shuffleCards();
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
     html += `<div class="back" name="${pic.img}"></div>`;
@@ -43,7 +48,49 @@ window.addEventListener("load", event => {
   document.querySelectorAll(".card").forEach(card => {
     card.addEventListener("click", () => {
       // TODO: write some code here
+      document.getElementById("pairs_clicked").innerHTML =
+        memoryGame.pairsClicked;
+
+      if (clicks < 2) {
+        tempCards.push(card);
+        card.className += " turned";
+        clicks++;
+        if (clicks == 2) {
+          card1 = tempCards[0].childNodes[1].outerHTML;
+          card2 = tempCards[1].childNodes[1].outerHTML;
+
+          if (memoryGame.checkIfPair(card1, card2)) {
+            test = "true";
+            document.getElementById("pairs_guessed").innerHTML =
+              memoryGame.pairsGuessed;
+            document.getElementById("pairs_clicked").innerHTML =
+              memoryGame.pairsClicked;
+            if (memoryGame.isFinished() == true) {
+              victory();
+            }
+          }
+          I;
+        }
+      } else if (clicks == 2 && test == "true") {
+        test = "";
+        tempCards = [];
+        clicks = 0;
+        tempCards.push(card);
+        card.className += " turned";
+        clicks++;
+      } else {
+        tempCards[0].className = "card";
+        tempCards[1].className = "card";
+        tempCards = [];
+        clicks = 0;
+      }
+
       console.log(`Card clicked: ${card}`);
     });
   });
 });
+
+function victory() {
+  document.getElementById("memory_board").style.display = "none";
+  document.getElementById("victory").style.display = "flex";
+}
