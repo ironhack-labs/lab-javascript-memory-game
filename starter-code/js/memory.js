92f07590e13b0ae2ed66a5e2bea33b4c366708e7
Jaime <jaimemoravallejo@gmail.com>
2020-01-19 22:51:45 +0100

    iteration 1 done and started the 2

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,30 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedCards;
+    this.pairsClicked ;
+    this.pairsGuessed;
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    this.cards.sort(function(){ return Math.random() -0.5})
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if(card1.name === card2.name){
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    let numCards = Object.keys(this.cards[0]);
+    console.log(numCards)
+    if (this.pairsGuessed == (numCards/2)){
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
