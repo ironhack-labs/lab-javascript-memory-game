0046fe3cc5ff9aefefc956dae809d4494a58e76d
Javier Varela <jvarela@strands.com>
2020-01-20 18:26:31 +0100

    Iteration 2: Plan your game

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,33 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  //Based on Fisher-Yates shuffle, this method walks the array in the reverse order 
+  //and swap each element with a random one before it
+  shuffleCards(cards) {
+    if (cards){
+      for (let i = cards.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * i);
+        const temp = cards[i];
+        cards[i] =cards[j];
+        cards[j] = temp;
+      }
+      return cards;
+    }
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2){
+      this.pairsGuessed++
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    return this.pairsGuessed === 12 ? true : false;
+  }
 }
