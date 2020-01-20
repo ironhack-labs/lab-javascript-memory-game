c7207c679fd78be2bf51ec31d093c95856e5957c
Leinadm <danielmonteru@gmail.com>
2020-01-20 17:31:33 +0100

    Iteration 2.2: The class methods. Steps 1, 2 & 3

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -6,7 +6,30 @@ class MemoryGame {
     this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    for (let i = this.cards.length -1; i > 0; i--){
+      let j = Math.floor(Math.random() * (i + 1));
+      let cardsJ = this.cards[j];
+      this.cards[j] = this.cards[i];
+      this.cards[i] = cardsJ;
+    }
+    return undefined;
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    let areSame = false;
+    if (card1 === card2){
+      this.pairsGuessed++;
+      areSame = true;
+    }
+    return areSame;
+  }
+  isFinished() {
+    let endGame = false
+    if(this.pairsGuessed === this.cards.length/2){
+      console.log("a");
+      endGame = true;
+    }
+    return endGame;
+  }
 }
