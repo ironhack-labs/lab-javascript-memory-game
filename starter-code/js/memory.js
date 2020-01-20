df83ed7e0fe418b2f3325fbe00ca18364225e0c7
Leinadm <danielmonteru@gmail.com>
2020-01-20 16:33:44 +0100

    Iteration 2.1: The MemoryGame class

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,6 +1,9 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
   shuffleCards() {}
