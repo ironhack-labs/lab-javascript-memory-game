646c7b2568f81e9e72288c90cf40778b37f3b2c1
Juan Antonio Martin <juanantoniomartinb@gmail.com>
2020-01-21 21:36:53 +0100

    jasmine hecho

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,43 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    let m = this.cards.length, t, i;
+
+    while (m) {
+
+      i = Math.floor(Math.random() * m--);
+
+      t = this.cards[m];
+      this.cards[m] = this.cards[i];
+      this.cards[i] = t;
+    }
+
+    return // HE SACADO ESTO PERO REALMENTE NO ENTIENDO MUY BIEN PORQUE ES ASÍ, RECUERDAME QUE LO COMENTEMOS PORFI
+    if (!this.cards) {
+      this.cards
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true
+    } else
+      return false
+  }
+
+
+  isFinished() {
+    if (this.pairsGuessed === (this.cards.length / 2)) {
+      return true
+    } else
+      return false
+  }
 }
