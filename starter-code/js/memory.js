9d0a069b6ffc6424433ff38198f0de215e4fdded
Pablolo <pablo_olondriz@hotmail.com>
2020-01-22 16:28:24 +0100

    iteration 2.1 done

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -2,8 +2,19 @@ class MemoryGame {
   constructor(cards){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+
+  }
+
+  checkIfPair(card1, card2) {
+
+  }
+
+  isFinished() {
+    
+  }
 }
