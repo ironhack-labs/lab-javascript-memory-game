6691dbef70a5866bacc5f86b461ad096d4c1a23d
Pablolo <pablo_olondriz@hotmail.com>
2020-01-22 17:29:27 +0100

    iteration 2.1 fisher yates method

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -7,7 +7,15 @@ class MemoryGame {
     this.pairsGuessed = 0;
   }
   shuffleCards() {
-
+    let i = 0;
+    let j = 0;
+    let temp = null;
+    for (i = this.cards.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1));
+      temp = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = temp;
+    }
   }
 
   checkIfPair(card1, card2) {
@@ -15,6 +23,22 @@ class MemoryGame {
   }
 
   isFinished() {
-    
+
   }
 }
+
+// var arr = ['A','B','C','D','E','F','G','H']
+// function shuffle (array) {
+//   var i = 0
+//     , j = 0
+//     , temp = null
+
+//   for (i = array.length - 1; i > 0; i -= 1) {
+//     j = Math.floor(Math.random() * (i + 1))
+//     temp = array[i]
+//     array[i] = array[j]
+//     array[j] = temp
+//   }
+// }
+
+// shuffle(arr);
