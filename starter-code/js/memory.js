2f8d8f6c5c0fac98436df9db4bc8f1d02271a0df
mfernandezfreire <mfernandezfreire@outlook.com>
2020-01-22 08:29:17 +0100

    Estilos agregados

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -10,15 +10,14 @@ class MemoryGame {
     this.cards = this.pickedCards;
   }
  checkIfPair(card1, card2) {
-    this.pairsClicked = [];
-    this.pairsGuessed = [];
-    this.pairsClicked.push(card1);
-    this.pairsGuessed.push(card2);
-    if (JSON.stringify(pairsClicked) !== JSON.stringify(pairsGuessed)) {
-      this.pairsGuessed.splice(pairsGuessed.length -1)
-      return false;
-    } else {
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    this.pairsClicked++;
+    if (card1 == card2) {
+      this.pairsGuessed++;
       return true;
+    } else {
+      return false;
     }
   }
   isFinished() {}
