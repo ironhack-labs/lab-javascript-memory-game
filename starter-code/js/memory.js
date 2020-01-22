3eddf2161c221fbba6f8616e6c6df75f8987242e
Rafael Bollit <rafa.bollit@gmail.com>
2020-01-22 09:35:51 +0100

    no done

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,40 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    let m = this.cards.length, t, i;
+
+    while (m) {
+
+
+      i = Math.floor(Math.random() * m--);
+
+
+      t = this.cards[m];
+      this.cards[m] = this.cards[i];
+      this.cards[i] = t;
+    }
+
+    return
+
+    if (!this.cards) {
+      this.cards
+    }
+
+  }
+
+
+  checkIfPair(card1, card2) {
+
+
+
+  }
+
+
+  isFinished() { }
 }
