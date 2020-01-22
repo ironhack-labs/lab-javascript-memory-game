744b81e8ab8a0e955eb3891e3c186336aba41719
davidvrl <veral.david@gmail.com>
2020-01-22 09:34:46 +0100

    cambio menor 3

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,28 +1,99 @@
-const cards = [
-  { name: "aquaman", img: "aquaman.jpg" },
-  { name: "batman", img: "batman.jpg" },
-  { name: "captain america", img: "captain-america.jpg" },
-  { name: "fantastic four", img: "fantastic-four.jpg" },
-  { name: "flash", img: "flash.jpg" },
-  { name: "green arrow", img: "green-arrow.jpg" },
-  { name: "green lantern", img: "green-lantern.jpg" },
-  { name: "ironman", img: "ironman.jpg" },
-  { name: "spiderman", img: "spiderman.jpg" },
-  { name: "superman", img: "superman.jpg" },
-  { name: "the avengers", img: "the-avengers.jpg" },
-  { name: "thor", img: "thor.jpg" },
-  { name: "aquaman", img: "aquaman.jpg" },
-  { name: "batman", img: "batman.jpg" },
-  { name: "captain america", img: "captain-america.jpg" },
-  { name: "fantastic four", img: "fantastic-four.jpg" },
-  { name: "flash", img: "flash.jpg" },
-  { name: "green arrow", img: "green-arrow.jpg" },
-  { name: "green lantern", img: "green-lantern.jpg" },
-  { name: "ironman", img: "ironman.jpg" },
-  { name: "spiderman", img: "spiderman.jpg" },
-  { name: "superman", img: "superman.jpg" },
-  { name: "the avengers", img: "the-avengers.jpg" },
-  { name: "thor", img: "thor.jpg" }
+const cards = [{
+    name: "aquaman",
+    img: "aquaman.jpg"
+  },
+  {
+    name: "batman",
+    img: "batman.jpg"
+  },
+  {
+    name: "captain america",
+    img: "captain-america.jpg"
+  },
+  {
+    name: "fantastic four",
+    img: "fantastic-four.jpg"
+  },
+  {
+    name: "flash",
+    img: "flash.jpg"
+  },
+  {
+    name: "green arrow",
+    img: "green-arrow.jpg"
+  },
+  {
+    name: "green lantern",
+    img: "green-lantern.jpg"
+  },
+  {
+    name: "ironman",
+    img: "ironman.jpg"
+  },
+  {
+    name: "spiderman",
+    img: "spiderman.jpg"
+  },
+  {
+    name: "superman",
+    img: "superman.jpg"
+  },
+  {
+    name: "the avengers",
+    img: "the-avengers.jpg"
+  },
+  {
+    name: "thor",
+    img: "thor.jpg"
+  },
+  {
+    name: "aquaman",
+    img: "aquaman.jpg"
+  },
+  {
+    name: "batman",
+    img: "batman.jpg"
+  },
+  {
+    name: "captain america",
+    img: "captain-america.jpg"
+  },
+  {
+    name: "fantastic four",
+    img: "fantastic-four.jpg"
+  },
+  {
+    name: "flash",
+    img: "flash.jpg"
+  },
+  {
+    name: "green arrow",
+    img: "green-arrow.jpg"
+  },
+  {
+    name: "green lantern",
+    img: "green-lantern.jpg"
+  },
+  {
+    name: "ironman",
+    img: "ironman.jpg"
+  },
+  {
+    name: "spiderman",
+    img: "spiderman.jpg"
+  },
+  {
+    name: "superman",
+    img: "superman.jpg"
+  },
+  {
+    name: "the avengers",
+    img: "the-avengers.jpg"
+  },
+  {
+    name: "thor",
+    img: "thor.jpg"
+  }
 ];
 
 const memoryGame = new MemoryGame(cards);
@@ -34,16 +105,20 @@ window.addEventListener("load", event => {
     html += `<div class="back" name="${pic.img}"></div>`;
     html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
     html += `</div>`;
+
   });
 
   // Add all the divs to the HTML
-  
+
   document.querySelector("#memory_board").innerHTML = html;
 
   // Bind the click event of each element to a function
   document.querySelectorAll(".card").forEach(card => {
     card.addEventListener("click", () => {
       // TODO: write some code here
+
+      card.classList.toggle("turned")
+
       console.log(`Card clicked: ${card}`);
     });
   });
