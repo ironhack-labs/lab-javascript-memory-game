8863712c6442b06ac93ac3247b4a497949b6e158
Carlos González <cargonamt@gmail.com>
2020-01-22 09:33:48 +0100

    not done

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,28 +1,99 @@
-const cards = [
-  { name: "aquaman", img: "aquaman.jpg" },
-  { name: "batman", img: "batman.jpg" },
-  { name: "captain america", img: "captain-america.jpg" },
-  { name: "fantastic four", img: "fantastic-four.jpg" },
-  { name: "flash", img: "flash.jpg" },
-  { name: "green arrow", img: "green-arrow.jpg" },
-  { name: "green lantern", img: "green-lantern.jpg" },
-  { name: "ironman", img: "ironman.jpg" },
-  { name: "spiderman", img: "spiderman.jpg" },
-  { name: "superman", img: "superman.jpg" },
-  { name: "the avengers", img: "the-avengers.jpg" },
-  { name: "thor", img: "thor.jpg" },
-  { name: "aquaman", img: "aquaman.jpg" },
-  { name: "batman", img: "batman.jpg" },
-  { name: "captain america", img: "captain-america.jpg" },
-  { name: "fantastic four", img: "fantastic-four.jpg" },
-  { name: "flash", img: "flash.jpg" },
-  { name: "green arrow", img: "green-arrow.jpg" },
-  { name: "green lantern", img: "green-lantern.jpg" },
-  { name: "ironman", img: "ironman.jpg" },
-  { name: "spiderman", img: "spiderman.jpg" },
-  { name: "superman", img: "superman.jpg" },
-  { name: "the avengers", img: "the-avengers.jpg" },
-  { name: "thor", img: "thor.jpg" }
+const cards = [{
+    name: "aquaman",
+    img: "aquaman.jpg"
+  },
+  {
+    name: "batman",
+    img: "batman.jpg"
+  },
+  {
+    name: "captain america",
+    img: "captain-america.jpg"
+  },
+  {
+    name: "fantastic four",
+    img: "fantastic-four.jpg"
+  },
+  {
+    name: "flash",
+    img: "flash.jpg"
+  },
+  {
+    name: "green arrow",
+    img: "green-arrow.jpg"
+  },
+  {
+    name: "green lantern",
+    img: "green-lantern.jpg"
+  },
+  {
+    name: "ironman",
+    img: "ironman.jpg"
+  },
+  {
+    name: "spiderman",
+    img: "spiderman.jpg"
+  },
+  {
+    name: "superman",
+    img: "superman.jpg"
+  },
+  {
+    name: "the avengers",
+    img: "the-avengers.jpg"
+  },
+  {
+    name: "thor",
+    img: "thor.jpg"
+  },
+  {
+    name: "aquaman",
+    img: "aquaman.jpg"
+  },
+  {
+    name: "batman",
+    img: "batman.jpg"
+  },
+  {
+    name: "captain america",
+    img: "captain-america.jpg"
+  },
+  {
+    name: "fantastic four",
+    img: "fantastic-four.jpg"
+  },
+  {
+    name: "flash",
+    img: "flash.jpg"
+  },
+  {
+    name: "green arrow",
+    img: "green-arrow.jpg"
+  },
+  {
+    name: "green lantern",
+    img: "green-lantern.jpg"
+  },
+  {
+    name: "ironman",
+    img: "ironman.jpg"
+  },
+  {
+    name: "spiderman",
+    img: "spiderman.jpg"
+  },
+  {
+    name: "superman",
+    img: "superman.jpg"
+  },
+  {
+    name: "the avengers",
+    img: "the-avengers.jpg"
+  },
+  {
+    name: "thor",
+    img: "thor.jpg"
+  }
 ];
 
 const memoryGame = new MemoryGame(cards);
@@ -36,14 +107,21 @@ window.addEventListener("load", event => {
     html += `</div>`;
   });
 
+
   // Add all the divs to the HTML
   document.querySelector("#memory_board").innerHTML = html;
 
   // Bind the click event of each element to a function
   document.querySelectorAll(".card").forEach(card => {
-    card.addEventListener("click", () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
+    card.onclick = e => {
+      e.currentTarget.classList.toggle('turned')
+
+      memoryGame.pickedCards.push(e.currentTarget)
+      console.log(this.pickedCards)
+    }
+    // TODO: write some code here
+    console.log(`Card clicked: ${card}`);
   });
 });
+
+console.log(memory_board.back)
--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,46 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
+    console.log(this.cards.length)
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+
+    let mixedCards = []
+    let n = this.cards.length
+    let i
+    while (n) {
+      i = Math.floor(Math.random() * n--);
+
+      mixedCards.push(this.cards.splice(i, 1)[0])
+    }
+    return mixedCards
+
+  }
+
+
+
+
+  checkIfPair(card1, card2) {
+    if (card1 === card2) {
+      (this.pairsClicked += 1) && (this.pairsGuessed += 1)
+      return true
+    } else {
+      this.pairsClicked++
+      return false
+    }
+  }
+
+
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length) {
+      return true
+    } else {
+      return false
+    }
+  }
+
 }
