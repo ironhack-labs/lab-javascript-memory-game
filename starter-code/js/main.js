91e87f56040947cdd9e2edfed48104de7895e6f8
michael <michael@miniguide.es>
2020-01-23 17:01:03 +0100

    Iteration 3

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -41,11 +41,34 @@ window.addEventListener("load", event => {
   // Add all the divs to the HTML
   document.querySelector("#memory_board").innerHTML = html;
 
+  function turnUnmatchedCards(card1, card2) {
+    card1.classList.toggle("turned");
+    card2.classList.toggle("turned");
+  }
+
   // Bind the click event of each element to a function
   document.querySelectorAll(".card").forEach(card => {
     card.addEventListener("click", () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      card.classList.toggle("turned", true);    // player can only turn card face up   
+      memoryGame.pickedCards.push(card);
+      if (memoryGame.pickedCards.length === 2) {
+        const card1 = memoryGame.pickedCards[0];
+        const card2 = memoryGame.pickedCards[1];
+        const match = memoryGame.checkIfPair(card1.getAttribute('data-card-name'), card2.getAttribute('data-card-name'));
+        if (!match) {
+          (function () {
+            setTimeout(function(){
+              turnUnmatchedCards(card1, card2);
+            }, 1000);
+          })();
+        }
+        memoryGame.pickedCards.splice(0,2);
+      }
+      document.getElementById('pairs_clicked').textContent = memoryGame.pairsClicked;
+      document.getElementById('pairs_guessed').textContent = memoryGame.pairsGuessed;
+      if (memoryGame.isFinished()) {
+        document.getElementById('memory_board').innerHTML = `<img src="https://media.giphy.com/media/3o7aCReOCepWnsNuSs/giphy.gif">`;
+      }
     });
   });
 });
