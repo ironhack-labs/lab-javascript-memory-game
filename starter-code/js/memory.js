537dd53dfbc0bb241f8b88621a4b9a120741bdd0
Marcelo Sallito <m.sallito@safened.com>
2020-01-23 11:41:40 +0100

    testing done

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,48 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {       
+
+      let currentIndex = this.cards.length;
+      let tmpValue = undefined;
+      let randomIndex;
+      
+      while (currentIndex > 0) {        
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+    
+        tmpValue = this.cards[currentIndex];
+        this.cards[currentIndex] = this.cards[randomIndex];
+        this.cards[randomIndex] = tmpValue;
+      }
+    
+      return this.cards;
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked ++;
+    let cards = false;
+
+    if(card1 === card2){
+      cards = true; 
+      this.pairsGuessed ++;     
+    } 
+
+    return cards;
+  }
+
+  isFinished() {
+    let hasFinished = false;
+    let totalPairs = this.cards.length / 2;
+
+    if(this.pairsGuessed >= totalPairs){
+      hasFinished = true;
+    }
+    return hasFinished;
+  }
 }
