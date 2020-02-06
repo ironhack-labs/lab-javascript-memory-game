3d331bdf4474c788858385d55bcc609657e6acb4
Andre <andre.talpo@gmail.com>
2020-02-05 21:21:47 -0300

    Terminada logica

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,31 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    let remain = this.cards.length, aux, indexRandom;
+    while (remain) {
+      indexRandom = Math.floor(Math.random() * remain--);
+      aux = this.cards[remain];
+      this.cards[remain] = this.cards[indexRandom];
+      this.cards[indexRandom] = aux;
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
+  }
+
+  isFinished() {
+    return this.pairsGuessed >= this.cards.length / 2;
+  }
 }
