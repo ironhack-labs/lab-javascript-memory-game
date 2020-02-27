909cd6c190589e3676d6bb0483b51f555edbaef2
Felix van Riessen <felixvanriessengurung@gmail.com>
2020-02-27 15:12:04 +0100

    finished

--- js/index.js
+++ js/index.js
@@ -28,7 +28,12 @@ const cards = [
 const memoryGame = new MemoryGame(cards);
 
 memoryGame.shuffleCards()
-let picked2 = 0
+let pickedCards = []
+let msg1 = true
+let msg2 = true
+let msg3 = true
+let msg4 = true
+
 
 
 window.addEventListener('load', event => {
@@ -45,14 +50,49 @@ window.addEventListener('load', event => {
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
-
     card.addEventListener('click', () => {
       // TODO: write some code here
       
-      console.log(`Card clicked: ${card}`)
       console.log(card.getAttribute('data-card-name'))
-      card.classList.add('turned')
       
+      if (pickedCards.length < 2){
+        pickedCards.push(card)
+      }
+      for (c of pickedCards){
+        c.classList.add('turned')
+      }
+
+      if (pickedCards.length == 2){
+        
+        let pairState = memoryGame.checkIfPair(pickedCards[0].getAttribute('data-card-name'),pickedCards[1].getAttribute('data-card-name'))
+        
+        if (!pairState){
+          setTimeout(function(){
+            for (c of pickedCards){
+              c.classList.remove('turned')
+              pickedCards = []
+            }
+            //joke alerts
+            if (memoryGame.pairsClicked == 10 && msg1){
+              msg1 = false
+              alert('a normal person would have finished by now')
+            } else if (memoryGame.pairsClicked == 20 && msg2){
+              msg2 = false
+              alert('are u even trying???')
+            } else if (memoryGame.pairsClicked == 40 && msg3){
+              msg3 = false
+              alert('uhhh, this game is designed for humans, not goldfish...')
+            } else if (memoryGame.pairsClicked == 40 && msg3) {
+              msg4 = false
+              alert('get gud noob')
+            }
+          },1500)          
+        } else {
+          document.querySelector('#pairs-guessed').innerHTML = memoryGame.pairsGuessed
+          pickedCards = []
+        }
+        document.querySelector('#pairs-clicked').innerHTML = memoryGame.pairsClicked 
+      }
     });
   });
 });
