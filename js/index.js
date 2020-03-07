474dd0d57334b392d0454637520252e28064c109
RuiMPer <rui.miguel.pereira.rp@gmail.com>
2020-03-07 12:42:50 +0000

    toggle

--- js/index.js
+++ js/index.js
@@ -43,6 +43,7 @@ window.addEventListener('load', event => {
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
       // TODO: write some code here
+      card.classList.add("turned")
       console.log(`Card clicked: ${card}`);
     });
   });
