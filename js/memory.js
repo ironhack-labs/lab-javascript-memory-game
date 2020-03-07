ca93b4c3eb6219cce480b9bdb3aaebf2a6ecd1d8
RuiMPer <rui.miguel.pereira.rp@gmail.com>
2020-03-07 12:10:05 +0000

    memory done

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,42 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards(cards) {
+    let m = this.cards.length, t, i;
+    // While there remain elements to shuffle...
+    while (m) {
+
+      // Pick a remaining element...
+
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = this.cards[m];
+      this.cards[m] = this.cards[i];
+      this.cards[i] = t;
+    }
+  
+    return cards;
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1
+    if (card1 === card2) {
+      this.pairsGuessed ++
+      return true
+    } else {
+      return false
+    }
+  }
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length/2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
