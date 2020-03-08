459c673d1a28d3490ba5fdc7ff143b3014216bcd
Stefano Santonocito <oddouu@gmail.com>
2020-03-08 18:54:30 +0000

    completed

--- js/index.js
+++ js/index.js
@@ -98,6 +98,13 @@ const cards = [{
 
 const memoryGame = new MemoryGame(cards);
 
+// runs ShuffleCards method right after defining a new memoryGame
+memoryGame.shuffleCards();
+
+// initializes an empty "helper" array that will be used below
+let $pickedCards = [];
+
+// builds list of card images basing itself on memoryGame.cards
 window.addEventListener('load', event => {
   let html = '';
   memoryGame.cards.forEach(pic => {
@@ -113,9 +120,69 @@ window.addEventListener('load', event => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
-      // card.classList.toggle("turned")
+
+      // turns card in order to show its image
       card.className = "card turned";
-      console.log(card);
+
+      // saves the name of the turned card into pickedCards array
+      memoryGame.pickedCards.push(card.dataset.cardName);
+
+      // saves card selector in a separated "helper" array that will be used in order to easily turn back the cards if they aren't a pair
+      $pickedCards.push(card);
+
+      // if two cards are picked, it checks them by calling checkIfPair method
+      if (memoryGame.pickedCards[1]) {
+
+        if (memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1])) {
+
+          // Updates Score HTML
+          document.querySelector('#pairs-clicked').innerText = memoryGame.pairsClicked;
+          document.querySelector('#pairs-guessed').innerText = memoryGame.pairsGuessed;
+
+          // Empties out the array of picked cards
+          memoryGame.pickedCards = [];
+
+          // if checkIfPair method returns true, checks if the game has finished. If this method also returns true, it launchs a location.reload() command within a set Timeout of 12 seconds. Another setTimeout and a setInterval add extra interactivity
+          if (memoryGame.isFinished()) {
+            window.setTimeout(() => {
+              let timer = 10;
+              const $youWon = $pickedCards[0].parentNode;
+
+              window.setInterval(() => {
+
+                $youWon.innerHTML = `<div style="text-align: center; font-size: 38px; color: red">YOU WON! <br> <br> A new game is going to start in ${timer}...</div>`;
+                timer--;
+              }, 1100);
+              window.setTimeout(() => {
+                location.reload();
+              }, 12000);
+            }, 200);
+          }
+
+          // if two cards have the same image, their divs visibility is forced to hidden. This allows to leave an empty space between cards on the board
+          window.setTimeout(() => {
+            $pickedCards[0].style.visibility = "hidden";
+            $pickedCards[1].style.visibility = "hidden";
+            $pickedCards = [];
+          }, 600);
+
+          // if the checkIfPair method returns false...
+        } else {
+
+          // Updates scores
+          document.querySelector('#pairs-clicked').innerText = memoryGame.pairsClicked;
+
+          // Empties out the picked cards array
+          memoryGame.pickedCards = [];
+
+          // Turns back the selected cards by using the HTML selector $pickedCards and updating their class name. A setTimeout is used in order to give a slight delay to this operation
+          window.setTimeout(() => {
+            $pickedCards[0].className = "card";
+            $pickedCards[1].className = "card";
+            $pickedCards = [];
+          }, 600);
+        }
+      }
     });
   });
 });
