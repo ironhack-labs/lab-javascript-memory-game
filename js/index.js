8ff75c77361cef3cad62938df78c5ff8723c0006
Patricia Jurado De Bilbao <patriciajdebilbao@gmail.com>
2020-04-01 00:32:06 +0200

    done

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ const cards = [
 ];
 
 const memoryGame = new MemoryGame(cards);
-
+memoryGame.shuffleCards() // Hace el shuffle card
 window.addEventListener('load', event => {
   let html = '';
   memoryGame.cards.forEach(pic => {
@@ -40,10 +40,26 @@ window.addEventListener('load', event => {
   document.querySelector('#memory-board').innerHTML = html;
 
   // Bind the click event of each element to a function
-  document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
-  });
-});
+  document.querySelectorAll('.card').forEach(card => { //itera el array
+    card.addEventListener('click', () => {            // hace el click en las cartas   
+      if (memoryGame.pickedCards.length < 2){        // Chequea que solo se puedan voltear 2 cartas a la vez 
+        memoryGame.pickedCards.push(card)           // las pone en el array de pickedCards
+        card.classList.toggle('turned')             // Le asigna la clase de turned para que se puedan voltear   
+      }
+      if (memoryGame.pickedCards.length === 2){     // Chequea que si las dos cartas son iguales 
+        if (memoryGame.checkIfPair(memoryGame.pickedCards[0].getAttribute('data-card-name'),memoryGame.pickedCards[1].getAttribute('data-card-name'))){      // Si la carta 1 y la carta 2 son iguales...
+          memoryGame.pickedCards.forEach(card => card.classList.toggle('blocked'))        // ...le añade clase block para dejarlas volteadas hacia arriba
+          memoryGame.pickedCards.splice(0, 2)                                            // vacia el array cuando sabe que son parejas                                                              
+          document.querySelector('#pairs-guessed').innerHTML = memoryGame.pairsGuessed   // refresca el contador de Pairs guessed cada par igual que se adivine 
+          if (memoryGame.isFinished()) alert("You Won!!!")                               // Sale alerta cuando consigues todos los pares iguales  
+        }
+        else {                                                                                             // si las cartas no son iguales 
+          setTimeout(() => memoryGame.pickedCards.forEach(card => card.classList.toggle('turned')), 1200)  // itera el array
+          setTimeout(() => memoryGame.pickedCards.splice(0, 2), 1200)                                      // vacia el array cuando no hay ninguna pareja valida   
+        }                                                                                               // añadi el timeout para que al voltear las cartas hacia arriba y se desvoltearan fuera un poco mas lento
+        document.querySelector('#pairs-clicked').innerHTML = memoryGame.pairsClicked                      // refresca el contador de pairsclicked cada par que se eliga sea correcto o no 
+      }
+    })
+  })
+})
+
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,43 @@
 class MemoryGame {
   constructor(cards){
-    this.cards = cards;
-    // add the rest of the class properties here
+    this.cards = cards
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    let currentIndex = this.cards.length, temporaryValue, randomIndex
+
+     while (0 !== currentIndex) {
+       randomIndex = Math.floor(Math.random() * currentIndex)
+       currentIndex -= 1
+
+       temporaryValue = this.cards[currentIndex]
+       this.cards[currentIndex] = this.cards[randomIndex]
+       this.cards[randomIndex] = temporaryValue
+     }
+     return cards
+   }
+   
+  checkIfPair(card1, card2) {
+
+     this.pairsClicked++ 
+      
+     if (card1 === card2) {
+      this.pairsGuessed++
+      return true
+      }  else {
+        return false
+      }
+  }
+
+  isFinished() {
+    const pairs = this.cards.length / 2
+    if (this.pairsGuessed === pairs){
+      return true
+    } else {
+      return false
+    }
+  }
 }
