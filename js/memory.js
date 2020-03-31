a945b0ad0d032f64375208b7ebe60ae51eb5f518
Elvira Ramírez <elvirarp92@gmail.com>
2020-03-31 18:44:49 +0200

    define checkIfPair

--- js/memory.js
+++ js/memory.js
@@ -1,11 +1,20 @@
 class MemoryGame {
-  constructor(cards){
-    this.cards = cards;
+  constructor(cards) {
+    this.cards = cards
     this.pickedCards = []
     this.pairsClicked = 0
     this.pairsGuessed = 0
   }
   shuffleCards() {}
-  checkIfPair(card1, card2) {}
+  checkIfPair(card1, card2) {
+    if (card1 === card2) {
+      this.pairsClicked++
+      this.pairsGuessed++
+      return true
+    } else {
+      this.pairsClicked++
+      return false
+    }
+  }
   isFinished() {}
 }
