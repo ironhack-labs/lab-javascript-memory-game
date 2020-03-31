313f7ed016243c0567c22a58579891ed5f7fa8c8
Hector Anton <hectoranf@gmail.com>
2020-03-31 21:35:30 +0200

    done

--- js/index.js
+++ js/index.js
@@ -42,8 +42,46 @@ window.addEventListener('load', event => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+
+      //La carta seleccionada se da la vuelta
+      card.classList.add('turned')
+
+      //Si es la SEGUNDA carta
+      if (memoryGame.pickedCards.length === 1) {
+        //Se actualiza el marcador de pares selecionados
+        document.querySelector('#pairs-clicked').innerHTML = memoryGame.pairsClicked
+        //Se añade la nueva carta
+        memoryGame.pickedCards.push(card)
+
+        //Si la pareja es CORRECTA
+        if (memoryGame.checkIfPair(memoryGame.pickedCards[0].getAttribute('data-card-name'), memoryGame.pickedCards[1].getAttribute('data-card-name'))) {
+
+          //Se actualiza el marcados de pares adivinados
+          document.querySelector('#pairs-guessed').innerHTML = memoryGame.pairsGuessed
+
+          //Se bloquean las cartas de la pareja acertada
+          memoryGame.pickedCards[0].classList.add('blocked')
+          memoryGame.pickedCards[1].classList.add('blocked')
+          //Se saca la pareja para que no se voltee
+          memoryGame.pickedCards.splice(0, 2)
+
+          //Se comprueba que esta pareja no sea la última que faltaba
+          if (memoryGame.isFinished()) alert("Congratulation! You've won the game")
+        }
+
+        //Si es la PRIMERA carta
+      } else {
+        //Si hay una pareja descubierta INCORRECTA se voltea y se quita del array pickedCards
+        if (memoryGame.pickedCards.length === 2) {
+          memoryGame.pickedCards[0].classList.remove('turned')
+          memoryGame.pickedCards[1].classList.remove('turned')
+
+          memoryGame.pickedCards.splice(0, 2)
+        }
+        //Se añade la que será la primera carta de la pareja
+        memoryGame.pickedCards.push(card)
+      }
+
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,43 @@
 class MemoryGame {
-  constructor(cards){
-    this.cards = cards;
-    // add the rest of the class properties here
+  constructor(cards) {
+    this.cards = cards
+    this.shuffleCards()
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards(arg) {
+    // if (!cards) return undefined //Si no obtenemos argumento
+
+    const shuffledCards = []
+    let index = 0
+
+    while (this.cards.length >= 1) {
+      index = Math.round(Math.random() * (this.cards.length - 1))
+
+      shuffledCards.push({ ...this.cards[index] })
+      this.cards.splice(index, 1)
+    }
+
+    this.cards = shuffledCards
+
+    if (!arg) return undefined
+    else return shuffledCards
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++
+
+    if (card1 == card2) {
+      this.pairsGuessed++
+      return true
+    }
+    else return false
+  }
+
+  isFinished() {
+    if (this.pairsGuessed == this.cards.length / 2) return true
+    else return false
+  }
 }
