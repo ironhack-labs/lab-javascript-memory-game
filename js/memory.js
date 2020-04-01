5c36ea2c149b7bb2e2c0642c446a7231ea568a53
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2020-04-01 08:34:39 +0200

    add js files and html file work not done

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,35 @@
 class MemoryGame {
-  constructor(cards){
-    this.cards = cards;
-    // add the rest of the class properties here
+	constructor(cards, pickedCards, pairsClicked, pairsGuessed) {
+    this.cards = cards,
+    this.pickedCards = [],
+		this.pairsClicked = 0,
+		this.pairsGuessed = 0
+		// add the rest of the class properties here
+	}
+  shuffleCards() {
+    let cards = this.cards
+    for (let i = cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cards[i], cards[j]] = [cards[j], cards[i]];
+    }
+	}
+  checkIfPair(card1, card2) {
+    if (card1 !== card2) {
+      this.pairsClicked++
+      return false
+    }
+    if (card1 === card2) {
+      this.pairsClicked++
+      this.pairsGuessed++
+      return true
+    }
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  isFinished() {
+    let memoryGame = false
+
+    if (this.pairsGuessed === this.cards.length / 2) {
+      memoryGame = true
+    }
+    return memoryGame
+  }
 }
