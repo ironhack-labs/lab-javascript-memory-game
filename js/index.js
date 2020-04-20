691d8158275f63b147babb5d63350cb647956865
David de Lamo <d.lamo.valverde@gmail.com>
2020-04-20 19:59:41 +0200

    lab done

--- js/index.js
+++ js/index.js
@@ -43,7 +43,50 @@ window.addEventListener('load', event => {
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      // Add class 'turned' to a card, is turned and pushed into the array pickedCards
+      card.classList.add('turned');
+      memoryGame.pickedCards.push(card);
+      // Case when we have selected 2 cards
+      if (memoryGame.pickedCards.length == 2) {
+        // Save the name of both cards
+        let firstCard = memoryGame.pickedCards[0].getAttribute('data-card-name');
+        let secondCard = memoryGame.pickedCards[1].getAttribute('data-card-name');
+        // Case we have a pair
+        if (memoryGame.checkIfPair(firstCard, secondCard)) {
+          // Increment clicked and guessed counters and restart pickedCArds array
+          document.querySelector('#pairs-clicked').innerHTML = memoryGame.pairsClicked;
+          document.querySelector('#pairs-guessed').innerHTML = memoryGame.pairsGuessed;
+          memoryGame.pickedCards = [];
+          // Dsiplay a message if the game is finished
+          setTimeout(endGame, 1000);
+        // Case we don't have a pair
+        } else {
+          // Increment clicked counter
+          document.querySelector('#pairs-clicked').innerHTML = memoryGame.pairsClicked;
+          // Apply a timeout in order to have time to see the second card turned before turning again the selected cards and restart pickedCards array
+          setTimeout(restartPickedCards, 1000);
+          function restartPickedCards() {
+            memoryGame.pickedCards.forEach(card => card.classList.remove('turned'));
+            memoryGame.pickedCards = [];
+          }
+        }
+      }    
     });
   });
 });
+
+// Final message function
+function endGame() {
+  if (memoryGame.isFinished()) {
+    // Hide memory board and score
+    document.querySelector('#memory-board').style.display = 'none';
+    document.querySelector('#score').style.display = 'none';
+    // create new h1 element and append it into the body
+    const h1Tag = document.createElement('h1');
+    h1Tag.style.fontSize = '86px';
+    h1Tag.style.color = 'gold';
+    h1Tag.style.paddingTop = '10%';
+    h1Tag.innerHTML = `End Game<br>Your final score is: ${memoryGame.pairsClicked}`;
+    document.body.appendChild(h1Tag);
+  }
+}
