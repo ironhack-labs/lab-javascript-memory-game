178bfa07cfb736d128f9603367ce38404c0c35da
JesusSantana <jesus@esmarketingdigital.com>
2020-04-21 12:47:20 +0200

    v2.1 The class Methods

--- js/memory.js
+++ js/memory.js
@@ -6,7 +6,26 @@ class MemoryGame {
     this.pairsClicked = 0;
     this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  
+  //Iteration 2.2: The class methods
+  shuffleCards() {
+    for (let i = 0; i < this.cards.length - 2; i++) {
+      let j = Math.floor(Math.random() * (this.cards.length - i) + i);
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if(card1 === card2){
+      this.pairsGuessed +=1;
+      return true;
+    } else return false;
+  }
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length / 2){
+      this.cards = "";
+      return true;
+    }else return false;
+  }
+
 }
