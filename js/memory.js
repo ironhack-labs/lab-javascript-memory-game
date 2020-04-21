70f2a14501503723a5e71167c4bd10832582d7d6
JesusSantana <jesus@esmarketingdigital.com>
2020-04-21 11:47:03 +0200

    v2.1 The MemoryGame class

--- js/memory.js
+++ js/memory.js
@@ -1,7 +1,10 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    //Iteration 2.1: The MemoryGame class
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
   shuffleCards() {}
   checkIfPair(card1, card2) {}
