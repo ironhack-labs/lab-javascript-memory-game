da0b527b7198250397d4430ccf87879595bd0f01
Alba Gustems <albagustems@gmail.com>
2020-04-22 13:45:41 +0200

    Iteration 1 (logic of the game) finished

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,50 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards(cards) {
+    // Solution with a for loop
+    /*for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+    return cards;*/
+
+    // Solution with the Fisher-Yates shuffle;
+    let m = this.cards.length,
+      t, i;
+
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = this.cards[m];
+      this.cards[m] = this.cards[i];
+      this.cards[i] = t;
+    }
+    return cards;
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+
+    if (card1 === card2) {
+      this.pairsClicked++;
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
