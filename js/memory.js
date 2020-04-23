9355a2caddeb6c264c0d93eb75127a2ea33e7e1c
Robert Ledezma <Robert@MacBook-Pro-de-Robert-2.local>
2020-04-23 10:11:39 +0200

    shuffle

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,30 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
   shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true
+    } else {
+      return false
+    }
+  }
+  isFinished() {
+    let final = false;
+    let pares = this.cards.length / 2
+    if (this.pairsGuessed === pares) {
+      final = true
+    }
+    return final
+
+
+  }
 }
