f63181f8f79c8817286a111007b111ee6443bb9a
alex gera <alex.ger93@gmail.com>
2020-04-27 22:13:57 +0200

    iteratin 2.2 done

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,36 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    var currentIndex = this.cards.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = this.cards[currentIndex];
+      this.cards[currentIndex] = this.cards[randomIndex];
+      this.cards[randomIndex] = temporaryValue;
+    }
+    console.log(array);
+    return this.cards; 
+  }
+
+  checkIfPair(card1, card2) {
+    ++this.pairsClicked;
+    if (this.cards.card1.name === this.cards.card2.name) {
+      ++this.pairsGuessed;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    return this.pairsGuessed === this.cards.length ? true : false;
+  }
 }
