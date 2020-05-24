01ba4c9558fa514fa7d27782add1a04ac931cbc2
fermarinsanchez <fermarinsanchez@yahoo.es>
2020-05-24 22:21:18 +0200

    exercise resolved

--- js/index.js
+++ js/index.js
@@ -28,6 +28,11 @@ const cards = [
 const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', event => {
+  // cada vez que se recarga la página, llamamos a la función que baraja las cartas.
+
+  memoryGame.shuffleCards()
+
+  // creamos una variable con html en la que con un forEach, asignamos cada carta a un elemento del DOM
   let html = '';
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
@@ -38,12 +43,56 @@ window.addEventListener('load', event => {
 
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
-
+  
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
       // TODO: write some code here
       console.log(`Card clicked: ${card}`);
+
+      // cada vez que hacemos click, hacemos toggle en la clase 'turned' de la carta clickada.
+
+      card.classList.toggle('turned');
+
+      // metemos la carta clickada en nuestro array pickedCards
+
+      memoryGame.pickedCards.push(card);
+
+      // comprobamos que hay dos cartas en pickedCards
+
+      if (memoryGame.pickedCards.length === 2) {
+
+        // asignamos a una variable "cardN" el nombre de los elementos clickados (2)
+
+        const card1 = memoryGame.pickedCards[0].getAttribute('data-card-name')
+        const card2 = memoryGame.pickedCards[1].getAttribute('data-card-name')
+
+        // comprobamos si son iguales y añadimos a los marcadores en función de si se cumple o no la coincidencia
+
+        if (memoryGame.checkIfPair(card1, card2)) {
+        
+          document.querySelector('#pairs-guessed').innerHTML = memoryGame.pairsGuessed;
+          document.querySelector('#pairs-clicked').innerHTML = memoryGame.pairsClicked;
+
+        // reiniciamos el array pickedCards
+          memoryGame.pickedCards = [];
+
+        // comprobamos si el ljuego ha terminado
+          if (memoryGame.isFinished()) {
+            setTimeout(() => {
+              alert("You're a real SUPERHERO!")
+            }, 1000);
+          }
+
+        } else {
+          document.querySelector('#pairs-clicked').innerHTML = memoryGame.pairsClicked;
+          setTimeout(() => {
+            memoryGame.pickedCards.forEach(card => card.classList.toggle('turned'));
+            memoryGame.pickedCards = [];
+          }, 1000)
+        }
+      }
+
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,44 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+   // función con el algoritmo Fisher-Yates para mezclar un array aleatoriamente.
+
+
+  shuffleCards() {
+   
+    for (let i = this.cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
+
+  // función que compara si las cartas son siguales.
+
+  checkIfPair(card1, card2) {
+    
+    this.pairsClicked += 1
+    if (card1 === card2) {
+      this.pairsGuessed += 1
+      return true
+    } else {
+      return false
+    }
+  }
+
+  // función que comprueba si el juego ha terminado.
+
+  isFinished() {
+
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true
+    } else {
+      return false
+    }
+  }
 }
