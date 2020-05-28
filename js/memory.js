c904b20e628d783eb0b81f80b2f760aaea61df4f
TL Garcia <tomas.bl.garcia@gmail.com>
2020-05-28 22:27:47 +0200

    Finished It2. All tests passed so far but could optimize shuffling algorithm.

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,38 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = null;
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
+
+
+  shuffleCards() {
+    let rand = null;
+    let temp = null;
+
+    for (let i = 0; i < this.cards.length; i++) {
+      rand = Math.floor(Math.random() * (this.cards.length - i) + i);
+
+      temp = this.cards[i];
+      this.cards[i] = this.cards[rand];
+      this.cards[rand] = temp;
+    }
+  }
+
+  
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+
+
   isFinished() {}
 }
