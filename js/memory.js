dc20a2337cb22d218a706c8734a29d12871ea68a
Irma Blanco <irblancofdz@gmail.com>
2020-05-30 20:07:16 +0200

    feat: isFinished

--- js/memory.js
+++ js/memory.js
@@ -9,17 +9,26 @@ class MemoryGame {
 
   shuffleCards() {
     // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-    const shuffled = [];
+    const shuffledDeck = [];
     // While there remain elements to shuffle...
     while (0 !== this.cards.length) {
       // Pick a remaining element...
       const random = Math.floor(Math.random() * this.cards.length);
-      shuffled.push(this.cards[random]);
+      shuffledDeck.push(this.cards[random]);
       this.cards = this.cards.filter(card => card !== this.cards[random]);
     }
-    this.cards = shuffled;
+    this.cards = shuffledDeck;
   }
 
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
+  }
+  isFinished() {
+    return this.pairsGuessed === this.cards.length / 2;
+  }
 }
