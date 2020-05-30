cba78c7314b95dceebbc3a52c051d2c4181c7b6c
Irma Blanco <irblancofdz@gmail.com>
2020-05-30 19:48:41 +0200

    feat: shuffleCards

--- js/memory.js
+++ js/memory.js
@@ -1,12 +1,25 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
     this.pickedCards = [];
-    this.pairsClicked  = 0;
+    this.pairsClicked = 0;
     this.pairsGuessed = 0;
 
   }
-  shuffleCards() {}
+
+  shuffleCards() {
+    // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    const shuffled = [];
+    // While there remain elements to shuffle...
+    while (0 !== this.cards.length) {
+      // Pick a remaining element...
+      const random = Math.floor(Math.random() * this.cards.length);
+      shuffled.push(this.cards[random]);
+      this.cards = this.cards.filter(card => card !== this.cards[random]);
+    }
+    this.cards = shuffled;
+  }
+
   checkIfPair(card1, card2) {}
   isFinished() {}
 }
