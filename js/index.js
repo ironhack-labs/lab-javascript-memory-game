695422fd517e19917d0936c757391c820e6cd684
Victor Sanchez <victor1305@icloud.com>
2020-06-09 17:37:21 +0200

    PartialDone

--- js/index.js
+++ js/index.js
@@ -43,7 +43,9 @@ window.addEventListener('load', event => {
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      
+      console.log(`Card clicked: ${card}`)
+      card.classList.toggle("turned")
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,49 @@
 class MemoryGame {
-  constructor(cards){
-    this.cards = cards;
+  constructor(cards) {
+    this.cards = cards
     // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    if (!this.cards) return undefined
+
+    for (let i = 0; i < this.cards.length; i++) {
+      const num = Math.floor(Math.random() * i)
+      const tempArray = this.cards[i]
+      this.cards[i] = this.cards[num]
+      this.cards[num] = tempArray
+    }
+  }
+  
+  checkIfPair(card1, card2) { 
+
+    this.pairsClicked += 1
+
+    if (card1 === card2) {
+      this.pairsGuessed += 1
+      return true
+
+    } else {
+
+      return false
+    }
+    
+  }
+  isFinished() { 
+
+    let arrayCards = this.cards.length / 2
+
+    if (this.pairsGuessed === arrayCards){
+
+      return true
+
+    } else {
+
+      return false
+    }
+  }
 }
