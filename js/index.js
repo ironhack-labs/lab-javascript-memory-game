1c307c6c89ab31c7797e8480bc30d185446b22a4
Roberto Gonzalez <rob.gonzalez3@gmail.com>
2020-06-09 17:39:31 +0200

    Done without DOM

--- js/index.js
+++ js/index.js
@@ -47,3 +47,4 @@ window.addEventListener('load', event => {
     });
   });
 });
+
--- js/memory.js
+++ js/memory.js
@@ -2,8 +2,31 @@ class MemoryGame {
   constructor(cards){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [ ]
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    this.cards.forEach((elm, i) => {
+      let cardShuffled = Math.floor(Math.random() * this.cards.length)
+      if (i != cardShuffled) { 
+        let shuffleCards =elm
+        elm = this.cards[cardShuffled]
+        this.cards[cardShuffled] = shuffleCards
+      }
+    })
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++
+    if (card1 === card2) {
+      this.pairsGuessed++
+      return true 
+    } else {
+      return false
+    }
+  }
+  isFinished() {
+    return (this.cards.length / 2) === this.pairsGuessed
+  }
+  
 }
