63f3a198ae0379d065df6ec60c3763b6aa19120b
Axel Chauvin <axelchauvin@gmail.com>
2020-06-19 16:17:40 +0200

    Completed memory game

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import {MemoryGame} from "/js/memory.js";
+
 const cards = [
   { name: 'aquaman', img: 'aquaman.jpg' },
   { name: 'batman', img: 'batman.jpg' },
@@ -26,8 +28,73 @@ const cards = [
 ];
 
 const memoryGame = new MemoryGame(cards);
+memoryGame.shuffleCards();
 
-window.addEventListener('load', event => {
+function flipBackCarts(){
+  setTimeout( () => {
+    memoryGame.pickedCards[0].classList.remove("turned");
+    memoryGame.pickedCards[1].classList.remove("turned");
+    memoryGame.pickedCards = [];
+  }, 1500)
+}
+
+function updateUiScore(){
+  const clickedSpan = document.getElementById("pairs-clicked");
+  const guessedSpan = document.getElementById("pairs-guessed");
+  clickedSpan.textContent = String(memoryGame.pairsClicked);
+  guessedSpan.textContent = String(memoryGame.pairsGuessed);
+}
+
+function endGame(){
+  document.querySelectorAll(".card").forEach( card => {
+    card.classList.add("hide");
+  });
+  setTimeout( () => {
+    document.querySelectorAll(".card").forEach( card => {
+      card.style.display = "none";
+    });
+  }, 3000);
+  document.getElementById("end-game-box").style.display = "flex";
+  document.getElementById("end-game-box").classList.remove("hide");
+}
+
+function checkCards(){
+  const cardName1 = memoryGame.pickedCards[0].getAttribute("data-card-name");
+  const cardName2 = memoryGame.pickedCards[1].getAttribute("data-card-name");
+  if(memoryGame.checkIfPair(cardName1, cardName2)){
+    memoryGame.pickedCards = [];
+    if(memoryGame.isFinished()) endGame();
+  } else {
+    flipBackCarts();
+  }
+  updateUiScore();
+}
+
+function flipCard(){
+  if(memoryGame.pickedCards.length != 2){
+    this.classList.add("turned");
+  memoryGame.pickedCards.push(this);
+  if(memoryGame.pickedCards.length === 2) checkCards();
+  }
+}
+
+function resetGame(){
+  document.querySelectorAll(".card").forEach( card => {
+    card.classList.remove("hide", "turned");
+  });
+  document.getElementById("end-game-box").classList.add("hide");
+
+  memoryGame.reset();
+  updateUiScore();
+  
+  document.querySelectorAll(".card").forEach( card => {
+    card.style.display = "";
+  });
+  document.getElementById("end-game-box").style.display = "none";
+
+}
+
+function start() {
   let html = '';
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
@@ -37,13 +104,16 @@ window.addEventListener('load', event => {
   });
 
   // Add all the divs to the HTML
-  document.querySelector('#memory-board').innerHTML = html;
+  document.querySelector('#memory-board').innerHTML += html;
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
+    card.onclick = flipCard;
   });
-});
+
+  document.getElementById("reset-game").onclick = resetGame;
+
+};
+
+
+start();
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,36 @@
-class MemoryGame {
+export class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    for(let i = 0; i <= this.cards.length -2; i++){
+      const j = Math.floor(Math.random() * this.cards.length);
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked ++;
+    if(card1 === card2) {
+      this.pairsGuessed ++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    if(this.pairsGuessed === this.cards.length/2) return true;
+    else return false;
+  }
+
+  reset(){
+    this.shuffleCards();
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+  }
 }
