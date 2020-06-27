5bb030edb3a4d2eb95ec78d1312e966ffc02c617
Alex Ugodnikov <alex.ugodnikov@gmail.com>
2020-06-27 16:52:49 -0400

    Backend logic completed, tests are passing

--- js/memory.js
+++ js/memory.js
@@ -2,8 +2,30 @@ class MemoryGame {
   constructor(cards){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    this.numberOfCards = cards.length;  //24 
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
-}
+  shuffleCards() {
+    let randomCard;
+    let newArr = []
+    for (let i = 0; i<this.numberOfCards; i++) {
+      randomCard = Math.floor(Math.random() * this.cards.length);
+      newArr.push(this.cards[randomCard]);
+      this.cards.splice(randomCard,1);
+      //console.log("i:", i, "Rand:", randomCard, " Length:", this.cards.length);
+    }
+    this.cards = newArr;
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }return false;
+  }
+  isFinished() {
+    return this.pairsGuessed === this.numberOfCards / 2 ? true : false;
+    }
+  }
