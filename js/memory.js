c89577259ab0a6a17a4c8e2affffb35896d5017f
Alejandro Garcia <agarc371@gmail.com>
2020-06-29 17:36:50 -0400

    add constructor methods

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,36 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards(cards) {
+    if (typeof (cards) != 'object') {
+      return undefined;
+    }
+    const cardsArray = cards.length;
+    let temp, index;
+
+    while (cardsArray) {
+      index = Math.floor(Math.random() * cardsArray--);
+
+      temp = cards[cardsArray];
+      cards[cardsArray] = cards[index];
+      cards[index] = temp;
+    }
+    return cards;
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1.name === card2.name) {
+      this.pairsGuessed++;
+      return true;
+    } else return false;
+  }
+
+  isFinished() {
+    const totalPairs = this.cards.length / 2;
+    this.pairsGuessed === totalPairs ? true : false;
+  }
 }
