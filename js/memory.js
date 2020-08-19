c2f48a61ff7a86d7806ea39d5473cea1b4dcbe86
Olesya Steinmetz <newsabome@icloud.com>
2020-08-19 18:00:06 +0200

    WDFT-Berlin-Olesya

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,37 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards, pickedCards, pairsClicked, pairsGuessed){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  
+  shuffleCards() {
+  let randomCardA;
+  let randomCardB;
+  let tempX;
+  for (let index = 0; index < this.cards.length; index += 1) {
+    randomCardA = Math.floor(Math.random() * this.cards.length);
+    randomCardB = Math.floor(Math.random() * this.cards.length);
+    tempX = this.cards[randomCardA];
+    this.cards[randomCardA] = this.cards[randomCardB];
+    this.cards[randomCardB] = tempX;
+  }
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1
+      if (card1 !== card2){
+        return false
+      }
+    this.pairsGuessed += 1
+    return true
+  }
+
+  isFinished() {
+    if(this.pairsGuessed === this.cards.length/2){
+      return true
+    } 
+  return false
+  }
 }
