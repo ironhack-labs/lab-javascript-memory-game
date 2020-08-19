5eea3e89a376260765d1fb8a9ea98add6dc7b9ee
Tolga Kara <tolgakara2.0@gmail.com>
2020-08-19 16:42:50 +0200

    Logic in memory done

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,32 @@
 class MemoryGame {
-  constructor(cards){
-    this.cards = cards;
-    // add the rest of the class properties here
-  }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+	constructor(cards) {
+		this.cards = cards
+
+		// add the rest of the class properties here
+		this.pickedCards = []
+		this.pairsClicked = 0
+		this.pairsGuessed = 0
+	}
+	shuffleCards() {
+		for (let i = this.cards.length - 1; i > 1; i--) {
+			let j = Math.round(Math.random() * i)
+			let tempCard = this.cards[i]
+			this.cards[i] = this.cards[j]
+			this.cards[j] = tempCard
+		}
+	}
+
+	checkIfPair(card1, card2) {
+		this.pairsClicked++
+		if (card1 === card2) {
+			this.pairsGuessed += 1
+			return true
+		}
+		return card1 === card2
+	}
+
+	isFinished() {
+		let isPairsGuest = this.pairsGuessed >= this.cards.length / 2
+		return isPairsGuest
+	}
 }
