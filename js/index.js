3c33b134131bf5f2c0279c8715e97b4cc9902cfb
Tolga Kara <tolgakara2.0@gmail.com>
2020-08-19 18:44:59 +0200

    done

--- js/index.js
+++ js/index.js
@@ -24,9 +24,8 @@ const cards = [
 	{ name: "the avengers", img: "the-avengers.jpg" },
 	{ name: "thor", img: "thor.jpg" },
 ]
-
 const memoryGame = new MemoryGame(cards)
-
+memoryGame.shuffleCards()
 window.addEventListener("load", (event) => {
 	let html = ""
 	memoryGame.cards.forEach((pic) => {
@@ -35,17 +34,36 @@ window.addEventListener("load", (event) => {
 		html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`
 		html += `</div>`
 	})
-
 	// Add all the divs to the HTML
 	document.querySelector("#memory-board").innerHTML = html
-
 	// Bind the click event of each element to a function
 	document.querySelectorAll(".card").forEach((card) => {
-		card.addEventListener("click", () => {
-			// TODO: write some code
-			let cardBack = card.querySelector(".back")
-
-			console.log(`Card clicked: ${cardBack}`)
+		card.addEventListener("click", (event) => {
+			// TODO: write some code here
+			memoryGame.pickedCards.push(event.target.parentNode)
+			console.log(event.target.parentNode)
+			card.classList.add("turned")
+			console.log(memoryGame.pickedCards)
+			if (memoryGame.pickedCards.length == 2) {
+				setTimeout(() => {
+					let card1 = memoryGame.pickedCards[0].getAttribute("data-card-name")
+					let card2 = memoryGame.pickedCards[1].getAttribute("data-card-name")
+					document.querySelector("#pairs-clicked").innerHTML = memoryGame.pairsClicked
+					console.log(card1, card2)
+					if (!memoryGame.checkIfPair(card1, card2)) {
+						console.log("Here")
+						memoryGame.pickedCards.forEach((card) => card.classList.remove("turned"))
+					} else {
+						memoryGame.pickedCards.forEach((card) => card.classList.add("blocked"))
+						if (memoryGame.isFinished()) {
+							alert("You won")
+						}
+						document.querySelector("#pairs-guessed").innerHTML = memoryGame.pairsGuessed
+						console.log("match")
+					}
+					memoryGame.pickedCards = []
+				}, 1000)
+			}
 		})
 	})
 })
