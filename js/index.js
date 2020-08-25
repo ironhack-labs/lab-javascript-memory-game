6ec16416d818b912f9e383726b51f50175699ced
Javier de Salas <chinostrike@gmail.com>
2020-08-26 00:21:19 +0200

    Finished and working

--- js/index.js
+++ js/index.js
@@ -28,7 +28,10 @@ const cards = [
 const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', event => {
+  memoryGame.shuffleCards(cards)
+
   let html = '';
+  
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
     html += `<div class="back" name="${pic.img}"></div>`;
@@ -38,12 +41,72 @@ window.addEventListener('load', event => {
 
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
+ 
+
+  let clickedArray = ['','']
+  let firstCard
+  let clickedScore = document.querySelector('#pairs-clicked')
+  let guessedScore = document.querySelector('#pairs-guessed')
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
+    card.onclick = function() {
+
+      this.classList.add('turned') // Clicas y Hacer que gire 
+
+
+
+      if (!clickedArray[0]) { // Si es la primera carta 
+        clickedArray[0] = this.dataset.cardName // Saber qué carta es y guardarla en clickedArray=[card1, card2] para compararla con la siguiente 
+        firstCard = this //hago un selector para poder cambiar las propiedades de la carta después
+      }
+
+      else {
+        clickedArray[1] = this.dataset.cardName // Clicas la segunda carta  
+        
+        secondCard = this
+        
+          if (memoryGame.checkIfPair(clickedArray[0],clickedArray[1])) { //si aciertas
+
+            clickedArray = ['','']
+
+          }
+
+          else { 
+
+            setTimeout(function() { //giramos de nuevo las cartas
+              firstCard.classList.remove('turned')
+              secondCard.classList.remove('turned')
+              console.log(memoryGame.pairsClicked)
+              clickedArray = ['','']              
+            }, 1500)
+          
+          }   
+        clickedScore.innerText = memoryGame.pairsClicked //actualizamos puntuación
+        guessedScore.innerText = memoryGame.pairsGuessed
+        if (memoryGame.isFinished()) {
+          window.alert ('Enhorabuena, has ganado')
+        }
+
+      }
+    
+
+
+    console.log(`Card clicked: ${card}`);
+    }
   });
+
+    //     // TODO: write some code here
+
+  //       // Clicas la primera carta      
+  //       // Hacer que gire      
+  //       // Saber qué carta es y guardarla en clickedArray=[indexOf(card1), card2] para compararla con la siguiente
+  //       // Mantenerla girada
+  //       // Clicas la segunda carta 
+  //       // Comparar las cartas (checkIfPair)
+  //       // Actualizar las puntuaciones en pantalla
+  //       // Si checkIfPair true tenemos que cambiar el css de las cartas para que no se giren
+  //             // isFinished
+  //       // Si checkIf Pair false tenemos que girar las cartas y vaciar el comparador
 });
+
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,58 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [] ;
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  
+  // No pasa Jasmine pero devuelve el array randomizado https://codepen.io/chinostrike/pen/GRZWMRw
+  shuffleCards(cards) {
+      if (!cards) {
+        return undefined
+      }
+  
+      for (let i = 0; i < cards.length ; i++) {
+        let j = Math.floor(Math.random() * cards.length)
+        let temp = cards[i]
+        cards[i] = cards[j]
+        cards[j] = temp 
+  
+      }
+      return cards
+  }
+  
+    //   INTENTAMOS HACER EL SHUFFLE USANDO SORT PERO NO PASABA EL TEST
+    //   for (let i = 0; i < cards.length; i++) {
+    //   cards.sort((a,b) => {
+    //     const swap = Math.random()
+    //     if (swap > 0,5) {
+    //       return 1
+    //     }
+    //     else {
+    //       return -1
+    //     }
+    //   })
+    // }
+  
+  checkIfPair(card1, card2) {
+    this.pairsClicked++
+console.log(card1, card2)
+    if(card1 === card2) {
+      this.pairsGuessed++
+      return true
+    }
+    else{
+      return false
+    }
+  }
+
+  isFinished() {
+    if (2 * this.pairsGuessed == this.cards.length) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
 }
