68f7388023b0bc6d34366e2315d8c9a09e970ff8
root <root@DESKTOP-53RFEB6.localdomain>
2020-08-25 20:44:37 +0200

    [WEB-MAD-PP] Amanda Ordonez

--- js/index.js
+++ js/index.js
@@ -29,7 +29,8 @@ const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', event => {
   let html = '';
-  memoryGame.cards.forEach(pic => {
+  // Change the memory game cards for the suffleCards function that return the array shuffle
+  memoryGame.shuffleCards().forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
     html += `<div class="back" name="${pic.img}"></div>`;
     html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
@@ -41,9 +42,21 @@ window.addEventListener('load', event => {
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
+    
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      card.classList.add("turned")
+      let cardName = card.getAttribute("data-card-name")
+      memoryGame.pickedCards.unshift(cardName)
+      // if (memoryGame.pickedCards.length % 2== 0) {
+      //   memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1])
+
+      //   if (memoryGame.pairsGuessed === 1) {
+      //   card.classList.add("blocked")
+      //   memoryGame.pairsClicked = 0
+      // }
+
+      // console.log(`Card clicked: ${card}`, );
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,44 @@
 class MemoryGame {
   constructor(cards){
-    this.cards = cards;
-    // add the rest of the class properties here
+    this.cards = cards,
+    this.pickedCards = [],
+    this.pairsClicked = 0,
+    this.pairsGuessed = 0
+
+    
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    
+    if (this.cards.length === 0) {
+      return undefined
+    }
+    
+      for (let i = this.cards.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = this.cards[i];
+        this.cards[i] = this.cards[j];
+        this.cards[j] = temp;
+      }
+  return this.cards
+    
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked = 1
+    if (card1 === card2) {
+      this.pairsGuessed = 1
+      return true
+    } else {
+      return false
+    }
+  }
+  isFinished() {
+    if (this.pairsClicked === 0) {
+      return false
+    } else if (this.pairsGuessed !== this.cards.length / 2) {
+      return false
+
+    } else if (this.pairsGuessed === this.cards.length / 2) {
+      return true
+    }
+  }
 }
