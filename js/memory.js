f41ba4f6704047958a2104aeceae7ee6738d2d98
Daniela Gutiérrez <dangtz14@gmail.com>
2020-10-10 17:07:06 -0500

    Web-Mex-PT2 Daniela

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,33 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards(cards) {
+    for (let i = this.cards.length -1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      let shuffle = this.cards[i];
+       this.cards[i] = this.cards[j];
+       this.cards[j] = shuffle;
+    };
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked +=1;
+    if (card1 === card2) {
+      this.pairsGuessed +=1;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    if(this.pairsGuessed ===(this.cards.length/2)){
+      return true;
+    } else{
+      return false;
+    }
+  }
 }
