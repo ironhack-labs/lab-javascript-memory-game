f29d3d5ca8a7b4d69ce9331a03a78bd556f5605e
JuanBatkis <juanbatkis@gmail.com>
2020-10-10 22:28:46 -0300

    Deleted console log

--- js/memory.js
+++ js/memory.js
@@ -8,8 +8,6 @@ class MemoryGame {
 
   shuffleCards() {
 
-    console.log('hola')
-
     for (let i = this.cards.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
