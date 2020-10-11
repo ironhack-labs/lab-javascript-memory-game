6835f69d61c23f39196a71fc7d4d2ef77496b22f
Zhenni Qian <zhenni.qian913@gmail.com>
2020-10-11 14:11:06 +0200

    game done

--- js/index.js
+++ js/index.js
@@ -27,23 +27,96 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
-window.addEventListener('load', event => {
-  let html = '';
-  memoryGame.cards.forEach(pic => {
-    html += `<div class="card" data-card-name="${pic.name}">`;
-    html += `<div class="back" name="${pic.img}"></div>`;
-    html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
-    html += `</div>`;
-  });
-
-  // Add all the divs to the HTML
-  document.querySelector('#memory-board').innerHTML = html;
-
-  // Bind the click event of each element to a function
-  document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
-  });
+window.addEventListener('DOMContentLoaded', event => {
+    addAllCards();
 });
+
+function addAllCards() {
+    let html = '';
+    memoryGame.cards.forEach(pic => {
+      html += `<div class="card" data-card-name="${pic.name}">`;
+      html += `<div class="back" name="${pic.img}"></div>`;
+      html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
+      html += `</div>`;
+    });
+  
+    document.querySelector('#memory-board').innerHTML = html;
+  
+    document.querySelectorAll('.card').forEach(card => {
+      card.addEventListener('click', onClickCard);
+    });
+}
+
+function removeAllCards() {
+    document.querySelector('#memory-board').innerHTML = '';
+}
+
+function onClickCard(event) {
+    if (memoryGame.isFinished()) {
+        return;
+    }
+
+    const card = event.currentTarget;
+    flipCard(card);
+    updateScore();
+    if (memoryGame.isFinished()) {
+        onWin();
+    }
+}
+
+function flipCard(card) {
+    const pickedCards = memoryGame.pickedCards;
+    
+    card.classList.add('turned');
+    pickedCards.push(card.getAttribute('data-card-name'));
+
+    if (pickedCards.length !== 2) {
+        return;
+    }
+
+    const result = memoryGame.checkIfPair(pickedCards[0], pickedCards[1]);
+    const firstCard = document.querySelector(`.turned[data-card-name="${pickedCards[0]}"]`);
+    
+    if (result) {
+        firstCard.classList.add('blocked');
+        card.classList.add('blocked');
+    } else {
+        flipCardBack(firstCard);
+        flipCardBack(card);
+    }
+
+    memoryGame.pickedCards = [];
+}
+
+function flipCardBack(card) {
+    setTimeout(function() {
+        card.classList.remove('turned');
+    }, 1000);
+}
+
+function updateScore() {
+    document.getElementById('pairs-clicked').textContent = memoryGame.pairsClicked;
+    document.getElementById('pairs-guessed').textContent = memoryGame.pairsGuessed;
+}
+
+function onWin() {
+    const FLIP_DELAY = 200;
+    const FLIP_DURATION = 600;
+
+    setTimeout(() => {
+        alert(`Congratualations! You've won!`);
+        
+        document.querySelectorAll('.card.turned').forEach((card, index) => {
+            setTimeout(function() {
+                flipCardBack(card);
+            }, index * FLIP_DELAY);
+        });
+
+        setTimeout(() => {
+            memoryGame.resetGame();
+            removeAllCards();
+            updateScore();
+            addAllCards();
+        }, FLIP_DELAY * memoryGame.cards.length + FLIP_DURATION * 2);
+    }, 500);    
+}
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,50 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.initializeGame();
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  initializeGame() {
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    this.shuffleCards();
+  }
+
+  shuffleCards() {
+    let currentIndex = this.cards.length;
+    let temporaryValue;
+    let randomIndex;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = this.cards[currentIndex];
+      this.cards[currentIndex] = this.cards[randomIndex];
+      this.cards[randomIndex] = temporaryValue;
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    if (typeof card1 === "string" && typeof card2 === "string") {
+      this.pairsClicked += 1;
+    }
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+    }
+    return card1 === card2;
+  }
+
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length/2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  resetGame() {
+    this.initializeGame();
+  }
 }
