eab6938c2161735ebc0529a9f12cebd98420b856
Dirk Kelderman <dirkkelderman@gmail.com>
2020-10-11 14:35:04 +0200

    got the logic done, HTML/CSS not

--- js/index.js
+++ js/index.js
@@ -28,6 +28,7 @@ const cards = [
 const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', event => {
+  
   let html = '';
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
