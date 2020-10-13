95c032ecddf65b0494e3b95515c066a1ca28ad1b
Dirk Kelderman <dirkkelderman@gmail.com>
2020-10-13 16:25:00 +0200

    almost done

--- js/index.js
+++ js/index.js
@@ -43,7 +43,40 @@ window.addEventListener('load', event => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
+      card.classList.toggle('turned');
+
+      memoryGame.pickedCards.push(card);
+      
+      console.log(memoryGame.pickedCards)
+
+      if (memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1])) {
+        
+      };
+
+      console.log(memoryGame.pairsGuessed);
+      console.log(memoryGame.pairsClicked);
+
+      // if (memoryGame.pickedCards.length >= 2) {
+      //   memoryGame.pickedCards = [];
+      // }
+
+      // let clickedCards = document.getElementsByClassName('turned');
+
+      // memoryGame.checkIfPair(clickedCards, clickedCards);
+
+      // console.log(memoryGame.pickedCards);
+
+      // let clicked = document.getElementById('pairs-clicked');
+      // let guessed = document.getElementById('pairs-guessed');
+
+      // if (memoryGame.checkIfPair(clickedCards, clickedCards) === false) {
+      //   clicked.innerHTML = memoryGame.pairsClicked; 
+      // } else if (memoryGame.checkIfPair(clickedCards, clickedCards) === true) {
+      //   guessed.innerHTML = memoryGame.pairsGuessed;
+      // }
+
+
+
       console.log(`Card clicked: ${card}`);
     });
   });
