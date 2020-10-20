348fd389d92d6514d8ee87970f0bc0508fd99a48
Dennis Deuling <dennis.deuling@gmail.com>
2020-10-20 18:18:30 +0200

    done

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,39 @@
 class MemoryGame {
-  constructor(cards){
-    this.cards = cards;
-    // add the rest of the class properties here
-  }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+    constructor(cards) {
+        this.cards = cards;
+        this.pickedCards = [];
+        this.pairsClicked = 0;
+        this.pairsGuessed = 0;
+    }
+    shuffleCards(array) {
+        const currentIndex = array.length;
+        let tempValue, randomIndex;
+
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            tempValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = tempValue;
+        }
+        return array;
+    }
+
+    checkIfPair(card1, card2) {
+        this.pairsClicked++;
+        if (!card1.localeCompare(card2)) {
+            this.pairsGuessed++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    isFinished() {
+        if (this.pairsGuessed == (this.cards.length / 2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
