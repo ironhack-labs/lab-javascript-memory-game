cbe25359a5f0542adbda055cc271f44802cdb823
Emma Northridge <emmanorthridge1@gmail.com>
2020-10-21 18:27:57 +0200

    Logic applied

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,40 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    let j, x, i;
+
+    if (!this.cards) {
+      return undefined;
+    }
+    for (i = this.cards.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = x;
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      this.pairsClicked += 1;
+      return true;
+    } else {
+      this.pairsClicked += 1;
+      return false;
+    }
+  }
+
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
