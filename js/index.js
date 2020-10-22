e6e0f61b77d6fb42661ad86d50d15a9c13c6e476
Maya Ausset <mayaausset@gmail.com>
2020-10-22 14:25:36 +0200

    almost done

--- js/index.js
+++ js/index.js
@@ -42,8 +42,28 @@ window.addEventListener('load', event => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
-      
-      console.log(`Card clicked: ${card}`);
+      let cardName = card.getAttribute("data-card-name");
+      console.log(`card picked is ${cardName}`);
+      card.querySelectorAll('.card div').forEach(div =>{
+        div.classList.toggle("front")
+        div.classList.toggle("back")
+      })
+      memoryGame.pickedCards.push(cardName)
+      console.log(memoryGame.pickedCards)
+      let card1 = memoryGame.pickedCards[0];
+      let card2 = memoryGame.pickedCards[1];
+      //console.log(memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1]))
+      if (memoryGame.checkIfPair(card1, card2) === true){
+        console.log(`it is a match !`)
+        let pairsGuessed = document.getElementById("pairs-guessed")
+        pairsGuessed.innerHTML++;
+        memoryGame.pickedCards.length = 0
+        let pairsClicked = document.getElementById("pairs-clicked") ;
+        pairsClicked.innerHTML++;
+      }else if (memoryGame.checkIfPair(card1, card2) === false){
+        let pairsClicked = document.getElementById("pairs-clicked") ;
+        pairsClicked.innerHTML++;
+      }
     });
   });
 });
