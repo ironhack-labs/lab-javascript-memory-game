de81d47e14baddd9ac35bd2887705f597a61c1f1
Sabela <sabela.ons2@gmail.com>
2020-10-22 14:16:02 +0200

    memory.js

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,46 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards, pickedCards, pairsClicked, pairsGuessed) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = pickedCards;
+    this.pairsClicked = pairsClicked;
+    this.pairsGuessed = pairsGuessed;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    var currentIndex = this.cards.length,
+      temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = this.cards[currentIndex];
+      this.cards[currentIndex] = this.cards[randomIndex];
+      this.cards[randomIndex] = temporaryValue;
+    }
+
+    return this.cards;
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1.name === card2.name) {
+      this.pairsGuessed++;
+      return true
+    } else {
+      return false
+    };
+
+
+  }
+  isFinished() {
+    if (this.pairsGuessed === this.cards / 2)
+  }
 }
+
+shuffle(cards);
+console.log(cards);
