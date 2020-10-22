cb06b5a1561b5b08d29416e615ed57f85994b036
Rich Alossi <alossix@gmail.com>
2020-10-22 16:27:13 +0200

    Initial commit - shuffle function working

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,27 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    // this.pickedCards = pickedCards;
+    // this.pairsClicked = pairsClicked;
+    // this.pairsGuessed = pairsGuessed;
   }
-  shuffleCards() {}
+  shuffleCards(cardsObj) {
+    if (!cardsObj) {
+      return undefined;
+    }
+    for (let i = 0; i < cardsObj.length; i++) {
+      let rand = Math.floor(Math.random() * cardsObj.length);
+      cardsObj.push(cardsObj[rand]);
+      cardsObj.splice(rand, 1);
+    }
+  }
   checkIfPair(card1, card2) {}
   isFinished() {}
+}
+
+let cardsArrTemp = cardsArr;
+for (let i = 0; i < cardsArrTemp.length; i++) {
+  let rand = Math.floor(Math.random() * cardsArrTemp.length);
+  cardsArrTemp.push(cardsArr[rand]);
+  cardsArr.splice(rand, 1);
 }
