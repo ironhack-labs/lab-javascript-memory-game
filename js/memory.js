690b66814e77fd645c2b1e9ac802fee334401c34
Irene <ihatepineapple@gmail.com>
2020-10-22 23:00:31 +0200

    logic almost done

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,44 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  
+  shuffleCards(cards) {
+    // for(let i = this.cards.length — 1; i > 0; i--){
+    //   const j = Math.floor(Math.random() * i)
+    //   const temp = this.cards[i]
+    //   this.cards[i] = this.cards[j]
+    //   this.cards[j] = temp
+    // } 
+    // return temp;
+    
+  }
+
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++
+    if (card1 === card2) {
+      this.pairsGuessed++
+      return true;
+
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    // false at the begnning
+    // false some pairs still to be guessed
+    // true all pairs guessed
+    // if (this.pairsClicked === 0){
+    //   return false;
+    // } else if (this.pairsGuessed !==0){
+    //   return false;
+    // } else if (this.pairsGuessed === this.cards.length / 2){
+    //   return true;
+    // }
+    return this.cards.length/2 == this.pairsGuessed ? true : false;
+  }
 }
