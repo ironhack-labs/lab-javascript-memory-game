cb3f149f2776e447fb316203c2b65b7306ebfb6b
Abbie <abbie@summers.gg>
2020-10-22 23:08:31 +0200

    Need help with shuffleCards and isFinished

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,33 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards(array){}
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked ++;
+    if (card1 === card2) {
+      this.pairsGuessed ++;
+      return true
+    } else {
+      return false
+    }
+  }
+  isFinished() {
+    if(this.pairsGuessed === 12) {
+      //You Win
+      return true }
+    if(this.pairsClicked === 12) {
+      //You lose?
+    return false
+    } else {
+    return false
+    //The games continues
+    }
+  }
 }
