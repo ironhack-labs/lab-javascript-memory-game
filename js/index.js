673fdf759137bacc32093873cfd5c3c3c174f50f
Issa Nikole Gruber <issanikolegruber@Issas-MacBook-Pro.local>
2020-10-23 16:06:01 +0200

    done

--- js/index.js
+++ js/index.js
@@ -43,6 +43,45 @@ window.addEventListener('load', event => {
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
       // TODO: write some code here
+
+      card.querySelectorAll('.card div').forEach(div => {
+        div.classList.toggle('front');
+        div.classList.toggle('back');
+      })
+
+      memoryGame.pickedCards.push(card)
+ 
+      if(memoryGame.pickedCards.length === 2) {
+
+        const firstCard = memoryGame.pickedCards[0]
+        const secondCard = memoryGame.pickedCards[1]
+
+        if(memoryGame.checkIfPair(memoryGame.pickedCards[0].getAttribute('data-card-name'), memoryGame.pickedCards[1].getAttribute('data-card-name'))) {
+
+          document.getElementById('pairs-guessed').innerHTML = memoryGame.pairsGuessed;
+          document.getElementById('pairs-clicked').innerHTML = memoryGame.pairsClicked;
+          memoryGame.pickedCards = [];
+        } else {
+          document.getElementById('pairs-clicked').innerHTML = memoryGame.pairsClicked;
+          setTimeout(() => {
+            firstCard.querySelectorAll('.card div').forEach(div => {
+              div.classList.toggle('front');
+              div.classList.toggle('back');
+            }),
+            secondCard.querySelectorAll('.card div').forEach(div => {
+              div.classList.toggle('front');
+              div.classList.toggle('back');
+            })
+            memoryGame.pickedCards = [];
+          }, 1000)
+        }
+        
+      }
+      if (memoryGame.isFinished()) {
+        setTimeout(() => {
+          window.alert('Congrats, you won!')
+        }, 1000)
+      }
       console.log(`Card clicked: ${card}`);
     });
   });
