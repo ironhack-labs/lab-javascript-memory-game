00e1d5566adbaf2fc46a446b16236774ef9315ec
Rich Alossi <alossix@gmail.com>
2020-10-23 16:28:18 +0200

    Updates DOM elements; need to work on time delay of animation

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ const memoryGame = new MemoryGame(cards);
 
 window.addEventListener("load", (event) => {
   let html = "";
-  // memoryGame.shuffleCards();
+  memoryGame.shuffleCards();
   memoryGame.cards.forEach((pic) => {
     html += `<div class="card" data-card-name="${pic.name}">`;
     html += `<div class="back" name="${pic.img}"></div>`;
@@ -42,19 +42,23 @@ window.addEventListener("load", (event) => {
 
   // Bind the click event of each element to a function
   let pickedCards = memoryGame.pickedCards;
+  let clicked = document.querySelector("#pairs-clicked");
+  let guessed = document.querySelector("#pairs-guessed");
   document.querySelectorAll(".card").forEach((card) => {
     card.addEventListener("click", function () {
       card.classList.toggle("turned");
       console.log(`Card clicked: ${card}`);
-      console.log(pickedCards.length);
       if (pickedCards.length === 0) {
         pickedCards.push(card);
       } else if (pickedCards.length === 1) {
         pickedCards.push(card);
         if (memoryGame.checkIfPair(pickedCards)) {
           pickedCards.splice(0, pickedCards.length);
+          guessed.innerText = memoryGame.pairsGuessed;
+          clicked.innerText = memoryGame.pairsClicked;
         } else {
-          let timeout = setTimeout(() => {
+          clicked.innerText = memoryGame.pairsClicked;
+          setTimeout(() => {
             pickedCards[0].classList.toggle("turned");
             pickedCards[1].classList.toggle("turned");
             pickedCards.splice(0, pickedCards.length);
