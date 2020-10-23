da14a3a6c8522966cf055b5abc88356a05ba13c2
sophiepa <sophiepa@hotmail.com>
2020-10-23 16:44:34 +0200

    latest fix

--- js/index.js
+++ js/index.js
@@ -48,28 +48,30 @@ window.addEventListener('load', event => {
 
       card.querySelectorAll('.card div').forEach(div => {
         div.classList.toggle('front');
-        div.classList.toggle('back')
+        div.classList.toggle('back');
       })
 
       memoryGame.pickedCards.push(card);
 
+
       if (memoryGame.pickedCards.length === 2) {
-
+        const firstCard = memoryGame.pickedCards[0]
+        const secondCard = memoryGame.pickedCards[1]
         if (memoryGame.checkIfPair(memoryGame.pickedCards[0].getAttribute('data-card-name'), memoryGame.pickedCards[1].getAttribute('data-card-name'))) {
-
           document.getElementById('pairs-guessed').innerHTML = memoryGame.pairsGuessed;
           document.getElementById('pairs-clicked').innerHTML = memoryGame.pairsClicked;
           memoryGame.pickedCards = [];
-
         } else {
           document.getElementById('pairs-clicked').innerHTML = memoryGame.pairsClicked;
           setTimeout(() => {
-            card.querySelectorAll('.card div').forEach(div => {
+            firstCard.querySelectorAll('.card div').forEach(div => {
               div.classList.toggle('front');
               div.classList.toggle('back');
-            }
-
-            )
+            }),
+              secondCard.querySelectorAll('.card div').forEach(div => {
+                div.classList.toggle('front');
+                div.classList.toggle('back');
+              })
             memoryGame.pickedCards = [];
           }, 1000)
         }
@@ -89,26 +91,3 @@ window.addEventListener('load', event => {
   });
 });
 
-/*  card.classList.toggle("turned"); // add "turned" class to turned card
-      // cards need to be pickedCards
-      memoryGame.pickedCards.push(card);
-      //declare card1 and card2 (by their names to compare)
-      if (memoryGame.pickedCards.length === 2) {
-        let card1 = memoryGame.pickedCards[0].getAttribute("data-card-name");
-        let card2 = memoryGame.pickedCards[1].getAttribute("data-card-name");
-      }
-      //check if the cards are a pair,
-      if (memoryGame.checkIfPair(card1, card2)) {
-
-        //let it show in the HTML
-        document.querySelector('#pairs-clicked').innerHTML = memoryGame.pairsClicked
-        document.querySelector('#pairs-guessed').innerHTML = memoryGame.pairsGuessed
-        //set array of pickedCards back to empty
-        memoryGame.pickedCards = []
-
-      } else {
-        console.log("No match")
-        document.querySelector('#pairs-clicked').innerHTML = memoryGame.pairsClicked
-        //set array of pickedCards back to empty
-        memoryGame.pickedCards = []
-      }*/
