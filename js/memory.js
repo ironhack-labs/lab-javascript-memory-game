2d9f26ae02d9f0521bfaec9c8db098aa941f00b0
Javi Fernandez <javierfernandezriv@gmail.com>
2020-10-27 19:25:55 +0100

    Jasmine Finished

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,45 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      let randomIndex = Math.floor(Math.random() * i);
+      let temp = this.cards[i];
+      this.cards[i] = this.cards[randomIndex];
+      this.cards[randomIndex] = temp;
+      }
+  }
+
+  checkIfPair(card1, card2) {
+
+    this.pairsClicked++
+
+    if (card1 === card2) {
+      this.pairsGuessed++
+      return true
+    }
+    else {
+      return false
+    }
+
+  }
+  isFinished() {
+
+    if (this.pairsClicked === 0) {
+      return false
+    }
+    else if (this.pairsGuessed === (this.cards.length/2)){
+      return true
+    }
+    else {
+      return false
+    }
+
+  }
 }
