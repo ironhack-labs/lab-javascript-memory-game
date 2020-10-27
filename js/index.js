3767b94ba13b00df7cfd2f6290a62ff30cb73a19
projav <projav23@gmail.com>
2020-10-27 17:21:33 +0100

    comentarios añadidos

--- js/index.js
+++ js/index.js
@@ -36,8 +36,7 @@ window.addEventListener('load', logicGame())
 
 //Funcion load que hace toda la logica del juego
 function logicGame(){
-  //Mezclar las cartas y actualizar el DOM
-  memoryGame.shuffleCards()
+  memoryGame.shuffleCards()//Mezclar las cartas y actualizar el DOM
   let html = '';
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
@@ -45,50 +44,33 @@ function logicGame(){
     html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
     html += `</div>`;
   });
-  //Añade las cartas al memory board
-    document.querySelector('#memory-board').innerHTML = html;
-    //Seleccionar cada carta
-    document.querySelectorAll('.card').forEach(card => {
-      //Crear evento para el click sobre cada carta
-      card.addEventListener('click', () => {
-        //Girar la carta sobre la que se hace click con la clase turned
-        card.classList.toggle("turned")
-        //Añadir esa carta pulsada en el array pickedCards que está vacio
-        memoryGame.pickedCards.push(card)
-        //Si el array tiene 2 cartas hacer lo que se incluye
-        if(memoryGame.pickedCards.length === 2){
-          //Coger el nombre de las cartas y almacenarlos en dos variables 
-          let carta1 = memoryGame.pickedCards[0].getAttribute("data-card-name");
+    document.querySelector('#memory-board').innerHTML = html;//Añade las cartas al memory board
+    document.querySelectorAll('.card').forEach(card => { //Seleccionar cada carta
+      card.addEventListener('click', () => {//Crear evento para el click sobre cada carta
+        card.classList.toggle("turned")//Girar la carta sobre la que se hace click con la clase turned
+        memoryGame.pickedCards.push(card)//Añadir esa carta pulsada en el array pickedCards que está vacio
+        if(memoryGame.pickedCards.length === 2){//Si el array tiene 2 cartas hacer lo que se incluye 
+          let carta1 = memoryGame.pickedCards[0].getAttribute("data-card-name");//Coger el nombre de las cartas y almacenarlos en dos variables
           let carta2 = memoryGame.pickedCards[1].getAttribute("data-card-name");
-          //Se comparar el nombre de las cartas, si coincide hacer lo del if
-          if (memoryGame.checkIfPair(carta1, carta2)){
+          if (memoryGame.checkIfPair(carta1, carta2)){//Se comparar el nombre de las cartas, si coincide hacer lo del if
             let pairsGuessedText = document.getElementById("pairs-guessed")
-            //Sumar 1 en pares adivinados
-            pairsGuessedText.innerHTML++;
+            pairsGuessedText.innerHTML++;//Sumar 1 en pares adivinados
             let pairsClickedText= document.getElementById("pairs-clicked") ;
-            //Sumar 1 en pares pulsados
+            pairsClickedText.innerHTML++//Sumar 1 en pares pulsados
+            memoryGame.pickedCards.splice(0, memoryGame.pickedCards.length);//Borrar array y dejar vacia
+          }else if (!memoryGame.checkIfPair(carta1, carta2)){//Si no coinciden los nombres de las cartas hacer el if
+            let pairsClickedText= document.getElementById("pairs-clicked") ;//Sumar 1 en pares clickados
             pairsClickedText.innerHTML++
-            //Borrar array y dejar vacia
-            memoryGame.pickedCards.splice(0, memoryGame.pickedCards.length);
-            //Si no coinciden los nombres de las cartas hacer el if
-          }else if (!memoryGame.checkIfPair(carta1, carta2)){
-            //Sumar 1 en pares clickados
-            let pairsClickedText= document.getElementById("pairs-clicked") ;
-            pairsClickedText.innerHTML++
-            //Por cada carta del array hacer que se gire a los 500ms tan solo una vez (setTimeout)
-            memoryGame.pickedCards.forEach(div =>{
+            memoryGame.pickedCards.forEach(div =>{//Por cada carta del array hacer que se gire a los 500ms tan solo una vez (setTimeout)
               setTimeout(()=>{
                 div.classList.toggle("turned")
               },500)
             })
-            //Borrar array
-            memoryGame.pickedCards.splice(0, memoryGame.pickedCards.length);
+            memoryGame.pickedCards.splice(0, memoryGame.pickedCards.length);//Borrar array
             }
           }
-        //Comprobar si el juego ha terminado
-        if(memoryGame.isFinished()){
-          //Mostrar el div donde indica que has ganado y el boton de play Again a los 1000ms
-          setTimeout(() => {
+        if(memoryGame.isFinished()){//Comprobar si el juego ha terminado
+          setTimeout(() => {//Mostrar el div donde indica que has ganado y el boton de play Again a los 1000ms
             divWin.style.visibility = "visible"
           }, 1000);
         }
@@ -97,18 +79,18 @@ function logicGame(){
 }
 //Seleccionar el boton play again y guardarlo en una variable
 let btnPlayAgain = document.querySelector(".playAgain")
-    //Al hacer click en el botón hacer las siguientes:
-    btnPlayAgain.addEventListener("click", ()=>{
-      divWin.style.visibility = "hidden"
-      //Contadores a cero
-      memoryGame.pairsClicked = 0
-      memoryGame.pairsGuessed = 0
-      document.getElementById("pairs-clicked").textContent = memoryGame.pairsClicked
-      document.getElementById("pairs-guessed").textContent = memoryGame.pairsGuessed
-      //Girar todas las cartas porque estarán todas descubiertas si has ganado
-      document.querySelectorAll('.card').forEach(card => {
-        card.classList.toggle("turned")
-      })
-      //Volver a ejecutar toda la logica del juego
-      logicGame()
-    })
+//Al hacer click en el botón hacer las siguientes:
+btnPlayAgain.addEventListener("click", ()=>{
+  divWin.style.visibility = "hidden"
+  //Contadores a cero
+  memoryGame.pairsClicked = 0
+  memoryGame.pairsGuessed = 0
+  document.getElementById("pairs-clicked").textContent = memoryGame.pairsClicked
+  document.getElementById("pairs-guessed").textContent = memoryGame.pairsGuessed
+  //Girar todas las cartas porque estarán todas descubiertas si has ganado
+  document.querySelectorAll('.card').forEach(card => {
+    card.classList.toggle("turned")
+  })
+  //Volver a ejecutar toda la logica del juego
+  logicGame()
+})
