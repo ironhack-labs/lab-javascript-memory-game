30e61c99c4edc6890a80438e7a01c06e82e58ef0
Héctor Carramiñana <hector.crm.hcca@gmail.com>
2020-10-27 22:06:55 +0100

    sin terminar

--- js/index.js
+++ js/index.js
@@ -39,11 +39,15 @@ window.addEventListener('load', event => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
+
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+
+      card.classList.add('turned')
+
+      console.log(`Card clicked: ${{card}}`);
     });
   });
 });
