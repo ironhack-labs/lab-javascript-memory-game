8c0f376ed318867fd0057c3ed182d1de6a208099
Monch87 <montserrat.mosqueda@gmail.com>
2020-10-27 18:48:27 +0100

    commit

--- js/memory.js
+++ js/memory.js
@@ -2,6 +2,9 @@ class MemoryGame {
   constructor(cards){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards 
+    this.pairClicked 
+    this.pairGuessed
   }
   shuffleCards() {}
   checkIfPair(card1, card2) {}
