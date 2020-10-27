5e1d82e4bf5f8b3328ec0d17b9f0fc19a917511d
monibix <monibixv2@gmail.com>
2020-10-27 15:37:04 +0100

    Memory.js bien, index.js hasta donde he llegado...

--- js/index.js
+++ js/index.js
@@ -44,6 +44,35 @@ window.addEventListener('load', event => {
     card.addEventListener('click', () => {
       // TODO: write some code here
       console.log(`Card clicked: ${card}`);
+
+      card.classList.add("turned")
+      let arrOfcards = memoryGame.pickedCards.push(card)
+
+      if (arrOfcards === 2) {
+        let card1 = memoryGame.pickedCards[0].getAttribute("data-card-name")
+        let card2 = memoryGame.pickedCards[1].getAttribute("data-card-name")
+        let card1turned = document.querySelector('.card') 
+          if (card1 === card2) {
+            //console.log("true")
+            memoryGame.pairsGuessed += 1
+            document.querySelector('#pairs-guessed').innerHTML = memoryGame.pairsGuessed
+          } else {
+            //console.log("false")
+            memoryGame.pairsClicked += 1
+            document.querySelector('#pairs-clickd').innerHTML = memoryGame.pairsClicked
+            setInterval(() => {
+              card1turned.classList.remove("turned")
+              card.classList.remove("turned")
+            },5000)
+            arrOfcards = []
+            console.log(arrOfcards)
+          }
+
+      }
+
     });
   });
 });
+//Me ha costado mucho llegar hasta aquí... 
+//Supongo que se podría poner dentro de un bucle for para que hiciera el ciclo cada vez que arrOfcards sea 2. 
+
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,37 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards, pickedCards, pairsClicked, pairsGuessed){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+
+  //2. we need a method to shuffle cards,
+  shuffleCards() {
+    this.cards.sort(() => Math.random()- 0.5) //devolverá un número aleatorio entre -0.5 i 0.5
+  }
+
+    //3.we need a method to compare cards, and
+
+  checkIfPair(card1, card2) {
+    if (card1 === card2) {
+      this.pairsGuessed += 1
+      return true
+    } else {
+      this.pairsClicked += 1
+      return false
+    }   
+  }
+
+    //4.we need a method to check if the game is finished.
+
+  isFinished() {
+    if (this.cards.length/2 == this.pairsGuessed) {
+      return true //no pasa jasmine
+    } else {
+      return false
+    }
+  }
 }
