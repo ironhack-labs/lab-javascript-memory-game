2df7fee7166b6056b242e761ebd6c9d03ebb2417
cdvlpr <contacto.dvlpr@protonmail.com>
2020-10-28 09:19:08 +0100

    0.2-MoreIterationsStarted

--- js/memory.js
+++ js/memory.js
@@ -1,16 +1,22 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    this.pickedCards = cards.map((elm) => elm);
-    this.pairsClicked = cards - cards.length;
-    this.pairsGuessed = cards / cards.length;
+    this.pickedCards = cards.map((elm) => elm)
+    this.pairsClicked = cards - cards.length
+    this.pairsGuessed = cards / cards.length
   }
 
   shuffleCards() {
+
     this.cards.forEach((card) => {
-      let randomPosition = Math.floor(Math.random() * 24);
-      return card;
-    });
+      let randomPosition = Math.floor(Math.random() * 24)
+
+      //return the shuffled (mixed) array of cards
+
+      return card
+
+
+    })
   }
 
   checkIfPair(card1, card2) {
@@ -18,7 +24,7 @@ class MemoryGame {
     if (card1, card2) {
 
       document.querySelector('.pairs-clicked')
-      //add1
+      //should add 1 to `pairsClicked` when we call it
 
       if (card1 === card2) {
         document.querySelector('.pairs-guessed')
@@ -38,30 +44,19 @@ class MemoryGame {
 
   isFinished() {
 
-    if (this.pairsGuessed < cards.length)
-
+    if (this.pairsGuessed = 0) {
+      //return false at the beginning of the game
 
       if (this.pairsGuessed < cards.length) {
-
+        //still some pairs to be guessed
         return false
 
       } else {
+        //true if all pairs are guessed
         return true
       }
 
+    }
   }
+
 }
-
-// Iteration 1: Initial set up
-// Iteration 2: Plan your game
-// Iteration 2.1: The MemoryGame class
-// Iteration 2.2: The class methods
-
-// for(let i = array.length — 1; i > 0; i--){
-//   const j = Math.floor(Math.random() * i)
-//   const temp = array[i]
-//   array[i] = array[j]
-//   array[j] = temp
-// }
-
-// array.sort(function (a, b) { return 0.24 — Math.random() })
