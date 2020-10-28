8e9020da80d1cb72366878ddcf48c56cb2d9c483
Mark <mark.sanjuan.sole@gmail.com>
2020-10-28 19:42:50 +0100

    done

--- js/index.js
+++ js/index.js
@@ -27,7 +27,9 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+
 window.addEventListener('load', event => {
+  memoryGame.shuffleCards()
   let html = '';
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
@@ -39,11 +41,65 @@ window.addEventListener('load', event => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
+  let hero1 = ''
+  let hero2 = ''
+  const cardsArr = []
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+
+        card.className = 'card turned'
+        cardsArr.push(card.getAttribute('data-card-name'))
+        let pair = false
+
+        setTimeout(() => {
+
+          if(cardsArr.length === 2){
+            pair = memoryGame.checkIfPair(cardsArr[0], cardsArr[1])
+            if(!pair && cardsArr.length === 2){
+              const turned = document.getElementsByClassName('card turned')
+              const turnedArr = [...turned]
+              turnedArr.forEach(turnedCard => {
+                if(!turnedCard.className.includes('blocked')){
+                  turnedCard.className = 'card'
+                }
+                cardsArr.pop()
+                cardsArr.pop()
+              })
+            }else {
+              const turned = document.getElementsByClassName('card turned')
+              const turnedArr = [...turned]
+              turnedArr.forEach(turnedCard => {
+                turnedCard.className = 'card turned blocked'
+              })
+              cardsArr.pop()
+              cardsArr.pop()
+            }
+          }
+
+        const pairsClicked = document.getElementById('pairs-clicked')
+        pairsClicked.innerHTML = memoryGame.pairsClicked
+        const pairsGuessed = document.getElementById('pairs-guessed')
+        pairsGuessed.innerHTML = memoryGame.pairsGuessed
+
+        if(memoryGame.isFinished()){
+          const turnedCards = document.getElementsByClassName('card turned')
+          const turnedArr = [...turnedCards]
+          turnedArr.forEach(card => {
+            card.className = 'card'
+          })
+          memoryGame.shuffleCards()
+          memoryGame.pairsClicked = 0
+          memoryGame.pairsGuessed = 0
+        }
+
+        
+        },800)
+
+  
+
     });
   });
+
 });
--- js/memory.js
+++ js/memory.js
@@ -2,8 +2,45 @@ class MemoryGame {
   constructor(cards){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    if(!this.cards){
+      return undefined
+    }
+    let cards = this.cards.map(item=>item)
+    let index = 0
+    let newCards = []
+    let indexArr = []
+    const generateRandom = (num) => {
+      return Math.floor((Math.random()*num))
+    }
+    while (newCards.length < cards.length){
+      index = generateRandom(cards.length)
+      if(!indexArr.includes(index)){
+        indexArr.push(index)
+        newCards.push(cards[index])
+      }
+    }
+    this.cards = newCards
+    return
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++
+    if(card1 === card2){
+      this.pairsGuessed++
+      this.pickedCards.push(card1)
+      this.pickedCards.push(card2)  
+      return true
+    }else { return false}
+  }
+  isFinished() {
+    if(this.pairsGuessed === this.cards.length/2){
+      return true
+    }
+    return false
+  }
 }
