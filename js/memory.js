2ba37525da72414bbfac942852a0928b38c9d6e6
prullansky <owenchboy@gmail.com>
2020-10-29 22:24:38 +0100

    logic done. working on the HTML/CSS interactions

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,38 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+
+  shuffleCards(cards) {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const random = Math.floor(Math.random() * i)
+      const shuffle = this.cards[i]
+      this.cards[i] = this.cards[random]
+      this.cards[random] = shuffle;
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if (card1 === card2){
+    this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+  isFinished() {
+    if (this.pairsGuessed === (this.cards.length / 2)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 }
