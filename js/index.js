e76d1d4fafa0faa2312fa1fe870da3462bb0f15d
Alba Cardona <alba.cardonajimenez@gmail.com>
2020-11-01 14:10:48 +0100

    done

--- js/index.js
+++ js/index.js
@@ -67,7 +67,7 @@ window.addEventListener('load', (event)=> {
       }
 
       if(memoryGame.isFinished()) {
-        document.body.innerHTML = '<h1>YOU WON! 🐼<h1>';
+        document.body.innerHTML = '<h1>YOU WON!!!<h1>';
       }
     });
   });
