a89854273c7cfcc0558a789a6c5a570f8882d66d
Jesus Rua <jesus.rua.pareja@gmail.com>
2020-11-08 17:39:37 +0100

    COMPLETED

--- js/index.js
+++ js/index.js
@@ -27,7 +27,9 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+
 window.addEventListener('load', event => {
+  //memoryGame.shuffleCards()
   let html = '';
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
@@ -42,8 +44,32 @@ window.addEventListener('load', event => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      
+
+      card.classList.toggle("turned")
+
+      memoryGame.pickedCards.push(card)
+      
+      if(memoryGame.pickedCards.length === 2) {
+          const card1 = memoryGame.pickedCards[0].getAttribute("data-card-name");
+          const card2 = memoryGame.pickedCards[1].getAttribute("data-card-name");
+
+        if (memoryGame.checkIfPair(card1, card2)) {
+
+          document.getElementById("pairs-guessed").innerText = memoryGame.pairsGuessed
+         
+          } else {
+          memoryGame.pickedCards.forEach((card)=>{
+            setTimeout(()=>{
+              card.classList.toggle("turned")
+            }, 1000)
+          })
+        }
+        document.getElementById("pairs-clicked").innerText = memoryGame.pairsClicked
+        memoryGame.pickedCards.length = 0
+      } 
+
+  
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,28 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+      this.cards = this.cards.sort((a, b)=>{
+        return 0.5 - Math.random()
+      })
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked ++
+    if(card1 === card2) {
+      this.pairsGuessed ++ 
+      return true
+    }
+    return false
+  }
+
+  isFinished() {
+    if(this.pairsGuessed === (this.cards.length / 2) ){
+      return true
+    }
+    return false
+  }
 }
