e1644f68118eae9990470c824fc5db818f30f658
Thiarhes <injesusservice@hotmail.com>
2020-11-15 01:34:29 -0300

    WDPT SAO 10.2020 Thiarhes

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,23 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards, pickedCards, pairsClicked, pairsGuessed) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [pickedCards];
+    this.pairsClicked = 1;
+    this.pairsGuessed = 1;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
+  shuffleCards(cards) {
+return ;
+  }
+
+  checkIfPair(card1, card2) {
+    if (card1 == card2) {
+      this.pairsGuessed += 1;
+    } else if (card1 === card2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   isFinished() {}
 }
