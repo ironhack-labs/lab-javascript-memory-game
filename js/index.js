7217d60f696db808bfe0c3cc5fbfdaeeb06230ec
Llorenç Palliser <palliser@palliser.es>
2020-11-19 11:32:17 +0100

    Añadidos botones de temporizador

--- js/index.js
+++ js/index.js
@@ -1,32 +1,3 @@
-const chronometer = new Chronometer();
-
-let minDec = document.getElementById('minDec');
-let minUni = document.getElementById('minUni');
-let secDec = document.getElementById('secDec');
-let secUni = document.getElementById('secUni');
-
-
-
-function printTime() {
-
-  printMinutes();
-  printSeconds();
-
-}
-
-function printMinutes() {
-  let minutes = chronometer.getMinutes()
-  let twoDigitMinutes = chronometer.twoDigitsNumber(minutes)
-  minDec.innerText = twoDigitMinutes[0];
-  minUni.innerText = twoDigitMinutes[1];
-}
-function printSeconds() {
-  let seconds = chronometer.twoDigitsNumber(chronometer.getSeconds())
-  secDec.innerText = seconds[0];
-  secUni.innerText = seconds[1];
-}
-
-
 const cards = [
   { name: 'aquaman', img: 'aquaman.jpg' },
   { name: 'batman', img: 'batman.jpg' },
@@ -54,125 +25,169 @@ const cards = [
   { name: 'thor', img: 'thor.jpg' }
 ];
 
+let card1 = ""
+let card2 = ""
+let cardsTurned = 0;
+let started = false;
+let timer = 1000;
+const timerButton1 = document.getElementById('time1')
+const timerButton2 = document.getElementById('time2')
+const resetButton = document.getElementById('reset')
+
+const chronometer = new Chronometer();
+const memoryGame = new MemoryGame(cards);
+
+let minDec = document.getElementById('minDec');
+let minUni = document.getElementById('minUni');
+let secDec = document.getElementById('secDec');
+let secUni = document.getElementById('secUni');
+
+function printTime() {
+  printMinutes();
+  printSeconds();
+}
+
+function printMinutes() {
+  // let minutes = chronometer.getMinutes()
+  // let twoDigitMinutes = chronometer.twoDigitsNumber(minutes)
+  let minutes = chronometer.twoDigitsNumber(chronometer.getMinutes())
+
+  minDec.innerText = minutes[0];
+  minUni.innerText = minutes[1];
+}
+function printSeconds() {
+  let seconds = chronometer.twoDigitsNumber(chronometer.getSeconds())
+  secDec.innerText = seconds[0];
+  secUni.innerText = seconds[1];
+}
 
 
-const button = document.getElementById('reset')
-button.addEventListener('click', () => {
+function drawBoard() {
+  card1 = ""
+  card2 = ""
+  cardsTurned = 0;
+  memoryGame.pairsClicked = 0;
+  memoryGame.pairsGuessed = 0;
+  memoryGame.shuffleCards(cards)
+  let html = '';
 
-  window.addEventListener
+  memoryGame.cards.forEach(pic => {
+    html += `<div class="card" data-card-name="${pic.name}">`;
+    html += `<div class="back" name="${pic.img}"></div>`;
+    html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
+    html += `</div>`;
+  });
+  
+  document.querySelector('#memory-board').innerHTML = html;
+}
 
+
+resetButton.addEventListener('click', () => {
+  // window.addEventListener
   chronometer.stopClick()
   chronometer.resetClick()
   printTime();
-  let card1 = ""
-  let card2 = ""
-  let cardsTurned = 0;
-  memoryGame.pairsClicked = 0;
-  memoryGame.pairsGuessed = 0;
-  // pairsClicked.innerText = '0'
-  // pairsGuessed.innerText = '0'
-  memoryGame.shuffleCards(cards)
-  document.querySelectorAll('.card').forEach(card => {
-    card.className = card.className.replace(' turned', '');
-    card.className = card.className.replace(' linked', '');
-
-  })
+  drawBoard()
+  buttonsListener()
 })
 
+timerButton1.addEventListener('click', () => {
+  timer = 1000
+  timerButton1.className = timerButton1.className.replace("time", 'time pressed');
+  timerButton2.className = timerButton2.className.replace(' pressed', '');
+}
+)
+timerButton2.addEventListener('click', () => {
+  timerButton2.className = timerButton2.className.replace("time", 'time pressed');
+  timerButton1.className = timerButton1.className.replace(' pressed', '');
+  timer = 2000
+}
+)
+
+const clearCards = function () {
+  setTimeout(function () {
+    let card1 = document.getElementById('card1')
+    let card2 = document.getElementById('card2')
+    card1.className = card1.className.replace(' turned', '');
+    card2.className = card2.className.replace(' turned', '');
+    card1.removeAttribute('id')
+    card2.removeAttribute('id')
+    card1 = ""
+    card2 = ""
+    cardsTurned = 0
+  }, timer)
+}
+
+const buttonsListener = function() {
+
+  document.querySelectorAll('.card').forEach(card => {
+    
+    card.addEventListener('click', () => {
 
 
-  const memoryGame = new MemoryGame(cards);
-  memoryGame.shuffleCards(cards)
+      started = chronometer.state;
 
-  window.addEventListener('load', event => {
-    let html = '';
+      if (!started) { chronometer.startClick(printTime) }
 
-    memoryGame.cards.forEach(pic => {
-      html += `<div class="card" data-card-name="${pic.name}">`;
-      html += `<div class="back" name="${pic.img}"></div>`;
-      html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
-      html += `</div>`;
-    });
+      if (cardsTurned === 0 && card.className === 'card') {
+        card.className += ' turned';
+        card.id = 'card1'
+        cardsTurned = 1;
+        card1 = card
+      }
 
-    document.querySelector('#memory-board').innerHTML = html;
+      else if (cardsTurned === 1 && card.className === 'card') {
+        card.className += ' turned';
+        card.id = 'card2'
+        cardsTurned = 2;
+        card2 = card;
+        const resultat = memoryGame.checkIfPair(card1.dataset.cardName, card2.dataset.cardName);
+        if (!resultat) { clearCards(); }
+      }
 
-    let card1 = ""
-    let card2 = ""
-    let cardsTurned = 0;
+      if (cardsTurned === 2 && card1.dataset.cardName === card2.dataset.cardName) {
+        card1.id = "linked"
+        card2.id = "linked"
 
+        cardsTurned = 0;
 
+      }
 
-    document.querySelectorAll('.card').forEach(card => {
-      card.addEventListener('click', () => {
+      if (memoryGame.isFinished()) {
+        let minutes = chronometer.getMinutes()
+        let twoDigitMinutes = chronometer.twoDigitsNumber(minutes)
+        let seconds = chronometer.twoDigitsNumber(chronometer.getSeconds())
+        chronometer.stopClick()
 
-        const checkPaused = function () { if (started && cardsTurned === 0) { chronometer.stopClick() } }
+        printResult = `You finished the game with ${memoryGame.pairsClicked} pairs of cards raised; in ${twoDigitMinutes[0]}${twoDigitMinutes[1]}:${seconds[0]}${seconds[1]}, what it supposes ${Math.round(chronometer.currentTime / memoryGame.pairsGuessed * 100) / 100} seconds each card couple.`
+        const element = document.getElementById('result')
+        const pElem = document.createElement('p');
+        pElem.innerText = printResult;
+        element.appendChild(pElem);
+      }
 
-        let started = chronometer.state;
-
-        if (!started) { chronometer.startClick(printTime) }
-
-        if (cardsTurned === 0 && card.className === 'card') {
-          card.className += ' turned';
-          cardsTurned = 1;
-          card1 = card
-
-        }
-        else if (cardsTurned === 1 && card.className === 'card') {
-          card.className += ' turned';
-          cardsTurned = 2;
-          card2 = card;
-          turnedCards = document.querySelectorAll('.tunned');
-          memoryGame.checkIfPair(card1.dataset.cardName, card2.dataset.cardName);
-
-        }
-        else if (card.className.includes('turned')) {
-          if (card.className.includes('linked')) { }
-
-          else {
-            card.className = card.className.replace(' turned', '');
-            card1 = ""
-            card2 = ""
-            cardsTurned = cardsTurned - 1
-            checkPaused();
-          }
-        }
-        if (cardsTurned === 2 && card1.dataset.cardName === card2.dataset.cardName) {
-          card1.classList.add("linked");
-          card2.classList.add("linked");
-          cardsTurned = 0;
-
-        }
-
-        if (memoryGame.isFinished()) {
-          let minutes = chronometer.getMinutes()
-          let twoDigitMinutes = chronometer.twoDigitsNumber(minutes)
-          let seconds = chronometer.twoDigitsNumber(chronometer.getSeconds())
-          chronometer.stopClick()
-
-          printResult = `You finished the game with ${memoryGame.pairsClicked} pairs of cards raised; with the time ${twoDigitMinutes[0]}${twoDigitMinutes[1]}:${seconds[0]}${seconds[1]}, what it supposes ${Math.round(chronometer.currentTime / memoryGame.pairsGuessed * 100) / 100} seconds each card couple `
-
-
-          const element = document.getElementById('result')
-
-
-          var pElem = document.createElement('p');
-          pElem.innerText = printResult;
-          element.appendChild(pElem);
-
-
-        }
-        let pairsClicked = document.querySelector('#pairs-clicked')
-        pairsClicked.innerText = memoryGame.pairsClicked
-        let pairsGuessed = document.querySelector('#pairs-guessed')
-        pairsGuessed.innerText = memoryGame.pairsGuessed
-
-      });
+      let pairsClicked = document.querySelector('#pairs-clicked')
+      pairsClicked.innerText = memoryGame.pairsClicked
+      let pairsGuessed = document.querySelector('#pairs-guessed')
+      pairsGuessed.innerText = memoryGame.pairsGuessed
 
     });
 
   });
+  
+}
 
-  var node = document.getElementsByTagName('div');
-  var divLength = node.length;
-  var randomDiv = Math.random() * divLength;
+window.addEventListener('load', event => {
+
+
+  drawBoard()
+  buttonsListener()
+}
+
+
+);
+
+var node = document.getElementsByTagName('div');
+var divLength = node.length;
+var randomDiv = Math.random() * divLength;
 
--- js/memory.js
+++ js/memory.js
@@ -1,26 +1,17 @@
 class MemoryGame {
   constructor(cards, pickedCards, pairsClicked, pairsGuessed) {
-
-
     this.cards = cards;
     this.pickedCards = [];
     this.pairsClicked = 0;
     this.pairsGuessed = 0;
-    // add the rest of the class properties here
   }
 
   shuffleCards(cards) {
-
     let currentIndex = this.cards.length, temporaryValues, randomIndex;
 
-    // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-
-      // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
-
-      // And swap it with the current element.
       temporaryValues = this.cards[currentIndex];
       this.cards[currentIndex] = this.cards[randomIndex];
       this.cards[randomIndex] = temporaryValues;
@@ -29,22 +20,17 @@ class MemoryGame {
   }
 
   checkIfPair(card1, card2) {
-
     this.pairsClicked = this.pairsClicked + 1;
-
     if (card1 === card2) {
       this.pairsGuessed++;
       return true
     }
-    else {
-
+    else { 
       return false
-
     }
   }
 
   isFinished() {
-
     if (this.pairsGuessed == (this.cards.length / 2)) {
       return true
     }
