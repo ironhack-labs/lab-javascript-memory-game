cb286a8ca129286fc2964891b17a6749fd4d25f2
Thiarhes <injesusservice@hotmail.com>
2020-11-19 01:54:17 -0300

    WDPT SAO 10.2020 Thiarhes

--- js/memory.js
+++ js/memory.js
@@ -6,13 +6,25 @@ class MemoryGame {
     this.pairsGuessed = 1;
   }
   shuffleCards(cards) {
-return ;
+    let currentIndex = cards.length, temporaryValue, randomIndex;
+ // while there remain elements to shuffle...
+    while (0 !== currentIndex) {
+ // pick a remain element
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+ // and swap it with the current element
+    
+ temporaryValue = cards[currentIndex];
+ cards[currentIndex] = cards[randomIndex]
+ cards[randomIndex] = temporaryValue;     
+       
+    }
+    return currentIndex;
   }
 
   checkIfPair(card1, card2) {
-    if (card1 == card2) {
+    if (card1 === card2) {
       this.pairsGuessed += 1;
-    } else if (card1 === card2) {
       return true;
     } else {
       return false;
