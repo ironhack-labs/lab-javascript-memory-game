5393df02f6c0c6cd414d82246855a691c66487e5
RFeldman84 <rachel.feldman84@gmail.com>
2020-11-21 00:03:58 -0500

    figure out some functionality for cards, update pairs n guessed working

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const cl = (...p) => console.log(...p)
-window.addEventListener('load', ()=>{
+
   console.log("index.js connected")
-});
+
 
 const board = document.querySelector('#memory-board')
 
@@ -33,28 +33,176 @@ const cards = [
   { name: 'thor', img: 'thor.jpg' }
 ];
 
-//cl(cards)
-
 const memoryGame = new MemoryGame(cards);
-cl(memoryGame)
+
+function newGame(){
+  /// ... code for new game
+  ///create html for button... onclick do below not return
+ return memoryGame.shuffleCards() /// shuffle works...do last figure out game first!!!!
+}
+
+//    clear array after compare cards
+//   memoryGame.pickedCards = []
+
+// function compare(){
+//   if(memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1]) === true){
+//     memoryGame.pickedCards = []
+//   } else {
+//     turnBack()
+//   }
+
+// }
+
+//toggle functions
+
+//block
+// const blocked = ()=> {
+//   let front = document.querySelector('.front')
+//    front.classList.toggle('blocked')
+//    //... glazed overlay other changed? cuz turn keeps it turned
+//    // 
+//  } 
+
+
+
+
+
 window.addEventListener('load', event => {
   let html = '';
   memoryGame.cards.forEach(pic => {
-    html += `<div class="card" data-card-name="${pic.name}">`;
+    html += `<div class="card " data-card-name="${pic.name}">`;
     html += `<div class="back" name="${pic.img}"></div>`;
     html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
     html += `</div>`;
 
   });
- cl({e: event.currentTarget})
-  // Add all the divs to the HTML
-  document.querySelector('#memory-board').innerHTML = html;
   
+  // Add all the divs to the HTML
+document.querySelector('#memory-board').innerHTML = html;
+
+
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
+      const turn = () => card.classList.toggle('turned')
+      const turnBack = ()=> setTimeout(turn, 2000)
+      turn()
+      /// if statment after checked to turnBack() or block() 
+      //turnBack()
+      
+
+ 
+  
+   
+   //// works DONT TOUCH
+      if(card.hasAttribute('turned')=== true && memoryGame.pickedCards.length ===0 || memoryGame.pickedCards.length <2){
+      memoryGame.pickedCards.push(card.attributes[1].nodeValue)
+      cl(memoryGame.pickedCards)}
+ ////////
+        memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1])
+        //// wrong IF STATEMENT!  clearing works on matches & non
+         if(memoryGame.pickedCards[0] === memoryGame.pickedCards[1]){
+          memoryGame.pickedCards = []   
+       } 
+
+        ///// ~~~doesn't count as clicked pair justs turns back~~~ 
+         //else {
+        //  turnBack()
+        //  memoryGame.pickedCards = []
+        // }
+           
+        // }
+          
+         
+         /// not work if statement works for all even pairs... WRONG IFFFFFFFF
+          // if(memoryGame.pickedCards[0] !== memoryGame.pickedCards[1]){
+          //   memoryGame.pickedCards = []
+          //   turnBack()
+          // }
+         
+        /// calls it twice pairs clicked thrown off
+        //   if(memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1])=== false)
+        //   turnBack()
+        //   memoryGame.pickedCards = []
+        //  }
+      
+      
+      // memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1])
+      
+      
+  //     cl(card)
+     
       console.log(`Card clicked: ${card}`);
     });
   });
 });
+
+
+
+
+cl(memoryGame.pairsClicked)
+cl(memoryGame.pairsGuessed)
+
+
+
+// if(card.hasAttribute('turned')=== true && memoryGame.pickedCards.length ===0 || memoryGame.pickedCards.length <2)
+// {
+//   memoryGame.pickedCards.push(card.attributes[1].nodeValue)
+//   cl(memoryGame.pickedCards)
+
+//   memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1])
+//   // if(memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1])=== false){
+//   //   turnBack()
+//   //   memoryGame.pickedCards = []
+//   // }
+// }
+
+
+
+
+
+
+///// messes stuff up but idea
+// function toTurn(){
+//   let card2arr = []
+//   if(card2arr.length >= 0 || card2arr.length < 2){
+//     card.classList.toggle('turned')
+//     card2arr.push(card)
+//  }
+// }
+// toTurn()
+// /// not passing to array
+// cl(card2arr)  
+    
+//   if(card2arr.length === 2){
+//     !card.classList.toggle('turned') /// not correct coding just idea
+//   }
+// }   
+
+
+
+
+
+
+// window.addEventListener('load', event => {
+//   let html = '';
+//   memoryGame.cards.forEach(pic => {
+//     html += `<div class="card " data-card-name="${pic.name}">`;
+//     html += `<div class="back" name="${pic.img}"></div>`;
+//     html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
+//     html += `</div>`;
+
+//   });
+  
+//   // Add all the divs to the HTML
+// document.querySelector('#memory-board').innerHTML = html;
+
+//   // Bind the click event of each element to a function
+//   document.querySelectorAll('.card').forEach(card => {
+//     card.addEventListener('click', () => {
+//       // TODO: write some code here
+//       console.log(`Card clicked: ${card}`);
+//     });
+//   });
+// });
+
--- js/memory.js
+++ js/memory.js
@@ -1,4 +1,4 @@
-
+//const cl = (...p) => console.log(...p)
 window.addEventListener('load', ()=>{
   console.log("memory.js connected")
 });
@@ -6,6 +6,7 @@ window.addEventListener('load', ()=>{
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    //this.pickedCards = [cards[0].name,cards[12].name]
     this.pickedCards = []
     this.pairsClicked = 0
     this.pairsGuessed = 0
@@ -22,11 +23,14 @@ class MemoryGame {
     }  
   }
 
-  checkIfPair(card1, card2) {
-    this.pairsClicked++
+  checkIfPair(card1,card2) {
+   if(this.pickedCards.length === 2) this.pairsClicked++
+    let pClick = document.querySelector('#pairs-clicked')
+    pClick.innerHTML = this.pairsClicked
     if(card1 === card2){
       this.pairsGuessed++;
-      this.isFinished()
+      let pGuessed = document.querySelector('#pairs-guessed')
+      pGuessed.innerHTML = this.pairsGuessed
       return true 
     } else {
       return false
@@ -34,6 +38,22 @@ class MemoryGame {
   }
 
   isFinished() {
+    // if true dom manip post game over
+    // if true button start new game
+    // if false keep playing etc
     return this.pairsGuessed === this.cards.length/2  ? true : false
   }
 }
+
+
+
+
+
+
+
+
+//cl(game.pairsClicked)
+
+// cl(memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1]))
+// cl(memoryGame.pairsClicked)
+// cl(memoryGame.pairsGuessed)
