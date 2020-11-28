3505b7e3c061804874316b61012911870c16e507
Alessandro Cammarata <alessandro.cammarata@ehl.ch>
2020-11-28 10:26:32 +0100

    forgot to submit assignment :/

--- js/index.js
+++ js/index.js
@@ -42,7 +42,8 @@ window.addEventListener('load', event => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
+      picked.push(card);
+      card.setAttribute('class', 'card turned');
       console.log(`Card clicked: ${card}`);
     });
   });
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,33 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    var i, n, x;
+    for (i = this.cards.length -1; i > 0; i--) {
+        n = Math.floor(Math.random() * i);
+        x = this.cards[i];
+        this.cards[i] = this.cards[n];
+    }
+  }
+  checkIfPair(card1, card2) {
+    if (card1 === card2){
+      this.pairsClicked++;
+      this.pairsGuessed++;
+      return true
+    }else{
+      this.pairsClicked++;
+      return false
+    }
+  }
+  isFinished() {
+    if(this.pairsGuessed === this.cards.length/2){
+      return true
+    }else{
+      return false
+    }
+  }
 }
