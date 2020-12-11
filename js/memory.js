e7ae53a91be98db0207547a7f4fee565ec42a250
Alicia Parra <alicia@redparra.com>
2020-12-11 18:59:37 +0100

    Do the logic part.Complete iteration 1, 2.1 and 2.2

--- js/memory.js
+++ js/memory.js
@@ -2,8 +2,29 @@ class MemoryGame {
   constructor(cards){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked =0;
+    this.pairsGuessed =0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    for (let i = this.cards.length - 1; i >= 0; i--){
+      const r = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[r]] = [this.cards[r], this.cards[i]];
+    }
+
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if(card1===card2){
+      this.pairsGuessed++;
+      return true
+    }
+    return false;
+  }
+  isFinished() {
+    if(this.pairsGuessed===this.cards.length/2){
+      return true
+    }
+    return false;
+  }
 }
