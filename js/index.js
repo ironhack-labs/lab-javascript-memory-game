755f78d2d055c4c3a8cfc5a237d496a3c9918be8
Alicia Parra <alicia@redparra.com>
2020-12-11 20:34:28 +0100

    Exercise done. All tests working

--- js/index.js
+++ js/index.js
@@ -27,6 +27,10 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+//I shuffle cards before the game begins
+memoryGame.shuffleCards();
+
+//Add the cards html content
 window.addEventListener('load', event => {
   let html = '';
   memoryGame.cards.forEach(pic => {
@@ -38,12 +42,59 @@ window.addEventListener('load', event => {
 
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
+  
 
   // Bind the click event of each element to a function
+
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+
+      // Add the class turned on click
+      card.classList.add('turned');
+
+      //Add one card to the pickedCards array
+      memoryGame.pickedCards.push(card)
+    
+      //Logic starts if there are 2 cards on the pickedCards array
+        if(memoryGame.pickedCards.length===2){
+          const firstCard= memoryGame.pickedCards[0]
+          const secondCard= memoryGame.pickedCards[1]
+
+          //Get card name propertie to access the method checkIfPair
+          const firstCardName= memoryGame.pickedCards[0].getAttribute('data-card-name');
+          const secondCardName= memoryGame.pickedCards[1].getAttribute('data-card-name');
+          //Call method in a variable to make the code easy to read
+          const equalCards= memoryGame.checkIfPair(firstCardName,secondCardName);
+
+          //If the cards are not equal, they'll be turned back after one sec
+          if(!equalCards){
+            setTimeout(()=> {
+              firstCard.classList.remove('turned')
+              secondCard.classList.remove('turned')
+          },1000)
+          }else{
+            firstCard.classList.add('blocked')
+            firstCard.classList.add('blocked')
+          }
+
+          //I clean pickedArray to start over with two different cards on next iteration
+          memoryGame.pickedCards=[];
+        }
+      
+      //Access and display counter numbers on html
+      const clickedCounter= document.getElementById('pairs-clicked');
+      const guessedCounter= document.getElementById('pairs-guessed');
+
+      clickedCounter.innerHTML=memoryGame.pairsClicked;
+      guessedCounter.innerHTML=memoryGame.pairsGuessed;
+
+        //Give an alert if you won the game
+        if(memoryGame.isFinished()){
+          setTimeout(()=>{
+            alert('Congrats, you won the game')
+          },1000)
+        }
+      
     });
   });
 });
