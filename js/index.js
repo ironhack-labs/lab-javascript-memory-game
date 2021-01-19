4d3bbafb129f8bd9db7bc43dbbf19e8157c8c547
Alejandro Herrero <a.herrerolorente@gmail.com>
2021-01-19 22:28:31 +0100

    lab done

--- js/index.js
+++ js/index.js
@@ -29,6 +29,7 @@ const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', event => {
   let html = '';
+  memoryGame.shuffleCards()
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
     html += `<div class="back" name="${pic.img}"></div>`;
@@ -43,7 +44,51 @@ window.addEventListener('load', event => {
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      
+      card.setAttribute('class', 'card turned')
+
+      if (memoryGame.isFinished()) {
+        setTimeout(() => alert('YOU WON! Refresh the page if you want to play more'), 750)
+      }
+      
+      memoryGame.pickedCards.push(card)
+
+      const card1 = memoryGame.pickedCards[0]
+      const card2 = memoryGame.pickedCards[1]
+
+      const cardName1 = card1.attributes[1].value
+      const cardName2 = card2.attributes[1].value
+
+      if (memoryGame.pickedCards.length === 2){
+
+
+        if (memoryGame.checkIfPair(cardName1, cardName2)) {
+          if (memoryGame.isFinished()) {
+            alert('YOU WON! Refresh the page if you want to play more')
+          }
+          memoryGame.pickedCards = []
+
+        } else {
+          setTimeout(() => card1.setAttribute('class', 'card'), 500)
+          setTimeout(() => card2.setAttribute('class', 'card'), 500)
+          memoryGame.pickedCards = []
+          
+        }
+
+        const pairsClicked = document.querySelector('#pairs-clicked')
+        pairsClicked.innerText = memoryGame.pairsClicked
+        const pairsGuessed = document.querySelector('#pairs-guessed')
+        pairsGuessed.innerText = memoryGame.pairsGuessed  
+
+        
+
+      }
+     
+    
+      
+
     });
   });
 });
+
+
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,44 @@
 class MemoryGame {
   constructor(cards){
+    
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0   
+
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+
+    let m = this.cards.length
+    let t = 0
+    let i = 0
+
+    while (m) {
+      i = Math.floor(Math.random() * m--)
+
+      t= this.cards[m]
+      this.cards[m] = this.cards[i]
+      this.cards[i] = t
+    }
+
+    return this.cards 
+
+  }
+  checkIfPair(card1, card2) {
+
+    this.pairsClicked ++
+    if (card1 === card2) {
+
+      this.pairsGuessed ++
+      return true
+
+    } else {return false} 
+
+  }
+  isFinished() {
+
+    return this.pairsGuessed === this.cards.length/2
+
+  }
 }
