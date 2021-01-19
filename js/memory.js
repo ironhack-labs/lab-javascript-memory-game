97fa8234e69ecf6897d22c76088b1eae501f1123
Veronica Alcala <veronica.alcala@gmail.com>
2021-01-19 19:57:01 +0100

    Listo todo el iteration 1 y 2.

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,38 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1
+    if (card1 === card2) {
+      this.pairsGuessed += 1
+      return true
+    } else if (card1 != card2) {
+      return false
+    }
+  }
+
+  isFinished() {
+    if (this.pairsGuessed === 8) {
+      return true
+    } else {
+      return false
+    }
+  }
+    
 }
+
+
+
