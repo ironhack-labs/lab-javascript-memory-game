39ef26079b989d6ab40742e03be5b06d4ca386cb
Barbara Diaz <barbarad91@gmail.com>
2021-01-19 17:18:19 +0100

    Add properties to MemoryGame

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,12 @@
 class MemoryGame {
-  constructor(cards){
-    this.cards = cards;
-    // add the rest of the class properties here
-  }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+    constructor(cards) {
+        this.cards = cards;
+        // add the rest of the class properties here
+        this.pickedCards = []
+        this.pairsClicked = 0
+        this.pairsGuessed = 0
+    }
+    shuffleCards() {}
+    checkIfPair(card1, card2) {}
+    isFinished() {}
 }
