2aede99514c52b14f380d061fe2a7aa61247f418
Barbara Diaz <barbarad91@gmail.com>
2021-01-19 18:01:43 +0100

    Add MemoryGame methods

--- js/memory.js
+++ js/memory.js
@@ -6,7 +6,30 @@ class MemoryGame {
         this.pairsClicked = 0
         this.pairsGuessed = 0
     }
-    shuffleCards() {}
-    checkIfPair(card1, card2) {}
-    isFinished() {}
+
+    shuffleCards() {
+        if (this.cards) {
+            let m = this.cards.length - 1
+            while (m) {
+                const i = Math.floor(Math.random())
+                const randomElement = this.cards[i]
+                this.cards[i] = this.cards[m]
+                this.cards[m] = randomElement
+                m--
+            }
+        }
+    }
+
+    checkIfPair(card1, card2) {
+        this.pairsClicked++
+            if (card1 === card2) {
+                this.pairsGuessed++
+                    return true
+            }
+        return false
+    }
+
+    isFinished() {
+        return this.pairsGuessed === this.cards.length / 2
+    }
 }
