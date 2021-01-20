be5a9b216cbfd38a0e70d4fb72fac9732ebdad33
Patrick Lehmann <ptrcklehmann@gmail.com>
2021-01-20 13:47:23 +0100

    done

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,31 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    const cardsCopy = this.cards.slice()
+    let shuffleCards = []
+
+      while (cardsCopy.length > 0) {
+        const randomIndex = Math.floor(Math.random() * cardsCopy.length)
+        shuffleCards.push(cardsCopy[randomIndex])
+        cardsCopy.splice(randomIndex, 1)
+      }
+      this.cards=shuffleCards
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++
+    if(card1 === card2) {
+      this.pairsGuessed++
+      return true
+    } return false
+  }
+  isFinished() {
+    if (this.cards.length === 24) return false 
+    if (this.pairsGuessed === this.cards.length/2) return true 
+    else return false
+  }
 }
