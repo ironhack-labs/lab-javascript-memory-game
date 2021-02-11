e35dd942763d3b2c5ef3d3b46650f7e23dcbe6b8
Mélusine Rey <zelumine@live.fr>
2021-02-11 22:46:12 +0100

    Not done yet...

--- js/index.js
+++ js/index.js
@@ -26,6 +26,7 @@ const cards = [
 ];
 
 const memoryGame = new MemoryGame(cards);
+const turnedCards = document.querySelectorAll(".card .turned");
 
 window.addEventListener('load', event => {
   let html = '';
@@ -39,11 +40,40 @@ window.addEventListener('load', event => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
+  // Functions
+  function clickCard(card) {
+    if (memoryGame.pickedCards.length < 2) {
+      card.classList.add("turned");
+      memoryGame.pickedCards.push(card.getAttribute("data-card-name"));
+      console.log("pickedCards:", memoryGame.pickedCards);
+    } else return alert("You can only pick two cards!");
+    if (memoryGame.pickedCards.length === 2) checkCards();
+  }
+
+  function checkCards() {
+    console.log("...checking cards");
+    document.getElementById("pairs-clicked").textContent = memoryGame.pairsClicked;
+    let result = memoryGame.checkIfPair(memoryGame.pickedCards[0], memoryGame.pickedCards[1]);
+
+    if (result) {
+      document.getElementById("pairs-guessed").textContent = memoryGame.pairsGuessed;
+      turnedCards.forEach(card => card.classList.add("blocked"));
+
+    } else {
+      setTimeout(() => {
+        turnedCards.forEach(card => card.classList.remove("turned"));
+      }, 1500);
+    }
+    memoryGame.pickedCards = [];
+  }
+
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
+    card.addEventListener('click', (event) => {
       // TODO: write some code here
+      clickCard(card);
       console.log(`Card clicked: ${card}`);
+      memoryGame.isFinished();
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -2,8 +2,51 @@ class MemoryGame {
   constructor(cards){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    // console.log("this.cards: ", this.cards);
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  
+  shuffleCards(cardsArr) {
+    if (cardsArr) {        //this isn't right I guess, because I can't console.log my shuffled array if I leave the "if"
+      cardsArr  = [...this.cards];
+      let shuffled = [];
+      let deleted;
+      
+      for (let i = cardsArr.length; i > 0 ; i--) {
+        deleted = cardsArr.splice(Math.floor(Math.random() * Math.floor(cardsArr.length)), 1);
+        shuffled.push(deleted[0]);
+      }
+      // console.log("shuffled:", shuffled);    
+      return shuffled;
+    }
+    else return undefined;
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else return false;
+  }
+
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length / 2) return true;
+    else return false;
+  }
 }
+
+
+// isFinished() {
+//   if (this.pairsGuessed === this.cards.length / 2) {
+//     //alert("You won!!!");
+//     this.pickedCards = [];
+//     this.pairsGuessed = 0;
+//     this.pairsClicked = 0;
+//     this.shuffleCards();
+//     return true;
+//   }
+//   else return false;
+// }
