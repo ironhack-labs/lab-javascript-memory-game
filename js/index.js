13b6e993bb86f3414e59022dda66a1b23214c472
august159 <augustin.leman@gmail.com>
2021-02-11 21:56:57 +0100

    Lab completed but cards either turn quicly or not at all with timeout

--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ window.addEventListener('load', event => {
   let html = '';
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
-    html += `<div class="back" name="${pic.img}"></div>`;
+    html += `<div class="back " name="${pic.img}"></div>`;
     html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
     html += `</div>`;
   });
@@ -41,9 +41,43 @@ window.addEventListener('load', event => {
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
+    card.addEventListener('click', () => {          // On click...
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
-  });
-});
+      card.classList.add("turned");                 // ...turn card
+      memoryGame.pickedCards.push(card);            // ... add card to pickedCards()
+  
+      if (memoryGame.pickedCards.length===2) {    // if 2 turned cards => checkIfPair sur .pickedCards()
+
+        if (memoryGame.checkIfPair(memoryGame.pickedCards[0].getAttribute('data-card-name'), memoryGame.pickedCards[1].getAttribute('data-card-name'))) {
+
+          memoryGame.pickedCards.forEach(pickedCard => {  // if pair => class+=blocked
+            pickedCard.classList.add('blocked');
+          });
+          if (memoryGame.isFinished()) {          // Game over ?
+            setTimeout(()=>{
+              alert("Congrats, you have the memory of an elephant ! Let's play again")
+            },1000)
+            memoryGame.shuffleCards();            // Shuffle cards
+            memoryGame.pickedCards.forEach(pickedCard => {  // unblock & turn all cards face down
+              pickedCard.classList.remove('blocked');
+              pickedCard.classList.remove('blocked');
+            });            
+            memoryGame.pairsClicked = 0;          // Reset counters
+            memoryGame.pairsGuessed = 0;
+          }
+        } else {
+          setTimeout(() => {                        
+            memoryGame.pickedCards.forEach(pickedCard => { 
+              pickedCard.classList.remove('turned');         // Turn back cards after 1s
+            });
+          }, 1000);
+        }
+        
+        memoryGame.pickedCards = [];                  // Reset .pickedCards()
+        document.querySelector('#pairs-clicked').textContent = `${memoryGame.pairsClicked}`;  // Always counters++
+        document.querySelector('#pairs-guessed').textContent = `${memoryGame.pairsGuessed}`;
+
+      }
+    })
+  })
+})
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,28 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() { //Fisher-Yates: shuffle
+    for (let i = 0; i < this.cards.length-2; i++) {
+      let j = Math.floor(Math.random() * this.cards.length);
+      let acc = {} // ? Quel est le type de acc ?
+      if(j >= i) { 
+        acc = this.cards[j];
+        this.cards[j] = this.cards[i];
+        this.cards[i] = this.cards;
+      }
+    }
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    this.pairsGuessed += (card1 === card2);
+    return (card1 === card2);
+  }
+
+  isFinished() {
+    return this.pairsGuessed === this.cards.length/2;
+  }
 }
