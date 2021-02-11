86570b015ced659fff32a1832f4c26deea86ddf6
Pauline Bellaud <pauline.bellaud@gmail.com>
2021-02-12 00:10:18 +0100

    redone, can turn the card once, but not twice

--- js/index.js
+++ js/index.js
@@ -43,15 +43,8 @@ window.addEventListener('load', event => {
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      card.classList.toggle("turned");
-      if (card.classList = 2) {
-        let result = card.checkIfPair(card1, card2)
-        if (result = true) {
-          card.classList.add("blocked");
-          card.isFinished();
-        } else {
-
-        }
+      if (card.classList.contains("card")){
+        card.classList.add("turned");
       }
       console.log(`Card clicked: ${card}`);
     });
