5899323b6507338ee36301fdd6bf4876da3d28fc
Mackenzie Minch <madameminch@gmail.com>
2021-02-11 20:51:15 +0100

    first draft

--- js/memory.js
+++ js/memory.js
@@ -2,8 +2,26 @@ class MemoryGame {
   constructor(cards){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
   shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if (card1 === card2); {
+      return true;
+      this.pairsGuessed += 1;
+    } else {
+         return false;
+       }
+  }
+  isFinished() {
+    if (this.pairsGuessed < 16) {
+      return false;
+    } 
+    if (this.pairsGuessed = 16) {
+      return true;
+    }
+  }
 }
