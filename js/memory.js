b7cbbd5747ae975ea0018aedb859313d62bbbe36
Paul Parant <paul.parant@gmail.com>
2021-02-11 23:05:43 +0100

    Fisher-Yates

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,48 @@
+
 class MemoryGame {
+
+
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
+
+
+
+
+  shuffleCards(cards) {
+  
+    function shuffle(array) {
+     
+      var currentIndex = array.length, temporaryValue, randomIndex;
+    
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+    
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+    
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+    
+      return array;
+    }
+  
+  console.log(array);
+  }
+
+  checkIfPair(card1, card2) {
+
+    
+
+  }
+  
   isFinished() {}
 }
+
