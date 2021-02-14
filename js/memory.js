3bac731feba7f8e42f01f816c7d9f3a34404b9ee
Alexis Verthuy <alexis.verthuy@gmail.com>
2021-02-14 15:25:22 +0100

    Done without HTML CSS Connexion correctly done

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,35 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+
+  shuffleCards(cardsArray) {
+    cardsArray = this.cards;
+    for(let i = cardsArray.length - 1; i > 0; i--) {
+      let randomInd = Math.floor(Math.random() * (i+1));
+      let temp = cardsArray[i];
+      cardsArray[i] = cardsArray[randomInd];
+      cardsArray[randomInd] = temp;
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++
+    if (card1 == card2) {
+      this.pairsGuessed++
+      return true
+    } return false
+  }
+
+  isFinished() {
+    if (this.cards.length = 0) {
+      return true
+    } else {
+      return false
+    }
+  }
 }
