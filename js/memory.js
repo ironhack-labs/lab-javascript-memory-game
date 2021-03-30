0ef423a05eadfd446a99526740c8b8e43cf67c24
Yerai Pinedo <yeraipinedog@gmail.com>
2021-03-30 19:22:38 +0200

    changes

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,49 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards, pickedCards, pairsClicked, pairsGuessed) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    function shuffle(cards) {
+      
+    }
+
+    
+  }
+
+  checkIfPair(card1, card2) {
+    if (card1 === card2) {
+
+      return true
+
+    } else {
+
+      return false
+    }
+
+  }
+
+
+
+
+
+
+  isFinished() {
+    if (this.pairsGuessed === 0)
+
+      return false
+
+    else if (this.pairsGuessed < 12)
+
+      return false
+
+    else (this.pairsGuessed === 12)
+    return true
+
+  }
+
+
 }
