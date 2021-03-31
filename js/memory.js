ad829039e20f28a84814d780b4669a0b45987714
Hector Martinez <thornnk1@gmail.com>
2021-03-31 21:31:29 +0200

    Finished Iteration 2

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,57 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards = ()=>{
+    for(let i = this.cards.length -1; i>0; i--){
+      let k = Math.floor(Math.random() * (i + 1))
+      let temp = this.cards[i]
+      this.cards[i] = this.cards[k]
+      this.cards[k] = temp; 
+    }
+  }
+  checkIfPair = (card1, card2)=>{
+    this.pairsClicked++
+    if (card1 === card2) {
+      this.pairsGuessed++
+      return true
+    } else {
+      return false
+    }
+  }
+  isFinished() {
+    if (this.pairsGuessed*2 === this.cards.length){
+      return true
+    } else {
+      return false
+    }
+  }
 }
+
+let cardsArray = [
+  { name: "aquaman", img: "aquaman.jpg" },
+  { name: "batman", img: "batman.jpg" },
+  { name: "captain america", img: "captain-america.jpg" },
+  { name: "fantastic four", img: "fantastic-four.jpg" },
+  { name: "flash", img: "flash.jpg" },
+  { name: "green arrow", img: "green-arrow.jpg" },
+  { name: "green lantern", img: "green-lantern.jpg" },
+  { name: "ironman", img: "ironman.jpg" },
+  { name: "aquaman", img: "aquaman.jpg" },
+  { name: "batman", img: "batman.jpg" },
+  { name: "captain america", img: "captain-america.jpg" },
+  { name: "fantastic four", img: "fantastic-four.jpg" },
+  { name: "flash", img: "flash.jpg" },
+  { name: "green arrow", img: "green-arrow.jpg" },
+  { name: "green lantern", img: "green-lantern.jpg" },
+  { name: "ironman", img: "ironman.jpg" }
+];
+let memoryGame = new MemoryGame(cardsArray);
+
+console.log(array)
+const asdf = game.shuffleCards(game.cards)
+console.log(array, '&', asdf)
+console.log(array.toString() === asdf.toString())
