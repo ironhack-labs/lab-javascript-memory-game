404363d8ce8893c60917b6832e9b060c7bc5127f
Katarina Orlovska <k.orlovska@posteo.de>
2021-04-02 16:34:04 +0200

    Katka Orlovska finished

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//console.log('test')
+
 const cards = [
   { name: 'aquaman', img: 'aquaman.jpg' },
   { name: 'batman', img: 'batman.jpg' },
@@ -29,21 +31,66 @@ const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', event => {
   let html = '';
+  //console.log(memoryGame);
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
     html += `<div class="back" name="${pic.img}"></div>`;
     html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
     html += `</div>`;
   });
-
   // Add all the divs to the HTML
+
   document.querySelector('#memory-board').innerHTML = html;
 
+  //console.log(document.querySelectorAll('.card'))
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+    card.addEventListener('click', (event) => {
+     currentPickedCards.length==2 ? (nextTurn(), pickCard(event)) : pickCard(event) ;
+      //console.log(`Card clicked`);
     });
   });
 });
+
+let currentPickedCards = [];   // array of elements line 60
+
+function pickCard (event) {
+  let clicked = event.currentTarget
+  clicked.classList.add('turned');  
+ 
+  currentPickedCards.push(clicked); // defined outside
+
+  let name1 = currentPickedCards[0].getAttribute('data-card-name');
+  let name2 = currentPickedCards[1].getAttribute('data-card-name')
+
+  console.log(clicked);
+ 
+  // pair guessed logic
+  if ( currentPickedCards.length === 2 
+      && name1 === name2) {
+    console.log('yay')
+    currentPickedCards.forEach(c=> c.classList.add('blocked'));
+    // show count: 
+    memoryGame.pairsGuessed++
+    document.getElementById('pairs-guessed').innerText = memoryGame.pairsGuessed.toString();
+   }
+   //check if all clicked and if finished
+   if (memoryGame.isFinished()) {
+    const win =  '<div> <img src="yawnscream.jpg" id="scream"></div>';
+    document.querySelector('#memory-board').innerHTML = win;
+  }
+
+}
+
+function nextTurn () {
+  currentPickedCards.forEach(c => {
+    if (!c.classList.contains('blocked')) {
+      c.classList.remove('turned') 
+    }
+  })
+  currentPickedCards=[];  //reset
+  memoryGame.pairsClicked++  //count
+  //show count:
+  document.getElementById('pairs-clicked').innerText = memoryGame.pairsClicked.toString(); 
+  
+}
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,49 @@
+// console.log("I'm running")
+
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    this.score = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  // I've found this on stackoverflow // for now
+  shuffleCards(cards) {
+    let currentIndex = cards.length, temporaryValue, randomIndex;
+
+      while (currentIndex !==0) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = cards[currentIndex];
+        cards[currentIndex] = cards[randomIndex];
+        cards[randomIndex] = temporaryValue;
+      }
+      return cards;
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked ++
+    if (card1===card2) {
+      this.pairsGuessed ++
+      return true
+    }
+    return false
+  }
+
+  isFinished() {
+    if (this.pairsGuessed*2 ===this.cards.length) return true
+    else return false
+  }
 }
+
+// console.log(MemoryGame);
+// const game = new MemoryGame (['hey', 'yo', 'hey']);
+// game.cards = 4
+// game.pairsClicked = 2
+// game.pairsGuessed = 2
+// console.log(game.pairsGuessed);
+// console.log(game.isFinished())
