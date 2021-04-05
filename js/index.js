d52448d6e709ceeec642bbf2432cddbc9538e80f
hisamparker <hisamparker@gmail.com>
2021-04-05 06:55:30 +0200

    done

--- js/index.js
+++ js/index.js
@@ -26,8 +26,112 @@ const cards = [
 ];
 
 const memoryGame = new MemoryGame(cards);
+pickedCards = memoryGame.pickedCards;
+
+
+const flipCard = (e) => {
+  const target = e.currentTarget;
+  
+  if (!target.classList.contains('turned')){
+    target.classList.add('turned');
+  } else {
+    target.classList.remove('turned');
+  }
+
+  if (pickedCards.length < 2) {
+    pickedCards.push(target);
+    console.log(pickedCards);
+  }
+
+  if (pickedCards.length === 2) {
+    blockPlay();
+    compareCards(pickedCards);
+  }
+  
+};
+
+//this blocks the player from flipping more cards in the case that they have already selected two cards
+const blockPlay = () => {
+  document.querySelector('body').classList.add('block_play');
+};
+
+const compareCards = ([card1, card2]) => {
+  card1 = card1.dataset.cardName;
+  card2 = card2.dataset.cardName;
+  
+  const result = memoryGame.checkIfPair(card1,card2);
+
+  disableMatched(result);
+  reSet();
+  printStats();
+
+  const finished = memoryGame.isFinished(); 
+  if (finished) {youWin();} 
+
+};
+
+const disableMatched = (result) => {
+  if (result) {
+    pickedCards[0].classList.add('disabled'); 
+    pickedCards[1].classList.add('disabled');
+   } 
+};
+
+const printStats = () => {
+  document.getElementById('pairs-clicked').innerText = memoryGame.pairsClicked;
+  document.getElementById('pairs-guessed').innerText = memoryGame.pairsGuessed;
+};
+
+const reSet = () => {
+  setTimeout(() => {
+    document.querySelector('body').classList.remove('block_play');
+    document.querySelectorAll('.card:not(.disabled)').forEach(card => {
+      card.classList.remove('turned');
+    });
+      pickedCards.length = 0;
+  }, 1000);
+};
+
+const youWin = () => {
+  setTimeout(() => {
+    const body = document.querySelector('body');
+    
+    const pyroDiv = document.createElement('div'); 
+    pyroDiv.classList.add('pyro');
+    pyroDiv.style.margin = '0';
+    pyroDiv.style.padding = '0';
+    pyroDiv.style.width = '100%';
+    pyroDiv.style.height = '1800px';
+    pyroDiv.style.background = 'black';
+    pyroDiv.style.overflow = 'hidden';
+    pyroDiv.style.position = 'fixed';
+    pyroDiv.style.top = '0';
+    pyroDiv.style.zIndex = '10';
+
+    body.appendChild(pyroDiv);
+
+    const winText = document.createElement('h1');
+    winText.innerText = 'You are a fucking wizard.';
+
+    pyroDiv.appendChild(winText);
+
+    const beforePyroDiv = document.createElement('div');
+    beforePyroDiv.classList.add('pyro');
+    beforePyroDiv.classList.add('before');
+    const afterPyroDiv = document.createElement('div');
+    afterPyroDiv.classList.add('pyro');
+    afterPyroDiv.classList.add('after');
+
+    
+    pyroDiv.appendChild(beforePyroDiv);
+    pyroDiv.appendChild(afterPyroDiv);
+    }, 1000
+  );
+};
+
 
 window.addEventListener('load', event => {
+  memoryGame.shuffleCards();
   let html = '';
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
@@ -41,9 +145,9 @@ window.addEventListener('load', event => {
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+    card.addEventListener('click', (e) => {
+      flipCard(e);
     });
   });
 });
+
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,41 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  
+  shuffleCards() {
+    let numOfCards = this.cards.length, temporaryValue, randomIndex;
+
+    while (numOfCards) {
+      //use math.random to generate a random floating num between 0 and < 1, then multiply it by the number of cards and use math.floor to convert it to an integer
+      randomIndex = Math.floor(Math.random() * numOfCards);
+      
+      numOfCards -= 1;
+      //swap the values
+      // Bind this.cards[numOfCards] value to temporary value  
+      temporaryValue = this.cards[numOfCards];
+      this.cards[numOfCards] = this.cards[randomIndex];
+      //assign this.cards[randomIndex]the value of temporaryValue
+      this.cards[randomIndex] = temporaryValue;
+    }
+  }
+  
+  checkIfPair(card1, card2) {
+   this.pairsClicked++;
+    
+    if (card1 === card2) {
+      //if the cards match, add 1 to pairsGuessed and return true
+      this.pairsGuessed++;
+      return true;
+    } 
+    //return false if pairs don't match
+    return false;
+  }
+  
+  isFinished() {
+    return this.pairsGuessed * 2 === this.cards.length;
+  }
 }
