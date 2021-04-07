0712a946b42093c543eef6a64f8aab59b5dc0a37
Sophie Krag <sophie.9@live.nl>
2021-04-07 14:48:24 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,33 +1,105 @@
-const cards = [
-  { name: 'aquaman', img: 'aquaman.jpg' },
-  { name: 'batman', img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four', img: 'fantastic-four.jpg' },
-  { name: 'flash', img: 'flash.jpg' },
-  { name: 'green arrow', img: 'green-arrow.jpg' },
-  { name: 'green lantern', img: 'green-lantern.jpg' },
-  { name: 'ironman', img: 'ironman.jpg' },
-  { name: 'spiderman', img: 'spiderman.jpg' },
-  { name: 'superman', img: 'superman.jpg' },
-  { name: 'the avengers', img: 'the-avengers.jpg' },
-  { name: 'thor', img: 'thor.jpg' },
-  { name: 'aquaman', img: 'aquaman.jpg' },
-  { name: 'batman', img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four', img: 'fantastic-four.jpg' },
-  { name: 'flash', img: 'flash.jpg' },
-  { name: 'green arrow', img: 'green-arrow.jpg' },
-  { name: 'green lantern', img: 'green-lantern.jpg' },
-  { name: 'ironman', img: 'ironman.jpg' },
-  { name: 'spiderman', img: 'spiderman.jpg' },
-  { name: 'superman', img: 'superman.jpg' },
-  { name: 'the avengers', img: 'the-avengers.jpg' },
-  { name: 'thor', img: 'thor.jpg' }
+const cards = [{
+    name: 'aquaman',
+    img: 'aquaman.jpg'
+  },
+  {
+    name: 'batman',
+    img: 'batman.jpg'
+  },
+  {
+    name: 'captain america',
+    img: 'captain-america.jpg'
+  },
+  {
+    name: 'fantastic four',
+    img: 'fantastic-four.jpg'
+  },
+  {
+    name: 'flash',
+    img: 'flash.jpg'
+  },
+  {
+    name: 'green arrow',
+    img: 'green-arrow.jpg'
+  },
+  {
+    name: 'green lantern',
+    img: 'green-lantern.jpg'
+  },
+  {
+    name: 'ironman',
+    img: 'ironman.jpg'
+  },
+  {
+    name: 'spiderman',
+    img: 'spiderman.jpg'
+  },
+  {
+    name: 'superman',
+    img: 'superman.jpg'
+  },
+  {
+    name: 'the avengers',
+    img: 'the-avengers.jpg'
+  },
+  {
+    name: 'thor',
+    img: 'thor.jpg'
+  },
+  {
+    name: 'aquaman',
+    img: 'aquaman.jpg'
+  },
+  {
+    name: 'batman',
+    img: 'batman.jpg'
+  },
+  {
+    name: 'captain america',
+    img: 'captain-america.jpg'
+  },
+  {
+    name: 'fantastic four',
+    img: 'fantastic-four.jpg'
+  },
+  {
+    name: 'flash',
+    img: 'flash.jpg'
+  },
+  {
+    name: 'green arrow',
+    img: 'green-arrow.jpg'
+  },
+  {
+    name: 'green lantern',
+    img: 'green-lantern.jpg'
+  },
+  {
+    name: 'ironman',
+    img: 'ironman.jpg'
+  },
+  {
+    name: 'spiderman',
+    img: 'spiderman.jpg'
+  },
+  {
+    name: 'superman',
+    img: 'superman.jpg'
+  },
+  {
+    name: 'the avengers',
+    img: 'the-avengers.jpg'
+  },
+  {
+    name: 'thor',
+    img: 'thor.jpg'
+  }
 ];
 
 const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', event => {
+  memoryGame.shuffleCards();
   let html = '';
   memoryGame.cards.forEach(pic => {
     html += `<div class="card" data-card-name="${pic.name}">`;
@@ -44,6 +116,41 @@ window.addEventListener('load', event => {
     card.addEventListener('click', () => {
       // TODO: write some code here
       console.log(`Card clicked: ${card}`);
+
+      card.classList.toggle("turned");
+
+      const pickedCards = memoryGame.pickedCards;
+      pickedCards.push(card);
+
+
+      if (pickedCards.length === 2) {
+        let firstCard = pickedCards[0];
+        let secondCard = pickedCards[1];
+
+        firstCardName = firstCard.getAttribute("data-card-name");
+        secondCardName = secondCard.getAttribute("data-card-name");
+
+        if (memoryGame.checkIfPair(firstCardName, secondCardName)) {
+          firstCard.classList.add("stop");
+          secondCard.classList.add("stop");
+          pickedCards.length = 0;
+
+        } else {
+          setTimeout(() => {
+            firstCard.classList.toggle("turned");
+            secondCard.classList.toggle("turned");
+
+          }, 1000);
+
+          pickedCards.length = 0;
+
+        }
+        document.getElementById("pairs-clicked").innerHTML = memoryGame.pairsClicked;
+        document.getElementById("pairs-guessed").innerHTML = memoryGame.pairsGuessed;
+      }
+      if (memoryGame.isFinished()) {
+        alert(`Good job! You have finished the game in ${memoryGame.pairsClicked} guesses`)
+      }
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,33 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    let i = this.cards.length,
+      j, temp;
+    while (--i > 0) {
+      j = Math.floor(Math.random() * (i + 1))
+      temp = this.cards[j]
+      this.cards[j] = this.cards[i]
+      this.cards[i] = temp
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+
+    if (card1 === card2) {
+      this.pairsGuessed++
+      return true
+    }
+    return false
+  }
+
+  isFinished() {
+    return this.pairsGuessed === (this.cards.length / 2)
+  }
 }
