09ab5b19cc8d3d2406b7ce1c3e96a53fe82b7b0f
joserra <jrcid@mib.isdi.es>
2021-04-17 20:05:02 +0200

    item1 and item2

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,25 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+
     // add the rest of the class properties here
   }
   shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked ++;
+    const areEquals = card1 === card2;
+    if (areEquals) {
+      this.pairsGuessed += 1;
+    }
+    return areEquals;
+  }
+  isFinished() {
+    return this.pairsGuessed === this.cards.length/2
+  }
 }
