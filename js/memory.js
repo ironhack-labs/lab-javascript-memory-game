34d6332088af7ee26b4e42afd45525045d19a751
santiago <gnovoa013.w@gmail.com>
2021-05-01 16:06:49 -0500

    Added changes to class and jazmine work

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,35 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  
+  shuffleCards() {
+    for (let i = this.cards.length; i > 0; i--) {
+      let random = Math.floor(Math.random() * i--);
+      let temp = this.cards[i];
+      this.cards[i] = this.cards[random];
+      this.cards[random] = temp;
+    }
+  }
+  
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    let aux_bool = false;
+    if(card1 === card2){
+      aux_bool = true;
+      this.pairsGuessed += 1;
+    }
+    return aux_bool;
+  }
+  
+  isFinished() {
+    let finished = false;
+    if(this.pairsGuessed == this.cards.length/2){
+      finished = true;
+    }
+    return finished;
+  }
 }
