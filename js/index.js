b51fe2aeea3b867083046ed8bba2d168faeded1b
andres_rog <andres_rog@outlook.com>
2021-05-04 12:51:42 -0500

    lab finished

--- js/index.js
+++ js/index.js
@@ -26,6 +26,10 @@ const cards = [
 ];
 
 const memoryGame = new MemoryGame(cards);
+const pairGuessedCounter = document.getElementById('pairs-guessed');
+const pairsClickedCounter = document.getElementById('pairs-clicked');
+
+memoryGame.shuffleCards(cards);
 
 window.addEventListener('load', event => {
   let html = '';
@@ -39,11 +43,65 @@ window.addEventListener('load', event => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
-  // Bind the click event of each element to a function
+  function blockClickedCards(selectedCards){
+    selectedCards.forEach(card => {
+      card.classList.add('blocked');
+    });
+  }
+
+  function unblockClickedCards(selectedCards){
+    selectedCards.forEach(card => {
+      card.classList.remove('blocked');
+    });
+  }
+
+  function turnCardsBack(selectedCards) {
+    selectedCards.forEach(card => {
+      card.classList.remove('turned');
+    });
+  
+    //Permitir al usuario seleccionar cartas hasta voltearlas de regreso
+    unblockClickedCards(selectedCards);
+    memoryGame.pickedCards = [];
+  }
+
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      //Verificar que la carta no este bloqueada
+
+      if(!card.classList.contains('blocked') && memoryGame.pickedCards.length<2) {
+        //Voltear la carta con la clase turned
+        card.classList.add('turned');
+
+        //Agregarla a el array pickedCards
+        memoryGame.pickedCards.push(card);
+
+        //Si la cantidad de cartas seleccionadas es 2, verificar si es par o no.
+        if(memoryGame.pickedCards.length === 2) {
+          const pickedCards = memoryGame.pickedCards;
+          const isPair = memoryGame.checkIfPair(pickedCards[0].getAttribute('data-card-name'), pickedCards[1].getAttribute('data-card-name'));
+
+          pairsClickedCounter.innerHTML = memoryGame.pairsClicked;
+
+          if(!isPair) {
+            blockClickedCards(pickedCards);
+            setTimeout(()=>{turnCardsBack(pickedCards)}, '1000');
+          }
+          else {
+            blockClickedCards(pickedCards);
+
+            memoryGame.pickedCards = [];
+
+            pairGuessedCounter.innerHTML = memoryGame.pairsGuessed;
+
+            if(memoryGame.isFinished()){
+              setTimeout(()=>{alert("You won!!!");}, '500');
+              
+            }
+          }
+        }
+      }
+
     });
   });
 });
