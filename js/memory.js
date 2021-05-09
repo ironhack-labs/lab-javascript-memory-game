07ddad8da102924c1ca194d125cad632a0a97144
Lucia Sanchez <luci.sanchez.ortiz@gmail.com>
2021-05-09 10:47:34 +0200

    Iteration2

--- js/memory.js
+++ js/memory.js
@@ -2,8 +2,33 @@ class MemoryGame {
   constructor(cards){
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    for (let i = this.cards.length; i > 0; i--){
+      let j = Math.floor(Math.random()*(i--));
+      let temp = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = temp;
+    } 
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked = 1;
+    if(card1 === card2){
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+  isFinished() {
+    if(this.pairsGuessed === this.cards.length/2){
+      return true;
+    }else {
+      return false;
+    }
+  }
 }
