19e14fcb9ce78db4ac943d74aa92b891132becd5
Andrei <andrei_1002@hotmail.com>
2021-05-13 18:15:12 +0200

    terminado constructor y segundo método

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,33 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
+
+  shuffleCards() {
+   /*Fisher yates algorithm
+    const shuffleArray = array => {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }*/
+  }
+
+  checkIfPair(card1, card2) {
+    if (card1 === card2){
+      this.pairsGuessed++;
+      this.pairsClicked++;
+      return true;
+    } else {
+      this.pairsClicked++;
+      return false;
+    }
+
+  }
   isFinished() {}
 }
