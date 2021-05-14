39e5ac9a32bb761842571acba0b5c90170725954
Andrei <andrei_1002@hotmail.com>
2021-05-14 16:44:46 +0200

    terminado lab

--- js/index.js
+++ js/index.js
@@ -42,8 +42,38 @@ window.addEventListener('load', event => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
+
       console.log(`Card clicked: ${card}`);
+
+      card.classList.toggle("turned");
+
+      memoryGame.pickedCards.push(card);
+
+      if (memoryGame.pickedCards.length === 2) {
+        const card1 = memoryGame.pickedCards[0];
+        const card2 = memoryGame.pickedCards[1];
+        const card1Name = card1.getAttribute("data-card-name");
+        const card2Name = card2.getAttribute("data-card-name");
+
+        if (memoryGame.checkIfPair(card1Name, card2Name)){
+          card1.classList.add("blocked");
+          card2.classList.add("blocked");
+        } else {
+          setTimeout(() => {
+            card1.classList.toggle("turned");
+            card2.classList.toggle("turned");
+          }, 1000);
+        }
+        document.getElementById("pairs-clicked").innerHTML = memoryGame.pairsClicked;
+        document.getElementById("pairs-guessed").innerHTML = memoryGame.pairsGuessed;
+
+        memoryGame.pickedCards.length = 0;
+      }
+
+      if(memoryGame.isFinished()) {
+
+        alert("You are a winner, baby!")
+      }
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -7,15 +7,13 @@ class MemoryGame {
   }
 
   shuffleCards() {
-   /*Fisher yates algorithm
-    const shuffleArray = array => {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
-    }*/
+   
+    for (let i = this.cards.length -1; i > 0; i--){
+      const j = Math.floor(Math.random()*(i + 1));
+      const temp = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = temp;
+    }
   }
 
   checkIfPair(card1, card2) {
@@ -29,5 +27,12 @@ class MemoryGame {
     }
 
   }
-  isFinished() {}
+  isFinished() {
+
+    if (this.pairsGuessed === this.cards.length/2){
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
