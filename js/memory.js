7170ac0df6ee6f3b00557a1adb402d18485dffd8
Miki Geoghegan <michaelageoghegan@hotmail.co.uk>
2021-06-09 21:27:04 +0200

    all bar one complete

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,65 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
-  shuffleCards() {
-    // ... write your code here
+  shuffleCards(argument1) {
+    if (!argument1) {
+      return undefined
+    } else {
+      let newArray = this.cards;
+      for (let i = this.cards.length-1; i>0; i--) {
+        let shuffledCards = Math.floor(Math.random()*(i+1))
+        let randomCard = newArray[shuffledCards];
+        newArray[shuffledCards] = newArray[i];
+        newArray[i] = randomCard;
+    }
+    return newArray;
+    }
   }
+
+    /*
+    let currentIndex = cardsArray.length, randomIndex;
+    if (cardsArray) {
+      return undefined;
+    } else {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [cardsArray[currentIndex]], cardsArray[randomIndex] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return cardsArray
+  }
+
+
+    shuffledCards = eachCard.sort(() => Math.random() - 0.5);
+    }
+    return shuffledCards;
+  }
+  */
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    if (card1 === card2) {
+      this.pairsClicked += 1;
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      this.pairsClicked += 1;
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    let correctPairs = this.pairsGuessed
+    if (correctPairs < this.cards.length/2) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
 
