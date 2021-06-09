ec9533b6aebedb912cfde048424f7811ee181bba
alexandreoliv <alexandrebo@gmail.com>
2021-06-09 13:07:45 +0200

    done

--- js/index.js
+++ js/index.js
@@ -44,8 +44,38 @@ window.addEventListener('load', (event) => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      let isPair = false;
+      let pairsClicked = document.getElementById('pairs-clicked');
+      let pairsGuessed = document.getElementById('pairs-guessed');
+      let score = document.getElementsByTagName('h2');
+
+      console.log('Card clicked: ', card);
+      card.classList.add('turned'); // Shows the card
+
+      if (memoryGame.pickedCards.length < 2) { // Player hasn't clicked on two cards yet, so pickedCards can be updated with the clicked card
+        memoryGame.pickedCards.push(card);
+      }
+      else { // Player has already selected two cards which were not a pair, so pickedCards needs to be emptied before the clicked card is added
+        memoryGame.pickedCards[1].classList.remove('turned');
+        memoryGame.pickedCards.pop();
+        memoryGame.pickedCards[0].classList.remove('turned');
+        memoryGame.pickedCards.pop();
+        memoryGame.pickedCards.push(card);
+      }
+      
+      if (memoryGame.pickedCards.length === 2) { // If there are two picked cards, compare if they form a pair
+        isPair = memoryGame.checkIfPair(memoryGame.pickedCards[0].attributes[1].value,memoryGame.pickedCards[1].attributes[1].value);
+        pairsClicked.textContent = memoryGame.pairsClicked; // Updates the score with the pairs clicked
+        if (isPair) {
+          // Cleans the pickedCards array but leaves the cards open
+          memoryGame.pickedCards.pop();
+          memoryGame.pickedCards.pop();
+          pairsGuessed.textContent = memoryGame.pairsGuessed; // Updates the score
+        }  
+      }
+      
+      if (memoryGame.checkIfFinished())
+        score[0].innerText = 'YOU WON';
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -1,21 +1,75 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.shuffleCards(this.cards);
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    var m = this.cards.length, t, i;
+    
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = this.cards[m];
+      this.cards[m] = this.cards[i];
+      this.cards[i] = t;
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      console.log('Pairs guessed: ' + this.pairsGuessed)
+      return true;
+    }
+    return false;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed == this.cards.length/2) return true
+    return false
   }
 }
+
+// let game = new MemoryGame(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']);
+// console.log(game.cards);
+// game.cards = game.shuffleCards();
+// console.log(game.cards);
+// console.log('Opening two cards... Are they the same? ' + game.checkIfPair(game.cards[0],game.cards[1]))
+// console.log('Opening two cards... Are they the same? ' + game.checkIfPair(game.cards[0],game.cards[2]))
+// console.log('Opening two cards... Are they the same? ' + game.checkIfPair(game.cards[0],game.cards[3]))
+// console.log('Opening two cards... Are they the same? ' + game.checkIfPair(game.cards[0],game.cards[4]))
+// console.log('Opening two cards... Are they the same? ' + game.checkIfPair(game.cards[0],game.cards[5]))
+// console.log('Opening two cards... Are they the same? ' + game.checkIfPair(game.cards[0],game.cards[6]))
+// console.log('Opening two cards... Are they the same? ' + game.checkIfPair(game.cards[0],game.cards[7]))
+// console.log('Opening two cards... Are they the same? ' + game.checkIfPair(game.cards[0],game.cards[8]))
+// console.log('Opening two cards... Are they the same? ' + game.checkIfPair(game.cards[0],game.cards[9]))
+// console.log('Opening two cards... Are they the same? ' + game.checkIfPair(game.cards[0],game.cards[10]))
+// console.log('Has the game finished yet? ' + game.checkIfFinished())
+
+// let game2 = new MemoryGame(['1', '2', '1', '2']);
+// console.log(game2.cards);
+// game2.cards = game2.shuffleCards();
+// console.log(game2.cards);
+// console.log('Opening two cards... Are they the same? ' + game2.checkIfPair(game2.cards[0],game2.cards[1]))
+// console.log('Has the game finished yet? ' + game2.checkIfFinished())
+// console.log('Opening two cards... Are they the same? ' + game2.checkIfPair(game2.cards[0],game2.cards[2]))
+// console.log('Has the game finished yet? ' + game2.checkIfFinished())
+// console.log('Opening two cards... Are they the same? ' + game2.checkIfPair(game2.cards[0],game2.cards[3]))
+// console.log('Has the game finished yet? ' + game2.checkIfFinished())
+// console.log('Opening two cards... Are they the same? ' + game2.checkIfPair(game2.cards[1],game2.cards[2]))
+// console.log('Has the game finished yet? ' + game2.checkIfFinished())
+// console.log('Opening two cards... Are they the same? ' + game2.checkIfPair(game2.cards[1],game2.cards[3]))
+// console.log('Has the game finished yet? ' + game2.checkIfFinished())
+
 
 // The following is required for automated testing. Please, ignore it.
 if (typeof module !== 'undefined') module.exports = MemoryGame;
