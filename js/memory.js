5cb03f6a04ac61052d15748f15b3ad07f547d10d
Nicholas Zanotto <nicholas.zanotto@hotmail.com>
2021-06-09 23:54:23 +0200

    [WEB MAD DE]

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,40 @@
 class MemoryGame {
   constructor(cards) {
-    this.cards = cards;
-    // add the rest of the class properties here
+    this.cards = cards
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined;
+    } else {
+      for (let i = this.cards.length; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i-- ));
+        this.cards[i] = this.cards[j]
+        this.cards[j] = this.cards[i];
+      }
+    }
   }
+  
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+     if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      this.pairsClicked += 1;
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
