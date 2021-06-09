37f1b70e2825129566b4f58230694b3d9d5bf557
Kikec82 <kikecolo2007@yahoo.es>
2021-06-09 23:13:49 +0200

    memory game

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,46 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards=[];
+    this.pairsClicked= 0;
+    this.pairsGuessed=0;
     // add the rest of the class properties here
   }
 
-  shuffleCards() {
-    // ... write your code here
-  }
+  shuffleCards(array) {
+      if (array === undefined ){
+        return undefined
+      }
+      var currentIndex = array.length, randomIndex;   //Fisher Yates Shuffle
+      while (0 !== currentIndex) {
+       randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
+        }
+      return array;
+}
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked=this.pairsClicked+1;
+    
+    if(card1===card2){
+      this.pairsGuessed = this.pairsGuessed+1;
+      return true;
+    } else {
+      return false
+    }
+        
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pickedCards.length===0){
+      return false
+    }
+    if (this.pairsGuessed === this.cards.length/2){
+      return true
+    }
   }
 }
 
