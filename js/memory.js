5557300a73b10d9ec838808de1723f18c17d79b0
juanfuerteshdz@gmail.com <juanfuerteshdz@gmail.com>
2021-06-10 00:08:59 +0200

    Done

--- js/memory.js
+++ js/memory.js
@@ -1,20 +1,44 @@
 class MemoryGame {
   constructor(cards) {
-    this.cards = cards;
-    // add the rest of the class properties here
+    this.cards = cards
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+
   }
 
   shuffleCards() {
-    // ... write your code here
+    for (let i = this.cards.length - 1; i > 0; i--) {   /// Copiado de internet (stackoverflow.com)
+      let j = Math.floor(Math.random() * (i + 1))       /// Y cambiando algunas palabras
+      let temp = this.cards[i]                          /// No entiendo ni el procedimiento que está siguiendo LOL
+      this.cards[i] = this.cards[j]
+      this.cards[j] = temp
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    if (card1 === card2) {
+      this.pairsClicked += 1
+      this.pairsGuessed += 1
+      return true
+    } else {
+      this.pairsClicked += 1
+      return false
+    }
+
   }
 
+
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true
+    } else {
+      return false
+    }
+
+
   }
+
 }
 
 // The following is required for automated testing. Please, ignore it.
