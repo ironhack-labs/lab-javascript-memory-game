418bcf725be726b4c8a510b2e408017d98eca476
Marco Santonastasi <marco.santonastasi@gmail.com>
2021-06-10 11:09:45 +0200

    WIP game logic done

--- js/memory.js
+++ js/memory.js
@@ -2,18 +2,37 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsGuessed = 0;
+    this.pairsClicked = 0;
+    this.shuffleCards();
   }
 
   shuffleCards() {
     // ... write your code here
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      this.checkIfFinished();
+      return true;
+    }
+    return false;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    let correctPairs = this.pairsGuessed;
+    if (correctPairs < this.cards.length / 2) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
 
