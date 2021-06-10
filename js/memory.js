0d4250f7e0642db87b0b547c3c0face24cd28b3d
Miki Geoghegan <michaelageoghegan@hotmail.co.uk>
2021-06-10 11:38:44 +0200

    newest

--- js/memory.js
+++ js/memory.js
@@ -6,22 +6,29 @@ class MemoryGame {
     this.pairsGuessed = 0;
   }
 
-  shuffleCards(argument1) {
-    if (!argument1) {
-      return undefined
-    } else {
-      let newArray = this.cards;
+  shuffleCards() {
+    for (let i = this.cards.length -1; i > 0; i--) {
+      const j = Math.floor(Math.random()*(i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+    return this.cards;
+  }
+
+
+    /*
+  shuffleCards() {
       for (let i = this.cards.length-1; i>0; i--) {
         let shuffledCards = Math.floor(Math.random()*(i+1))
-        let randomCard = newArray[shuffledCards];
-        newArray[shuffledCards] = newArray[i];
-        newArray[i] = randomCard;
+        let randomCard = this.cards[shuffledCards];
+        this.cards[shuffledCards] = newArray[i];
+        this.cards[i] = randomCard;
     }
-    return newArray;
+    return this.cards;
     }
   }
 
-    /*
+
+
     let currentIndex = cardsArray.length, randomIndex;
     if (cardsArray) {
       return undefined;
@@ -36,26 +43,56 @@ class MemoryGame {
   }
 
 
-    shuffledCards = eachCard.sort(() => Math.random() - 0.5);
-    }
+    shuffleCards(argument1) {
+    shuffledCards = eachCard.sort((a, b) => Math.random() - 0.5);
     return shuffledCards;
   }
+
+  
+    shuffleCards(argument1) {
+      for (let i = this.cards.length -1; i > 0; i--) {
+        const j = Math.floor(Math.random()*(i + 1));
+        [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+      }
+    }
+  
+
+      shuffleCards(argument1) {
+    if (!argument1) {
+      return undefined
+    } else {
+      let newArray = this.cards;
+      for (let i = this.cards.length-1; i>0; i--) {
+        let shuffledCards = Math.floor(Math.random()*(i+1))
+        let randomCard = newArray[shuffledCards];
+        newArray[shuffledCards] = newArray[i];
+        newArray[i] = randomCard;
+    }
+    return newArray;
+    }
+  }
+
   */
 
   checkIfPair(card1, card2) {
+    this.pairsClicked++;
     if (card1 === card2) {
-      this.pairsClicked += 1;
-      this.pairsGuessed += 1;
+      this.pairsGuessed++;
+      this.checkIfFinished();
       return true;
     } else {
-      this.pairsClicked += 1;
       return false;
     }
   }
 
+  resetClickedPair() {
+    this.pickedCards = [];
+  }
+
+
   checkIfFinished() {
     let correctPairs = this.pairsGuessed
-    if (correctPairs < this.cards.length/2) {
+    if (correctPairs < this.cards.length / 2) {
       return false;
     } else {
       return true;
