daa831ec2e7c9b07bbe62e7174eebc460472caa9
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-06-13 20:09:00 +0200

    unfinished

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,9 @@
 class MemoryGame {
-  constructor(cards, pickedCards, pairsClicked, pairsGuessed) {
+  constructor(cards) {
     this.cards = cards;
-    this.pickedCards = pickedCards;
-    this.pairsClicked = pairsClicked;
-    this.pairsGuessed = pairsGuessed;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
 
   }
 
@@ -12,11 +12,23 @@ class MemoryGame {
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1;
+
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true
+    }
+    else {
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true
+    } else {
+      return false
+    }
   }
 }
 
