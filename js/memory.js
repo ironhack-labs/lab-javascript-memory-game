724bdf33573afc67a5e62b6910166a3a822a4908
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-06-13 19:46:36 +0200

    Iteration 2.1

--- js/memory.js
+++ js/memory.js
@@ -1,7 +1,10 @@
 class MemoryGame {
-  constructor(cards) {
+  constructor(cards, pickedCards, pairsClicked, pairsGuessed) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = pickedCards;
+    this.pairsClicked = pairsClicked;
+    this.pairsGuessed = pairsGuessed;
+
   }
 
   shuffleCards() {
