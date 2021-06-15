0a09dfb9b7c759dcc0248575e9b643c55efe400d
Thiago Henrique <thsc47@gmail.com>
2021-06-14 22:51:23 -0300

    interation I

--- js/memory.js
+++ js/memory.js
@@ -1,21 +1,47 @@
 class MemoryGame {
   constructor(cards) {
-    this.cards = cards;
     // add the rest of the class properties here
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-
   shuffleCards() {
-    // ... write your code here
+    let arr = this.cards;
+    let i = arr.length; 
+    let j, temp;
+
+    while(--i > 0) {
+      j = Math.floor(Math.random() * (i+1));
+      temp = arr[j];
+      arr[j] = arr[i];
+      arr[i] = temp;
+    }
+    return arr
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    if (card1 === card2) {
+      this.pairsClicked += 1
+      this.pairsGuessed += 1
+      return true
+    } else {
+      this.pairsClicked += 1
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if(this.pairsGuessed === this.cards.length /2){
+      return true
+    }else{
+      return false
+    }
   }
 }
+
+
 
 // The following is required for automated testing. Please, ignore it.
 if (typeof module !== 'undefined') module.exports = MemoryGame;
