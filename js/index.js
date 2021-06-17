8cc70d56d5b8f0b56fa4af224da6322481cc1aab
Ana Carolina R Lima <anacarolina.rdel@gmail.com>
2021-06-17 01:49:57 -0300

    solved lab

--- js/index.js
+++ js/index.js
@@ -27,7 +27,62 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+const pairsClickedElement = document.getElementById('pairs-clicked');
+const pairsGuessedElement = document.getElementById('pairs-guessed');
+
+function turnCard(card) {
+  if (memoryGame.pickedCards.length < 2) {
+    card.classList.add("turned")
+  }
+}
+
+function addToPickedCards(card) {
+  if (memoryGame.pickedCards.length < 2) {
+    memoryGame.pickedCards.push(card.getAttribute('data-card-name'));
+  }
+};
+
+function checkIfPickedCardsIsPair() {
+  if (memoryGame.pickedCards.length === 2) {
+    let firstCard = memoryGame.pickedCards[0];
+    let secondCard = memoryGame.pickedCards[1];
+    let isPair = memoryGame.checkIfPair(firstCard, secondCard);
+    updatePairsElements();
+    if (isPair === false) {
+      setTimeout(() => {
+        unturnCard(firstCard);
+        unturnCard(secondCard);
+        memoryGame.pickedCards = [];
+      }, 1000)
+    } else {
+      memoryGame.pickedCards = [];
+      if (memoryGame.checkIfFinished()) {
+        let html = ''
+        html += `
+        <div><h1>YOU WON!</h1></div>
+        `
+        document.querySelector('#memory-board').innerHTML = html;
+      }
+    }
+  }
+}
+
+function updatePairsElements() {
+  pairsClickedElement.innerText = memoryGame.pairsClicked;
+  pairsGuessedElement.innerText = memoryGame.pairsGuessed;
+}
+
+function unturnCard(card) {
+  document.querySelectorAll('.card').forEach((cardElement) => {
+    if (cardElement.getAttribute('data-card-name') === card) {
+      card.classList.remove("turned");
+      break;
+    }
+  });
+}
+
 window.addEventListener('load', (event) => {
+  memoryGame.shuffleCards();
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
@@ -44,8 +99,11 @@ window.addEventListener('load', (event) => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      if (memoryGame.pickedCards.length < 2) {
+        turnCard(card);
+        addToPickedCards(card);
+        checkIfPickedCardsIsPair();
+      }
     });
   });
 });
