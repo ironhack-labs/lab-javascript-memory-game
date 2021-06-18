d8fca4cd0455134181b1a5dff1bc736b918a16cd
Fran Lara <francesc.lara@hotmail.com>
2021-06-18 12:26:06 +0200

    lab done

--- js/memory.js
+++ js/memory.js
@@ -1,42 +1,47 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    // add the rest of the class properties here
     this.pickedCards = [];
-    this.pairsClicked = 0;
     this.pairsGuessed = 0;
+    this.pairsClicked = 0;
+    this.shuffleCards();
+    this.score = 0;
   }
 
   shuffleCards() {
-if (!this.cards) return undefined;
-  else{
-    for(let i=this.cards.length; i>0;i--){
-      const randomNum = Math.floor(Math.random() * i--);
-      this.cards[i] = this.cards[randomNum];
-      this.cards[randomNum] = this.cards[i];
+    // ... write your code here
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
     }
-
-  }  
-
-}
-
-
-
+  }
 
   checkIfPair(card1, card2) {
-    this.pairsClicked++
-    if (card1 === card2){
-      this.pairsGuessed++
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      this.checkIfFinished();
       return true;
     } else {
       return false;
     }
   }
 
+  resetClickedPair() {
+    this.pickedCards = [];
+  }
+
   checkIfFinished() {
-    if(this.pairsGuessed === this.cards.length / 2) return true
-    else return false;
+    let correctPairs = this.pairsGuessed;
+    if (correctPairs < this.cards.length / 2) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
 
 // The following is required for automated testing. Please, ignore it.
 if (typeof module !== 'undefined') module.exports = MemoryGame;
+
