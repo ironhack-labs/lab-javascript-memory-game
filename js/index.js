e9500a8218cf885886eb26a8c890ccd8a265b19e
DeAT1995 <debora2013aguiartavares@gmail.com>
2021-06-22 17:08:44 -0300

    work finished

--- js/index.js
+++ js/index.js
@@ -44,8 +44,15 @@ window.addEventListener('load', (event) => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
       console.log(`Card clicked: ${card}`);
+      flipCard(card);
     });
   });
 });
+
+function flipCard (card) {
+  card.classList.toggle ( 'abc', someBool ) ;
+    });
+  });
+});
+}
--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,50 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if(!this.cards){
+      return undefined;
+    }
+
+    let arr = this.cards;
+    let i = arr.length;
+    let j, temp;  
+    while (--i > 0) {
+      j = Math.floor(Math.random() * (i + 1));
+      temp = arr[j];
+      arr[j] = arr[i];
+      arr[i] = temp;
+    }
+    return arr;
   }
 
+
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked+=1;
+
+    if (card1 === card2) {
+
+      this.pairsGuessed++;
+      return true;
+    } else {
+
+      return false;
+    }
+
   }
 
   checkIfFinished() {
-    // ... write your code here
+   if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else {
+      return false;
+    }
+
   }
 }
 
