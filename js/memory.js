b29c4b931a266eb0838840b7bdb36cfa70e0648b
Thiago Marques <morais.marques@hotmail.com>
2021-08-08 16:27:39 -0300

    done

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,42 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
-    // ... write your code here
+
+    if(!this.cards) {
+      return undefined;
+    } 
+      let len = this.cards.length;
+      while (len > 0) {
+      len--;
+      let randomIndex = Math.floor(Math.random() * len);
+      const randomCard = this.cards[randomIndex];
+      this.cards[len] = randomCard;
+      }
   }
+
+
+    // ... write your code here
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked += 1;
+
+      if (card1 === card2) {
+        this.pairsGuessed += 1;
+        return true;
+      }
+        return false;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    return this.cards.length / 2 === this.pairsGuessed;
   }
 }
 
