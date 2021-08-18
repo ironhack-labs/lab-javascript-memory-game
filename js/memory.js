6310946a6482057b4167512066d54277c0d0c829
Alberto Massa <massa.alberto@outlook.com>
2021-08-18 20:07:31 +0200

    wip
    missing DOM part

--- js/memory.js
+++ js/memory.js
@@ -1,20 +1,37 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-
   shuffleCards() {
-    // ... write your code here
-  }
+    if (!this.cards) {
+      return undefined;
+    } else {
+      this.cards.length -= 1;
+			return Math.floor(Math.random() * this.cards.length) +1;
+      };
+  };
+  
 
   checkIfPair(card1, card2) {
-    // ... write your code here
-  }
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   checkIfFinished() {
-    // ... write your code here
-  }
+    if (this.pairsGuessed === 0 || this.pairsGuessed < this.cards.length / 2) {
+      return false;
+    } else {
+      return true;
+    }
+  };
 }
 
 // The following is required for automated testing. Please, ignore it.
