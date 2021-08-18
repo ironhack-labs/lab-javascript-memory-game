31b516d21b5a68b04d385819502686c14c756951
Ivan Espinoza <icaro_19@hotmail.com>
2021-08-18 21:58:24 +0200

    update

--- js/index.js
+++ js/index.js
@@ -27,6 +27,27 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+let turn = []
+
+// function cardTurn() {
+//   memoryGame.checkPair()
+
+// }
+function pickCard(card) {
+  card.setAttribute('class', 'card turned')
+}
+
+function isEqualsCard(jugadas) {
+  if (memoryGame.checkIfPair(jugadas[0], jugadas[1])) {
+    console.log("iguales")
+  } else {
+    console.log("no Equals")
+    turn.pop()
+    turn.pop()
+  }
+  // console.log(memoryGame.checkIfPair(jugadas[0], jugadas[1]))
+}
+
 window.addEventListener('load', (event) => {
   let html = '';
   memoryGame.cards.forEach((pic) => {
@@ -44,8 +65,24 @@ window.addEventListener('load', (event) => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
+
+      // memoryGame.checkPair()
+      pickCard(card)
+
+      let cardName = card.getAttribute("data-card-name")
+      // compareNameImages( card.getAttribute("data-card-name"))
+      if (turn.length < 2) {
+        turn.push(card.getAttribute("data-card-name"))
+
+        isEqualsCard(turn)
+        console.log(card.getAttribute("data-card-name"))
+      }
+      console.log(turn)
+
+      // let cardItem = document.getElementsByName(`${car}`)
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      // console.log(`Card clicked: ${card}`);
+
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,51 @@
 class MemoryGame {
   constructor(cards) {
-    this.cards = cards;
+    this.cards = cards
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
     // add the rest of the class properties here
+
   }
 
   shuffleCards() {
+    try {
+      if (this.cards) {
+        for (var i = this.cards.length - 1; i > 0; i--) {
+          const swapIndex = Math.floor(Math.random() * (i - 1))
+          const currentCard = this.cards[i]
+          const cardToSwap = this.cards[swapIndex]
+          this.cards[i] = cardToSwap
+          this.cards[swapIndex] = currentCard
+        }
+        // return this.pickedCards
+      }
+    } catch (error) {
+      throw new Error(error)
+    }
+    // return this.pickedCards
     // ... write your code here
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    try {
+      this.pairsClicked++
+      if (card1 === card2) {
+        this.pickedCards.push(card1)
+        this.pairsGuessed++
+        return true
+      } else {
+        return false
+      }
+    } catch (error) {
+      console.log(error.message)
+    }
   }
 
   checkIfFinished() {
     // ... write your code here
+    return this.pairsGuessed === this.cards.length / 2
+
   }
 }
 
