14ec07f6b5775ce1e4ddd718de52f5f6490f4a44
lauraAlabau <arlaura3@gmail.com>
2021-08-18 16:59:58 +0200

    Lab - JS Logic - Laura y Vladyslav

--- js/memory.js
+++ js/memory.js
@@ -2,18 +2,47 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0; //almacenaremos las tarjetas en las que el usuario ha hecho clic para poder compararlas.
+    this.pairsGuessed = 0; // almacena las tarjetas emparejadas
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined;
+    }
+    // copia superficial
+    // const cardsCopy = [...this.cards]
+    // copia profunda ambas funcionarian
+    const cardsCopy = JSON.parse(JSON.stringify(this.cards))
+    var m = cardsCopy.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+      // And swap it with the current element.
+      t = cardsCopy[m];
+      cardsCopy[m] = cardsCopy[i];
+      cardsCopy[i] = t;
+    }
+    this.cards = cardsCopy;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked = this.pairsClicked + 1;
+    if(card1 === card2){
+      this.pairsGuessed = this.pairsGuessed + 1;
+      return true;
+    } 
+    return false
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if ((this.cards.length/2) === this.pairsGuessed) {
+      return true;
+    }
+    return false
   }
 }
 
