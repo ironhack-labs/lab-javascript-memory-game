93e60778822964cd5f6f2214b287d99bf942a2b8
VanBan273 <87871463+VanBan273@users.noreply.github.com>
2021-08-19 00:01:00 +0200

    WD082021 - Iván Romero Oyola
    Grupo con nina

--- js/memory.js
+++ js/memory.js
@@ -1,20 +1,50 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!arguments) return undefined;
+
+    // copia superficial
+    // const cardsCopy = [...this.cards]
+    // copia profunda ambas funcionarian
+    const cardsCopy = JSON.parse(JSON.stringify(this.cards))
+    var m = cardsCopy.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = cardsCopy[m];
+      cardsCopy[m] = cardsCopy[i];
+      cardsCopy[i] = t;
+    }
+
+    this.cards = cardsCopy;
   }
 
-  checkIfPair(card1, card2) {
-    // ... write your code here
+
+    checkIfPair(card1, card2) {
+      this.pairsClicked += 1
+     if (card1 === card2){
+      this.pairsGuessed +=1
+       return true
+     }
+     return false
+
+
   }
 
   checkIfFinished() {
-    // ... write your code here
-  }
+    return this.cards.length / this.pairsGuessed === 2
+}
 }
 
 // The following is required for automated testing. Please, ignore it.
