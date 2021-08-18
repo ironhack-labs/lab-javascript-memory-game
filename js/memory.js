74a8dd573b4e3ef8f490e4b70038b14909362878
jdavesa <86001040+jdavesa@users.noreply.github.com>
2021-08-18 18:33:52 +0200

    Update memory.js

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,43 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
-    // ... write your code here
+    if(!this.cards) return undefined;
+    let currentIndex = this.cards.length;
+    let randomIndex;
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      // And swap it with the current element.
+      [this.cards[currentIndex], this.cards[randomIndex]] = [ this.cards[randomIndex],this.cards[currentIndex]];
+      
+     // console.log(this.cards);
+    }
+    return this.cards;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked ++
+
+    if(card1 === card2) this.pairsGuessed++
+    
+    return card1===card2
   }
+
+    // ... write your code here
+  
 
   checkIfFinished() {
     // ... write your code here
+    return (this.cards.length/2) === this.pairsGuessed
   }
 }
 
