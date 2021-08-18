485bffaf3f7ee0f4168227195a8aedbdbd46947c
Sherab Pereira <sherabpereira@gmail.com>
2021-08-18 15:54:12 +0200

    Method shuffleCards implemented and tested

--- js/memory.js
+++ js/memory.js
@@ -2,10 +2,30 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+
+    if(!this.cards) return undefined;
+
+    let currentIndex = this.cards.length;
+    let randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [this.cards[currentIndex], this.cards[randomIndex]] = [ this.cards[randomIndex],this.cards[currentIndex]];
+      
+    }
+
+    return this.cards;
   }
 
   checkIfPair(card1, card2) {
