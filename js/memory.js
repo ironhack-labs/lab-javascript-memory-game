1bb4c84c1eba9e319e131d64ee1748a4e98ffcb2
Vanessa Zagone <vanessa.zgn@gmail.com>
2021-08-18 20:12:00 +0200

    Logic done

--- js/memory.js
+++ js/memory.js
@@ -1,21 +1,39 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) return undefined;
+
+    let index = this.cards.length;
+
+    while (index != 0) {
+      let randomIndex = Math.floor(Math.random() * index);
+      index--;
+
+      const a = this.cards[index];
+      const b = this.cards[randomIndex];
+
+      this.cards[index] = b;
+      this.cards[randomIndex] = a;
+    }
+
+    return this.cards;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1;
+    if (card1 === card2) this.pairsGuessed += 1;
+    return card1 === card2;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    return this.cards.length / 2 !== this.pairsGuessed ? false : true;
   }
 }
-
 // The following is required for automated testing. Please, ignore it.
 if (typeof module !== 'undefined') module.exports = MemoryGame;
