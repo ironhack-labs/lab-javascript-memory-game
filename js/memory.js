26aa51ca63ef428dde67ee55899f815e00ce2335
jaumebt90 <jaume.mx90@gmail.com>
2021-08-18 18:23:08 +0200

    Update memory.js

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,51 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
     // ... write your code here
+    if (Object.keys(arguments).length === 0) return undefined;
+    // copia superficial
+    // const cardsCopy = [...this.cards]
+    // copia profunda ambas funcionarian
+    const cardsCopy = JSON.parse(JSON.stringify(this.cards))
+    var m = cardsCopy.length, t, i;
+    // While there remain elements to shuffle…
+    while (m) {
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+      // And swap it with the current element.
+      t = cardsCopy[m];
+      cardsCopy[m] = cardsCopy[i];
+      cardsCopy[i] = t;
+    }
+    this.cards = cardsCopy;
   }
+  
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    if(card1 === card2){
+      this.pairsClicked +=1;
+      this.pairsGuessed +=1;
+      return true
+    } else if(this.pairsClicked +=1){
+      return false;
+    }
   }
 
   checkIfFinished() {
     // ... write your code here
+    if((this.cards.length/2) === this.pairsGuessed){
+      return true
+    } else {
+      return false
+    }
   }
 }
 
