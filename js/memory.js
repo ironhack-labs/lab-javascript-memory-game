d8963a521c90077ed53d906ada9ba1bb59cde6c5
Fede <fedemarinasruiz@gmail.com>
2021-08-18 21:26:40 +0200

    [MAD DE] Fede Marinas

--- js/memory.js
+++ js/memory.js
@@ -1,21 +1,39 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards)
+      return undefined;
+
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else if (card1 !== card2) {
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+
+    return this.pairsGuessed === this.cards.length / 2;
+
   }
 }
+
 
 // The following is required for automated testing. Please, ignore it.
 if (typeof module !== 'undefined') module.exports = MemoryGame;
