b33050a4c68685e135c23ec00f5af9e96bdfed03
Lilli Reilly <lilli.reilly@gmail.com>
2021-10-09 09:59:01 +0200

    Finished Iterations 1 and 2

--- js/memory.js
+++ js/memory.js
@@ -1,21 +1,47 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
-  shuffleCards() {
-    // ... write your code here
-  }
+  shuffleCards(cards) {
+    let randomCardA;
+    let randomCardB;
+    let tempDeck;
+    if (!cards) {
+      return undefined
+    }
+    for (let i = 0; i < cards.length; i += 1) {
+      randomCardA = Math.floor(Math.random() * cards.length);
+      randomCardB = Math.floor(Math.random() * cards.length);
+      tempDeck = cards[randomCardB];
+      cards[randomCardA] = cards[randomCardB];
+      cards[randomCardB] = tempDeck
+    }
+    return tempDeck;
+    }
+  
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    }
+    return false;
+    }
   }
-}
+
+
 
 // The following is required for automated testing. Please, ignore it.
 if (typeof module !== 'undefined') module.exports = MemoryGame;
