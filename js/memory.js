270d79a316299d6e479e0f590a90ea426414cc0d
Dominika Lipka <dominika.lipka96@gmail.com>
2021-10-09 16:21:10 +0200

    iteration 2.2 (the logic of the game) finished

--- js/memory.js
+++ js/memory.js
@@ -2,20 +2,41 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     this.pickedCards = [];
-    this.pairsClicked = [];
-    this.pairsGuessed = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined;
+    }
+    let currentIndex = this.cards.length, randomIndex;
+    while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [this.cards[currentIndex], this.cards[randomIndex]] = [
+      this.cards[randomIndex], this.cards[currentIndex]];
+    }
+    return this.cards;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    if (card1 === card2) {
+      this.pairsClicked++;
+      this.pairsGuessed++;
+      return true;
+    } else {
+      this.pairsClicked++;
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
