422e8b8c882a235ecf678f22877e627d0d2dfd69
CodingEinhornDeu <88826323+CodingEinhornDeu@users.noreply.github.com>
2021-10-12 00:11:43 +0200

    Iteration 1 & Iteration 2.1 & Iteration 2.2 done without shuffle function

--- js/memory.js
+++ js/memory.js
@@ -1,7 +1,9 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
@@ -9,11 +11,25 @@ class MemoryGame {
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+
+    if (this.pairsGuessed === 0) {
+      return false;
+    } else if (this.pairsGuessed < this.cards.length / 2) {
+      return false;
+    } else if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    }
+
   }
 }
 
