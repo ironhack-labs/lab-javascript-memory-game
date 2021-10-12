914a9ab7381ea6482820531a1c8260a30334d41a
Joana Brito <jobritomar@gmail.com>
2021-10-12 13:57:38 +0100

    final

--- js/index.js
+++ js/index.js
@@ -29,6 +29,13 @@ const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', (event) => {
   let html = '';
+  let card1= "";
+  let card2= "";
+  
+  memoryGame.shuffleCards()
+
+
+
   memoryGame.cards.forEach((pic) => {
     html += `
       <div class="card" data-card-name="${pic.name}">
@@ -41,11 +48,51 @@ window.addEventListener('load', (event) => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
+
+  // toggled 
+ //Quando apenas um argumento está presente: Toggle class value; Ou seja, se a classe existir, em seguida, removê-lo e retornar false, se não, então adicioná-lo e retornar true.
+//Quando um segundo argumento está presente: Se o segundo argumento é avaliado como true, adicione o valor especificado da classe e, se ele for avaliado como false, remova-o.
+
+
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
+
     card.addEventListener('click', () => {
-      // TODO: write some code here
+      debugger
+
+  // See if it has the class turned. If yes, remove the clase. If no, add the class.
+      card.classList.toggle('turned')
+      
+
+  // see if there is 2 cards turnned and apply the function for comparation
+  
+      const allCardsTurn = document.querySelectorAll('.card.turned')
+
+      if(allCardsTurn.length===2){
+        if((memoryGame.checkIfPair(allCardsTurn[0].attributes["data-card-name"].value,allCardsTurn[1].attributes["data-card-name"].value))) {
+          allCardsTurn[0].classList.toggle('blocked')
+          allCardsTurn[1].classList.toggle('blocked')
+        } 
+        setTimeout(()=>{
+          allCardsTurn[0].classList.toggle('turned')
+          allCardsTurn[1].classList.toggle('turned')
+        }, 1000)
+
+      }
+
+
+      document.querySelector("#pairs-clicked").innerHTML = memoryGame.pairsClicked
+      document.querySelector("#pairs-guessed").innerHTML = memoryGame.pairsGuessed
+
+      if(memoryGame.checkIfFinished()){
+        alert("you won!")
+      }
+
+
+
       console.log(`Card clicked: ${card}`);
     });
+
+    
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,50 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = []
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+
     // add the rest of the class properties here
   }
 
-  shuffleCards() {
-    // ... write your code here
+
+//Write down the numbers from 1 through N.
+//Pick a random number k between one and the number of unstruck numbers remaining (inclusive).
+//Counting from the low end, strike out the kth number not yet struck out, and write it down at the end of a separate list.
+//Repeat from step 2 until all the numbers have been struck out.
+//The sequence of numbers written down in step 3 is now a random permutation of the original numbers.
+  
+
+shuffleCards() {
+  if(!this.cards){
+    return undefined
   }
 
+   const suffleArray = cards => {
+     for(let i= cards.length - 1; i>0; i--) {
+       const j= Math.floor(Math.random()*(i+1))
+       const temp = cards[i];
+       cards[i] = cards[j];
+       cards[j] = temp;
+     
+     }
+   }
+   suffleArray(this.cards)
+  }
+
+
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked = this.pairsClicked +1
+    if (card1=== card2) {
+      this.pairsGuessed=this.pairsGuessed+1;
+      return true
+    }
+    return false
   }
 
   checkIfFinished() {
-    // ... write your code here
+    return this.pairsGuessed === this.cards.length/2
   }
 }
 
