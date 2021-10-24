31200d1bc7fdf9435dfcda44992d1945dd84a44d
David Castillo <david@castillog.com>
2021-10-24 17:39:53 -0400

    memory.js done

--- js/index.js
+++ js/index.js
@@ -45,6 +45,7 @@ window.addEventListener('load', (event) => {
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
+      
       console.log(`Card clicked: ${card}`);
     });
   });
