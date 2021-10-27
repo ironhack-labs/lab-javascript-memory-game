a907240ef05f455a5f6866aa462b94bc6b168ee1
Fernando Cardona <fernando.cardona@the-lion.com>
2021-10-27 20:36:18 +0200

    memory.js DONE con shuffleCardsStack con sort()

--- js/memory.js
+++ js/memory.js
@@ -2,18 +2,44 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    //1.1 Creamos las variables que contendran nuestras cartas:
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+
   }
 
   shuffleCards() {
     // ... write your code here
+    
+
+    if (!this.cards){
+      return undefined;
+    }
+   let cardsStack = this.cards.length;
+   let shuffleCardsStack = this.cards.sort(() => Math.random() - 0.5);
+   return shuffleCardsStack;
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked ++
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
     // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    }else {
+    return false;
+
+    }
   }
 }
 
