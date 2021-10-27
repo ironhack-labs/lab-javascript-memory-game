6ecffa99b1556d9d8cf05cb819ec883a70864be5
Adrian Ramirez <ramiire@gmail.com>
2021-10-27 23:28:53 +0200

    done

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,42 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
-    // ... write your code here
+
+    // no me sale
+    // let shuffleOne = this.cards.pop()
+    // let shuffleTwo = this.cards.shift()
+    
+    // if (this.pickedCards = 0){
+    //   return undefined
+    // } else {
+    // this.cards.unshift(shuffleOne)
+    // this.cards.push(shuffleTwo) }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++
+    if (card1 === card2){
+      this.pairsGuessed++
+      return true
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+
+    if (this.pairsGuessed === this.cards.length / 2){
+      return true
+    } else {
+      return false
+    }
   }
 }
 
