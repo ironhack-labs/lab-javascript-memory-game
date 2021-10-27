ebd95235b14747695f67967c6dc9b5f8087c4082
abdelmalek.saksioui <abdelmalek.saksioui@cgm.com>
2021-10-27 11:49:43 +0200

    Js Part done

--- js/memory.js
+++ js/memory.js
@@ -1,20 +1,40 @@
 class MemoryGame {
-  constructor(cards) {
-    this.cards = cards;
-    // add the rest of the class properties here
-  }
+    constructor(cards) {
+        this.cards = cards;
+        this.pickedCards = []
+        this.pairsClicked = 0;
+        this.pairsGuessed = [];
+    }
 
-  shuffleCards() {
-    // ... write your code here
-  }
+    shuffleCards() {
+        if (!this.cards) return undefined;
 
-  checkIfPair(card1, card2) {
-    // ... write your code here
-  }
+        for (const [index, card] of this.cards.entries()) {
 
-  checkIfFinished() {
-    // ... write your code here
-  }
+            const randomIndex = this.getRandom(index);
+            const holder = this.cards[randomIndex];
+            this.cards[randomIndex] = this.cards[index];
+            this.cards[index] = holder;
+        }
+        return this.cards;
+    }
+
+    getRandom(endIdx) {
+        return Math.floor(Math.random() * endIdx);
+    }
+
+    checkIfPair(card1, card2) {
+        this.pairsClicked++;
+        if (card1 === card2) {
+            this.pairsGuessed++;
+            return true;
+        }
+        return false;
+    }
+
+    checkIfFinished() {
+        return this.pairsGuessed === this.cards.length / 2;
+    }
 }
 
 // The following is required for automated testing. Please, ignore it.
