85b6df4e7ae9658a12778c148c7df3189ea23fb9
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-10-28 00:36:06 +0200

    Done

--- js/index.js
+++ js/index.js
@@ -26,6 +26,7 @@ const cards = [
 ];
 
 const memoryGame = new MemoryGame(cards);
+memoryGame.shuffleCards() 
 
 window.addEventListener('load', (event) => {
   let html = '';
@@ -45,9 +46,34 @@ window.addEventListener('load', (event) => {
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       card.classList.toggle('turned', memoryGame.pickedCards.length <= 1)
+      memoryGame.pickedCards.push(card.getAttribute('data-card-name'))
 
+      if (memoryGame.pickedCards.length === 2) {
+        const card1 = memoryGame.pickedCards[0]
+        const card2 = memoryGame.pickedCards[1]
 
-      
+        if (memoryGame.checkIfPair(card1, card2)) {
+          document.querySelectorAll('.card.turned').forEach((elm) => elm.classList.add('blocked'))
+
+          if (memoryGame.checkIfFinished()) {
+            alert('Game Over!')
+          }
+          memoryGame.pickedCards = []
+        } else {
+          setTimeout(() => {
+            document
+              .querySelectorAll('.card:not(.blocked)')
+              .forEach((elm) => elm.classList.remove('turned'))
+            memoryGame.pickedCards = []
+          }, 1000)
+        }
+      }
+      updateScore()
     });
   });
 });
+
+const updateScore = () => {
+  document.querySelector('#pairs-clicked').innerText = `${memoryGame.pairsClicked}`
+  document.querySelector('#pairs-guessed').innerText = `${memoryGame.pairsGuessed}`
+}
