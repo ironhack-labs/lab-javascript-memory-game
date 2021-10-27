e7bd6a49eb425a49aa98b37a11223dccd9882be4
Alvaro Teran <alvaro.teran97@gmail.com>
2021-10-28 00:09:30 +0200

    Done

--- js/index.js
+++ js/index.js
@@ -31,21 +31,62 @@ window.addEventListener('load', (event) => {
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
-      <div class="card" data-card-name="${pic.name}">
-        <div class="back" name="${pic.img}"></div>
-        <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
-      </div>
-    `;
+        <div class="card" data-card-name="${pic.name}">
+          <div class="back" name="${pic.img}"></div>
+          <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
+        </div>
+      `;
   });
-
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
-
+  const pairsClicked = document.getElementById('pairs-clicked');
+  const pairsGuessed = document.getElementById('pairs-guessed');
+  const win = document.getElementById('win');
+  const play = document.getElementById('play');
+  let pikedCard;
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      card.classList.add('turned');
+      setTimeout(() => {
+        if (pikedCard === undefined) {
+          pikedCard = card;
+        } else {
+          if (
+            memoryGame.checkIfPair(
+              pikedCard.dataset.cardName,
+              card.dataset.cardName
+            )
+          ) {
+            pikedCard.classList.add('blocked');
+            card.classList.add('blocked');
+            pikedCard = undefined;
+            pairsClicked.innerHTML = memoryGame.pairsClicked;
+            pairsGuessed.textContent = memoryGame.pairsGuessed;
+            if (memoryGame.checkIfFinished()) {
+              winDisplay();
+            }
+          } else {
+            pairsClicked.innerHTML = memoryGame.pairsClicked;
+            pikedCard.classList.remove('turned');
+            card.classList.remove('turned');
+            pikedCard = undefined;
+          }
+        }
+      }, 1000);
     });
   });
 });
+function winDisplay() {
+  win.classList.remove('hidden');
+  win.classList.add('display');
+  play.classList.remove('display');
+  play.classList.add('hidden');
+  win.addEventListener('click', () => {
+    memoryGame.shuffleCards();
+    win.classList.remove('display');
+    win.classList.add('hidden');
+    play.classList.add('display');
+    play.classList.remove('hidden');
+  });
+}
