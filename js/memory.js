8dfea0b53767041186436dd1904b73280950c9b8
Santi Jimenez <jimenez.santi@gmail.com>
2021-10-27 20:01:18 +0200

    Set up and logic

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,38 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if(!this.cards) return undefined
+    let shuffledCards = this.cards;
+    let currentIndex = shuffledCards.length;
+    let randomIndex = 0;
+    // console.log("antes: ",shuffledCards);
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [shuffledCards[currentIndex], shuffledCards[randomIndex]] = [shuffledCards[randomIndex], shuffledCards[currentIndex]];
+    }
+    // console.log("despues: ", shuffledCards);
+    return shuffledCards;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    if(card1 === card2) {
+      this.pairsGuessed += 1;
+    }
+    this.pairsClicked += 1;
+    return card1 === card2;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if(this.pairsGuessed === this.cards.length/2) return true
+    return false;
   }
 }
 
