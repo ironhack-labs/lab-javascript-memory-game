aa88574b2799bd4919828d6b764935b83c92c5b1
Alex Lopez <alexlopezz21@gmail.com>
2021-10-27 19:50:53 +0200

    logic done

--- js/memory.js
+++ js/memory.js
@@ -1,20 +1,42 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
   shuffleCards() {
-    // ... write your code here
-  }
+    if(!this.cards){return undefined}
+          for (var i = this.cards.length - 1; i > 0; i--) {
+   
+       // Generate random number
+              var j = Math.floor(Math.random() * (i + 1));
+              [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]]
+   }
+       
+             return this.cards;
+}
+  
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+      const guessWhat = this.pairsClicked ++
+    if (card1 === card2){ 
+      const yay = this.pairsGuessed ++
+      return true
+    }  else  {
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
-  }
+    const guessed = this.cards.length / 2
+    if (this.pairsGuessed == guessed) {
+      return true
+    } else {
+      return false
+    }
+}
 }
 
 // The following is required for automated testing. Please, ignore it.
