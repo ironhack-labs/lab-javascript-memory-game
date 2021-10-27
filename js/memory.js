c519f3fc9a4d6e3b1520369106990c6d03fc0cce
mask <mahsaaaaa@gmail.com>
2021-10-27 21:51:34 +0200

    Solution for memory game

--- js/memory.js
+++ js/memory.js
@@ -2,18 +2,52 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
+    const cards = this.cards;
+    if (!cards) {
+      return cards;
+    }
     // ... write your code here
+    let currentIndex = cards.length,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [cards[currentIndex], cards[randomIndex]] = [
+        cards[randomIndex],
+        cards[currentIndex]
+      ];
+    }
+
+    return cards;
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked++;
+    if (card1 == card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
   }
 
   checkIfFinished() {
     // ... write your code here
+    if (this.pairsGuessed == this.cards.length / 2) {
+      return true;
+    }
+    return false;
   }
 }
 
