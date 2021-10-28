bf8438ecc7def9dabe15df6ca90b45523bc24e8a
besaga <bsgallardo93@gmail.com>
2021-10-28 07:54:38 +0200

    ejercicio

--- js/memory.js
+++ js/memory.js
@@ -1,21 +1,46 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []//cartas que coges 
+    this.pairsClicked = 0
+    this.pairsGuessed = []
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined
+    }
   }
+
+
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++
+    if (card1 === card2) {
+      this.pairsGuessed++
+      return true
+    } else {
+      return false
+    }
   }
+  // if(card1 === card2){
+  //   this.pairsGuessed +=1  }
+  //   else{ 
+  //     return false }
+
+
+
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true
+    }
+    else {
+      return false
+    }
   }
-}
 
-// The following is required for automated testing. Please, ignore it.
-if (typeof module !== 'undefined') module.exports = MemoryGame;
+
+
+// // The following is required for automated testing. Please, ignore it.
+// if (typeof module !== 'undefined') module.exports = MemoryGame;
