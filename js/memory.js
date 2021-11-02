446ff739ca9a395fbfb2dba135e1a78b13f3b10a
Evaniomne <edmoura@leroymerlin.com.br>
2021-11-02 16:52:29 -0300

    Lab pronto

--- js/memory.js
+++ js/memory.js
@@ -1,20 +1,48 @@
 class MemoryGame {
-  constructor(cards) {
-    this.cards = cards;
-    // add the rest of the class properties here
-  }
+    constructor(cards) {
+        this.cards = cards;
+        // add the rest of the class properties here
+        this.pickedCards = [];
+        this.pairsClicked = 0;
+        this.pairsGuessed = 0;
+    }
 
-  shuffleCards() {
-    // ... write your code here
-  }
+    shuffleCards() {
+        // ... write your code here
+        if (!this.cards) return undefined;
 
-  checkIfPair(card1, card2) {
-    // ... write your code here
-  }
+        let tempStack = [];
+        this.cards.forEach((element) => {
+            tempStack.splice(
+                Math.floor(Math.random() * tempStack.length),
+                0,
+                element
+            );
+            this.cards = tempStack;
+        });
 
-  checkIfFinished() {
-    // ... write your code here
-  }
+    }
+
+    checkIfPair(card1, card2) {
+        // ... write your code here
+        this.pairsClicked += 1;
+
+        if (card1 === card2) {
+
+            this.pairsGuessed++;
+            return true;
+        } else {
+
+            return false;
+        }
+    }
+
+    checkIfFinished() {
+        // ... write your code here
+        if (this.pairsGuessed === this.cards.length / 2) return true;
+        return false;
+    }
+
 }
 
 // The following is required for automated testing. Please, ignore it.
