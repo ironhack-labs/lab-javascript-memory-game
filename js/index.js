9ad8d04997cbfbdefcb7b96e4ae407cb3e37e853
Rachouan Rejeb <info@rachouan.com>
2021-11-27 15:44:39 +0000

    Solved lab

--- js/index.js
+++ js/index.js
@@ -26,8 +26,14 @@ const cards = [
 ];
 
 const memoryGame = new MemoryGame(cards);
+memoryGame.shuffleCards();
+console.log(memoryGame.cards)
 
 window.addEventListener('load', (event) => {
+
+  const pairsClickedEl = document.querySelector('#pairs-clicked');
+  const pairGuessedEl = document.querySelector('#pairs-guessed');
+
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
@@ -39,13 +45,46 @@ window.addEventListener('load', (event) => {
   });
 
   // Add all the divs to the HTML
-  document.querySelector('#memory-board').innerHTML = html;
+  const gameboard = document.querySelector('#memory-board');
+  gameboard.innerHTML = html;
+
+  function setScore(){
+    pairsClickedEl.innerHTML =  memoryGame.pairsClicked;
+    pairGuessedEl.innerHTML =  memoryGame.pairsGuessed;
+
+    if(memoryGame.checkIfFinished()){
+      gameboard.innerHTML = `<h1>You Won!</h1><a href="/" class="btn">START AGAIN</a>`
+    }
+
+  }
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      if(memoryGame.pickedCards.length >= 2) {
+        return false;
+      }else{
+        card.classList.toggle('turned');
+        memoryGame.pickedCards.push(card);
+      }
+      
+      if(memoryGame.pickedCards.length == 2) {
+
+        setTimeout(() =>{
+          let result = memoryGame.checkIfPair(memoryGame.pickedCards[0].dataset.cardName,memoryGame.pickedCards[1].dataset.cardName);
+          console.log(result,memoryGame.pairsGuessed,memoryGame.pairsClicked);
+          if(!result){
+            memoryGame.pickedCards.map(card => card.classList.toggle('turned'));
+          }
+          memoryGame.pickedCards = [];
+          setScore();
+
+        },1000)
+        
+      }
+      //console.log(`Card clicked: ${card.querySelector('.back')}`);
+      //card.querySelector('.back').style.visibility = 'hidden';
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,37 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
     // ... write your code here
+    let oldElement;
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      let rand = Math.floor(Math.random() * (i + 1));
+      oldElement = this.cards[i];
+      this.cards[i] = this.cards[rand];
+      this.cards[rand] = oldElement;
+    }
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked ++;
+    if(card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
   }
 
   checkIfFinished() {
     // ... write your code here
+    console.log(this.pairsGuessed*2,this.cards.length)
+    return this.pairsGuessed*2 === this.cards.length;
   }
 }
 
