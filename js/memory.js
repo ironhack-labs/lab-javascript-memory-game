2a9834b2e2dda78dd332597f929b1c9b537c802d
Vicente Garcia <vicentegarciaayllon@gmail.com>
2021-11-29 00:45:56 +0100

    finish logic

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,39 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if(!this.cards){
+      return undefined;
+    }
+    for(let i = 0; i < this.cards.length; i++){
+      const j = Math.floor(Math.random() * (this.cards.length));
+      const card = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = card;
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+    if(card1 === card2){
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if(this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
