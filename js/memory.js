b981c6a112cf0d2b0f7670e19e85e488bc49f185
Michael Foley <foley.p.michael@gmail.com>
2021-11-29 15:27:47 +0000

    Finished functions in the memory js page, but conditions not clearing tests completely.

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,36 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    let currentIndex = this.cards, randomIndex
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [this.cards[currentIndex], this.cards[randomIndex]] = [this.cards[randomIndex], this.cards[currentIndex]];
+    }
+    return this.cards;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked ++;
+    if (card1 === card2) {
+      this.pairsGuessed ++;
+      return true;
+    } 
+    return false;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed * 2 < this.cards.length) {
+      return false;
+    } else if (this.pairsGuessed * 2 === this.cards.length) {
+      return true;
+    }
   }
 }
 
