a7d63408586c9840bde97260b26ddbb4e3fd5643
Arturo Vivar <arthas1237@hotmail.com>
2021-11-30 15:51:47 +0100

    10/18test

--- js/memory.js
+++ js/memory.js
@@ -2,10 +2,27 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+
+
   }
 
   shuffleCards() {
     // ... write your code here
+   if(!this.cards){
+    return undefined;
+   }
+    // used a for loop with a shufflecard method, change temp to card
+   for (let i = 0; i < this.cards.length; i++) {
+    let j = Math.floor(Math.random() * (this.cards.length));
+    let card = deck[i];
+    deck[i] = deck[j];
+    deck[j] = card;
+}
+
+
   }
 
   checkIfPair(card1, card2) {
