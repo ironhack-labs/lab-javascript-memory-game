929e5b15e708a29175b6d8e2f8fc9a6fe8a1d86e
Felipe Leite Mantovani <mantovani.devilz@gmail.com>
2021-12-07 10:43:58 -0300

    class MemoryGame done, I don't understand how to implement the shuffle

--- js/memory.js
+++ js/memory.js
@@ -7,15 +7,30 @@ class MemoryGame {
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!arguments.length) return
+    // const shuffleArray = array => {
+    //   for (let i = array.length - 1; i > 0; i--) {
+    //     const j = Math.floor(Math.random() * (i + 1));
+    //     const temp = array[i];
+    //     array[i] = array[j];
+    //     array[j] = temp;
+    //   }
+    // }
+    // return shuffleArray
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1;
+    if (card1 === card2){
+      this.pairsGuessed += 1;
+      return true
+    }
+    return false
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === (this.cards.length / 2)) return true;
+    return false
   }
 }
 
