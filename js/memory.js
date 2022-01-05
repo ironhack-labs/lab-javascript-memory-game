ace82fd7eec224c4ea7b5f754a9c194c9676547a
[your <jessica.menezs@gmail.com>
2022-01-05 13:59:39 -0300

    not finished

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,43 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
+    if (this.cards === undefined){
+      return undefined;
+    }
+    this.cards = this.cards.sort(() => {
+      if (Math.random() > 0.5){
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    
+    
     // ... write your code here
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1;
+    if (card1 === card2){
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+     return false;
+    } 
   }
 
   checkIfFinished() {
-    // ... write your code here
+   if(this.pairsGuessed === (this.cards.length/2)){
+     return true;
+   }else{
+     return false;
+   }
   }
 }
 
