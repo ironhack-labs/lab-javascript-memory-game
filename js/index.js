0674e45d626dde67e3f0082fac0c51f3bbd89da3
Eduardo Gordillo <edu.m.gordillo@gmail.com>
2022-01-19 22:15:43 +0100

    Not done, works somethings...

--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ window.addEventListener('load', (event) => {
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
-      <div class="card" data-card-name="${pic.name}">
+      <div class="card" data-hero="${pic.name}">
         <div class="back" name="${pic.img}"></div>
         <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
       </div>
@@ -46,6 +46,55 @@ window.addEventListener('load', (event) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
       console.log(`Card clicked: ${card}`);
+      memoryGame.shuffleCards()
+      memoryGame.pickedCards++
+      
+      let cardclick = card.dataset.hero
+    
+      let card1
+      let card2
+
+
+      // console.log(`card 1${card1}`)
+      // console.log(`card 2${card2}`)
+      // console.log(memoryGame.pickedCards)
+      // console.log(card.className)
+
+      if (memoryGame.pickedCards === 1) {
+        card.className = 'card turned'
+        card1 = card.dataset.hero
+        console.log(card1)
+      }
+      if (memoryGame.pickedCards === 2) {
+        card.className = 'card turned'
+        card2 = card.dataset.hero
+        
+        console.log(card2)
+        
+        if (memoryGame.checkIfPair(card1, card2)) {
+          alert('pair')
+        } else {
+          
+          setTimeout(() => alert('not pair'), 2000)
+          setTimeout(() => location.reload(), 2000)
+          
+         }
+
+      
+      
+      }
+
+
+      
+
+
+
+
+
+
+
+
+
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -2,18 +2,54 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = 0
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+  
+  
   }
 
   shuffleCards() {
     // ... write your code here
+    
+    
+       
+    let currentIndex = this.cards.length, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [this.cards[currentIndex], this.cards[randomIndex]] = [
+        this.cards[randomIndex], this.cards[currentIndex]];
+    }
+
+    return this.cards; 
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked++
+    if (card1 === card2){
+      this.pairsGuessed++
+      return true
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
     // ... write your code here
+    if(this.pairsGuessed === 12){
+      window.alert('you win')
+      return true
+    } else {
+      return false
+    }
   }
 }
 
