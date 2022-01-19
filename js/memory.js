17a50dc345356cea701ca795e8fff87889cb68f2
Doan Tran <doan7tran@gmail.com>
2022-01-19 22:45:36 +0100

    lab solved, except shuffleCards method

--- js/memory.js
+++ js/memory.js
@@ -3,20 +3,42 @@ class MemoryGame {
     this.cards = cards;
     // add the rest of the class properties here
     this.pickedCards = [];
-    this.pairsClicked = [];
-    this.pairsGuessed = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards(arr) {
     // ... write your code here
+    if (!arr) {
+      return undefined;
+    }
+    for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * i);
+      let shuffledArr = arr[i];
+      arr[i] = arr[j];
+      arr[j] = shuffledArr;
+    }
+    this.cards = shuffledArr;
+    return shuffledArr;
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
   }
 
   checkIfFinished() {
     // ... write your code here
+
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    }
+    return false;
   }
 }
 
