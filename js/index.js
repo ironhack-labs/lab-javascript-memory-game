f647aa478cd079c1ec1a67bd19c55af05d56bf46
AChFr <chirkovarseni@gmail.com>
2022-01-20 00:56:34 +0100

    done. uno de los tests no pasa. el codgo es un caos. pero hace el 80% de su función

--- js/index.js
+++ js/index.js
@@ -28,24 +28,78 @@ const cards = [
 const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', (event) => {
+
+  //memoryGame.shuffleCards(cards)
+  let card1
+  let card2
+  let card1Name = ''
+  let card2Name = ''
   let html = '';
   memoryGame.cards.forEach((pic) => {
+    // en los renglones de abajo he tenido que convertir
+    // 'data-cardname' en 'cardname'. De lo contrario no sabía referenciarlo
+    //
+    // me he olvidado completamente del array de this.pickedCards. funciona sin usarlo
+    //cuando me di cuenta de eso, era demasiado tarde para cambiar toda la mecánica
+
     html += `
-      <div class="card" data-card-name="${pic.name}">
+      <div class="card" data-cardname="${pic.name}">
+    
         <div class="back" name="${pic.img}"></div>
         <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
       </div>
     `;
   });
 
-  // Add all the divs to the HTML
+
+
+
   document.querySelector('#memory-board').innerHTML = html;
 
-  // Bind the click event of each element to a function
+
   document.querySelectorAll('.card').forEach((card) => {
+
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
+
+      card.classList.toggle('turned')
+
+      if (card1Name === '') {
+        card1 = card
+        card1Name = card.dataset.cardname
+        console.log(card1Name)
+      }
+      else {
+        card2 = card
+        card2Name = card.dataset.cardname
+        console.log(card2Name)
+        if (memoryGame.checkIfPair(card1Name, card2Name)) {
+          card1Name = ''
+          card2Name = ''
+          card1.classList.add('blocked')
+          card2.classList.add('blocked')
+
+          console.log(card1)
+          console.log(card2)
+          document.getElementById("pairs-guessed").innerText = memoryGame.pairsGuessed
+        }
+        else {
+
+
+          card1.classList.toggle('turned')
+          card2.classList.toggle('turned')
+          document.getElementById("pairs-clicked").innerText = memoryGame.pairsClicked
+          console.log('fracaso')
+          card1Name = ''
+          card2Name = ''
+        }
+
+      }
+
+
+    })
+
+    memoryGame.checkIfFinished()
+
+
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -2,18 +2,70 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+
+
+
+
   }
 
-  shuffleCards() {
-    // ... write your code here
+  shuffleCards(arr) { // scalpeado de internet ¯\_(ツ)_/¯
+    // por alguna razon el test no pasa.
+    // he probado a devolverlo en otro array, pero tampoco
+
+
+    if (arr) {
+
+
+      let m = arr.length
+      let t
+      let i
+
+      // While there remain elements to shuffle…
+      while (m) {
+
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = arr[m];
+        arr[m] = arr[i];
+        arr[i] = t;
+      }
+
+      return arr;
+    }
+
+
+    else { return undefined }
   }
+
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+
+    if (card1 === card2) {
+      this.pairsClicked++
+      this.pairsGuessed++
+
+      return true
+    }
+    else {
+      this.pairsClicked++
+      console.log('no es pareja')
+      return false
+    }
+
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed < this.cards.length / 2) {
+
+      return false
+    }
+    alert("YOU'VE BEATEN THE GAME")
+    return true
   }
 }
 
