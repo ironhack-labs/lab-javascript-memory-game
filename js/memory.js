b7b0485ac22f35e0e88ec059f083082abc6e7ab7
ana.sanmua@gmail.com <86674956+anasanmua@users.noreply.github.com>
2022-01-19 20:23:19 +0100

    done

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,41 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    n = array.length, i;
+    while (n) {
+      i = Math.floor(Math.random() * n--);
+      this.cards.push(array.splice(i, 1)[0]);
+    }
+    return copy;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1
+    if (card1 !== card2) {
+
+      return false
+
+    } else {
+      this.pairsClicked += 1
+      this.pairsGuessed += 1
+      return true
+
+    }
+
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if ((this.cards.length / 2) > this.pairsGuessed) {
+      return false
+    } else {
+      return true
+    }
   }
 }
 
