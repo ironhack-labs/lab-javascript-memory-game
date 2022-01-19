9fefec8bd379807a68254fbc857eae65111e34b6
Laura Del valle <l.delvallebermejo@gmail.com>
2022-01-19 22:45:02 +0100

    Fail

--- js/memory.js
+++ js/memory.js
@@ -1,7 +1,9 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
@@ -9,11 +11,32 @@ class MemoryGame {
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    // ... write your code here    
+
+    this.pairsClicked ++;
+    if (card1 === card2) {
+    
+      this.pairsGuessed ++
+
+      return true
+      
+    } else {
+
+      return false
+
+    } 
+
+
   }
 
   checkIfFinished() {
-    // ... write your code here
+    
+    if (this.pairsGuessed <= 12){ //no entiendo por que me da error
+      return false
+    } else {
+    return true
+    } 
+
   }
 }
 
