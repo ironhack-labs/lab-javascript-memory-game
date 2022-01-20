c714fb98669968b9b7ef5b230bdad9d958cc5f64
VmMad <88061365+VmMad@users.noreply.github.com>
2022-01-20 09:13:22 +0100

    done

--- js/index.js
+++ js/index.js
@@ -43,9 +43,9 @@ window.addEventListener('load', (event) => {
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
+        card.addEventListener('click', () => {
+          card.classList.toggle('turned')
+        }
+        );
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,42 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
-  shuffleCards() {
-    // ... write your code here
+  shuffleCards(cards) {
+    if (this.cards == undefined){
+      return undefined
+    } else{
+    let i = this.cards.length, k, temp
+    while (--i > 0) {
+      k = Math.floor(Math.random() * (i + 1));
+      temp = this.cards[k];
+      this.cards[k] = this.cards[i];
+      this.cards[i] = temp;
+    }
   }
+}
+  
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++
+    if (card1 === card2){
+      this.pairsGuessed++
+      return true
+    } if (card1 !== card2.name) {
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === (this.cards.length / 2)){
+      return true
+    }else{
+      return false
+    }
   }
 }
 
