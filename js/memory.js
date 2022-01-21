4c31e436b563449a681c799cbdbf6d1e06a672c5
Joana Carballo <joana16cruz@gmail.com>
2022-01-21 21:23:26 +0100

    solved lab till Iteration 2.2.

--- js/memory.js
+++ js/memory.js
@@ -2,18 +2,60 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
   shuffleCards() {
     // ... write your code here
+    if (this.cards === undefined) {
+      return undefined
+    }
+    let newCards = this.cards.slice()
+    let m = newCards.length 
+    let t
+    let i
+
+      // While there remain elements to shuffle…
+    while (m > 0) {
+    
+        // Pick a remaining element…
+      i = Math.floor(Math.random() * m);
+      m--
+        // And swap it with the current element.
+      t = newCards[m];
+      newCards[m] = newCards[i];
+      newCards[i] = t;
+      
+    }
+    this.cards = newCards;
+    return this.cards
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.card1 = card1
+    this.card2 = card2
+
+    
+    if (this.card1 === this.card2) {
+      this.pairsClicked += 1
+      this.pairsGuessed += 1
+      return true
+    } else {
+      this.pairsClicked += 1
+      return false
+    }
   }
 
   checkIfFinished() {
     // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true
+    } else {
+      return false
+    }
   }
 }
 
