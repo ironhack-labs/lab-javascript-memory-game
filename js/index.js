65ca5546d80ed8364b92e6c4ab3ba4ebb4901eca
juancarlosdnz <dnzjuancarlos@gmail.com>
2022-03-30 22:13:53 +0200

    tests passed, logic done

--- js/index.js
+++ js/index.js
@@ -38,7 +38,6 @@ window.addEventListener('load', (event) => {
     `;
   });
 
-  // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
   // Bind the click event of each element to a function
--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,39 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+
   }
 
   shuffleCards() {
-    // ... write your code here
+
+    if (typeof this.cards === 'undefined') {
+      return undefined
+    }
+    this.cards.sort(() => Math.random() - 0.5)
+
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++
+
+    if (card1 === card2) {
+
+      this.pairsGuessed++
+      return true
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed < 12) {
+      return false
+    } else if (this.Guessed = 12) {
+      return true
+    }
   }
 }
 
