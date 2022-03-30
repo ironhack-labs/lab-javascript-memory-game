03d2ff7ecb936d318cb397c77cdaef2442d8db59
AlvaroBenito <alvaro.dbg5@hotmail.com>
2022-03-30 18:55:04 +0200

    done

--- js/index.js
+++ js/index.js
@@ -44,7 +44,13 @@ window.addEventListener('load', (event) => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
+      card.classList.toggle('turned')
+      memoryGame.pickedCards.push(card)
+
+      if (memoryGame.pickedCards.length === 2) {
+
+      }
+
       console.log(`Card clicked: ${card}`);
     });
   });
