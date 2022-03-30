f8066a24623af59ddaae3bbd6d5a0680d3f0d5e7
arneeder <arne.eder@gmail.com>
2022-03-30 16:17:38 +0200

    classes defined, not yet tested

--- js/memory.js
+++ js/memory.js
@@ -2,20 +2,50 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
   shuffleCards() {
-    // ... write your code here
+    const arr = this.cards
+    
+    let j;
+    let oldItemI;
+    
+    let oldItemJ;
+    for (let i = arr.length - 1; i >= 0; i--) {
+      j = Math.floor(Math.random()*arr.length)
+      console.log(j)
+      oldItemI = arr[i]
+      oldItemJ = arr[j]
+      arr[i] = oldItemJ
+      arr[j] = oldItemI
+    }
+    return arr
   }
 
+
   checkIfPair(card1, card2) {
-    // ... write your code here
+    pairsClicked += 1
+    if (card1 === card2) {
+      pairsGuessed += 1
+      return true
+    } else return false
   }
 
   checkIfFinished() {
-    // ... write your code here
+    return this.pairsGuessed === 12
   }
 }
+
+// TEST
+
+const testGame = new MemoryGame([1,2,3,4,5,6,7,8])
+const testCards = testGame.shuffleCards()
+console.log(testCards)
+
+// TEST END 
 
 // The following is required for automated testing. Please, ignore it.
 if (typeof module !== 'undefined') module.exports = MemoryGame;
