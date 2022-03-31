7526201ac7485a78d211fa539f31acf57b04f039
Roberto D. Ezquerro <roberto.ezquerro@gmail.com>
2022-03-31 09:12:41 +0200

    [MAD DE] Roberto Ezquerro

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,37 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
-  shuffleCards() {
-    // ... write your code here
+  shuffleCards() {  
+   if (this.cards === undefined) {
+     return undefined
+   } else {
+     this.cards.sort((a, b) => 0.5 - Math.random())
+   }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    if (card1 === card2) {
+      this.pairsGuessed++
+      return true
+    } else { 
+      this.pairsClicked++
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === 0) {
+      return false
+    } else if (this.pairsGuessed < 12) {
+      return false
+    } else {
+      return true
+    }
   }
 }
 
