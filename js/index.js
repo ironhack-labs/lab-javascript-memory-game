a440e5339c32093a72e5ff8f296620e17e4127d4
Azdhy Galvan <azdhy16@gmail.com>
2022-05-02 17:51:58 -0500

    Done

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ const cards = [
   { name: 'captain america', img: 'captain-america.jpg' },
   { name: 'fantastic four', img: 'fantastic-four.jpg' },
   { name: 'flash', img: 'flash.jpg' },
-  { name: 'green arrow', img: 'green-arrow.jpg' },
+  { name: 'green arrow', img: 'green-arrow.jpg'  },
   { name: 'green lantern', img: 'green-lantern.jpg' },
   { name: 'ironman', img: 'ironman.jpg' },
   { name: 'spiderman', img: 'spiderman.jpg' },
@@ -41,11 +41,25 @@ window.addEventListener('load', (event) => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
+
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      card.className=`card turned`;
+
+     
+      memoryGame.pickedCards.unshift(card)
+      // let currentGame = memoryGame.checkIfPair(memoryGame.pickedCards[0],memoryGame.pickedCards[1]);
+      if (memoryGame.pickedCards.length %2 ==0){
+       let currentGame = memoryGame.checkIfPair(memoryGame.pickedCards[0].dataset.cardName,memoryGame.pickedCards[1].dataset.cardName);
+       if (currentGame === false){setTimeout(()=>{
+         console.log(memoryGame.pickedCards[0].hasAttributes()),
+         memoryGame.pickedCards[0].className  = 'card';
+         memoryGame.pickedCards[1].className = 'card';
+       }, 1000) }
+      }
+      document.getElementById('pairs-clicked').innerHTML = `${memoryGame.pairsClicked}`;
+      document.getElementById('pairs-guessed').innerHTML = `${memoryGame.pairsGuessed}`;
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,53 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
     // ... write your code here
+    let arr = this.cards;
+    if (arr !== undefined) {
+      function shuffle(arr) {
+        var m = arr.length,
+          t,
+          i;
+
+        while (m) {
+          i = Math.floor(Math.random * arr.length);
+          t = arr[m];
+          arr[m] = arr[i];
+          arr[i] = t;
+        }
+        return arr;
+      }
+      return shuffle(arr);
+    } else {
+      return arr;
+    }
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked += 1;
+    if (card1 == card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else return false;
   }
 
   checkIfFinished() {
     // ... write your code here
+    const totalCards = (this.cards.length) / 2;
+
+    if (this.pairsGuessed < totalCards) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
 
