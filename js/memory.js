3aa5deef6f515a37be7eb1feeaa14e5d5e93ab47
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-05-12 19:41:13 -0400

    Iteration 2.2: shuffleCards()

--- js/memory.js
+++ js/memory.js
@@ -7,8 +7,17 @@ class MemoryGame {
     this.pairsGuessed = 0;
   }
 
-  shuffleCards() {
+  shuffleCards = () => {
     // ... write your code here
+    if (!this.cards) return undefined;
+
+    let oldElement;
+    for (let i = this.cards.length - 1; i > 0; i -= 1) {
+      const rand = Math.floor(Math.random() * (i + 1));
+      oldElement = this.cards[i];
+      this.cards[i] = this.cards[rand];
+      this.cards[rand] = oldElement;
+    };
   }
 
   checkIfPair(card1, card2) {
