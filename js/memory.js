d77a0b7ea850a7c517a5e827c6f4518a5e0e38a6
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-05-13 13:28:29 -0400

    Iteration 2.2: checkIfPair()

--- js/memory.js
+++ js/memory.js
@@ -20,8 +20,15 @@ class MemoryGame {
     };
   }
 
-  checkIfPair(card1, card2) {
+  checkIfPair = (card1, card2) => {
     // ... write your code here
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    };
   }
 
   checkIfFinished() {
