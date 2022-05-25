e049504c23c8d812f4a8c125a40fcbd439148f5e
Almudena <almudena.vigara@gmail.com>
2022-05-25 13:09:47 +0200

    Memory.js DONE

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,49 @@
+
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
     // ... write your code here
-  }
+    
+    if(!this.cards){
+      return undefined
+    };
+    
+    // https://www.youtube.com/watch?v=tLxBwSL3lPQ ==> video Fisher-Yates Shuffle
+    // Usando Fisher-Yates Shuffle:
 
+    for(let i = this.cards.length - 1; i > 0; i--){
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      // randomIndex = genera un numero entre 0 y el ultimo index del array
+      const currentValueOfCards = this.cards[randomIndex];
+      this.cards[randomIndex] = this.cards[i];
+      this.cards[i] = currentValueOfCards;
+    }  
+  }
+  
   checkIfPair(card1, card2) {
     // ... write your code here
+   if(card1 === card2){
+     this.pairsGuessed += 1
+     return true
+   } else{
+     this.pairsClicked += 1
+     return false
+   }
   }
 
   checkIfFinished() {
     // ... write your code here
+   if(this.pairsGuessed === this.cards.length / 2){
+    return true 
+   } else {
+    return false
+   }
   }
 }
 
