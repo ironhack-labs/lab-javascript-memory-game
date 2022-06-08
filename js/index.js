9c8bd4a2bb65a09fdfe7762c134743525edabe1f
Natalia Pinzon <nanuta522@gmail.com>
2022-06-09 00:38:26 +0200

    done

--- js/index.js
+++ js/index.js
@@ -28,6 +28,7 @@ const cards = [
 const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', (event) => {
+  memoryGame.shuffleCards(cards)
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
@@ -44,8 +45,30 @@ window.addEventListener('load', (event) => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      console.log(`Card clicked: ${card}`)
+
+      // console.log(`Card clicked:` + card.getAttribute('data-card-name'))
+
+      card.classList.toggle('turned', true)
+     
+      
+      if (memoryGame.pickedCards.length === 0) {
+        memoryGame.pickedCards.push(card.getAttribute('data-card-name'))
+        // console.log('first-if')
+      } else {
+        console.log(memoryGame.pickedCards.length)
+        let resultBoolean = memoryGame.checkIfPair(card.getAttribute('data-card-name'), memoryGame.pickedCards[memoryGame.pickedCards.length - 1] )
+        console.log(resultBoolean)
+        if (resultBoolean === false) {
+          const timeoutId = setTimeout(() => {
+            card.classList.toggle('turned', false)
+          }, 1500)
+        } 
+        // console.log('second-if')
+      }
+      
+      document.getElementById('pairs-clicked').innerHTML = memoryGame.pairsClicked
+      document.getElementById('pairs-guessed').innerHTML = memoryGame.pairsGuessed
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,33 @@
 class MemoryGame {
   constructor(cards) {
-    this.cards = cards;
-    // add the rest of the class properties here
+    this.cards = cards
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (this.cards === undefined) {
+      return undefined
+    }
+    this.cards.sort((a, b) => 0.5 - Math.random())
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1
+    if (card1 === card2) {
+      this.pairsGuessed += 1
+      return true 
+    } else {
+    return false
+    }  
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === 12) {
+      return true
+    }
+    return false
   }
 }
 
