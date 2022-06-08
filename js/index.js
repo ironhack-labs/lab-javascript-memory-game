3619fc2b29ded4fe2fe90076b012e3290c822469
Víctor <victorsobrinos@gmail.com>
2022-06-08 21:56:05 +0200

    done

--- js/index.js
+++ js/index.js
@@ -27,7 +27,11 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+// let { pickedCards, pairsClicked, pairsGuessed, shuffleCards, checkIfPair, checkIfFinished } = memoryGame
+
 window.addEventListener('load', (event) => {
+  // memoryGame.shuffleCards()
+
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
@@ -38,14 +42,65 @@ window.addEventListener('load', (event) => {
     `;
   });
 
+
   // Add all the divs to the HTML
+
   document.querySelector('#memory-board').innerHTML = html;
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
+      card.classList.toggle('turned')
       console.log(`Card clicked: ${card}`);
+
+
+      let pairsClickedTitle = document.querySelector('#pairs-clicked')
+      let pairsGuessedTitle = document.querySelector('#pairs-guessed')
+
+      pairsClickedTitle.innerHTML = memoryGame.pairsClicked
+      pairsGuessedTitle.innerHTML = memoryGame.pairsGuessed
+
+      memoryGame.pickedCards.unshift(card)
+
+      // console.log(memoryGame.pickedCards[1].dataset.cardName)
+
+      // console.log(memoryGame.pickedCards[0].dataset.cardName)
+
+      if (memoryGame.checkIfPair(memoryGame.pickedCards[0].dataset.cardName, memoryGame.pickedCards[1].dataset.cardName)) {
+        console.log(memoryGame.pickedCards[0].dataset.cardName)
+        console.log(memoryGame.pickedCards[1].dataset.cardName)
+
+        memoryGame.pickedCards[0].classList.add('blocked')
+        memoryGame.pickedCards[1].classList.add('blocked')
+        memoryGame.pickedCards = []
+        console.log(memoryGame.pickedCards)
+      }
+
+      else {
+        setTimeout(function () {
+          memoryGame.pickedCards[0].classList.toggle('turned')
+          memoryGame.pickedCards[1].classList.toggle('turned')
+          memoryGame.pickedCards = []
+          console.log(memoryGame.pickedCards)
+        }, 1000)
+
+      }
+
+      if (memoryGame.checkIfFinished()) {
+        let cards = document.querySelectorAll('.card')
+        cards.forEach(element => {
+          element.classList.toggle('blocked')
+          element.classList.toggle('turned')
+        })
+
+        memoryGame.pairsGuessed = 0
+        window.alert('ENHORABUENA, TE HAS PASADO EL JUEGO!!!!')
+      }
+
     });
   });
 });
+
+
+
