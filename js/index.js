9baf260ec5b9d6215b17f7dbfc51a362099db77e
Santiago Moncada <santiago.m.a@hotmail.com>
2022-06-08 21:12:55 +0200

    MVP

--- js/index.js
+++ js/index.js
@@ -27,7 +27,14 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
-window.addEventListener('load', (event) => {
+function removeCards(){
+  const cards = document.querySelectorAll(".card");
+  cards.forEach(card=>{
+    card.remove();
+  });
+}
+
+function setCards(){
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
@@ -41,11 +48,99 @@ window.addEventListener('load', (event) => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
-  // Bind the click event of each element to a function
-  document.querySelectorAll('.card').forEach((card) => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+}
+
+window.addEventListener('load', (event) => {
+  memoryGame.shuffleCards();
+  
+  setCards();
+  bindCards();
+  function updateInfo (){
+    const pairsClicked = document.querySelector("#pairs-clicked");
+    const pairsGuessed = document.querySelector("#pairs-guessed");
+    pairsGuessed.innerText = memoryGame.pairsGuessed;
+    pairsClicked.innerText = memoryGame.pairsClicked;
+  }
+
+  function checkWin(){
+    if(memoryGame.checkIfFinished()){
+      const cards = document.querySelectorAll(".card");
+
+      setTimeout(()=>{
+        cards.forEach(card=>{
+          card.classList.remove("turned");          
+        })
+        
+        
+        memoryGame.pickedCards = [];
+
+        alert(`YOU WON WITH ${memoryGame.pairsClicked - memoryGame.pairsGuessed} MISSES :D`);
+
+        memoryGame.pairsClicked = 0;
+        memoryGame.pairsGuessed = 0;
+        memoryGame.upsideCard = 0;
+
+        
+        
+        setTimeout(()=>{
+          removeCards();
+          memoryGame.shuffleCards();
+          setCards();
+          bindCards();
+          updateInfo();
+        },1000);
+        
+      },500);
+    }
+  }
+  function bindCards(){
+    // Bind the click event of each element to a function
+    document.querySelectorAll('.card').forEach((card) => {
+      card.addEventListener('click', () => {
+        // TODO: write some code here
+
+        console.log(memoryGame.pairsClicked);
+
+        if (!card.className.includes("turned")) {
+          card.classList.add("turned");
+
+          if (memoryGame.upsideCard === 1) {
+
+            memoryGame.upsideCard = 0;
+            const wasPair = memoryGame.checkIfPair(memoryGame.pickedCards[0], card.dataset.cardName);
+            //const wasPair = memoryGame.pickedCards[0] == card.dataset.cardName;
+
+            if (wasPair) {
+              //memoryGame.pairsGuessed++;
+            } else {
+              const unturnedCard1 = document.querySelector(`.turned[data-card-name='${card.dataset.cardName}']`);
+              const unturnedCard2 = document.querySelector(`.turned[data-card-name='${memoryGame.pickedCards[0]}']`);
+              memoryGame.pickedCards.shift();
+
+
+              setTimeout(() => {
+                unturnedCard1.classList.remove("turned");
+                unturnedCard2.classList.remove("turned");
+              }, 500);
+
+
+
+            }
+
+          } else if (memoryGame.upsideCard === 0) {
+            memoryGame.pickedCards.unshift(card.dataset.cardName);
+            memoryGame.upsideCard++;
+          }
+
+          updateInfo();
+          checkWin();
+        }
+
+
+      });
     });
-  });
+  }
+  
 });
+// nodo.onClick(setTimout(console.log("popino"), 5000))
+// nodo.onClick(()=>setTimout(console.log("popino"),5000))
--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,49 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    this.upsideCard = 0;
     // add the rest of the class properties here
   }
 
-  shuffleCards() {
-    // ... write your code here
+  getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
   }
 
+  shuffleCards(cards) {
+    //TODO
+    // if(cards === undefined){return undefined};
+    // const shuffledCards = [];
+    
+    // for(let i = 0; i<this.cards.length ; i++){
+    //   //get random number between i and length
+    //   const r = this.getRandom(i, this.cards.length);
+    //   //swap i and r
+    //   shuffledCards[r] = {...cards[i]};
+    //   shuffledCards[i] = {...cards[r]};
+    // }
+
+    // this.cards = shuffledCards;
+    if (this.cards === undefined) { return undefined };
+    this.cards = this.cards.sort((a, b) => 0.5 - Math.random());
+  }
+  
+
   checkIfPair(card1, card2) {
-    // ... write your code here
+    
+    this.pairsClicked++;
+    const sameCards = card1 == card2;
+    if(sameCards){
+      this.pairsGuessed ++;
+    };
+
+    return sameCards;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    return this.pairsGuessed === 12;
   }
 }
 
