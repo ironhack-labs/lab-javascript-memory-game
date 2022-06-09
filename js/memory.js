d0136aff0371cffb29d7f0f807682fb115bad9a1
iva <ivvvpol@gmail.com>
2022-06-09 11:40:35 +0200

    done

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,37 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (this.cards) {
+      this.cards.sort(() => 0.5 - Math.random());
+      return this.cards
+    }
+    return undefined
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    const allPairs = this.cards.length / 2;
+
+    if (this.pairsGuessed < allPairs) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
 
