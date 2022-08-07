a6846d63eb50b3094c0f56d8d17ac1abd8efe5a3
Meryem Ben Jouda <miryambjzidi@gmail.com>
2022-08-08 00:22:45 +0200

    solved lab

--- src/index.js
+++ src/index.js
@@ -38,13 +38,10 @@ window.addEventListener("load", (event) => {
     `;
   });
 
-  // Add all the divs to the HTML
   document.querySelector("#memory-board").innerHTML = html;
 
-  // Bind the click event of each element to a function
   document.querySelectorAll(".card").forEach((card) => {
     card.addEventListener("click", () => {
-      // TODO: write some code here
       card.classList.add("turned");
       memoryGame.pickedCards.push(card);
       memoryGame.checkIfPair;
