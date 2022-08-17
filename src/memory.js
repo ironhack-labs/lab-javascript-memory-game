30b80d558c1c00414e8929fd41e2d756cb349f82
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2022-08-17 19:39:06 +0200

    the DOM has not been completed

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,38 @@
 class MemoryGame {
   constructor(cards) {
+    // añadir el resto de las propiedades de la clase aquí
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+
   }
 
-  shuffleCards() {
+  shuffleCards() {  //Barajear cartas
     // ... write your code here
+    if (!this.cards) {
+      return undefined
+    }
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); //random index
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]]; // swap
+    }
   }
 
-  checkIfPair(card1, card2) {
+  checkIfPair(card1, card2) {  //comprobar Si es par
     // ... write your code here
+    if (card1 !== card2) {
+      this.pairsClicked++
+      return false
+    } else {
+      this.pairsGuessed++
+      return true
+    }
+
   }
 
-  checkIfFinished() {
+  checkIfFinished() { //compruebe si ha finalizado
     // ... write your code here
+    return this.cards.length / 2 === this.pairsGuessed
   }
 }
