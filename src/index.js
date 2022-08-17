24997074f2d47c9718dbb95b87551a74a11ecc2c
Hector <villaescusahector@gmail.com>
2022-08-17 22:10:46 +0200

    solved Lab

--- src/index.js
+++ src/index.js
@@ -38,8 +38,14 @@ window.addEventListener('load', (event) => {
     `;
   });
 
+
+
+
+
+
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
+
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,55 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
     // add the rest of the class properties here
   }
 
   shuffleCards() {
-    // ... write your code here
+
+    if (this.cards == null) {
+      return undefined
+    }
+
+    let m = this.cards.length, t, i;
+
+
+    while (m) {
+
+
+      i = Math.floor(Math.random() * m--);
+
+
+      t = this.cards[m];
+      this.cards[m] = this.cards[i];
+      this.cards[i] = t;
+    }
+
+    return this.cards;
+
+
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++
+
+    if (card1 == card2) {
+      this.pairsGuessed++
+      return true
+    } else {
+      return false
+    }
+
+
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed == this.cards.length / 2) {
+      return true
+    } else {
+      return false
+    }
   }
 }
