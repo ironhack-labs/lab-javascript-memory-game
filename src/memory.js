d3321326a5787552d8652c6e99f12a16a876fec2
Jaime Martin <martinjaime1119@gmail.com>
2022-08-17 21:32:42 +0200

    no DOM yet

--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,32 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
     // ... write your code here
+    if (this.cards === undefined) return undefined;
+    return this.cards.sort(compare);
+    function compare(a, b) {
+      return 0.5 - Math.random();
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
     // ... write your code here
+    return this.pairsGuessed === this.cards.length / 2 ? true : false;
   }
 }
