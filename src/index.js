dfde9f310d97d158202c6d94c2f3b4bd3b0efb2e
RstefaniB <rickies72@gmail.com>
2022-08-17 21:09:06 +0200

    wout DOM

--- src/index.js
+++ src/index.js
@@ -44,8 +44,11 @@ window.addEventListener('load', (event) => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      // console.log('i clicked');
+      // console.log(`Card clicked: ${card}`);
+      card.classList.add('flip')
+      console.log(card);
     });
   });
 });
+
--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,52 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+
+    if (this.cards == null) {
+      return undefined
+    }
+
+  var m = this.cards.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = this.cards[m];
+    this.cards[m] = this.cards[i];
+    this.cards[i] = t;
+  }
+
+  return this.cards;
+    
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1
+    if (card1 === card2) {
+      this.pairsGuessed += 1
+      return true }
+      else {
+        return false
+      }
+
+    
   }
 
   checkIfFinished() {
-    // ... write your code here
-  }
+    if (this.pairsGuessed == this.cards.length / 2) {
+      return true 
+    } else {
+      return false
+    }
+  } 
 }
