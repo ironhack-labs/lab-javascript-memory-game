1d1f01e9236e8ea211156035c191ebcec5ed6466
Numan Torres <numantorresr@gmail.com>
2022-08-17 19:21:29 +0200

    done

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,34 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (this.cards === undefined) {
+      return undefined
+    }
+    this.cards.sort((a, b) => 0.5 - Math.random());
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++
+    if (card1 === card2) {
+      this.pairsGuessed += 1
+      return true
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true
+    } else {
+      return false
+    }
   }
 }
