bc2cf18ef24ab7ba78e89b779004a3c5afa17ba1
Your Name <you@example.com>
2022-08-17 19:31:16 +0200

    Lab Done Without dom

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,36 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (this.cards === undefined) {
+      return undefined
+    }
+    if (this.cards.sort(() => 0.5 - Math.random())) {
+      return true
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++
+    if (card1 === card2) {
+      this.pairsGuessed++
+      return true
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true
+    } else {
+      return false
+    }
   }
+
 }
