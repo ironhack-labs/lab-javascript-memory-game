05539e4e3367bac2f643c1ad5576eb6b8c70addb
juca25 <jserranocucarella@gmail.com>
2022-08-18 09:17:29 +0200

    logic done

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,36 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsGuessed = 0
+    this.pairsClicked = 0
   }
 
   shuffleCards() {
-    // ... write your code here
-  }
+      if (this.cards === undefined) return undefined
+      
+      this.cards.sort((a,b) => 0.5 - Math.random())
+      return this.cards
+       }
+  
+  
+  
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    if(card1 === card2){
+      this.pairsClicked++
+      this.pairsGuessed++
+      return true
+    }else{
+      this.pairsClicked++
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if ( this.pairsGuessed === this.cards.length/2){
+      return true
+    }return false
+    
   }
 }
