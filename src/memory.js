d389094797759b0915289942c47c239207f2aba3
Mario Fernandez <marioteleco@outlook.com>
2022-08-18 07:54:18 +0000

    Memory.js part donegit status! Dom part was hard to implement, need help about it!

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,54 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
+
   shuffleCards() {
-    // ... write your code here
+
+    /*for (let i = deck.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * i);
+      let temp = this.pickedCards[i];
+      this.pickedCards[i] = this.pickedCards[j];
+      this.pickedCards[j] = temp;
+
+    }
+    return this.cards;*/
+    if (this.cards === undefined) return undefined;
+    let shuffle =
+      this.cards.sort(() => Math.random() - 0.5);
+    return shuffle;
+
+
+
+
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked = 1;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+
+    } else {
+
+      return false;
+
+    }
+    return this.pairsClicked;
+
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed * 2 === this.cards.length) {
+      return true;
+    } else {
+      return false;
+    }
+
+
   }
 }
