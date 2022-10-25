947eb5860f843480f73838486a14a11e459db8dc
Gonzalo García Corrales <115544633+gongarco@users.noreply.github.com>
2022-10-25 19:29:33 +0200

    Done

--- src/memory.js
+++ src/memory.js
@@ -4,19 +4,44 @@ class MemoryGame {
     this.pickedCards = []
     this.pairsClicked = 0
     this.pairsGuessed = 0
-    
+
   }
 
   shuffleCards() {
-    // ... write your code here
+    // ...he intentado adaptar esta estructura que he encontrado investigando. La medio entiendo, pero no termina de funcionar del todo.
+    const pickRandom = (cards, card) => {
+      const cloneArray = [...cards]
+      const randomPicks = []
+
+      for (let index = 0; index < card; index++) {
+        const randomIndex = Math.floor(Math.random() * cloneArray.length)
+
+        randomPicks.push(cloneArray[randomIndex])
+        cloneArray.splice(randomIndex, 1)
+      }
+      return randomPicks
+    }
 
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1
+
+    if (card1 === card2) {
+      this.pairsGuessed += 1
+      return true
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === 12) {
+      return true
+    } else if (this.pairsGuessed === 0) {
+      return false
+    } else if (this.pairsGuessed < 12) {
+      return false
+    }
   }
 }
