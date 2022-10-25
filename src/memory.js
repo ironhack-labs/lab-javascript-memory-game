c43ba6823b4f89b28941f85f30b1b1a2ee14d726
Raquel Tejada <tejadadejesusraquel@gmail.com>
2022-10-25 22:33:06 +0200

    Memory game

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,35 @@
 class MemoryGame {
   constructor(cards) {
-    this.cards = cards;
-    // add the rest of the class properties here
+    this.cards = cards
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined
+    } else {
+      return this.cards.sort(() => Math.random() - 0.5)
+    }
+
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1
+    if (card1 === card2) {
+      this.pairsGuessed += 1
+      return true
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === 24 / 2) {
+      return true
+    } else {
+      return false
+    }
   }
 }
