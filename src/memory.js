d59fa917e873e22bb0aa1c1c7310031db9ae01ac
Gonzalo García Corrales <115544633+gongarco@users.noreply.github.com>
2022-10-25 18:14:25 +0200

    Iteration_2.1

--- src/memory.js
+++ src/memory.js
@@ -1,11 +1,15 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+    
   }
 
   shuffleCards() {
     // ... write your code here
+
   }
 
   checkIfPair(card1, card2) {
