f1822b855860b30959273847d2e4bf21fc094919
NaomiBurguesMontesdeoca <naomi.burgues@gmail.com>
2022-10-25 19:39:59 +0200

    not solved lab

--- src/index.js
+++ src/index.js
@@ -46,6 +46,9 @@ window.addEventListener('load', (event) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
       console.log(`Card clicked: ${card}`);
+      card.classList.toggle('turned')
+
+
     });
   });
 });
