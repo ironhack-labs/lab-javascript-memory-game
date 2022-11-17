3eeb0d916293d7dc213fc806f47bbf6e49cd49f3
Henrique Mazzoleni <73548364+Henrique-mazzoleni@users.noreply.github.com>
2022-11-17 11:00:11 +0000

    iteration #2 implemented but not verified

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,29 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    let j, key;
+    for (let i=this.cards.length-1; i>=1; i--) {
+      j = Math.floor(Math.random()*(i+1))
+      key = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = key;
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    if (card1 === card2) {
+      this.pairsGuessed++;
+    }
+    this.pairsClicked++;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    return this.pairsGuessed === this.cards.length / 2
   }
 }
