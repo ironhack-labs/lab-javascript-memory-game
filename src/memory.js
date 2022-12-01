6085c70bf2c1fb01b701a65cd58cb4ae852661b9
David Mullally <mullallydavid7@gmail.com>
2022-12-01 01:10:36 +0100

    complete

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,41 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
-  shuffleCards() {
+  shuffleCards(cardsArray) {
     // ... write your code here
+    if (cardsArray === undefined) {
+      return undefined;
+    } else {
+      let shuffledCards = cardsArray.sort(function () {
+        return Math.random() - 0.5;
+      });
+      return shuffledCards;
+    }
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
     // ... write your code here
+    if (this.pairsGuessed === this.cards / 2) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
