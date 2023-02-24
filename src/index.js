f5dd50a92dedad3cdb40e5374d15ff101b7a4bc8
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-02-24 21:05:45 +0100

    Added checkIfPair and checkIfFinished method to check two pairs

--- src/index.js
+++ src/index.js
@@ -28,24 +28,56 @@ const cards = [
 const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', (event) => {
+  let index = 0;
   let html = '';
+  memoryGame.shuffleCards();
   memoryGame.cards.forEach((pic) => {
     html += `
-      <div class="card" data-card-name="${pic.name}">
+      <div id="${index}" class="card" data-card-name="${pic.name}">
         <div class="back" name="${pic.img}"></div>
         <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
       </div>
     `;
+    index++;
   });
 
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
+   let compareCards = [];
+
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      card.classList.add('turned');
+      console.log(card);
+      compareCards.push(card);
+      if(compareCards.length === 2){
+       if(memoryGame.checkIfPair(compareCards[0], compareCards[1]) === true){
+        const firstCard = compareCards[0].getAttribute('id');
+        const secondCard = compareCards[1].getAttribute('id');
+        const firstCardId = document.getElementById(`${firstCard}`);
+        const secondCardId = document.getElementById(`${secondCard}`);
+        firstCardId.classList.add('blocked');
+        secondCardId.classList.add('blocked');
+        if(memoryGame.checkIfFinished() === true){
+          alert("The Game is finished");
+        }
+       }else{
+        const firstCard = compareCards[0].getAttribute('id');
+        const secondCard = compareCards[1].getAttribute('id');
+        const firstCardId = document.getElementById(`${firstCard}`);
+        const secondCardId = document.getElementById(`${secondCard}`);
+        setTimeout(() => {
+          firstCardId.classList.remove('turned');
+          secondCardId.classList.remove('turned');
+        }, 1500)
+        if(memoryGame.checkIfFinished() === true){
+          alert("The Game is finished");
+        }
+       }
+      compareCards = [];
+      }
     });
   });
 });
