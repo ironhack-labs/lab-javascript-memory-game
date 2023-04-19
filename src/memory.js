5a322ef8658755f544650d776858a3ceeb9e1785
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2023-04-19 21:35:08 +0200

    Done Lab

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,36 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined
+    } else {
+      this.cards.sort((a, b) => 0.5 - Math.random());
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1
+    if (card1 === card2) {
+      this.pairsGuessed += 1
+      return true
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    const halfNumber = this.cards.length / 2
+    if (this.pairsGuessed < halfNumber) {
+      return false
+    }
+    else {
+      return true
+    }
   }
 }
