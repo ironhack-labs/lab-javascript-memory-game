fa99f1638fff78ec69d10005bf37058db9199ad4
CrisRamosLazaro <crislr@hotmail.com>
2023-04-20 00:32:44 +0200

    memory.js done

--- src/index.js
+++ src/index.js
@@ -27,7 +27,8 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
-window.addEventListener('load', (event) => {
+
+window.addEventListener('load', () => {
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
@@ -38,14 +39,21 @@ window.addEventListener('load', (event) => {
     `;
   });
 
+  memoryGame.shuffleCards();
+
   // Add all the divs to the HTML
+
+
   document.querySelector('#memory-board').innerHTML = html;
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
+      memoryGame.checkIfPair(card.dataset.cardName)
       console.log(`Card clicked: ${card}`);
+      memoryGame.checkIfFinished();
+
     });
   });
 });
--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,44 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
   shuffleCards() {
-    // ... write your code here
+
+    if (this.cards = []) {
+      return undefined
+    } else {
+
+      const cardsShuffled = this.cards.map((card, i) => {
+        let randomIndex = Math.floor(Math.random() * (i + 1));
+        [this.cards[randomIndex], this.cards[i]] = [this.cards[i], this.cards[randomIndex]]
+      })
+
+      return cardsShuffled
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+
+    this.pairsClicked++
+
+    if (card1 === card2) {
+      this.pairsGuessed++
+      return true
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+
+    if (this.pairsGuessed === 12) {
+      return true
+    } else {
+      return false
+    }
   }
 }
