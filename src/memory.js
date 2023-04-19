da1c64004a5d7842ef354deaf33bc4a5bf33f6bb
“Roberto <robertoarcosmolina@gmail.com>
2023-04-19 23:11:06 +0200

    memory game

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,36 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = []
+    this.pairClicked = 0
+    this.pairGuessed = 0
     // add the rest of the class properties here
   }
 
   shuffleCards() {
+
     // ... write your code here
   }
 
   checkIfPair(card1, card2) {
+    this.PairsClick++
+    if (card1 === card2) {
+      this.PairsGuessed++
+      return true
+    } else {
+      return false
+    }
+
     // ... write your code here
   }
 
   checkIfFinished() {
+    if (this.pairGuessed === this.cards / 2) {
+      return true
+    } else {
+      return false
+    }
+
     // ... write your code here
   }
 }
