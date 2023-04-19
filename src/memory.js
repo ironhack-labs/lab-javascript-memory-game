b56dbf7c5a5b4847c6eb2de56049d54449fe48a7
MSiesto <miguelsiesto@gmail.com>
2023-04-19 18:26:16 +0200

    Plan your game

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,38 @@
 class MemoryGame {
+
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if(!this.cards) {
+      return undefined
+    }
+
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = temp;
+    }
+
   }
+  
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++
+      if(card1===card2) {
+      this.pairsGuessed++
+      return true
+    }
+    return false
   }
 
   checkIfFinished() {
-    // ... write your code here
+    //quizás esté petando por esto
+    return this.pairsGuessed === 12 ? true : false 
   }
 }
