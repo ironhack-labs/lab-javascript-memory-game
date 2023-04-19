8fddd07e5e445f3701adb69ea78a12df5b0beaab
Pedro de la Torre <pedrodltl@gmail.com>
2023-04-19 23:51:27 +0200

    lab memory game

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,43 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
+
+
   shuffleCards() {
-    // ... write your code here
+
+    if (!this.cards) {
+      return undefined
+    }
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+
+    if (card1 === card2) {
+      this.pairsClicked++
+      this.pairsGuessed++
+      return true
+    } else {
+      this.pairsClicked++
+      return false
+    }
+
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === 12) {
+      return true
+    } else {
+      return false
+    }
   }
 }
