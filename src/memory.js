759cfbb8cf558acd456a9a2f2863e09f6add5783
Sirkem112 <meingithub01@gmail.com>
2023-06-07 17:59:22 +0200

    lab Card games solved

--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,49 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
+
+  //   shuffleCards method
+  // should be declared
+  // should return undefined if argument (cards array) is not passed
+  // should return the shuffled (mixed) array of cards
 
   shuffleCards() {
     // ... write your code here
+    if (!this.cards) {
+      return undefined;
+    }
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+    return this.Cards;
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
     // ... write your code here
+
+    if (this.pairsGuessed === this.cards.legth / 2) {
+      return true;
+    } else this.pairsGuessed < this.cards.legth / 2;
+    {
+      return false;
+    }
   }
 }
+
+// ------------------------------------------------------------
