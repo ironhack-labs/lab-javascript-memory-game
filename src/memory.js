ecc4155e595528a52d9080e007ea055794cf9fbe
Veronica Galvez <veronicagalvez88@gmail.com>
2023-06-16 23:12:54 +0200

    change memory game

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,47 @@
 class MemoryGame {
   constructor(cards) {
-    this.cards = cards;
-    // add the rest of the class properties here
+    this.cards = cards
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+   }
+   
+  shuffleCards() {
+    const shuffledCard = []
+    if (!this.cards) {
+      return undefined
+    }
+    const shuffledCards = cards.map(card) 
+    
+    for (let i = shuffledCard.length -1; i > 0; i--) {
+      const randomShuffleCard = Math.floor(Math.random() * (i + 1))
+      [shuffledCard[i], shuffledCard[randomShuffleCard]] = [shuffledCard[randomShuffleCard], shuffledCard[i]]
+    }
+    return shuffledCard
   }
 
-  shuffleCards() {
-    // ... write your code here
-  }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    
+    this.pairsClicked += 1
+      if (card1 === card2) {
+        this.pairsGuessed += 1
+        return true
+      } else {
+        return false
+    } 
+  } 
+
+
+  
+checkIfFinished() {
+  
+  if (this.pairsGuessed === this.cards.length / 2) {
+    return true
+    } else {
+    return false
+    }
   }
 
-  checkIfFinished() {
-    // ... write your code here
-  }
 }
+
