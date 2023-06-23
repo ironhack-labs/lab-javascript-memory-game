9ecc3a671c3df982414fa7edac409587fbfbab49
Agustin Paredes <nomegustabergamo@outlook.com>
2023-06-23 21:27:19 +0200

    solved

--- src/index.js
+++ src/index.js
@@ -41,11 +41,46 @@ window.addEventListener('load', (event) => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
+  let card1 = null;
+  let card2 = null;
+
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      // console.log(`Card clicked: ${card}`);
+      if (!card1) {
+        card1 = card;
+        card1.classList.add('turned');
+      } else if (!card2) {
+        card2 = card;
+        card2.classList.add('turned');
+
+
+        const cardName1 = card1.getAttribute('data-card-name');
+        const cardName2 = card2.getAttribute('data-card-name');
+        const isPairMatch = memoryGame.checkIfPair(cardName1, cardName2);
+
+        if (isPairMatch) {
+          card1.classList.add('blocked');
+          card2.classList.add('blocked');
+          card1 = null;
+          card2 = null;
+
+          if (memoryGame.checkIfFinished()) {
+            alert('You won!!!');
+          }
+        } else {
+          setTimeout(() => {
+            card1.classList.remove('turned');
+            card2.classList.remove('turned');
+            card1 = null;
+            card2 = null;
+          }, 1000);
+        }
+      }
+
+
     });
   });
 });
