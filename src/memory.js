247b941abd62cc6925d3115dcaeb5b2fd932d92b
FERGAZA <fernandomgzapata@gmail.com>
2023-07-18 20:00:15 +0200

    Task

--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,39 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
   shuffleCards() {
     // ... write your code here
+    if (!this.cards) {
+      return undefined
+    }
+
+    this.cards.sort(() => Math.random() - 0.5)
+
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+
+    this.pairsClicked++
+    if (card1 === card2) {
+      this.pairsGuessed++
+      return true
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
     // ... write your code here
+    if (this.pairsGuessed < 12) {
+      return false
+    } else {
+      return true
+    }
   }
 }
