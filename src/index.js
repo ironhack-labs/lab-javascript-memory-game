77be8d1a3e818fc2a36c55e12a8a30002ca570a6
cadelme <carlosd@MacBook-Pro-de-Carlos-2.local>
2023-07-18 22:39:25 +0200

    carlosd not solved memory game

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', (event) => {
   let html = '';
+
   memoryGame.cards.forEach((pic) => {
     html += `
       <div class="card" data-card-name="${pic.name}">
@@ -41,11 +42,59 @@ window.addEventListener('load', (event) => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
+  let card1
+  let card2
+  let turned = true
+
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
+      card.classList.add("turned")
+      //console.log(card.dataset.clicked)
+
+      if (card1 === undefined){
+        card1 = card.dataset.cardName
+      }else{
+        card2 = card.dataset.cardName
+      }
+
+      if (card1 && card2){
+        let turned = checkIfPair(card1, card2)
+        
+      }
+
+      
+      
+      if (turned == false){
+        
+        card.classList.remove("turned")
+        
+      }
+      
+   
+        
+      
+      
+      
+      
+        
+        //console.log(turned)
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      //console.log(`Card clicked: ${card}`);
     });
   });
+
+  function checkIfPair(card1, card2){
+
+    console.log("esta carta es--------", card1)
+    console.log("esta carta es 2--------", card2)
+
+    //console.log(card1)
+    if (card1 === card2){
+      return true
+    }else{
+      return false
+
+    }
+  }
 });
