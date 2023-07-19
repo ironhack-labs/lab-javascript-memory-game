5df44bd319d467f8ee1f9d286803c6b8afbe7ce6
Claudia Alvarez Posada <daibhaith@hotmail.com>
2023-07-19 09:00:00 +0200

    Lab Done

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,39 @@
 class MemoryGame {
-  constructor(cards) {
+  constructor(cards, /*pickedCards, pairsClicked, pairsGuessed*/) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
-    // ... write your code here
+
+    if (!this.cards) {
+      return undefined
+    } else {
+      const newGame = this.cards.sort(() => Math.random() - 0.5);
+      return newGame
+    }
+
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    if (card1 === card2) {
+      this.pairsClicked += 1
+      this.pairsGuessed += 1;
+      return true
+    } else (card1 != card2)
+    this.pairsClicked += 1
+    return false
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === 12) {
+      return true
+    } else if (this.pairsGuessed != 12) {
+      return false
+    }
   }
 }
+
