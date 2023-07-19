ced31e6fd1ec630a4ccb3ee97030009c7d07f26c
helena-141 <helen_141@msn.com>
2023-07-19 09:13:45 +0200

    done

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,42 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+
     // add the rest of the class properties here
   }
 
   shuffleCards() {
     // ... write your code here
+    if (!this.cards ){
+      return undefined
+    }
+    
+    this.cards.sort(() => Math.random() - 0.5)
+
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true
+    } else {
+      this.pairsClicked++;
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+
+    if (this.pairsGuessed < 12) {
+      return false
+    }
+      
+    else if (this.pairsGuessed >= 12 ) {
+      return true
+    }
   }
 }
