6f1f213a912ebfcc13b56a9da006250edf003e43
Beatriz <beatrizrmb@outlook.com>
2023-08-09 15:10:15 +0100

    finished half of lab

--- src/memory.js
+++ src/memory.js
@@ -2,6 +2,9 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
@@ -10,6 +13,14 @@ class MemoryGame {
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked++;
+    if (card1 === card2){
+      this.pairsGuessed++;
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   checkIfFinished() {
