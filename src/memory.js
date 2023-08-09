987163bb5958d9145b809d01248640c1a9e21284
Merna <merna.wahdan@gmail.com>
2023-08-09 18:39:53 +0200

    finished tests & flipped the cards

--- src/memory.js
+++ src/memory.js
@@ -25,5 +25,10 @@ class MemoryGame {
 
   checkIfFinished() {
     // ... write your code here
+    if(this.pairsGuessed === 8) {
+      return true
+    } else {
+      return false 
+    }
   }
 }
