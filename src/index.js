382ca843badafb759de528b39ac4cbca40728ada
Monica Sanchez <monicaa.sanchezv@gmail.com>
2023-08-10 02:10:33 +0200

    Lab-in-progress

--- src/index.js
+++ src/index.js
@@ -29,23 +29,74 @@ const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', (event) => {
   let html = '';
+  
   memoryGame.cards.forEach((pic) => {
     html += `
       <div class="card" data-card-name="${pic.name}">
         <div class="back" name="${pic.img}"></div>
         <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
       </div>
-    `;
+    `
   });
-
+  
+  
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
-
+  
   // Bind the click event of each element to a function
+  
+  
   document.querySelectorAll('.card').forEach((card) => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
+    card.addEventListener('click', (event) => {
+
+      console.log(card)
+      card.classList.add("turned")
+      //console.log(event.target.parent.classList)
+
+      //card.children[0].classList.add("front")
+      //card.children[0].classList.remove("back")
+      //card.children[1].classList.add("back")
+      // card.children[1].classList.remove("front")
+
+      console.log(event.target)
+      //event.target.innerText = "Unicorns are amazing"
+
+      // event.target.classList.toggle ("front")
+      // card.target.innerText = "unicorns are amazing"
+
+
+
+      if (memoryGame.pickedCards.length < 2) {
+        memoryGame.pickedCards.push(card)
+      }
+
+
+
+      if (memoryGame.pickedCards.length === 2) {
+        const isPair = memoryGame.checkIfPair(memoryGame.pickedCards[0].getAttribute("data-card-name"), 
+        memoryGame.pickedCards[1].getAttribute("data-card-name"))
+
+        if (!isPair) {
+
+          card.classList.remove("turned")
+
+        }
+
+        memoryGame.pickedCards = []
+
+      }
+
+
+
+      //once clicked, add card to pickedCards
+      //once pickedCards has 2 cards, call checkIfPair, mind the value for the argument
+      //if true, don't flipt it back, if false, flip it back
+      //make sure to empty pickedCards
+      // TODO: write some code here =>checkifpair() - if false return cards back
       console.log(`Card clicked: ${card}`);
     });
+    memoryGame.checkIfPair()
   });
 });
+
+    
--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,42 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    //shuffle cards
   }
 
   shuffleCards() {
+    //loop over the cards and change its indexes
     // ... write your code here
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked++;
+
+    if (card1 === card2){
+      this.pairsGuessed++;
+      this.checkIfFinished()
+      return true;
+    }
+    
+    else{
+      return false;
+    }
+    
+    
   }
 
   checkIfFinished() {
+    
     // ... write your code here
+    if(this.pairsGuessed === 8){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 }
