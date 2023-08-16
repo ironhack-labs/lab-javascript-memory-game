7c43f1e91200ebc14fc2438efe64ed9458fb8f49
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-08-16 16:13:51 +0200

    wip

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,45 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;  // initialize pairsClicked to 0
+    this.pairsGuessed = 0;
   }
+    // let revealedCount = 0;
+    // let activeCard = null;
+    // let awaintingEndOfmove = false ;
+
+    shuffleArray() {
+
+      // const shuffleArray = [...this.cards];
+      this.cards.sort(() => Math.random() - 0.5); //for loop to get a random cards using index
+
+      return shuffleArray;
+      
+    }
 
   shuffleCards() {
-    // ... write your code here
+
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.card1 = card1;
+    this.card2 = card2;
+
+    this.pairsClicked++;
+
+
+    if (this.card1 === this.card2) {
+      this.pairsGuessed++; // nnncrement pairsguessed if the cards match
+      return true; // cards match
+    } else {
+      return false; // cards dont match
+    }
   }
+  
 
   checkIfFinished() {
-    // ... write your code here
+ 
   }
 }
+
