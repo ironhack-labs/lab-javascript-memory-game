e85adda136a122b0ead119c7209127a008c69596
Juan <juangalbea@gmail.com>
2023-08-19 19:52:36 +0200

    feat: implemented all the dom

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,35 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined
+    }
+    let allCards = [...this.cards]
+    this.cards = []
+   while (allCards.length > 0) {
+    let selectedCard = allCards.splice(Math.floor(Math.random()*allCards.length),1)
+    this.cards.push(selectedCard[0])
+   }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+   this.pairsClicked++
+
+   if (card1 === card2) {
+    this.pairsGuessed++
+    return true
+   } 
+    return false
+   
   }
 
   checkIfFinished() {
-    // ... write your code here
+    return this.pairsGuessed === this.cards.length / 2 ? true : false
   }
 }
