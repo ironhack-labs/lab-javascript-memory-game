0eda2796d3ab6e400013161e405b858dac3ba363
alvaro sanchez <alvaro_piro_@hotmail.com>
2023-10-19 21:18:57 +0200

    ....

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,52 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (this.cards == undefined) {
+      return undefined
+    }
+
+    else {
+      let i = this.cards.length;
+      while (--i > 0) {
+        let temp = Math.floor(Math.random() * (i + 1));
+        [this.cards[temp], this.cards[i]] = [this.cards[i], this.cards[temp]]
+      }
+    }
+
   }
 
+
   checkIfPair(card1, card2) {
-    // ... write your code here
+
+    if (card1 === card2) {
+      this.pairsGuessed += 1
+      return true
+    }
+    else {
+      this.pairsClicked += 1
+      return false
+    }
+
+
+
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed == 12) {
+      return true
+    } else if (this.pairsGuessed >= 1) {
+      return false
+    } else {
+      this.pairsGuessed == 0
+      return false
+    }
+
+
   }
 }
