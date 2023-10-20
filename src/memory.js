270d35a1b7eee3e6954c87e36dc0f73b9850bc31
Cesaria Colazzo <csr.colazzo@gmail.com>
2023-10-20 09:23:07 +0200

    done

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,87 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
+
+  pickedCards = []
+
+  // add the rest of the class properties here
+
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined
+
+
+    }
+
+
+
+
+    for (let i = 0; i < this.cards.length; i++) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+
+
+
+
+
+    }
+    {
+
+
+    }
+
   }
+
+
+
+
+
+
+
+
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    if (card1 === card2) {
+      this.pairsGuessed += 1
+      return true
+    }
+    else {
+      this.pairsClicked += 1
+      return false
+    }
   }
 
+
+
   checkIfFinished() {
-    // ... write your code here
+
+    if (this.pairsGuessed < 12) {
+      return false
+    } else {
+      return true
+    }
   }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
