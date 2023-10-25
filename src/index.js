158c88095930738b76070b64c5de2a77360aabab
WolfofHM <celebioglu476@gmail.com>
2023-10-25 19:04:37 +0300

    LabsolvedhalfWay

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ const cards = [
 const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', (event) => {
+  fillBoardwithCards();
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
@@ -45,6 +46,7 @@ window.addEventListener('load', (event) => {
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
+      
       console.log(`Card clicked: ${card}`);
     });
   });
