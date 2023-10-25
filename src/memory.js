05646d7a7ef95f939c7bfcc3183320822d0b87bc
“elisejonkers” <elisejonkers18@hotmail.com>
2023-10-25 20:59:42 +0200

    lab done for today

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,36 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+
   }
 
   shuffleCards() {
     // ... write your code here
+    //BONUS
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++
+    if (card1 === card2) {
+      this.pairsGuessed++
+      return true
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
     // ... write your code here
+    // check if PairsGuessed has reached the number of pairs the deck has
+    //Make it generic. Valid despite the number of cards a deck might have
+    // Return true or false
+    if (this.pairsGuessed !== this.cards.length /2) {
+      return false
+    } else {
+      return true
+    }
   }
 }
