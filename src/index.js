709dc2d2fd662b189c408cdf8817f3c32a39e3ee
pomerama <marpom@gmail.com>
2023-10-25 10:50:25 +0200

    done

--- src/index.js
+++ src/index.js
@@ -45,7 +45,46 @@ window.addEventListener('load', (event) => {
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      // console.log(`Card clicked: ${card}`);
+
+      // Turn card if it is not blocked (blocked cards are right guessed ones)
+      card.classList.toggle('turned', !card.className.includes('blocked'));
+
+      if (memoryGame.pickedCards.length < 2) {
+        memoryGame.pickedCards.push(card);
+      }
+
+      if (memoryGame.pickedCards.length == 2) {
+        // compare the cards using their name
+        if (memoryGame.checkIfPair(memoryGame.pickedCards[0].getAttribute('data-card-name'),
+          memoryGame.pickedCards[1].getAttribute('data-card-name'))) {
+          memoryGame.pickedCards[0].classList.add('blocked')
+          memoryGame.pickedCards[1].classList.add('blocked')
+        } else {
+          // TODO: figure out why setTimeout not working properly
+          // (not getting the memoryGame.pickedCards)          
+          flipAfterTimeout(memoryGame.pickedCards)
+        }
+        // empty the picked cards array
+        memoryGame.pickedCards = [];
+      }
+
+      document.getElementById('pairs-clicked').innerHTML = memoryGame.pairsClicked;
+      document.getElementById('pairs-guessed').innerHTML = memoryGame.pairsGuessed;
+
+      if (memoryGame.checkIfFinished()) {
+        console.log("----GAME FINISHED---")
+        alert(`Game finished. You managed with ${memoryGame.pairsClicked} attempts`);
+      }
     });
   });
 });
+
+// helper function to get setTimeout to work properly
+// assumes the cardsArray has exactly 2 card elements
+function flipAfterTimeout(cardsArray) {
+  setTimeout(() => {
+    cardsArray[0].classList.toggle('turned');
+    cardsArray[1].classList.toggle('turned');
+  }, 1000)
+}
