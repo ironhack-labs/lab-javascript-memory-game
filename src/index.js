b1680342a30f398a078570b19f82827ab4ef381b
Patricia Kroenert <p.kroenert98@gmail.com>
2023-10-26 10:51:24 +0200

    memory solved

--- src/index.js
+++ src/index.js
@@ -27,7 +27,11 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+let pickedCards = memoryGame.pickedCards;
+
 window.addEventListener('load', (event) => {
+  //event.preventDefault(); 
+  //memoryGame.shuffleCards(cards);
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
@@ -41,11 +45,53 @@ window.addEventListener('load', (event) => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
+
+
   // Bind the click event of each element to a function
-  document.querySelectorAll('.card').forEach((card) => {
-    card.addEventListener('click', () => {
+  document.querySelectorAll(".card").forEach((card) => {
+    card.addEventListener("click", () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
-  });
+  // add turned class after click;
+  card.classList.add("turned");
+  pickedCards.push(card);
+
+  let pairsClicked = document.getElementById("pairs-clicked");
+  pairsClicked.innerHTML++;
+
+  if(pickedCards.length === 2){
+    const dataNameFirst = pickedCards[0].getAttribute("data-card-name");
+    const dataNameSecond = pickedCards[1].getAttribute("data-card-name");
+
+    if(memoryGame.checkIfPair(dataNameFirst, dataNameSecond)){
+      memoryGame.checkIfPair(dataNameFirst, dataNameSecond);
+      pickedCards = [];
+
+      let pairsGuessed = document.getElementById("pairs-guessed");
+      pairsGuessed.innerHTML++;
+
+      console.log(
+        "Check if correct =>",
+        typeof pairsGuessed.innerHTML
+      );
+    if(cards.length/2 === +(pairsGuessed.innerHTML)){
+      console.log("You won!");
+      const header = document.getElementById("header");
+      header.style.color = "green";
+      header.innerHTML = "You won!";
+    }
+    }else{
+      setTimeout(() => {
+        if(pickedCards[0].classList.contains("turned")){
+          pickedCards[0].classList.remove("turned");
+        }
+        if(pickedCards[1].classList.contains("turned")){
+          pickedCards[1].classList.remove("turned");
+        }
+        pickedCards = [];
+      } , 1200);
+    }
+  }
 });
+});
+});
+
