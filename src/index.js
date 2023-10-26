aba6bfb8e56f1e24e48940616fbfa5ee5bc05b03
Zohra <zohra.boukhatem1@gmail.com>
2023-10-26 08:48:24 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -44,8 +44,8 @@ window.addEventListener('load', (event) => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      card.classList.add('turned');
+            console.log(`Card clicked: ${card}`);
     });
   });
 });
