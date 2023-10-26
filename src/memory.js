bdb59f18b5b13550a1ecaa767e2272511a7925ec
your_github_username <your_github_email>
2023-10-26 13:01:06 +0200

    memory-game-done

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,35 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
     // ... write your code here
+    if (!this.cards) return undefined;
+    const copy = [...this.cards];
+    this.cards.sort(() => Math.random() - 0.5);
+    return this.cards;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
   }
+
+  // ... write your code here
 
   checkIfFinished() {
     // ... write your code here
+    return this.pairsGuessed === this.cards.length / 2;
   }
 }
