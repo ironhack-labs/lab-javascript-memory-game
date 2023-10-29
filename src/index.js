fecb2db69f67977ab6990a089b1f0d257a55e4a4
Alessandra Scarpellini <scaalessandra@icloud.com>
2023-10-29 19:09:50 +0100

    remove cards array and store it in deck.js file, rename cardsDomElement, create board variableto store memory-board element, add comments trying to explain parts of the given code

--- src/index.js
+++ src/index.js
@@ -1,43 +1,15 @@
-// Array of cards we'll be displaying / playing with
-const cardsData = [
-  { name: 'aquaman', img: 'aquaman.jpg' },
-  { name: 'batman', img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four', img: 'fantastic-four.jpg' },
-  { name: 'flash', img: 'flash.jpg' },
-  { name: 'green arrow', img: 'green-arrow.jpg' },
-  { name: 'green lantern', img: 'green-lantern.jpg' },
-  { name: 'ironman', img: 'ironman.jpg' },
-  { name: 'spiderman', img: 'spiderman.jpg' },
-  { name: 'superman', img: 'superman.jpg' },
-  { name: 'the avengers', img: 'the-avengers.jpg' },
-  { name: 'thor', img: 'thor.jpg' },
-  { name: 'aquaman', img: 'aquaman.jpg' },
-  { name: 'batman', img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four', img: 'fantastic-four.jpg' },
-  { name: 'flash', img: 'flash.jpg' },
-  { name: 'green arrow', img: 'green-arrow.jpg' },
-  { name: 'green lantern', img: 'green-lantern.jpg' },
-  { name: 'ironman', img: 'ironman.jpg' },
-  { name: 'spiderman', img: 'spiderman.jpg' },
-  { name: 'superman', img: 'superman.jpg' },
-  { name: 'the avengers', img: 'the-avengers.jpg' },
-  { name: 'thor', img: 'thor.jpg' }
-];
 
-// Create an instance of the Memory Game passing the 'cardsData' array as argument
+// Create an instance of the MemoryGame class, passing the 'cardsData' array (from deck.js) as argument
 const memoryGame = new MemoryGame(cardsData);
 
-// 🌟 BONUS: Call the shuffleCards method rigth at the beginning, to shuffle the deck before displaying it
+// 🌟 BONUS: 👇 Call the shuffleCards method, to shuffle the deck before displaying it
 
 
-// when the page load, this code will be executed: 
-// will take the cards array, and for each card create divs with all card's info
+// when the page loads, for each card in the memoryGame.cards array, create divs with card's data
 window.addEventListener('load', (event) => {
-  let cardsDomElements = '';
+  let cardsDomElement = '';
   memoryGame.cards.forEach((cardObj) => {
-    cardsDomElements += 
+    cardsDomElement += 
     `
       <div class="card" data-card-name="${cardObj.name}">
         <div class="back" name="${cardObj.img}"></div>
@@ -46,17 +18,33 @@ window.addEventListener('load', (event) => {
     `
   });
 
-  // Inject all the divs into the HTML, inside targeted board
-  document.querySelector('#memory-board').innerHTML = cardsDomElements;
+  // target the div element with id = memory-board
+  let board = document.querySelector('#memory-board');
+  // inject the cards element into the board
+  board.innerHTML = cardsDomElement;
 
   // Bind the click event of each (<div class="card" ...>) element to a function
+  // every time a card is clicked your code will be executed 
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
+  
       console.log(`Card clicked: `, card);
 
       // 👇👇👇 TODO: write your code here 👇👇👇
 
+      // Iter 2.1
 
+
+      // Iter 2.2
+
+      
+      // Iter 2.3 
+
+        
+      // 🌟 BONUS: replace alert with message
+
+      // 🌟 BONUS: display score
+      
 
     });
   });
