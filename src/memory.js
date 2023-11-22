c1023bb40b5237d962aae435dd69c458826a3f0d
alberto-sclocchi <scloks75tv@gmail.com>
2023-11-22 18:40:08 -0500

    finish iteration 1 & 2

--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,43 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    //... write your code here
+    if(!this.cards){
+      return undefined;
+    }
+
+    for(let i = this.cards.length - 1; i > 0; i--){
+      const randPos = Math.floor(Math.random()*(i + 1));
+      [this.cards[i],this.cards[randPos]] = [this.cards[randPos],this.cards[i]] 
+    }
+    return this.cards;
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked += 1;
+
+    if(card1 === card2){
+      this.pairsGuessed += 1;
+      return true;
+    }
+
+    return false;
+
   }
 
   checkIfFinished() {
     // ... write your code here
+    if (this.pairsGuessed < this.cards.length/2){
+      return false;
+    } 
+
+    return true;
   }
 }
