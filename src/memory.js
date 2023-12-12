855cfeff78aff0fe2599f013c25b4d90e429ae68
Juan Morales Martinez <juanmoralesmartinez@Juans-MacBook-Pro.local>
2023-12-13 00:37:24 +0100

    Memory part done

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,49 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
   shuffleCards() {
-    // ... write your code here
+    if(!this.cards){
+      return undefined
+    }
+    else{
+      for(let i=this.cards.length -1; i>0;i--){
+        let j = Math.floor(Math.random()*(this.cards.length))
+        let shuffled = [this.cards[i],this.cards[j]]=[this.cards[j],this.cards[i]]
+        return shuffled
+      }
+
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++
+    if(card1===card2){
+      this.pairsGuessed++
+      return true
+    }
+    if(card1 !== card2){
+      return false
+    }
+  
+    
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if(this.pairsGuessed===0){
+      return false
+    }
+    let totalPairs = this.cards.length/2
+   if(this.pairsGuessed<totalPairs){
+    return false
+   }
+   else{
+    return true
+   }
+   
   }
 }
