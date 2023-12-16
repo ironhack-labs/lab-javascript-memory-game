7f53f798c3d536c269cadfe42a014412787d30ad
moreira8125 <your_github_email>
2023-12-16 09:27:01 +0000

    lab done

--- src/index.js
+++ src/index.js
@@ -45,8 +45,35 @@ window.addEventListener("load", (event) => {
   document.querySelectorAll(".card").forEach((card) => {
     card.addEventListener("click", () => {
       // TODO: write some code here
+      card.classList.toggle("turned");
+      memoryGame.pickedCards.push(card);
 
-      console.log(`Card clicked: ${card}`);
+      if (memoryGame.pickedCards.length === 2) {
+        const card1 = memoryGame.pickedCards[0];
+        const card1Name = card1.getAttribute("data-card-name");
+        const card2 = memoryGame.pickedCards[1];
+        const card2Name = card2.getAttribute("data-card-name");
+
+        const isMatch = memoryGame.checkIfPair(card1Name, card2Name);
+        if (!isMatch) {
+          setTimeout(() => {
+            card1.classList.toggle("turned");
+            card2.classList.toggle("turned");
+          }, 1000);
+        }
+
+        memoryGame.pickedCards = [];
+
+        const pairsClicked = document.getElementById("pairs-clicked");
+        const pairsGuessed = document.getElementById("pairs-guessed");
+
+        pairsClicked.innerText = memoryGame.pairsClicked;
+        pairsGuessed.innerText = memoryGame.pairsGuessed;
+
+        if (memoryGame.checkIfFinished()) {
+          alert("you won");
+        }
+      }
     });
   });
 });
