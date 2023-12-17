6cd5377e7a4096122c250885225e923298d0f75f
Irene <amoreperdesi@gmail.com>
2023-12-17 20:55:30 +0100

    Added iterations

--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,32 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
   shuffleCards() {
     // ... write your code here
+    return this.cards
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked ++
+
+    if (card1===card2){
+      this.pairsGuessed ++
+      return true
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
     // ... write your code here
+    return false
   }
 }
+
+
