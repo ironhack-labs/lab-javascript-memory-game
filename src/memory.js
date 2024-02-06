932de0df1baad7cc160a708188f95ba26667eba5
Ama Williams <53152990+Tetsewa@users.noreply.github.com>
2024-02-06 21:30:25 +0100

    do iteration 1 and 2.1

--- src/memory.js
+++ src/memory.js
@@ -2,11 +2,15 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards=[];
+    this.pairsClicked=0;
+    this.pairsGuessed=0;
   }
 
   shuffleCards() {
     // ... write your code here
-  }
+    this.cards.sort{}
+  } 
 
   checkIfPair(card1, card2) {
     // ... write your code here
