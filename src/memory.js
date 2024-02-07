be1332d1b4482d474c813c9a1c3823b39764f716
Sowjanya Kambhampati <sowjanyaakella87@gmail.com>
2024-02-07 14:30:34 +0100

    shufflecards() is finished

--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,47 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards= [];
+    this.pairsClicked= 0;
+    this.pairsGuessed= 0;
   }
 
   shuffleCards() {
     // ... write your code here
-  }
+    let shuffle = 0;
+     if (this.cards) {
+        for (let i = 0; i < this.cards.length - 1; i++) {  
+        let j = Math.floor(Math.random() * (this.cards.length - 1 - i) + i);  
+        shuffle = this.cards[j];
+        this.cards[j] = this.cards[i];
+        this.cards[i] = shuffle;
+}
+return this.cards;
+ }
+ return undefined;
+}
+    
+checkIfPair(card1, card2) {
+  
+  // ... write your code here
+    
+  if(card1== card2){
+         this.pairsGuessed += 1;
+         this.pairsClicked += 1;
+         return true;
+      }else {
+           this.pairsClicked += 1;
+           return false;
+    }
+    
 
-  checkIfPair(card1, card2) {
-    // ... write your code here
   }
 
   checkIfFinished() {
     // ... write your code here
+        if(this.pairsGuessed * 2== this.cards.length){
+           return true;
+        } else return false;
+
   }
 }
