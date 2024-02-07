7f0678e6073b77362e3c2fef0827398bb22d4bce
Ama Williams <53152990+Tetsewa@users.noreply.github.com>
2024-02-07 20:35:38 +0100

    add iteration 1 and 2.2.3

--- src/memory.js
+++ src/memory.js
@@ -6,17 +6,46 @@ class MemoryGame {
     this.pairsClicked=0;
     this.pairsGuessed=0;
   }
-
+//Iteration 2.2
   shuffleCards() {
     // ... write your code here
-    this.cards.sort{}
-  } 
-
-  checkIfPair(card1, card2) {
-    // ... write your code here
+    //should return the shuffled (mixed) array of cards-NOT WORKING
+    const shuffle = (cards) =>{
+      let oldElement;
+      for (let i = cards.length - 1; i > 0; i--) {
+        let rand = Math.floor(Math.random() * (i + 1));
+        oldElement = cards[i];
+        cards[i] = cards[rand];
+        cards[rand] = oldElement;
+      }
+      return cards;
+    }
   }
 
+  checkIfPair(card1, card2) {
+    this.pairsClicked++
+    if(card1.name===card2.name){
+      this.pairsGuessed++
+      return true
+    }
+    
+  //   else {
+      
+      
+  // return false}
+     
+   }
+  
+
   checkIfFinished() {
-    // ... write your code here
+    if(this.pairsGuessed===0){
+      return false
+    }
+    if(this.pairsGuessed===this.cards.length/2){
+      return true
+    }
+    else {
+      return false
+    }
   }
 }
