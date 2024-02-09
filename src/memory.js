e3cdd55a1ea02a8aa551b8ae8eb792030d33a030
guzegui <guzmanegui@gmail.com>
2024-02-09 09:17:04 +0100

    Remove debugging code and comments from memory.js

--- src/memory.js
+++ src/memory.js
@@ -1,19 +1,10 @@
-export class MemoryGame {
+class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     this.pickedCards = [];
     this.pairsClicked = 0;
     this.pairsGuessed = 0;
-
-    // add the rest of the class properties here
   }
-
-  /*The MemoryGame class: we created a MemoryGame class and, as we can see in its constructor, 
-  it will receive an array of cards as a parameter and set this array to a this.cards property.
-
-We also need a this.pickedCards array, where we will be storing the cards the user has clicked so we can compare them.
-
-Finally, a this.pairsClicked and this.pairsGuessed properties where will be adding every time a user choose and guess a pair. Go ahead and add these to the constructor.*/
 
   shuffleCards() {
     
@@ -46,46 +37,9 @@ Finally, a this.pairsClicked and this.pairsGuessed properties where will be addi
     } else {
       return false;
     }
-
-    /*When a user picks 2 cards, we will need to check if they are the same. Let's create logic for the method checkIfPair(), 
-which will receive two parameters, the names of both cards selected by the user (example: 'ironman' and 'batman'). 
-The method will add 1 to our pairsClicked property, and if the cards are the same also add 1 to pairsGuessed. 
-It should return true or false depending on the result of comparing both cards.
-
-So, to summarize, we will have to update pairsClicked on every two open cards by a user - it doesn't matter if the cards are the same. If two cards that we are comparing are the same, then pairsGuessed gets updated with +1.*/
   }
 
   checkIfFinished() {
     return this.pairsGuessed === 8;
   }
 }
-
-// DEBUGGING CODE
-
-const testCards = [
-  { name: "aquaman", img: "aquaman.jpg" },
-  { name: "batman", img: "batman.jpg" },
-  { name: "captain america", img: "captain-america.jpg" },
-  { name: "fantastic four", img: "fantastic-four.jpg" },
-  { name: "flash", img: "flash.jpg" },
-  { name: "green arrow", img: "green-arrow.jpg" },
-  { name: "green lantern", img: "green-lantern.jpg" },
-  { name: "ironman", img: "ironman.jpg" },
-  { name: "aquaman", img: "aquaman.jpg" },
-  { name: "batman", img: "batman.jpg" },
-  { name: "captain america", img: "captain-america.jpg" },
-  { name: "fantastic four", img: "fantastic-four.jpg" },
-  { name: "flash", img: "flash.jpg" },
-  { name: "green arrow", img: "green-arrow.jpg" },
-  { name: "green lantern", img: "green-lantern.jpg" },
-  { name: "ironman", img: "ironman.jpg" },
-];
-
-let testGame = new MemoryGame(testCards);
-
-let emptyMemoryGame = new MemoryGame();
-
-testGame.shuffleCards();
-emptyMemoryGame.shuffleCards();
-
-
