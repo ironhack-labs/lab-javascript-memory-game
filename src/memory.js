a07bd7b0213cefb4efd906583196f6a4b6a6b8e4
lunainde <lunainde@gmail.com>
2024-02-10 09:42:49 +0100

    lab memory game completed

--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,42 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined;
+    }
+  
+    for (let i = 0; i < this.cards.length; i++) {
+      const randomIndex = Math.floor(Math.random() * this.cards.length);
+      //destructuring assignment => the shuffled (mixed) array of cards A = B | B = A
+      [this.cards[i], this.cards[randomIndex]] = [this.cards[randomIndex], this.cards[i]];
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    
+    this.pairsClicked += 1
+    if (card1 === card2) {
+      // If they are the same, increment pairsGuessed []
+      this.pairsGuessed =+ 1;
+      return true;
+    }
+    return false;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    //each pair = 2 cards
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true; // all pairs guessed
+    } else {
+      return false; // still pairs to guess
+    }
   }
 }
+
+//---------------------------------------------------
